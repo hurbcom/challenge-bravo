154f5cfa1c4723b7dfa93af510c951ba330aeccc
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-11-15 20:39:55 -0200

    Update tests validate data

--- tests/unit/test_unit_api_validate.py
+++ tests/unit/test_unit_api_validate.py
@@ -6,21 +6,21 @@ from currency_conversion.api.validate import *
 class TestUnitValidate(unittest.TestCase):
     def test_validate_from_true(self):
         value = 'USD'
-        assert_equal = validate_from(value)
-        self.assertEqual(True, assert_equal)
+        expected = validate_from(value)
+        self.assertEqual(True, expected)
 
     def test_validate_from_false(self):
         value = 'USDD'
-        assert_equal = validate_from(value)
-        self.assertEqual(False, assert_equal)
+        expected = validate_from(value)
+        self.assertEqual(False, expected)
 
     def test_validate_amount_true(self):
         value = 100.00
-        assert_equal = validate_amount(value)
-        self.assertEqual(True, assert_equal)
+        expected = validate_amount(value)
+        self.assertEqual(True, expected)
 
     def test_validate_amount_string_false(self):
         value = 'test'
-        assert_equal = validate_amount(value)
-        self.assertEqual(False, assert_equal)
+        expected = validate_amount(value)
+        self.assertEqual(False, expected)
 
