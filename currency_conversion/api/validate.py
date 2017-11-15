faa6545085619ef6497ad34b171148dd2e7ca263
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-11-15 13:53:26 -0200

    Add functions validate data api

---
+++ currency_conversion/api/validate.py
@@ -0,0 +1,18 @@
+COINS = ['USD', 'BRL', 'EUR', 'BTC', 'ETH']
+
+def validate_from(value):
+    if value in COINS:
+        return True
+    return False
+
+def validate_to(value):
+    if value in COINS:
+        return True
+    return False
+
+def validate_amount(value):
+    try:
+        float(value)
+        return True
+    except:
+        return False
