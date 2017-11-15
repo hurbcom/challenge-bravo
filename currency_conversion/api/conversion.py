33f93fe9ac039d0331624d89b1ae2fa2fa6098b0
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-11-15 13:56:23 -0200

    Create property to get the quote

--- currency_conversion/api/conversion.py
+++ currency_conversion/api/conversion.py
@@ -6,8 +6,17 @@ class Conversion:
         self.ballast = 'USD'
         self.from_currency = from_currency
         self.from_quote = Quote(from_currency, to_currency).get()
+        self.to_quote = Quote(to_currency, from_currency).get()
         self.amount = amount
 
     def get(self):
         value = float("{0:4.4f}".format(self.amount * self.from_quote))
         return value
+
+    @property
+    def get_from_quote(self):
+        return self.from_quote
+
+    @property
+    def get_to_quote(self):
+        return self.to_quote
