466c0b43d801fc602e28cc37e2339a83d390f11f
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-11-15 20:42:11 -0200

    Update method get_json_api_quote

--- currency_conversion/api/quote.py
+++ currency_conversion/api/quote.py
@@ -24,38 +24,38 @@ class Quote:
         return result
 
     def convert_ballast_to(self):
-        quote_from_value = self.get_json_api_quote(self.from_coin)
+        quote_from_value = self.get_json_api_quote(self.from_coin, self.to_coin)
         return 1 / quote_from_value
 
     def convert_ballast_from(self):
-        quote_to_value = self.get_json_api_quote(self.to_coin)
+        quote_to_value = self.get_json_api_quote(self.to_coin, self.from_coin)
         return quote_to_value
 
     def convert_from_to_quote(self):
-        quote_from_value = self.get_json_api_quote(self.from_coin)
-        quote_to_value = self.get_json_api_quote(self.to_coin)
+        quote_from_value = self.get_json_api_quote(self.from_coin, self.to_coin)
+        quote_to_value = self.get_json_api_quote(self.to_coin, self.from_coin)
         return quote_from_value / quote_to_value
 
-    def get_json_api_quote(self, type):
-        if type in('BRL', 'EUR'):
-            if self.redis.get(self.from_to):
-                value = self.format_quote(float(self.redis.get(self.from_to)))
+    def get_json_api_quote(self, from_type, to_type):
+        if from_type in('BRL', 'EUR'):
+            if self.redis.get(from_type + to_type):
+                value = self.format_quote(float(self.redis.get(from_type + to_type)))
             else:
                 get_json = requests.get(self.BRL_EUR)
                 coin_json = get_json.json()
                 self.redis.set(
-                    self.from_to,
-                    str(coin_json['rates'][type]),
+                    from_type + to_type,
+                    str(coin_json['rates'][from_type]),
                     ex=100)
-                value = self.format_quote(coin_json['rates'][type])
+                value = self.format_quote(coin_json['rates'][from_type])
         else:
-            if self.redis.get(self.from_to):
-                value = self.format_quote(float(self.redis.get(self.from_to)))
+            if self.redis.get(from_type + to_type):
+                value = self.format_quote(float(self.redis.get(from_type + to_type)))
             else:
-                get_json = requests.get(self.ETH_BTC.format(type))
+                get_json = requests.get(self.ETH_BTC.format(from_type))
                 coin_json = get_json.json()
                 self.redis.set(
-                    self.from_to,
+                    from_type + to_type,
                     str(coin_json['lprice']),
                     ex=100)
                 value = self.format_quote(float(coin_json['lprice']))
@@ -64,7 +64,3 @@ class Quote:
 
     def format_quote(self, quote):
         return float("{0:4.4f}".format(quote))
-
-    @property
-    def from_to(self):
-        return self.from_coin + '_' + self.to_coin
