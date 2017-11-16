7467f9e30c85b1e036e43eede2fc1b9c8a130a5d
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-11-16 15:06:10 -0200

    Add redis in context app

--- currency_conversion/__init__.py
+++ currency_conversion/__init__.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, current_app
 from currency_conversion.api.views import conversion
+from redis import Redis
 
 
 app = Flask(__name__)
 
-app.register_blueprint(conversion, url_prefix='/api')
+with app.app_context():
+    app.redis = Redis(host='redis')
+    app.register_blueprint(conversion, url_prefix='/api')
