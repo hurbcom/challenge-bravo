359298e36e2e3e1c27f9b327438da69fcfc1e36f
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-11-16 15:10:03 -0200

    Update json api quote method

--- currency_conversion/api/quote.py
+++ currency_conversion/api/quote.py
@@ -1,17 +1,16 @@
 import requests
-from redis import Redis
+from flask import current_app
 
 
 class Quote:
 
-    BRL_EUR = 'https://api.fixer.io/latest?base=USD'
+    BRL_EUR = 'http://api.fixer.io/latest?base=USD'
     ETH_BTC = 'https://cex.io/api/last_price/{}/USD'
 
     def __init__(self, from_coin, to_coin):
         self.from_coin = from_coin
         self.to_coin = to_coin
         self.ballast = 'USD'
-        self.redis = Redis(host='redis')
 
     def get(self):
         if self.to_coin == self.ballast:
@@ -24,40 +23,45 @@ class Quote:
         return result
 
     def convert_ballast_to(self):
-        quote_from_value = self.get_json_api_quote(self.from_coin, self.to_coin)
+        from_to = self.from_coin + self.to_coin
+        quote_from_value = self.get_json_api_quote(from_to)
         return 1 / quote_from_value
 
     def convert_ballast_from(self):
-        quote_to_value = self.get_json_api_quote(self.to_coin, self.from_coin)
+        to_from = self.to_coin + self.from_coin
+        quote_to_value = self.get_json_api_quote(to_from)
         return quote_to_value
 
     def convert_from_to_quote(self):
-        quote_from_value = self.get_json_api_quote(self.from_coin, self.to_coin)
-        quote_to_value = self.get_json_api_quote(self.to_coin, self.from_coin)
+        from_to = self.from_coin + self.to_coin
+        quote_from_value = self.get_json_api_quote(from_to)
+
+        to_from = self.to_coin + self.from_coin
+        quote_to_value = self.get_json_api_quote(to_from)
         return quote_from_value / quote_to_value
 
-    def get_json_api_quote(self, from_type, to_type):
-        if from_type in('BRL', 'EUR'):
-            if self.redis.get(from_type + to_type):
-                value = self.format_quote(float(self.redis.get(from_type + to_type)))
-            else:
+    def get_json_api_quote(self, type):
+        get_value_redis = float(current_app.redis.get(type))
+        get_get_from_type = type[:3]
+
+        if get_value_redis:
+            value = get_value_redis
+        else:
+            if get_from_type in('BRL', 'EUR'):
                 get_json = requests.get(self.BRL_EUR)
                 coin_json = get_json.json()
-                self.redis.set(
-                    from_type + to_type,
-                    str(coin_json['rates'][from_type]),
-                    ex=100)
-                value = self.format_quote(coin_json['rates'][from_type])
-        else:
-            if self.redis.get(from_type + to_type):
-                value = self.format_quote(float(self.redis.get(from_type + to_type)))
+                current_app.redis.set(
+                    type,
+                    str(coin_json['rates'][type[2]]),
+                    ex=30000)
+                value = self.format_quote(coin_json['rates'][get_from_type])
             else:
-                get_json = requests.get(self.ETH_BTC.format(from_type))
+                get_json = requests.get(self.ETH_BTC.format(get_from_type))
                 coin_json = get_json.json()
-                self.redis.set(
-                    from_type + to_type,
+                current_app.redis.set(
+                    type,
                     str(coin_json['lprice']),
-                    ex=100)
+                    ex=30000)
                 value = self.format_quote(float(coin_json['lprice']))
 
         return value
