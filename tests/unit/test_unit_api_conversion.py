62538d2cc315c210351802497618623e23aa5d7a
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-11-16 22:07:04 -0200

    Add unit test to verify the return of api

--- tests/unit/test_unit_api_conversion.py
+++ tests/unit/test_unit_api_conversion.py
@@ -9,25 +9,6 @@ class TestUnitConversion(unittest.TestCase):
         self.app = app
         self.client = self.app.test_client()
 
-        self.headers = {
-            'content-type': 'application/json'
-        }
-
-        self.data = {
-            "data": {
-                "amount": 1,
-                "converted_amount": 3.2508,
-                "from": {
-                    "coin": "USD",
-                    "quote": 3.2508
-                },
-                "to": {
-                    "coin": "BRL",
-                    "quote": 334.1
-                }
-            }
-        }
-
     def test_request_response_200(self):
         response = self.client.get(
             '/api/conversion?from=USD&to=BRL&amount=1')
@@ -42,4 +23,9 @@ class TestUnitConversion(unittest.TestCase):
         response = self.client.get(
             '/api/conversion?from=USD&to=BRL')
         self.assertEqual(response.status_code, 400)
+
+    def test_return_json(self):
+        response = self.client.get(
+            '/api/conversion?from=USD&to=BRL')
+        self.assertEqual(response.headers['content-type'], 'application/json')
 
