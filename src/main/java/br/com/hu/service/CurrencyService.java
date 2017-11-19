d5cc396834c5688766f204dec328468ab52a08b2
Lucas Nable <pnable.lucas@gmail.com>
2017-11-19 13:29:29 -0200

    conversion and request

--- src/main/java/br/com/hu/service/CurrencyService.java
+++ src/main/java/br/com/hu/service/CurrencyService.java
@@ -15,33 +15,33 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CurrencyService {
-	
-	public double conversion(String to, String from, Double value) throws Exception {
-		if(valFormat(to, from)) {
-			return applyConversion(to, from, value);
-		}else {
-			throw new Exception("invalid currency");
+
+	public Double conversion(String to, String from, String amount) throws Exception {
+		Double amountDouble = Double.parseDouble(amount);
+		if (valFormat(to, from)) {
+			return applyConversion(to, from, amountDouble);
+		} else {
+			throw new Exception("Invalid currency");
 		}
-			
+
 	}
-	
+
 	private boolean valFormat(String to, String from) {
-		List<String> currencies = Arrays.asList("sup1", "sup2", "sup3");
-		if(currencies.contains(to))
+		List<String> currencies = Arrays.asList("USD", "BRL", "EUR", "BTC", "ETH");
+		if (currencies.contains(to))
 			return currencies.contains(from);
 		return false;
 	}
-	
-	
-	private double applyConversion(String to, String from, Double value) {
+
+	private Double applyConversion(String to, String from, Double amount) {
 		ExchangeRateProvider ecbRateProvider = MonetaryConversions.getExchangeRateProvider("ECB");
 		CurrencyUnit toCurrency = Monetary.getCurrency(to);
 		CurrencyUnit fromCurrency = Monetary.getCurrency(from);
-		
+
 		CurrencyConversion ecbConvertion = ecbRateProvider.getCurrencyConversion(fromCurrency);
-		
-		MonetaryAmount money = Money.of(value, toCurrency);
+
+		MonetaryAmount money = Money.of(amount, toCurrency);
 		return money.with(ecbConvertion).getNumber().doubleValue();
 	}
-	
+
 }
--- src/test/java/br/com/hu/test/controller/CurrencyControllerTest.java
+++ src/test/java/br/com/hu/test/controller/CurrencyControllerTest.java
@@ -1,10 +1,19 @@
 package br.com.hu.test.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.context.WebApplicationContext;
 
 import br.com.hu.App;
 
@@ -13,8 +22,21 @@ import br.com.hu.App;
 @WebAppConfiguration
 public class CurrencyControllerTest {
 
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Before
+	public void setup() throws Exception {
+		this.mockMvc = webAppContextSetup(webApplicationContext).build();
+	}
+
 	@Test
-	public void testConverter() {
+	public void getCurrency() throws Exception {
+		MvcResult result = mockMvc.perform(get("/v1/currency?from=BRL&to=EUR&amount=123.45")).andReturn();
+		MockHttpServletResponse response = result.getResponse();
+		int status = response.getStatus();
 	}
 
 }
