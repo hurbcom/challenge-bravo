32635aa86c6882aee9b60d4032ea4201e72d6684
Natanael Maia <gm.natanael@gmail.com>
2018-01-11 08:55:47 -0200

    Adding some layers to the solution.
    Adding layer for tests, business logic (it can be deleted in the future
    if it's not necessary) and utils layer, this last one can be used to
    build the http connector with the external api.

---
+++ HU_Challenge_Bravo/Project.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Project.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Project.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c484d858-c328-4f83-b93e-0f6950f6841d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
