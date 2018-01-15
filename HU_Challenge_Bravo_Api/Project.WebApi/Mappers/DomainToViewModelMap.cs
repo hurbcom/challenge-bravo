03a2c7b0064dd9964fe04c5b75fbf42e3e66547c
Natanael Maia <gm.natanael@gmail.com>
2018-01-15 07:55:19 -0200

    Automapper setup.

---
+++ HU_Challenge_Bravo_Api/Project.WebApi/Mappers/DomainToViewModelMap.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Project.Business.DTOs;
+using Project.WebApi.ViewModels;
+
+namespace Project.WebApi.Mappers
+{
+    public class DomainToViewModelMap : Profile
+    {
+        public DomainToViewModelMap()
+        {
+            CreateMap<CurrencyDTO, CurrencyDataViewModel>()
+                .ForMember(
+                    dest => dest.BaseCurrencySymbol,
+                    opt => opt.MapFrom(src => src.Symbol)
+                );
+        }
+    }
+}
---
+++ HU_Challenge_Bravo_Api/Project.WebApi/Mappers/ViewModelToDomainMap.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Project.Business.DTOs;
+using Project.WebApi.ViewModels;
+
+namespace Project.WebApi.Mappers
+{
+    public class ViewModelToDomainMap : Profile
+    {
+        public ViewModelToDomainMap()
+        {
+            CreateMap<CurrencyDataViewModel, CurrencyDTO>()
+                .ForMember(
+                    dest => dest.Symbol,
+                    opt => opt.MapFrom(src => src.BaseCurrencySymbol)
+                );
+        }
+    }
+}
