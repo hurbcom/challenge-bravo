0c0346f6df6f1d8c7cc5c86b1a642bf8b7b20430
Natanael Maia <gm.natanael@gmail.com>
2018-01-15 07:56:29 -0200

    Simple Injector setup.

---
+++ HU_Challenge_Bravo_Api/Project.WebApi/Helpers/SimpleInjectorHelper.cs
@@ -0,0 +1,26 @@
+﻿using Project.Business.Facades.Concrete;
+using Project.Business.Facades.Interface;
+using SimpleInjector;
+using SimpleInjector.Integration.WebApi;
+using System.Web.Http;
+
+namespace Project.WebApi.Helpers
+{
+    public static class SimpleInjectorHelper
+    {
+        public static Container RegisterInjector()
+        {
+            var container = new Container();
+
+            container.Register<ICurrencyConversionFacade, CurrencyConversionFacade>(Lifestyle.Singleton);
+
+            container.RegisterWebApiControllers(GlobalConfiguration.Configuration);
+
+            container.Verify();
+
+            GlobalConfiguration.Configuration.DependencyResolver = new SimpleInjectorWebApiDependencyResolver(container);
+
+            return container;
+        }
+    }
+}
