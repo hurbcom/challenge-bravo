cc9d604a7a05eced133cccdb6ec11eebf9e4f43e
Natanael Maia <gm.natanael@gmail.com>
2018-01-15 08:04:05 -0200

    Including the class helper for automapper. It will help in the process to map the way back and way out reading from the mapped classes.

---
+++ HU_Challenge_Bravo_Api/Project.WebApi/Helpers/AutoMepperHelper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Project.WebApi.Mappers;
+
+namespace Project.WebApi.Helpers
+{
+    public class AutoMepperHelper
+    {
+        public static void RegisterMappings()
+        {
+            Mapper.Initialize(i => {
+
+                i.AddProfile<DomainToViewModelMap>();
+                i.AddProfile<ViewModelToDomainMap>();
+            });
+        }
+    }
+}
