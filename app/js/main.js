8657860a2f0dab77d6d678b0e3111a2379ced435
lucio ariel <lucioariel.furtado@gmail.com>
2018-03-06 17:16:24 -0300

    Insere funções para conversão monetaria

---
+++ app/js/main.js
@@ -0,0 +1,65 @@
+var get_url = function get_url(obj) {
+    var url = decodeURIComponent(window.location.search.substring(1)),
+    var_url = url.split('&'),
+    parameters,
+    i;
+    for (i = 0; i < var_url.length; i++) {
+        parameters = var_url[i].split('=');
+        
+        if (parameters[0] === obj) {
+            return parameters[1] === undefined ? true : parameters[1];
+        }
+    }
+};
+
+$(document).ready(function () {
+    $('#form_currency').on('keyup', 'input' , function (e) { 
+        $(this).val($(this).val().replace(/[^0-9\.]/g,''));
+        var select_parent = $(this).parents('.form_item').find('select'),
+        select_value_from = $('#form_currency').find('.select_from').val(),
+        select_value_to = $('#form_currency').find('.select_to').val(),
+        select_from,
+        select_to,
+        count = $(this).val();
+        
+        $.ajax({url: "js/money.json", success: function(result){            
+            select_from = result.quotes['USD' + select_value_from];
+            select_to = result.quotes['USD' + select_value_to];
+            console.log(((1/select_from)*select_to)*count);
+            if(select_parent.hasClass('select_from')) {
+                $('.value_to').val((1/select_from)*select_to*count);
+            }
+            else {
+                $('.value_from').val((1/select_to)*select_from*count);
+            }
+        }});
+    });
+    $('#form_currency').on('change', 'select' , function (e) {
+        $('.value_from').trigger(
+            jQuery.Event( 'keyup', { keyCode: 13, which: 13 } )
+        );
+    });
+    $('#form_currency .toggle_form').click(function (e) { 
+        e.preventDefault();
+        var aux;
+        aux = $('.select_from').val();
+        $('.select_from').val($('.select_to').val());
+        $('.select_to').val(aux);
+        $('.value_from').trigger(
+            jQuery.Event( 'keyup', { keyCode: 13, which: 13 } )
+        );
+        
+    });
+    if(get_url('from')) {
+        $('.select_from').val(get_url('from'));
+    }
+    if(get_url('from')) {
+        $('.select_to').val(get_url('to'));
+    }
+    if(get_url('amount')) {
+        $('.value_from').val(get_url('amount'));
+        $('.value_from').trigger(
+            jQuery.Event( 'keyup', { keyCode: 13, which: 13 } )
+        );
+    }
+});
