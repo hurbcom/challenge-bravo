984216e864f2d083709dbc9a7c380b0c39fa6432
Anderson <anderson.moouro@gmail.com>
2018-03-09 19:00:57 -0300

    add notification errors

--- index.js
+++ index.js
@@ -4,26 +4,46 @@ const Router = require('koa-router')
 const converter = require('./converter')
 
 const router = new Router({})
+const is_valid = (v) => {
+    if (v == undefined || v == null || v == '')
+        return false
+    return true
+}
+const notifications = new Array()
 
 router.get('convert', '/convert', ctx => {
-    console.log(`${ctx.request.id}`)
+    console.info(`${ctx.request.id}`)
+    
     let from = ctx.request.query['from']
-    if (from == undefined || from == null || from == ''){
+    if (!is_valid(from))
+        notifications.push(`'from' attribute must be assigned.`)
+
+    let to = ctx.request.query['to']
+    if (!is_valid(to))
+        notifications.push(`'to' attribute must be assigned.`)
+
+    let amount = ctx.request.query['amount']
+    if (!is_valid(amount))
+        notifications.push(`'amount' attribute must be assigned.`)
+    
+    if (notifications.length > 0){
         ctx.status = 400
-        ctx.body = {error: `'from' attribute must be assigned.`}
+        ctx.body = {error: notifications}
         return
     }
-    let to = ctx.request.query['to']
-    let amount = ctx.request.query['amount']
+
     ctx.body = {response: converter(from, to, amount)}
 })
 
 const app = new Koa()
 app.use(async (ctx, next) => {
+    notifications.forEach(() => notifications.pop())
     ctx.request.id = uuidv1()
+    let start = Date.now()
     console.info(`${ctx.request.id} Starting request ${ctx.request.path}`)
     await next()
-    console.info(`${ctx.request.id} Ending request ${ctx.request.path}`)
+    console.info(`${ctx.request.id} Ending request ${ctx.request.path}`, {execution_ms: Date.now() - start})
 })
+
 app.use(router.routes())
 app.listen(5000)
--- test/converter_test.js
+++ test/converter_test.js
@@ -1,7 +1,7 @@
 const should = require('should')
 const converter = require('../converter')
 describe('Convert suite', () => {
-    it('convert testing', () => {
+    it('should_return_a_number', () => {
         let result = converter('BRL', 'USD', 10.2)
         result.should.be.type('number')
     })
