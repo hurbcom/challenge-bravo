b1370d254212cee2e4570e49f65cd6d1750747fb
Anderson <anderson.moouro@gmail.com>
2018-03-08 21:23:50 -0300

    add requestId for each request

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const Koa = require('koa')
+const uuidv1 = require('uuid/v1')
 const Router = require('koa-router')
 
 const router = new Router({})
 
-router.get('convert', '/convert', function(ctx){
-    console.log("Oi japa!")
+router.get('convert', '/convert', ctx => {
+    console.log(`${ctx.request.id}`)
+    ctx.body = {response: 'oi japa!'}
 })
 
 const app = new Koa()
+app.use(async (ctx, next) => {
+    ctx.request.id = uuidv1()
+    console.info(`${ctx.request.id} Starting request ${ctx.request.path}`)
+    await next()
+    console.info(`${ctx.request.id} Ending request ${ctx.request.path}`)
+})
 app.use(router.routes())
 app.listen(5000)
-
-
