ab41e2781a02f7103d0de0d435a49aee1f35e801
Amimaro <amir.zahlan@gmail.com>
2018-05-21 15:26:00 -0300

    Convertion algorithm :sparkles:

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 require('dotenv').config()
 
 app.use(express.static('public'));
 
-app.get("/api*", function (request, response) {
-  response.json(request.query);
+app.get("/api*", function(request, response) {
+    const from = request.query.from;
+    const to = request.query.to;
+    const amount = request.query.amount;
+    axios.get(`http://data.fixer.io/api/latest?access_key=${process.env.API_KEY}`)
+        .then((res) => {
+            const rates = res.data.rates;
+            // Conversion
+            response.json((amount / (1 / rates[to])) * (1 / rates[from]));
+        })
+        .catch((e) => {
+            response.sendStatus(404);
+        });
 });
 
-const listener = app.listen(8080, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(8080, function() {
+    console.log('Your app is listening on port ' + listener.address().port);
 });
