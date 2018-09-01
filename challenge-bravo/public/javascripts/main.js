cf89c1b8da72dcd2fc6241e0157b96616f23c5b7
Vanessa Soares <vanessa.ssoares.vs@gmail.com>
2018-09-01 15:29:49 -0300

    Adição de tema bootstrap; Adição de página de resultado

---
+++ challenge-bravo/public/javascripts/main.js
@@ -0,0 +1,57 @@
+
+$(document).ready(function(){
+	"use strict";
+
+	var window_width 	 = $(window).width(),
+	window_height 		 = window.innerHeight,
+	header_height 		 = $(".default-header").height(),
+	header_height_static = $(".site-header.static").outerHeight(),
+	fitscreen 			 = window_height - header_height;
+
+
+	$(".fullscreen").css("height", window_height)
+	$(".fitscreen").css("height", fitscreen);
+
+     
+     // -------   Active Mobile Menu-----//
+
+    $(".menu-bar").on('click', function(e){
+        e.preventDefault();
+        $("nav").toggleClass('hide');
+        $("span", this).toggleClass("lnr-menu lnr-cross");
+        $(".main-menu").addClass('mobile-menu');
+    });
+     
+    $('select').niceSelect();
+    $('.img-pop-up').magnificPopup({
+        type: 'image',
+        gallery:{
+        enabled:true
+        }
+    });
+
+    $('.active-bottle-carousel').owlCarousel({
+        items:1,
+        loop:true,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true
+    });
+
+    $('.play-btn').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+
+
+    $(document).ready(function() {
+        $('#mc_embed_signup').find('form').ajaxChimp();
+    });      
+    // -------   Mail Send ajax
+
+ });
--- challenge-bravo/routes/index.js
+++ challenge-bravo/routes/index.js
@@ -4,6 +4,12 @@ var router = express.Router();
 var app = express();
 var http = require('http');
 
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Convert Coins - Bravo Challenge', data: null });
+});
+
  
 router.get('/convert', function(req, res, next) {
  //valor para conversao 		
@@ -14,6 +20,19 @@ router.get('/convert', function(req, res, next) {
   
   //moeda de origem
   var from_coin = req.query.from;
+
+  console.log(amount);
+  if(amount===""){
+  	amount = 1;
+  }
+
+  //Criação de um json com os reais nomes de cada moeda
+  var jsonCoins = {};
+  jsonCoins["ETH"] = 'Ethereum';
+  jsonCoins["USD"] = 'Dólar Americano';
+  jsonCoins["BRL"] = 'Real Brasileiro';
+  jsonCoins["BTC"] = 'Bitcoin';
+  jsonCoins["EUR"] = 'Euro';
 
   /*
 
@@ -32,14 +51,24 @@ router.get('/convert', function(req, res, next) {
 
   //request da api cryptocompare (https://www.cryptocompare.com/)
 	request(url, function (error, response, body) {
-		var j = JSON.parse(body);
-		converted_value = parseFloat(j[to_coin]);  			 //valor da moeda destino após conversão
-		total_amount = converted_value * parseFloat(amount); //calculo total da conversão do valor informado
-
 		var jsonData = {};
-		jsonData["total_amount"] = total_amount;
 
-     	res.send(jsonData) //retorna json com total da conversão
+		try {
+			var j = JSON.parse(body);
+			converted_value = parseFloat(j[to_coin]);  			 //valor da moeda destino após conversão
+			total_amount = converted_value * parseFloat(amount); //calculo total da conversão do valor informado
+
+			jsonData["total_amount"] = total_amount;
+			jsonData["converted_value"] = converted_value;
+			jsonData["to"] = jsonCoins[to_coin];
+			jsonData["from"] = jsonCoins[from_coin];
+			jsonData["amount"] = parseFloat(amount);
+
+     		res.render('index', { title: 'Convert Coins - Bravo Challenge', data: jsonData});
+     	} catch(error) {
+     		jsonData["error"] = "Não foi possível realizar a conversão. Por favor tente novamente. " 
+     		res.render('index', { title: 'Convert Coins - Bravo Challenge', data: jsonData})
+     	}	
     });
 });
 
