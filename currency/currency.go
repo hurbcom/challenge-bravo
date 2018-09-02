10f664d3daf8a800122c2a9de944a68032534906
Yago Tomé <yago.tome@hotmail.com>
2018-09-02 20:36:46 -0300

    Create currency data structure

---
+++ currency/currency.go
@@ -0,0 +1,29 @@
+package currency
+
+// This package provides currency price conversion using a ballast currency
+// USD is going to be used as ballast currency here
+
+// Price is a data structure that encapsulates a hash table which maps currency symbol
+// to its price in a ballast currency.
+// This encapsulation gives flexibility to change inner data structure later if necessary
+type Price struct {
+	data map[string]float64
+}
+
+// NewPrice returns a new instace of Price
+func NewPrice() *Price {
+	return &Price{data: make(map[string]float64)}
+}
+
+// Convert converts and amount of a "from" currency to a "to" one
+func (p *Price) Convert(from, to string, amount float64) float64 {
+	priceFrom := p.data[from]
+	priceTo := p.data[to]
+	fromInBallast := amount / priceFrom
+	return priceTo * fromInBallast
+}
+
+// Save stores a price of a symbol
+func (p *Price) Save(symbol string, price float64) {
+	p.data[symbol] = price
+}
