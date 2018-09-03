c3513493cf6ba6ac57963adc1ae71c2b15b2a024
Yago Tomé <yago.tome@hotmail.com>
2018-09-03 12:55:04 -0300

    Add division by zero currency bug to tests

--- currency/currency_test.go
+++ currency/currency_test.go
@@ -14,10 +14,13 @@ func TestConvert(t *testing.T) {
 		"EUR": 0.86,
 		"BTC": 0.00014,
 		"ETH": 0.0034,
+		"IOT": 0,
 	})
 	assert.InDelta(t, .0, p.Convert("EUR", "ETH", 0), tolerance)
 	assert.InDelta(t, 24.285714286, p.Convert("BTC", "ETH", 1), tolerance)
 	assert.InDelta(t, 0.35, p.Convert("ETH", "BTC", 8.5), tolerance)
 	assert.InDelta(t, 77.3145, p.Convert("USD", "BRL", 19.09), tolerance)
 	assert.InDelta(t, 19.73, p.Convert("USD", "USD", 19.73), tolerance)
+	assert.InDelta(t, .0, p.Convert("IOT", "BRL", 6.66), tolerance)
+	assert.InDelta(t, .0, p.Convert("EUR", "IOT", 123.19), tolerance)
 }
