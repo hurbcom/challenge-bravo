4433e9e254ddbc8fb9dca9febfde92653beb41f6
Felipe J. L. Rita <felipe.rita@dcomp.sor.ufscar.br>
2018-09-04 23:32:24 -0300

    fetch rates tests

---
+++ src/rates/index.js
@@ -0,0 +1,10 @@
+/**
+ * @description Atualiza as cotações das moedas através de uma chamada de API
+ * @param {FetchFunction} fetch
+ */
+const update = (fetch) => {
+}
+
+module.exports = Object.assign({}, {
+  update
+})
---
+++ src/rates/index.test.js
@@ -0,0 +1,23 @@
+const rates = require('./index')
+const mock = require('../mock/rates')
+
+const callApi = (url) => {
+  return Promise.resolve(mock)
+}
+
+const fetch = jest.fn(callApi)
+
+it('request rates', () =>
+  rates.update(fetch).then(res => {
+    expect(fetch).toBeCalledWith('https://openexchangerates.org/api/latest.json?app_id=7c785382c5e840f6808c4f884560051b&show_alternative=true&symbols=USD,BRL,EUR,BTC,ETH')
+  }))
+
+it('get rate after update', () =>
+  rates.update(fetch).then(res => {
+    expect(rates.get('BRL')).toBe(mock.BRL)
+  }))
+
+it('get all rates after update', () =>
+  rates.update(fetch).then(res => {
+    expect(rates.get()).toBe(mock)
+  }))
