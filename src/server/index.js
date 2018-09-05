4b9eb7774a2460dcd30a5215bc591f069cc075d8
Felipe J. L. Rita <felipe.rita@dcomp.sor.ufscar.br>
2018-09-05 01:10:45 -0300

    server tests

---
+++ src/server/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+/**
+ * @description Rota de controle de conversão de moedas
+ * @author Felipe Rita <felipelopesrita@gmail.com>
+ * @param {Request} req
+ * @param {Response} res
+ * @returns {{ data: { value: Number } }}
+ */
+const controller = function (req, res) {
+  let params = { ...req.query }
+  let required = ['from', 'to', 'amount']
+  if (required.some(r => !params[r])) {
+    res.status(400).send({ error: `required params "from", "to" and "amount"` })
+  }
+  res.send('vish')
+}
+app.get('/', controller)
+
+const init = (process) => app.listen(process.env.PORT || 3000)
+
+module.exports = Object.assign({}, {
+  init,
+  app
+})
---
+++ src/server/index.test.js
@@ -0,0 +1,14 @@
+const { app } = require('./index')
+const request = require('supertest')
+
+it('required params not found', () =>
+  request(app)
+    .get('/')
+    .set('Accept', 'application/json')
+    .expect(400))
+
+it('happy request', () =>
+  request(app)
+    .get('/?from=BTC&to=EUR&amount=123.45')
+    .set('Accept', 'application/json')
+    .expect(200))
