b85afb2f1347bfcb12d1ae781786cf1a320bb599
Felipe J. L. Rita <felipe.rita@dcomp.sor.ufscar.br>
2018-09-05 00:01:05 -0300

    update mock and tests

---
+++ src/mock/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  process: {
+    env: {
+      API_ID: 'api1234'
+    }
+  },
+  rates: {
+    BRL: 4.15695,
+    BTC: 0.000135428411,
+    ETH: 0.0034767492,
+    EUR: 0.862328,
+    USD: 1
+  }
+}
--- src/rates/index.test.js
+++ src/rates/index.test.js
@@ -1,20 +1,20 @@
 const rates = require('./index')
-const mock = require('../mock/rates')
+const mock = require('../mock')
 
 const callApi = (url) => Promise.resolve({ json: () => mock })
 const fetch = jest.fn(callApi)
 
 it('request rates', () =>
-  rates.update(fetch).then(res => {
-    expect(fetch).toBeCalledWith('https://openexchangerates.org/api/latest.json?app_id=7c785382c5e840f6808c4f884560051b&show_alternative=true&symbols=USD,BRL,EUR,BTC,ETH')
+  rates.update(fetch, mock.process).then(res => {
+    expect(fetch).toBeCalledWith('https://openexchangerates.org/api/latest.json?app_id=api1234&show_alternative=true&symbols=USD,BRL,EUR,BTC,ETH')
   }))
 
 it('get rate after update', () =>
-  rates.update(fetch).then(res => {
+  rates.update(fetch, mock.process).then(res => {
     expect(rates.get('BRL')).toBe(mock.rates.BRL)
   }))
 
 it('get all rates after update', () =>
-  rates.update(fetch).then(res => {
+  rates.update(fetch, mock.process).then(res => {
     expect(rates.get()).toEqual(mock.rates)
   }))
