2509dc4062371d01e74702b09f463c7f283796e0
Felipe J. L. Rita <felipe.rita@dcomp.sor.ufscar.br>
2018-09-08 10:46:19 -0300

    converter tests

---
+++ src/converter/index.test.js
@@ -0,0 +1,23 @@
+const mock = require('../mock')
+const rates = require('../rates')
+const converter = require('../converter')
+
+describe('Converter tests', () => {
+  const fetch = (url) => Promise.resolve({ json: () => mock })
+  rates.update(fetch, mock.process)
+
+  it('convert BRL to BTC', () => {
+    let val = converter.from('BRL').to('BTC').value(1)
+    expect(val).toBe(0.00003257879)
+  })
+
+  it('convert BRL to USD', () => {
+    let val = converter.from('BRL').to('USD').value(4.15695)
+    expect(val).toBe(1)
+  })
+
+  it('convert BTC to ETH', () => {
+    let val = converter.from('BTC').to('ETH').value(2)
+    expect(val).toBe(51.344458291)
+  })
+})
