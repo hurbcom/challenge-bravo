eb5da0867e696693ef219215060c7875e8600154
Felipe J. L. Rita <felipe.rita@dcomp.sor.ufscar.br>
2018-09-08 14:35:47 -0300

    update server tests

--- src/server/index.test.js
+++ src/server/index.test.js
@@ -1,14 +1,25 @@
 const { app } = require('./index')
 const request = require('supertest')
 
-it('required params not found', () =>
-  request(app)
-    .get('/')
-    .set('Accept', 'application/json')
-    .expect(400))
+describe('Server tests', () => {
+  it('required params not found', () =>
+    request(app)
+      .get('/')
+      .set('Accept', 'application/json')
+      .expect(400))
 
-it('happy request', () =>
-  request(app)
-    .get('/?from=BTC&to=EUR&amount=123.45')
-    .set('Accept', 'application/json')
-    .expect(200))
+  it('happy request', () =>
+    request(app)
+      .get('/?from=BTC&to=EUR&amount=123.45')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .expect(res => {
+        expect(res.data).not.toBeNaN()
+      }))
+
+  it('Incorret params', () =>
+    request(app)
+      .get('/?from=BTC&to=EUR&amount=123.45')
+      .set('Accept', 'application/json')
+      .expect(400))
+})
