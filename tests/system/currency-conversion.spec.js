6428573cb7bbe7d9d08972d6c1fb6a8d122dd09a
Felippe <felippe.mauricio@olxbr.com>
2018-10-11 20:38:18 -0300

    Add more system tests

--- tests/system/currency-conversion.spec.js
+++ tests/system/currency-conversion.spec.js
@@ -11,9 +11,9 @@ describe('Currency Conversion', () => {
   const fromParam = 'USD';
   const toParam = 'BRL';
   const toCompoundParam = 'USD,BRL,EUR,BTC,ETH';
-  const amountParam = '23.9';
+  const amountParam = 23.90;
 
-  describe('Correct Status Code', () => {
+  describe('# Status Code', () => {
     it('Should response 422 if i don`t send a from', async (done) => {
       const { statusCode } = await request(app)
         .get('/currency-conversion')
@@ -68,6 +68,60 @@ describe('Currency Conversion', () => {
         .get('/currency-conversion')
         .query({ from: fromParam, to: toCompoundParam, amount: amountParam });
       expect(statusCode).toBe(200);
+      done();
+    });
+  });
+
+  describe('# Body', () => {
+    it('Should response contains amount', async (done) => {
+      const { body } = await await request(app)
+        .get('/currency-conversion')
+        .query({ from: fromParam, to: toParam, amount: amountParam });
+      expect(body).toHaveProperty('amount');
+      done();
+    });
+    it('Should response contains a valid amount', async (done) => {
+      const { body } = await await request(app)
+        .get('/currency-conversion')
+        .query({ from: fromParam, to: toParam, amount: amountParam });
+      const { amount } = body;
+      expect(amount).toBe(amountParam);
+      done();
+    });
+    it('Should response contains base', async (done) => {
+      const { body } = await await request(app)
+        .get('/currency-conversion')
+        .query({ from: fromParam, to: toParam, amount: amountParam });
+      expect(body).toHaveProperty('base');
+      done();
+    });
+    it('Should response contains a valid base', async (done) => {
+      const { body } = await await request(app)
+        .get('/currency-conversion')
+        .query({ from: fromParam, to: toParam, amount: amountParam });
+      const { base } = body;
+      expect(base).toBe(fromParam);
+      done();
+    });
+    it('Should response contains date', async (done) => {
+      const { body } = await await request(app)
+        .get('/currency-conversion')
+        .query({ from: fromParam, to: toParam, amount: amountParam });
+      expect(body).toHaveProperty('date');
+      done();
+    });
+    it('Should response contains a valid date', async (done) => {
+      const { body } = await await request(app)
+        .get('/currency-conversion')
+        .query({ from: fromParam, to: toParam, amount: amountParam });
+      const { date } = body;
+      const dateTest = new Date();
+      const yearTest = dateTest.getFullYear();
+      const monthAux = dateTest.getMonth() + 1;
+      const monthTest = monthAux >= 10 ? monthAux : `0${monthAux}`;
+      const dayAux = dateTest.getDate();
+      const dayTest = dayAux >= 10 ? dayAux : `0${dayAux}`;
+      expect(date).toBe(`${yearTest}-${monthTest}-${dayTest}`);
       done();
     });
   });
