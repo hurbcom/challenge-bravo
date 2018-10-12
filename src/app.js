83cabb18f4d8ed419283ea636178d4f39f562822
Felippe <felippe.mauricio@olxbr.com>
2018-10-11 22:37:38 -0300

    Add id to request

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 const app = require('express')();
+const uuidv4 = require('uuid/v4');
 const { register } = require('./core/helpers');
 const routers = require('./core/routers');
 
 
+const addRequestId = (req, _res, next) => {
+  req.id = uuidv4();
+  next();
+};
+
+
 register(
+  [addRequestId],
   [routers],
 )(app);
 
