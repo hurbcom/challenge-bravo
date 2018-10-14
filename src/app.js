c267bd6809caff36a6acb0631134ef4ae34fb39a
Felippe <felippe.mauricio@olxbr.com>
2018-10-13 21:37:31 -0300

    Remove assignId to uses an express-request-id

--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
 const app = require('express')();
+const addRequestId = require('express-request-id')();
 const helmet = require('helmet');
 const morgan = require('morgan');
-const uuidv4 = require('uuid/v4');
 const { register } = require('./core/helpers');
 const routers = require('./core/routers');
 
-
-const assignId = (req, _res, next) => {
-  req.id = uuidv4();
-  next();
-};
 
 const undefinedRouter = (_req, res) => {
   res.sendStatus(404);
@@ -17,7 +12,7 @@ const undefinedRouter = (_req, res) => {
 
 
 register(
-  [assignId],
+  [addRequestId],
   [morgan('dev')],
   [helmet()],
   [routers],
