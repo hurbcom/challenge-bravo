51b468d0bc8e7418cc71b13d48c28af3f86020d6
Felippe <felippe.mauricio@olxbr.com>
2018-10-13 21:09:26 -0300

    Add morgan to see requests

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const app = require('express')();
 const helmet = require('helmet');
+const morgan = require('morgan');
 const uuidv4 = require('uuid/v4');
 const { register } = require('./core/helpers');
 const routers = require('./core/routers');
@@ -13,6 +14,7 @@ const addRequestId = (req, _res, next) => {
 
 register(
   [addRequestId],
+  [morgan('dev')],
   [helmet()],
   [routers],
 )(app);
