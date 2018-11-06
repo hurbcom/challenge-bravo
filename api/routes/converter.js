487e6435519ccb902546de595f2a7f8ef5205dbc
Emanoel Faria <emanoel.paschoal@tvglobo.com.br>
2018-11-06 17:42:44 -0200

    transferindo responsabilidade de validacao de campos de query e tipos para rota de conversao e retirando da classe converter

--- api/routes/converter.js
+++ api/routes/converter.js
@@ -1,28 +1,28 @@
 const express           = require('express');
 const router            = express.Router();
 const ConverterClass    = require('../services/converter')
-const CONVERTER         = new ConverterClass()
+const converter         = new ConverterClass()
 
 router.get('/', function(req, res, next) {
-  
-    if(!CONVERTER.isConvertionAvailable()){
+    
+    if(!converter.isConvertionAvailable()){
         res.status(500).send({error:"There was an error while trying to convert. Try again"})
         return
     }
 
-    if(!CONVERTER.hasConversionFields(req.query)) {
+    if(!hasConversionFields(req.query)) {
         let fields = Object.keys(req.query).toString()
         res.status(400).send({error:`Parameters "from", "to" and "amount" are required. Received ${fields}`})
         return
     }
 
-    if(!CONVERTER.isValidNumber(req.query.amount)) {
+    if(isValidNumber(req.query.amount)) {
         res.status(400).send({error:`"amount" parameter must be a valid number greater than 0`})
         return
     }
 
-    if(!CONVERTER.isValidCurrency(req.query)){
-        let availableCurrencies = CONVERTER.getAvailableCurrencies()
+    if(isValidCurrency(req.query.from, req.query.to)){
+        let availableCurrencies = converter.getAvailableCurrencies().toString()
         res.status(400).send({error:`The currencies you want to convert are not available. Try one of these ${availableCurrencies}`})
         return
     }
@@ -31,7 +31,7 @@ router.get('/', function(req, res, next) {
     let to = req.query.to.toUpperCase()
     let amount = req.query.amount
     
-    let result = CONVERTER.convertfromToCurrency(from,to,amount)
+    let result = converter.convertfromToCurrency(from,to,amount)
 
     if(!result) {
         res.status(500).send({error:"There was an error while trying to convert. Try again"})
@@ -48,5 +48,41 @@ router.get('/', function(req, res, next) {
     res.status(200).send(response)
 
 });
+
+/**
+ * @description hasConversionFields Checks whether the object has the attributes "from", "to" and "amount"
+ * @param {*} body 
+ * @returns {boolean}
+ */
+function hasConversionFields(query){
+    return ['from','to','amount'].every(f => Object.keys(query).includes(f))
+}
+
+/**
+ * @description isValidNumber Checks if number is valid and greater than 0
+ * @param {string | integer} number
+ * @returns {boolean}
+ */
+function isValidNumber(number){
+    return !isNaN(number) && Number(number) > 0;
+}
+
+/**
+ * @description isValidCurrency Checks if the desired currency exists / is valid to be converted
+ * @param {Object} body 
+ * @returns {boolean}
+ */
+function isValidCurrency(from,to){
+
+    if(!from || !to) return false
+
+    let values = converter.getAvailableCurrencies()
+    let from = from.toUpperCase()
+    let to = to.toUpperCase()
+
+    if(!values.includes(from) || !values.includes(to)) return false
+
+    return true
+}
 
 module.exports = router;
--- api/services/converter.js
+++ api/services/converter.js
@@ -42,46 +42,13 @@ class Converter {
         return result
     }
 
-    /**
-     * @description hasConversionFields Checks whether the object has the attributes "from", "to" and "amount"
-     * @param {*} body 
-     * @returns {boolean}
-     */
-    hasConversionFields(body){
-        return ['from','to','amount'].every(f => Object.keys(body).includes(f))
-    }
-
-    /**
-     * @description isValidNumber Checks if number is valid and greater than 0
-     * @param {string | integer} number
-     * @returns {boolean}
-     */
-    isValidNumber(number){
-        return !isNaN(number) && Number(number) > 0;
-    }
-
-    /**
-     * @description isValidCurrency Checks if the desired currency exists / is valid to be converted
-     * @param {Object} body 
-     * @returns {boolean}
-     */
-    isValidCurrency(body){
-        let values = this.cache.values
-        let from = body.from.toUpperCase()
-        let to = body.to.toUpperCase()
-
-        if(!Object.keys(values).includes(from) || !Object.keys(values).includes(to)) return false
-
-        return true
-    }
-
     /** 
      * @description getAvailableCurrencies returns what currencies are available
      * @returns {string}
     */
     getAvailableCurrencies(){
        let values = this.cache.values
-       let currencies = Object.keys(values).toString()
+       let currencies = Object.keys(values)
        return currencies
     }
     
@@ -90,7 +57,7 @@ class Converter {
      * @returns {boolean}
     */
     isConvertionAvailable(){
-        if(this.cache.values != {}) {
+        if(JSON.stringify(this.cache.values) != JSON.stringify({})) {
             return true
         } else {
             return false
