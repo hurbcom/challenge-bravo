07a32e71e37b934ead6900f01f842eaeda039f35
Cleyson Leal Braga <cleysonwebdev@gmail.com>
2018-11-15 16:33:21 -0200

    adicionado o CORS

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
-const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('./app/routes/api/api_v1');
 
-app.use(cors);
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(routes);
