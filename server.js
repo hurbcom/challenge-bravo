d9c8873805e7d67e38df18df6323cc33f10d4bb9
Cleyson Leal Braga <cleysonwebdev@gmail.com>
2018-11-16 20:56:03 -0200

    adicionado variaveis de ambiente

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const app = express();
 const bodyParser = require('body-parser');
 const routes = require('./app/routes/api/api_v1');
 
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(routes);
 
-app.listen(3000);
+app.listen(PORT);
 
 module.exports = app;
