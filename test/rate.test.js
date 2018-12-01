1e044b509718ce07c08277eebb1abaebf5a87d0b
fossilva <fernanda.silva@radixeng.com.br>
2018-12-01 12:41:26 -0200

    test change

--- test/rate.test.js
+++ test/rate.test.js
@@ -9,40 +9,40 @@ const request = require ('supertest');
 const app = require ('../src/app');
 
 describe('GET /api/rates', function () {
-    it('respond with error in required parameters', function (done) {
+    it('respond with error in required parameters', function () {
         request(app)
             .get('/api/rates')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
-            .expect(400, done);
+            .expect(400);
     }),
-    it('respond with invalid currency in parameter "to"', function (done) {
+    it('respond with invalid currency in parameter "to"', function () {
         request(app)
             .get('/api/rates?from=USD&to=AAA&amount=1')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
-            .expect(401, done);
+            .expect(401);
     }),
-    it('respond with invalid currency in parameter "from"', function (done) {
+    it('respond with invalid currency in parameter "from"', function () {
         request(app)
             .get('/api/rates?from=AAA&to=USD&amount=1')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
-            .expect(401, done);
+            .expect(401);
     }),
-    it('respond that the parameter "amount" must be numeric.', function (done) {
+    it('respond that the parameter "amount" must be numeric.', function () {
         request(app)
             .get('/api/rates?from=BRL&to=USD&amount=AAA')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
-            .expect(401, done);
+            .expect(401);
     }),
-    it('respond to the converted currency.', function (done) {
+    it('respond to the converted currency.', function () {
         request(app)
             .get('/api/rates?from=BRL&to=USD&amount=1')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
-            .expect(200, done);
+            .expect(200);
     });
     
 });
