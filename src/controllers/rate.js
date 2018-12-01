56c28ca2710ed6ff6373a570713e960ab43bfb95
fossilva <fernanda.silva@radixeng.com.br>
2018-12-01 12:32:30 -0200

    implementation of parameter validation

--- src/controllers/rate.js
+++ src/controllers/rate.js
@@ -13,6 +13,28 @@ const axios = require('axios');
 
 exports.get = async (req, res) => {
     let api = config.api.url + 'latest.json?app_id=' + config.api.key + '&show_alternative=true&symbols='+ config.api.coins.toString();
+    let parmsRequered = ['to','from','amount'];
+
+    if(!parmsRequered.every((item) => req.query[item] )){
+        res.status(400).json({Erro:'The ' + parmsRequered.toString() + ' parameters are required.'});
+        return;
+    }
+
+    if(!config.api.coins.some((item) => item == req.query.to.toUpperCase()) || !config.api.coins.some((item) => item == req.query.from.toUpperCase())){
+        res.status(401).json({Erro:'Invalid currency in parameter "to". Valid currencies: ' + config.api.coins.toString()});
+        return;
+    }
+
+    if(!config.api.coins.some((item) => item == req.query.from.toUpperCase())){
+        res.status(401).json({Erro:'Invalid currency in parameter "from". Valid currencies: ' + config.api.coins.toString()});
+        return;
+    }
+
+    if (isNaN(req.query.amount)) {
+        res.status(401).json({Erro:'amount must be numeric.'});
+        return;
+    }
+
     try {
         const result =  await axios.get(`${api}`);   
         let conversionResult = await converter(result.data.rates, req.query.to.toUpperCase(), req.query.from.toUpperCase(), req.query.amount);
