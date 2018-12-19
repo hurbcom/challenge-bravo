2a32db05123eaf2d5d2a71b3514e1d1034c84ea5
Rodrigo Muniz <rodrigobmuniz@gmail.com>
2018-12-19 01:13:04 -0200

    adicionado monitoramento do status do express

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import bodyParse from 'body-parser';
 import dotenv from 'dotenv';
+import status from 'express-status-monitor';
 
 import routes from './src/routes/routes';
 import { updateExchangeRate } from './src/services/quotation-of-the-day.service';
+import serverStatusConfig from './src/data/express-status-monitor.json';
 
+const serverStatus = status(serverStatusConfig);
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(serverStatus);
 
 app.use(bodyParse.urlencoded({ extended: true }));
 app.use(bodyParse.json());
@@ -15,10 +20,6 @@ app.use(bodyParse.json());
 updateExchangeRate(process.env.EXCHANGE_RATE_PATH);
 
 routes(app);
-
-app.get('/', (req, res) => {
-    res.send('Serve is running...');
-});
 
 app.listen(PORT, () => {
     console.log(`server running on ${PORT}`);
