83eb91dc5cb6b0462401aa3ea28fd457188e6ba2
schonmann <antonio_schonmann@hotmail.com>
2018-12-22 16:42:32 -0200

    External API requests parametrized with base currency.

--- worker/strategy/strategy.go
+++ worker/strategy/strategy.go
@@ -25,7 +25,10 @@ type OpenExchangeRatesStrategy struct{}
 
 func (o OpenExchangeRatesStrategy) RetrieveRates() (*QuotasResponse, error) {
 	apiConfig := config.Get().Worker.ExternalAPIs.OpenExchangeRates
-	apiUrl := fmt.Sprintf("%slatest.json?app_id=%s&show_alternative=1", apiConfig.URL, apiConfig.APIKey)
+
+	apiUrl := fmt.Sprintf("%slatest.json?app_id=%s&show_alternative=1&base=%s",
+		apiConfig.URL, apiConfig.APIKey, config.Get().BaseCurrency)
+
 	response := QuotasResponse{}
 	if err := util.GetAndParseJSON(apiUrl, &response); err != nil {
 		return nil, err
@@ -37,7 +40,10 @@ type FixerStrategy struct{}
 
 func (o FixerStrategy) RetrieveRates() (*QuotasResponse, error) {
 	apiConfig := config.Get().Worker.ExternalAPIs.Fixer
-	apiUrl := fmt.Sprintf("%slatest?access_key=%s", apiConfig.URL, apiConfig.APIKey)
+
+	apiUrl := fmt.Sprintf("%slatest?access_key=%s&base=%s",
+		apiConfig.URL, apiConfig.APIKey, config.Get().BaseCurrency)
+
 	response := QuotasResponse{}
 	if err := util.GetAndParseJSON(apiUrl, &response); err != nil {
 		return nil, err
