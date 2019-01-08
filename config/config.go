d60db1d55ffd05da0e47455dfc653644f7204384
Thiago Morales <thiago.morales@olxbr.com>
2019-01-07 23:58:33 -0200

    Create StartConfig and SetupLogger functions.
    These functions will handle logging behaviour of application by initialized flags.

--- config/config.go
+++ config/config.go
@@ -1,6 +1,20 @@
 package config
 
+import "github.com/sirupsen/logrus"
+
 var Config = struct {
 	DevelopmentMode bool
 	Debug           bool
 }{}
+
+func StartConfig(newLogger *logrus.Logger) {
+	var log = newLogger.WithFields(logrus.Fields{"method": "StartConfig"})
+	if Config.Debug {
+		newLogger.SetLevel(logrus.DebugLevel)
+	}
+	if Config.DevelopmentMode {
+		log.Info("• Running Development profile")
+	} else {
+		log.Info("• Running Production profile")
+	}
+}
--- logger/logger.go
+++ logger/logger.go
@@ -1,7 +1,27 @@
 package logger
 
 import (
+	"curapi/config"
+
 	"github.com/sirupsen/logrus"
 )
 
 var Log = logrus.New()
+
+func SetupLogger() {
+	if config.Config.Debug {
+		Log.SetLevel(logrus.DebugLevel)
+	}
+
+	if config.Config.DevelopmentMode {
+		var pf = TextFormatter{
+			TimestampFormat: "Jan 02 2006 15:04:05",
+			ForceColors:     true,
+			FullTimestamp:   true,
+			ForceFormatting: true,
+		}
+		Log.Formatter = &pf
+	} else {
+		Log.Formatter = &logrus.JSONFormatter{}
+	}
+}
