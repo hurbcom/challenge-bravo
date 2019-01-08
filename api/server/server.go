57835b468e6b5fff0c2d2f55530be74813c488ef
Thiago Morales <thiago.morales@olxbr.com>
2019-01-08 06:24:59 -0200

    Rename public endpoint to 'convert'.

--- api/server/server.go
+++ api/server/server.go
@@ -28,7 +28,7 @@ func StartAPIService(serverPort int) {
 	// Configure router and server routes
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/health", api.HealthCheck).Methods("GET")
-	router.HandleFunc("/api/v1/public/rate", api.GetRate).Queries("from", "{from}", "to", "{to}", "amount", "{amount}").Methods("GET", "OPTIONS", "HEAD")
+	router.HandleFunc("/api/v1/public/convert", api.GetRate).Queries("from", "{from}", "to", "{to}", "amount", "{amount}").Methods("GET", "OPTIONS", "HEAD")
 	// router.HandleFunc("/api/v1/public/rates", api.GetAllRates).Methods("GET", "OPTIONS", "HEAD")
 	router.Use(api.LoggingMiddleware)
 
