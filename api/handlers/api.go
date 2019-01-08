02a5ee68fd1c489330bc3a573f7fda58e8fb9fd1
Thiago Morales <thiago.morales@olxbr.com>
2019-01-08 06:00:43 -0200

    Add GetRate func to process and return conv data.
    Create GetRate api function to process and return the converted data.

--- api/handlers/api.go
+++ api/handlers/api.go
@@ -1,26 +1,91 @@
 package api
 
 import (
+	"curapi/converter"
 	"curapi/logger"
 	"curapi/util"
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
+	jsoniter "github.com/json-iterator/go"
 	"github.com/sirupsen/logrus"
 )
 
 var getLogger = logger.Log
 var err error
 
+// HeartbeatResponse :: Struct for Healthcheck
 type HeartbeatResponse struct {
 	Status string `json:"status"`
 	Code   int    `json:"code"`
 }
 
+// OneErrorResponse :: Struct for error message
+type OneErrorResponse struct {
+	Error   string `json:"error"`
+	Message string `json:"message"`
+}
+
+// CurrencyResponse :: Struct for Currency converted response
+type CurrencyResponse struct {
+	Base string `json:"base"`
+	From struct {
+		Symbol string `json:"symbol"`
+		Label  string `json:"description"`
+	} `json:"from"`
+	To struct {
+		Symbol string `json:"symbol"`
+		Label  string `json:"description"`
+	} `json:"to"`
+	Value float64 `json:"value"`
+}
+
 // HealthCheck :: Simple healthcheck endpoint that returns HTTP 200
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(HeartbeatResponse{Status: "OK", Code: 200})
+}
+
+// GetRate :: Get rate value for given currencies and amounts
+func GetRate(w http.ResponseWriter, r *http.Request) {
+	var log = getLogger.WithFields(logrus.Fields{"method": util.GetPrefixName()})
+	qs := r.URL.Query()
+	if qs != nil {
+		from := strings.ToUpper(strings.Join(qs["from"], ""))
+		to := strings.ToUpper(strings.Join(qs["to"], ""))
+		amount, err := strconv.ParseFloat(strings.Join(qs["amount"], ""), 64)
+		if err != nil {
+			log.Error(err)
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(OneErrorResponse{Error: "Invalid amount. Please use dots instead commas."})
+			return
+		}
+
+		// Pass values and do the conversion
+		value, err := converter.CurrencyConverter(amount, from, to)
+		if value != nil && err == nil {
+
+			c := &CurrencyResponse{Base: "USD"}
+			c.From.Symbol = from
+			c.From.Label = converter.GetCurrencyLabel(from)
+			c.To.Symbol = to
+			c.To.Label = converter.GetCurrencyLabel(to)
+			c.Value = *value
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			w.WriteHeader(http.StatusOK)
+			jsoniter.NewEncoder(w).Encode(c)
+			return
+		}
+
+		// Things messed, report in
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(OneErrorResponse{Error: "Could not convert currency data", Message: err.Error()})
+		return
+	}
 }
 
 // LoggingMiddleware :: Simple middleware for logging purposes
