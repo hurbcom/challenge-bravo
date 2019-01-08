808fedce7c8358c634165bc515a4300ec471d0a4
Thiago Morales <thiago.morales@olxbr.com>
2019-01-07 23:49:07 -0200

    Create util package to helpers and reused funcs.
    In this package we've create two helper functions:
    * Fire HTTP requests
    * Return caller method name.

---
+++ util/util.go
@@ -0,0 +1,78 @@
+package util
+
+import (
+	"curapi/logger"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"runtime"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+var getLogger = logger.Log
+
+type Request struct {
+	Headers map[string]string
+	Params  map[string]string
+	Body    io.Reader
+}
+
+// ExecuteRequest :: Wrapper to fire HTTP requests and return HTTP response.
+func ExecuteRequest(httpMethod, url string, request *Request) (*http.Response, error) {
+	var log = getLogger.WithFields(logrus.Fields{"method": GetPrefixName()})
+
+	// Create HTTP client
+	client := &http.Client{}
+	headers := request.Headers
+	body := request.Body
+	params := request.Params
+	req, err := http.NewRequest(httpMethod, url, body)
+	if err != nil {
+		log.Errorf("• Error: %s", err.Error())
+	}
+
+	// Set query parameters
+	query := req.URL.Query()
+	if params != nil {
+		for k, v := range params {
+			query.Add(k, v)
+		}
+		req.URL.RawQuery = query.Encode()
+	}
+
+	// Set headers for Request
+	for k, v := range headers {
+		req.Header.Add(k, v)
+	}
+
+	// Fire GET request
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Errorf("• Error: %s", err.Error())
+	}
+
+	// Logging response errors
+	if resp.StatusCode != 200 {
+		errorResponse, _ := ioutil.ReadAll(resp.Body)
+		defer resp.Body.Close()
+		log.Errorf("• Received response error: [%v] - %s", resp.StatusCode, errorResponse)
+
+	}
+	return resp, err
+}
+
+// GetPrefixName :: Helper func to return caller function name
+func GetPrefixName(depthList ...int) string {
+	var depth int
+	if depthList == nil {
+		depth = 1
+	} else {
+		depth = depthList[0]
+	}
+	function, _, _, _ := runtime.Caller(depth)
+	fncName := runtime.FuncForPC(function).Name()
+	fncNameSlice := strings.Split(fncName, ".")
+	return fncNameSlice[1]
+}
