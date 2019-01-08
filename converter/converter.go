c22bff0f530f2044fe42bef92af387b461026c85
Thiago Morales <thiago.morales@olxbr.com>
2019-01-08 06:16:44 -0200

    Better decimal places handle to crypto currencies. Add 8 decimal places to crypto currencies and 4 for other currencies.

--- converter/converter.go
+++ converter/converter.go
@@ -15,6 +15,7 @@ var getLogger = logger.Log
 // CurrencyConverter :: Function responsible to conversion of currencies
 func CurrencyConverter(amount float64, from, to string) (*float64, error) {
 	var log = getLogger.WithFields(logrus.Fields{"method": util.GetPrefixName()})
+	var val float64
 	fromRawValue, err := cache.Get(strings.ToUpper(from))
 	if err != nil {
 		log.Error(err)
@@ -28,7 +29,11 @@ func CurrencyConverter(amount float64, from, to string) (*float64, error) {
 	fromFloatValue, _ := strconv.ParseFloat(fromRawValue, 64)
 	toFloatValue, _ := strconv.ParseFloat(toRawValue, 64)
 	ratio := toFloatValue / fromFloatValue
-	val := util.ToFixed((ratio * amount), 8) // Fixing 8 decimal places
+	if to == "BTC" || to == "ETH" {
+		val = util.ToFixed((ratio * amount), 8) // Fixing 8 decimal places for crypto currencies
+	} else {
+		val = util.ToFixed((ratio * amount), 4) // Fixing 4 decimal places for others
+	}
 	return &val, nil
 }
 
