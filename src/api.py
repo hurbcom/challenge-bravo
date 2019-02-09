3471f99ab0b5f2cc0cdcd6da13179a819af51344
Douglas Ferreira <drfdouglas@hotmail.com>
2019-02-09 16:26:57 -0200

    Teste de carga com server remoto

--- src/api.py
+++ src/api.py
@@ -12,4 +12,7 @@ app.route('/')
 api = Api(app)
 api.add_resource(Convert, '/convert/')
 
-app.run(threaded=True)
+#Para expor aplicação remotamente no Linux
+app.run(host='0.0.0.0', threaded=True)
+
+#app.run(threaded=True)
---
+++ tests/load_test.py
@@ -0,0 +1,44 @@
+import requests
+import threading
+import time, random
+from queue import Queue
+
+coins = ['USD', 'BRL', 'EUR', 'BTC', 'ETH']
+
+threads = 1000
+sucessful = []
+
+def test_request_load(t_queue, base, target, value):
+    url = "http://192.168.1.103:5000/convert"
+    querystring = {"base":base,"target":target,"value":value}
+    response = requests.request("GET", url, params=querystring)
+    
+    if response.status_code == 200:
+        sucessful.append(response)
+
+elapsed_time = 0.00
+
+def execute_requests():
+    global elapsed_time
+    start_time = time.time()
+    thread_list = []
+    q = Queue()
+
+    for i in range(threads):
+        base = coins[random.randint(0, len(coins) - 1)]
+        target = coins[random.randint(0, len(coins) - 1)]
+        value = random.randint(i, 1000000)
+
+        t = threading.Thread(target=test_request_load, args=(q, base, target, value))
+        thread_list.append(t)
+    for t in thread_list:
+        t.start()
+    for t in thread_list:
+        t.join()
+    elapsed_time = time.time() - start_time
+    return
+
+execute_requests()
+
+print(f'Elapsed time: {elapsed_time}')
+print(f'Sucessful requests: {len(sucessful)}')
