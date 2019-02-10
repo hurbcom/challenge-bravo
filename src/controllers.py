bfddb55171ea283302a92035a6a2ea31f513105e
root <root@localhost.localdomain>
2019-02-10 14:32:45 +0000

    Ajusts uWGSI

--- src/controllers.py
+++ src/controllers.py
@@ -12,10 +12,11 @@ class Convert(Resource):
     #GET da api que retorna a moeda convertida
     def get(self):
         coins_regex = "(\AUSD\Z)|(\ABRL\Z)|(\AEUR\Z)|(\ABTC\Z)|(\AETH\Z)"
-        base = request.args.get('base', str)
-        target = request.args.get('target', str)
-        value = float(request.args.get('value', float))
+
         try:
+            base = request.args.get('base', str)
+            target = request.args.get('target', str)
+            value = float(request.args.get('value', float))
             if base is None or target is None:   
                 return json.dumps({'erro':'Consulta Invalida.'}), 400
             if base is None or target is None:   
@@ -36,33 +37,3 @@ class Convert(Resource):
     # Reliza a conversão
     def __get_exchange(self, base, target):
         return (1 / usd_rates[base]) / (1 / usd_rates[target])
-
-#GET da api que retorna a moeda convertida
-    def get():
-        coins_regex = "(\AUSD\Z)|(\ABRL\Z)|(\AEUR\Z)|(\ABTC\Z)|(\AETH\Z)"
-        base = request.args.get('base', str)
-        target = request.args.get('target', str)
-        value = float(request.args.get('value', float))
-        try:
-            if base is None or target is None:   
-                return json.dumps({'erro':'Consulta Invalida.'}), 400
-            if base is None or target is None:   
-                return json.dumps({'erro':'Consulta Invalida.'}), 400
-            if not re.match(coins_regex, base) or not re.match(coins_regex, target):
-                return json.dumps({'erro':'Moedas nao suportadas'}), 400
-            if value == 0 or value is None:
-                value = 1
-
-            exchangePrice = self.__get_exchange(base, target)
-            data = { "Valor Total Convertido": exchangePrice * value, "Valor de Cambio": exchangePrice, "Data": str(datetime.datetime.today()) }
-
-            return json.dumps(data)
-
-        except:
-            return json.dumps({"erro":"Falha de servidor, tente novamente"}), 400
-    
-    # Reliza a conversão
-    def __get_exchange(self, base, target):
-        return (1 / usd_rates[base]) / (1 / usd_rates[target])
-
-
