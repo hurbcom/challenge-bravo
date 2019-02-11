f12d5c0967b94b6baaec33f3b6cf453c75cfdea8
Douglas Ferreira <drfdouglas@hotmail.com>
2019-02-11 12:56:49 -0200

    Cleanup

--- src/exchange_rates.py
+++ src/exchange_rates.py
@@ -6,7 +6,6 @@ import requests
 import json
 
 #Obtem as taxas de cambio atuais em dólar
-
 url = "https://min-api.cryptocompare.com/data/price"
 querystring = {"fsym":"USD","tsyms":"USD,BRL,EUR,BTC,ETH"}
 response = requests.request("GET", url, params=querystring)
--- tests/load_test.py
+++ tests/load_test.py
@@ -4,14 +4,12 @@ import time, random
 from queue import Queue
 
 coins = ['USD', 'BRL', 'EUR', 'BTC', 'ETH']
-
 threads = 1000
 sucessful = []
 
 def test_request_load(t_queue, base, target, value):
     url = "http://192.168.1.103:5000/api/convert"
     querystring = {"base":base,"target":target,"value":value}
-    
     try: 
         response = requests.request("GET", url, params=querystring) 
         if response.status_code == 200:
@@ -31,13 +29,15 @@ def execute_requests():
         base = coins[random.randint(0, len(coins) - 1)]
         target = coins[random.randint(0, len(coins) - 1)]
         value = random.randint(i + 1, 100000)
-
         t = threading.Thread(target=test_request_load, args=(q, base, target, value))
         thread_list.append(t)
+
     for t in thread_list:
         t.start()
+
     for t in thread_list:
         t.join()
+
     elapsed_time = time.time() - start_time
     return
 
