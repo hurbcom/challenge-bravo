a20685a80cfdcb970a6250e4728fe53347a35d52
Lucas Silva <lsilva@mosano.com>
2019-04-02 01:10:34 +0100

    feat(cache): Add cache strategy to avoid performance issues

--- src/currency/services/currency-converter/currency-converter.service.ts
+++ src/currency/services/currency-converter/currency-converter.service.ts
@@ -1,3 +1,5 @@
+import { Cache, ExpirationStrategy, MemoryStorage } from 'node-ts-cache';
+
 import { Currency } from '../../definitions/currency';
 import { ICurrencyConverter } from '../../interfaces/currency-converter.interface';
 import { CurrencyRatesService } from '../currency-rates/currency-rates.service';
@@ -5,6 +7,9 @@ import { CurrencyRatesService } from '../currency-rates/currency-rates.service';
 export class CurrencyConverterService implements ICurrencyConverter {
     constructor(private currentRatesService: CurrencyRatesService) {}
 
+    @Cache(new ExpirationStrategy(new MemoryStorage()), {
+        ttl: 3600,
+    })
     async convert(
         from: Currency,
         to: Currency,
