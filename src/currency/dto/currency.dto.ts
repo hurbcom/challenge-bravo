e12cd5152f58e30a83da42b80466c527ad20aace
Lucas Silva <lsilva@mosano.com>
2019-04-02 01:26:51 +0100

    feat(swagger): Add swagger module as a middleware

--- src/currency/dto/currency.dto.ts
+++ src/currency/dto/currency.dto.ts
@@ -1,5 +1,6 @@
 import { Transform } from 'class-transformer';
 import { IsIn, IsNumber, IsPositive } from 'class-validator';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 import { Currency } from '../definitions/currency';
 import { CURRENCY } from '../providers/currency';
@@ -7,14 +8,19 @@ import { IsNotEqualTo } from '../validators/is-not-equal-to/is-not-equal-to.vali
 
 export class CurrencyDto {
     @IsIn(CURRENCY)
+    @ApiModelProperty({ enum: CURRENCY })
     from: Currency;
 
     @IsIn(CURRENCY)
     @IsNotEqualTo('from')
+    @ApiModelProperty({ enum: CURRENCY })
     to: Currency;
 
     @IsNumber()
     @IsPositive()
     @Transform(value => +value)
+    @ApiModelProperty({
+        exclusiveMinimum: 0,
+    })
     amount: number;
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {
     FastifyAdapter,
     NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -12,7 +13,18 @@ async function bootstrap() {
         AppModule,
         new FastifyAdapter(),
     );
+
     app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+    const options = new DocumentBuilder()
+        .setTitle('Currency converter')
+        .setDescription('hiplatform tech challenge')
+        .setVersion('1.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(3000);
 }
 bootstrap();
