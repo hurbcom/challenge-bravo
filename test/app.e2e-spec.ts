5900ff86798304994fc7b4f13e1b29f9e6653038
Lucas Silva <lsilva@mosano.com>
2019-04-02 13:58:36 +0100

    test(e2e): Fix e2e to work with fastify adapter

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,43 +1,65 @@
-import * as request from 'supertest';
-import { Test, TestingModule } from '@nestjs/testing';
+import { expect } from 'chai';
 import { ValidationPipe } from '@nestjs/common';
+import {
+    FastifyAdapter,
+    NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { Test, TestingModule } from '@nestjs/testing';
 
 import { AppModule } from './../src/app.module';
 
 describe('ConverterController (e2e)', () => {
-    let app;
+    let app: NestFastifyApplication;
 
     beforeEach(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [AppModule],
         }).compile();
 
-        app = moduleFixture.createNestApplication();
+        app = moduleFixture.createNestApplication<NestFastifyApplication>(
+            new FastifyAdapter(),
+        );
         app.useGlobalPipes(new ValidationPipe({ transform: true }));
         await app.init();
     });
 
+    afterAll(async () => {
+        await app.close();
+    });
+
     it('/converter (GET) - Empty data', () => {
-        return request(app.getHttpServer())
-            .get('/converter')
-            .expect(400);
+        return app
+            .inject({
+                method: 'GET',
+                url: '/converter',
+            })
+            .then(({ statusCode }) => expect(statusCode).to.equal(400));
     });
 
     it('/converter?from=invalidFrom&to=invalidTo&amount=nan (GET) - Invalid data', () => {
-        return request(app.getHttpServer())
-            .get('/converter?from=invalidFrom&to=invalidTo&amount=nan')
-            .expect(400);
+        return app
+            .inject({
+                method: 'GET',
+                url: '/converter?from=invalidFrom&to=invalidTo&amount=nan',
+            })
+            .then(({ statusCode }) => expect(statusCode).to.equal(400));
     });
 
     it('/converter?from=USD&to=USD&amount=100 (GET) - Equal currencies', () => {
-        return request(app.getHttpServer())
-            .get('/converter?from=USD&to=USD&amount=100')
-            .expect(400);
+        return app
+            .inject({
+                method: 'GET',
+                url: '/converter?from=USD&to=USD&amount=100',
+            })
+            .then(({ statusCode }) => expect(statusCode).to.equal(400));
     });
 
     it('/converter?from=USD&to=BRL&amount=100 (GET) - Valid data', () => {
-        return request(app.getHttpServer())
-            .get('/converter?from=USD&to=BRL&amount=100')
-            .expect(200);
+        return app
+            .inject({
+                method: 'GET',
+                url: '/converter?from=USD&to=BRL&amount=100',
+            })
+            .then(({ statusCode }) => expect(statusCode).to.equal(200));
     });
 });
