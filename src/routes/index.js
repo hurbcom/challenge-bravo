a8eb2ae0fab9319f66b65061794ef7987995c2e4
Mateus Schenatto <mateus.schenatto@live.com>
2019-04-02 17:36:19 -0300

    Adicionando rota do controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const baseApiRoute = '/api'
+const baseRouteApi = '/api'
+
+const coinController = require('../controllers/coinController')
+
+router.get(`${baseRouteApi}/convert`, coinController.convert)
 
 module.exports = router
