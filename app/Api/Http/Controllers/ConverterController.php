e9293262fb292545012abaa84bf3d0114ce4286e
Clayson Oliveira <clayson.capo@gmail.com>
2019-04-09 17:00:26 -0300

    - improving validation messages

--- app/Api/Http/Controllers/ConverterController.php
+++ app/Api/Http/Controllers/ConverterController.php
@@ -21,12 +21,24 @@ class ConverterController extends BaseController
 
         $currencies = env('AVAILABLE_CURRENCIES');
 
+        $rules = [
+            'from'      => 'required|alpha|size:3|in:' . $currencies, // alpha chars, must have 3 chars in USD,BRL,EUR,BTC,ETH
+            'to'        => 'required|alpha|size:3|in:' . $currencies,
+            'amount'    => 'required|regex:^[-+]?[0-9]{1,3}(?:,?[0-9]{3})*\.[0-9]{2}$^' //2 decimal float regex
+        ];
+
+        $Messages = [
+            'required'  => 'The :attribute parameter is required.',
+            'alpha'     => 'The :attribute parameter should contain only alpha characters.',
+            'size'      => 'The :attribute must be exactly :size.',
+            'in'        => 'The :attribute must be one of the following types: :values',
+            'regex'     => 'The :attribute must be float with 2 decimals',
+        ];
+
         //request validation
-        $this->validate($request, [
-           'from' => 'required|alpha|size:3|in:' . $currencies, // alpha chars, must have 3 chars in USD,BRL,EUR,BTC,ETH
-            'to' => 'required|alpha|size:3|in:' . $currencies,
-            'amount' => 'required|regex:^[-+]?[0-9]{1,3}(?:,?[0-9]{3})*\.[0-9]{2}$^' //2 decimal float regex
-        ]);
+        $this->validate($request, $rules, $Messages);
+
+
 
         return $convert->run($request);
     }
