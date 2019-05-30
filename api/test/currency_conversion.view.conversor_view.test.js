e7bf6c93ed64db4e18d7c38f7a2366c0375a2a44
Leticia <leticiasoares@id.uff.br>
2019-05-30 12:40:53 -0300

    Separacao de teste por parametro

--- api/test/currency_conversion.view.conversor_view.test.js
+++ api/test/currency_conversion.view.conversor_view.test.js
@@ -8,9 +8,9 @@ const base = 'http://localhost:5000';
 
 describe('Conversor de moedas: GET / ', () => {
 
-    describe('when stubbed', () => {
+    describe('Verifica tratamento de erros no request', () => {
 
-        it('Request invalido: Moeda invalido', (done) => {
+        it('Request invalido: Moeda em FROM invalida', (done) => {
             const from = 'ABC';
             const to = 'USD';
             const amount = 12.3;
@@ -25,6 +25,24 @@ describe('Conversor de moedas: GET / ', () => {
                 body.success.should.eql(false);
             }  );
                 done();
+
+        });
+
+        it('Request invalido: Moeda em TO invalida', (done) => {
+            const from = 'BTC';
+            const to = 'DFS';
+            const amount = 12.3;
+
+            request.get(`${base}/currency?from=${from}&to=${to}&amount=${amount}`, (req, res, body) => {
+                res.statusCode.should.eql(200);
+                res.headers['content-type'].should.contain('application/json');
+
+                body = JSON.parse(body);
+                body.should.include.keys('success', 'message');
+
+                body.success.should.eql(false);
+            }  );
+            done();
 
         });
 
