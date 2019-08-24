f6a8dd204d77a6f4d8d735c1b3df94d874afe2eb
Tadeu Bastos <tadeu@ondular.net>
2019-08-24 16:49:49 -0300

    implement 'currencies' endpoint handle details

--- resthandler/currenciesHandle.go
+++ resthandler/currenciesHandle.go
@@ -1,10 +1,23 @@
 package resthandler
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/tmcb/challenge-bravo/currency"
 )
 
 func currenciesHandle(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	s := struct {
+		Currencies []string `json:"currencies"`
+	}{}
+	for _, c := range currency.Currencies() {
+		s.Currencies = append(s.Currencies, c.ToString())
+	}
+	err := json.NewEncoder(w).Encode(s)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
