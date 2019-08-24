c4fa7f01c79ed6c485131a110212ba16724fe175
Tadeu Bastos <tadeu@ondular.net>
2019-08-24 14:30:57 -0300

    add supported currency enumerations

--- currency/currency.go
+++ currency/currency.go
@@ -1,1 +1,14 @@
 package currency
+
+/*
+currency represents a currency unit.
+*/
+type currency int
+
+const (
+	USD currency = iota // US Dollar
+	BRL                 // Brazilian Real
+	EUR                 // Euro
+	BTC                 // Bitcoin
+	ETH                 // Ethereum
+)
