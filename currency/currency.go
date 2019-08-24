9b3e5d54254cb86f4bb30ada93b89d3085766d88
Tadeu Bastos <tadeu@ondular.net>
2019-08-24 14:56:49 -0300

    add function that returns codes representing supported currencies

--- currency/currency.go
+++ currency/currency.go
@@ -19,3 +19,11 @@ Currencies returns an array containing all supported currencies.
 func Currencies() []currency {
 	return []currency{USD, BRL, EUR, BTC, ETH}
 }
+
+/*
+ToString returns an ISO 4217-like code representing currency c.
+*/
+func (c currency) ToString() string {
+	s := []string{"USD", "BRL", "EUR", "BTC", "ETH"}
+	return s[c]
+}
--- currency/currency_test.go
+++ currency/currency_test.go
@@ -22,3 +22,21 @@ func TestCurrencies(t *testing.T) {
 		t.Error("supported currencies are missing")
 	}
 }
+
+func TestToString(t *testing.T) {
+	if USD.ToString() != "USD" {
+		t.Error("bad string conversion: USD")
+	}
+	if BRL.ToString() != "BRL" {
+		t.Error("bad string conversion: BRL")
+	}
+	if EUR.ToString() != "EUR" {
+		t.Error("bad string conversion: EUR")
+	}
+	if BTC.ToString() != "BTC" {
+		t.Error("bad string conversion: BTC")
+	}
+	if ETH.ToString() != "ETH" {
+		t.Error("bad string conversion: ETH")
+	}
+}
