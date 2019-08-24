024dcb959f95f5387dd4a8628a1c9c7aacb95961
Tadeu Bastos <tadeu@ondular.net>
2019-08-24 15:32:34 -0300

    remove index handler

--- resthandler/resthandler.go
+++ resthandler/resthandler.go
@@ -1,22 +1,14 @@
 package resthandler
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/julienschmidt/httprouter"
 )
-
-func index(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintln(w, "hello, world")
-}
 
 /*
 New returns the currency converter REST API handler.
 */
 func New() *httprouter.Router {
 	router := httprouter.New()
-	router.GET("/", index)
 	// TODO router.GET("/currencies", ...)
 	// TODO router.GET("/quotes/:currency", ...)
 	// TODO router.GET("/conversion", ...)
