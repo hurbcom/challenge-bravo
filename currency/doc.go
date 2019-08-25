5b1fbbcea27e0dfe6b22118dfafe3546c80e6150
Tadeu Bastos <tadeu@ondular.net>
2019-08-25 12:55:04 -0300

    update currency package documentation

--- currency/doc.go
+++ currency/doc.go
@@ -1,4 +1,8 @@
 /*
-Package currency is a work in progress.
+Package currency provides a USD-based currency quotes and a currency conversion
+API.
+
+In its default configuration, package currency fetches and caches up-to-date
+currency quotes from a third-party service.
 */
 package currency
