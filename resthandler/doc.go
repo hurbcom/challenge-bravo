94ac25acf2e7eb442662afb413e205e5dab205f7
Tadeu Bastos <tadeu@ondular.net>
2019-08-25 12:54:42 -0300

    update resthandler package documentation

--- resthandler/doc.go
+++ resthandler/doc.go
@@ -1,4 +1,10 @@
 /*
-Package resthandler is a work in progress.
+Package resthandler provides the following REST API endpoints:
+
+'GET /currencies' returns a list of supported currencies.
+
+'GET /quotes/{currency}' returns USD-based quotes for a given currency.
+
+'GET /conversion?from={from}&to={to}&amount={amount}' converts a given amount in currency from to currency to.
 */
 package resthandler
