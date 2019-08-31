c90d788fd52b13f4c16b04029ee0b63dc8228359
Raphael Ferreira <raphaelferreira.dev@gmail.com>
2019-08-31 18:27:01 -0300

    Add POST and DELETE methods

--- src/server.js
+++ src/server.js
@@ -26,15 +26,44 @@ app.get('/currency', function(req, res){
         } else {
             console.log(data)
             var obj = JSON.parse(data);
-            var result = 'Nenhuma moeda foi encontrado';
+            var result = {status: 'falha', resultado: 'Nenhuma moeda foi encontrada' };
 
             if (obj != null) {
                 result = obj;
             }
 
-            obj.currencies.forEach(function(currency) {
+            if (Object.keys(req.query).length > 0) {
+                obj.currencies.forEach(function(currency) {
+                    if (currency != null) {
+                        if (currency.code == req.query.code.toUpperCase()) {
+                            result = currency;
+                        }
+                    } 
+                });
+            }
+
+            var response = result;
+            res.json(response);
+        }
+    });
+});
+
+app.get('/currency/:code', function(req, res){
+    fs.readFile('./database.json', 'utf8', function(err, data){
+        if (err) {
+            var response = {status: 'falha', resultado: err};
+            res.json(response);
+        } else {            
+            var obj = JSON.parse(data);
+            var result = {status: 'falha', resultado: 'Nenhuma moeda foi encontrada' } ;
+
+            if (obj != null) {
+                result = obj;
+            }
+
+            obj.currencies.forEach(function(currency) {                
                 if (currency != null) {
-                    if (currency.code == req.query.code) {
+                    if (currency.code == req.params.code.toUpperCase()) {
                         result = currency;
                     }
                 } 
@@ -42,6 +71,62 @@ app.get('/currency', function(req, res){
 
             var response = result;
             res.json(response);
+        }
+    });
+});
+
+app.post('/currency', function(req, res){
+    fs.readFile('./database.json', 'utf8', function(err, data){
+        if (err) {
+            var response = {status: 'falha', resultado: err};
+            res.json(response);
+        } else {            
+            var obj = JSON.parse(data);            
+
+            if (Object.keys(req.body).length > 0) {
+                obj.currencies.push(req.body);
+
+                fs.writeFile('./database.json', JSON.stringify(obj), function(err) {
+                    if (err) {
+                    var response = {status: 'falha', resultado: err};
+                    res.json(response);
+                    } else {
+                    var response = {status: 'sucesso', resultado: 'Registro incluído com sucesso'};
+                    res.json(response);
+                    }
+                });
+            } else {
+                var response = {status: 'falha', resultado: 'Nenhum registro a ser incluído'};
+                res.json(response);
+            }            
+        }
+    });
+});
+
+app.delete('/currency/:code', function(req, res){
+    fs.readFile('./database.json', 'utf8', function(err, data){
+        if (err) {
+            var response = {status: 'falha', resultado: err};
+            res.json(response);
+        } else {
+            var obj = JSON.parse(data);
+            var codeDeleted = req.params.code.toUpperCase();
+            var codeIndex = obj.currencies.findIndex(e => e.code === codeDeleted)
+
+            console.log(codeDeleted)
+            console.log(codeIndex)
+            
+            obj.currencies.splice(codeIndex,1)
+
+            fs.writeFile('./database.json', JSON.stringify(obj), function(err) {
+                if (err) {
+                    var response = {status: 'falha', resultado: err};
+                    res.json(response);
+                } else {
+                    var response = {status: 'sucesso', resultado: `Registro ${codeDeleted} excluído com sucesso`};
+                    res.json(response);
+                }
+            });
         }
     });
 });
