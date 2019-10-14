649da9681d535ce85bcc7a344b90365a6942f9b5
felipe <felipe.bizzo@gmail.com>
2019-10-14 00:31:11 -0300

    adicao de nova moeda e comentarios

--- converter.py
+++ converter.py
@@ -2,19 +2,34 @@ from flask import Flask
 from flask_restful import Api, Resource, reqparse
 import requests, json
 
+#inicializando a biblioteca Flask para as requisições do json.
 app = Flask(__name__)
 api = Api(app)
 
-valid_currencies = ["usd", "eur", "brl", "bitcoin", "ethereum"]
+#data é o dicionário que armazena as informações das moedas que serão guardadas no arquivo json.
+data = {}
 
+#essas duas listas são usadas para verificar as moedas que atualmente existem no conversor, ela é modificada caso se adiciona uma nova moeda.
+valid_currencies_symbols = ["usd", "eur", "brl", "btc", "eth"]
+valid_currencies_names = ["dollar", "euro", "real", "bitcoin", "ethereum"]
+
+#número total de moedas aceitas pelo conversor.
 curr_amount = 5
 
-
+#função que verifica se duas moedas são válidas para esse conversor.
 def verify_valid_currencies(to_cur, from_cur):
     to_conf = False
     from_conf = False
 
-    for item in (valid_currencies):
+    #verificação se foram passados símolos válidos.
+    for item in valid_currencies_symbols:
+        if(to_cur == item):
+            to_conf = True
+        if(from_cur == item):
+            from_conf = True
+
+    #verificação se foram passados nomes válidos.
+    for item in valid_currencies_names:
         if(to_cur == item):
             to_conf = True
         if(from_cur == item):
@@ -26,57 +41,127 @@ def verify_valid_currencies(to_cur, from_cur):
         return False
     
 
-def test_json():
-    r = requests.get("currencies.json")
-    print(r.json())
-
-def teste_json(from_currency, to_currency, amount):
+#função que atualiza o json, é chamada na inicialização do programa pra atualizar os valores das moedas.
+def atualiza_json():
     
     import requests, json
+    global data
 
-    #getting the json from a floatrates
+    #pegando o json da floatrates, para os valores das moedas não cripto.
     r = requests.get('http://www.floatrates.com/daily/usd.json')
 
     result = r.json()
+    cont = 0
     
-    
-   
-    if(from_currency == "usd"):
-        from_rate = 1
-    else :
-        if(from_currency in result):
-            from_rate = result[from_currency]["inverseRate"]
-        else:
-            rc = requests.get('https://api.coinmarketcap.com/v1/ticker/'+from_currency)
-            resultc = rc.json()
-            print(rc)
-            if(True):
-                from_rate = float(resultc[0]['price_usd'])
-                
+    for item in valid_currencies_symbols:
+        #como o site lastreia o valor baseado no dolar, ele não inclui a moeda dolar, então o valor é setado inicialmente como 1.
+        if(item == "usd"):
+            rate = 1
+            code = 'usd'
+            name = 'Dollar'
+        else :
+            #verifica se a moeda que está sendo adicionada foi encontrada no json da floatrates.
+            if(item in result):
+                rate = result[item]["inverseRate"]
+                name = result[item]["name"]
+                code = item
             else:
-                print("moeda nao encontrada")
-                return -1
-    
-    if(to_currency == "usd"):
-        to_rate = 1
-    else :
-        to_rate = result[to_currency]["rate"]
-    
-    print("rate dollar/" + from_currency, from_rate)
-    print("rate dollar/" + to_currency , to_rate)
+                #fazendo a requisição para a api do coinmarketcap para pegar o valor das criptomoedas.
+                rc = requests.get('https://api.coinmarketcap.com/v1/ticker/'+valid_currencies_names[cont])
+                
+                #verificando se a requisição foi bem sucedida.
+                if(rc == "<response 200>"):
+                    resultc = rc.json()
+                    rate = resultc[0]['price_usd']
+                    name = valid_currencies_names[cont]
+                    code = resultc[0]['symbol'].lower()
+                
+                #caso não seja possível encontrar a moeda em nenhuma das APIs, retorna -1 como erro.
+                else:
+                    print("moeda nao encontrada")
+                    return -1
+        
+        #inclui os valores do dic data que será utilizado para criar o arquivo json
+        data[code] = []
+        data[code].append({
+        'name': name,
+        'code': code,
+        'rate': rate
+        })
+        cont+=1
 
-    total = (amount*from_rate)*to_rate
+    #abre o arquivo json e preenche com os valores do dicionario "data".    
+    with open('currencies.json', 'w') as outfile:
+        json.dump(data, outfile)
+    
+    
+    
+#função que calcula a conversão entre duas moedas.
+def realiza_conversao(from_cur, to_cur, amount):
 
-    print("total amount", total)
+
+    #abrindo o arquivo json no modo leitura, lendo os dados necessarios para realizar a conversão e retornando o total.
+    with open('currencies.json', 'r') as json_file:
+        data = json.load(json_file)
+        f = data[from_cur]
+        t = data[to_cur]
+        f_rate = float(f[0]['rate'])
+        t_rate = float(t[0]['rate'])
+        print(f_rate, t_rate, amount)
+        total = (amount*f_rate)/t_rate
+        print(total)
+    
     return total
 
-def teste_crypto(crypto_name):
-    import requests
+#função que adiciona uma nova moeda na lista de moedas válidas do conversor.
+def add_currency(symbol, name):
+    global curr_amount
+    global valid_currencies_names
+    global valid_currencies_symbols
+    global data
 
-    r = requests.get('https://api.coinmarketcap.com/v1/ticker/'+crypto_name)
-
+    #pegando as informações da moeda no caso em que não é uma criptomoeda.
+    r = requests.get('http://www.floatrates.com/daily/usd.json')
     result = r.json()
 
-    print(result[0]['price_usd'])
+    if(symbol in result):
+                rate = result[symbol]["inverseRate"]
+                name = result[symbol]["name"]
+                code = symbol.lower()
+    else:
+        #pegando as informações da moeda para o caso em que é uma criptomoeda.
+        rc = requests.get('https://api.coinmarketcap.com/v1/ticker/'+name)
+        print(rc)
+                
+                
+        if(rc == "<response 200>"):
+            resultc = rc.json()
+            rate = resultc[0]['price_usd']
+            name = name
+            code = symbol.lower()
+                
+        else:
+            print("moeda nao encontrada")
+            return False
+    
+    #adicionando as informações da nova moeda ao dicionário de dados.
+    data[symbol] = []
+    data[symbol].append({
+    'name': name,
+    'code': symbol,
+    'rate': rate
+    })
 
+    #adicionando a nova moeda a lista de moedas válidas.
+    valid_currencies_names.append(name)
+    valid_currencies_symbols.append(symbol)
 
+    #atualizando o arquivo json com a nova moeda.
+    with open('currencies.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+    #atualizando o número total de moedas válidas.
+    curr_amount+=1
+    return True
+    
+    
--- interface.py
+++ interface.py
@@ -1,28 +1,53 @@
+#importando a biblioteca gráfica tkinter do python e o script de conversão criado para esse projeto.
 from tkinter import *
 import converter as cv
   
 class Application:
     def __init__(self, master=None):
+
+        #quando o programa é iniciado, os dados das moedas já existentes é atualizado e colocados em um arquivo do tipo json.
+        cv.atualiza_json()
+
+        #o primeiro container da aplicação é o texto que aparece com o título da aplicação.
         self.fontePadrao = ("Arial", "10")
         self.primeiroContainer = Frame(master)
         self.primeiroContainer["pady"] = 10
         self.primeiroContainer.pack()
   
+        #o segundo container é o texto "de", que indica onde o usuário deve digitar a moeda da qual ele pretende converter o valor.
         self.segundoContainer = Frame(master)
         self.segundoContainer["padx"] = 20
         self.segundoContainer.pack()
   
+        #o terceiro container é o "para", que indica para qual moeda o usuário quer converter.
         self.terceiroContainer = Frame(master)
         self.terceiroContainer["padx"] = 20
         self.terceiroContainer.pack()
 
+        #o quarto container é onde o usuário expressa a quantidade de moedas que devem ser convertidas.
         self.quartoContainer = Frame(master)
         self.quartoContainer["padx"] = 20
         self.quartoContainer.pack()
 
+        #o quinto container é o botão que executa a conversão de moedas.
         self.quintoContainer = Frame(master)
         self.quintoContainer["pady"] = 20
         self.quintoContainer.pack()
+
+        #add1 é o container que representa o espaço para o usuário colocar o nome de uma moeda que ele deseja adicionar ao conversor.
+        self.add1Container = Frame(master)
+        self.add1Container["padx"] = 20
+        self.add1Container.pack()
+
+        #add2 é o container que representa o espaço para o usuário colocar a sigla de uma moeda que se deseja adicionar ao conversor.
+        self.add2Container = Frame(master)
+        self.add2Container["padx"] = 20
+        self.add2Container.pack()
+
+        #o sexto container contém o texto de adicionar uma nova moeda.
+        self.sextoContainer = Frame(master)
+        self.sextoContainer["pady"] = 20
+        self.sextoContainer.pack()
 
         self.respContainer = Frame(master)
         self.respContainer["pady"] = 20
@@ -32,16 +57,16 @@ class Application:
         self.titulo["font"] = ("Arial", "10", "bold")
         self.titulo.pack()
   
-        self.nomeLabel = Label(self.segundoContainer,text="De", font=self.fontePadrao)
-        self.nomeLabel.pack(side=LEFT)
+        self.fromLabel = Label(self.segundoContainer,text="De", font=self.fontePadrao)
+        self.fromLabel.pack(side=LEFT)
   
         self.from_currency = Entry(self.segundoContainer)
         self.from_currency["width"] = 10
         self.from_currency["font"] = self.fontePadrao
         self.from_currency.pack(side=LEFT)
   
-        self.senhaLabel = Label(self.terceiroContainer, text="Para ", font=self.fontePadrao)
-        self.senhaLabel.pack(side=LEFT)
+        self.toLabel = Label(self.terceiroContainer, text="Para ", font=self.fontePadrao)
+        self.toLabel.pack(side=LEFT)
 
         self.to_currency = Entry(self.terceiroContainer)
         self.to_currency["width"] = 10
@@ -56,40 +81,78 @@ class Application:
         self.quantidade["font"] = self.fontePadrao
         self.quantidade.pack(side=RIGHT)
   
-        self.autenticar = Button(self.quintoContainer)
-        self.autenticar["text"] = "Converter"
-        self.autenticar["font"] = ("Calibri", "8")
-        self.autenticar["width"] = 12
-        self.autenticar["command"] = self.Converter
-        self.autenticar.pack()
+        self.converter = Button(self.quintoContainer)
+        self.converter["text"] = "Converter"
+        self.converter["font"] = ("Calibri", "8")
+        self.converter["width"] = 12
+        self.converter["command"] = self.Converter
+        self.converter.pack()
+
+        self.addNomeLabel = Label(self.add1Container, text="nome ", font=self.fontePadrao)
+        self.addNomeLabel.pack(side=LEFT)
   
+        self.addNome = Entry(self.add1Container)
+        self.addNome["width"] = 10
+        self.addNome["font"] = self.fontePadrao
+        self.addNome.pack(side=RIGHT)
+
+        self.addSymbolLabel = Label(self.add2Container, text="sigla ", font=self.fontePadrao)
+        self.addSymbolLabel.pack(side=LEFT)
+  
+        self.addSymbol = Entry(self.add2Container)
+        self.addSymbol["width"] = 10
+        self.addSymbol["font"] = self.fontePadrao
+        self.addSymbol.pack(side=RIGHT)
+
+        self.adicionar = Button(self.sextoContainer)
+        self.adicionar["text"] = "adicionar moeda"
+        self.adicionar["font"] = ("Calibri", "8")
+        self.adicionar["width"] = 12
+        self.adicionar["command"] = self.Adicionar
+        self.adicionar.pack()
+  
+        #o respContainer é onde o resultado da conversão mostrado para o usuário.
         self.msg = Label(self.respContainer, text="", font=self.fontePadrao)
         self.msg.pack()
   
-    #Método verificar senha
+    #Método atrelado ao botão de conversao.
     def Converter(self):
+
+        #pegando as variáveis preenchidas pelo usuário.
         from_currency = self.from_currency.get()
         to_currency = self.to_currency.get()
         amount = self.quantidade.get()
 
+        #fazendo uma verificação se o a quantidade é um valor válido, float não negativo.
         try:
             amount = int(self.quantidade.get())
             if(amount < 0):
                 print("Valor invalido. Por favor digite escolha um numero nao negativo.")
-            
-        #checking for input errors
+
         except ValueError:
             print("Valor invalido. Por favor digite apenas numeros.")
         
         else:
+            #a função verify verifica se as moedas escolidas existem no conversor.
             verify = cv.verify_valid_currencies(to_currency, from_currency)
             if(verify):
-                result = cv.teste_json(from_currency, to_currency, amount)
+                result = cv.realiza_conversao(from_currency, to_currency, amount)
                 
                 self.msg["text"] = "Convertido de", '{:10,.2f}'.format(amount), from_currency, "para", '{:10,.2f}'.format(result), to_currency
             else:
                 self.msg["text"] = "moeda inválida ou não disponível"
+
+    #função atrelada ao botão para adicionar uma nova moeda ao conversor.
+    def Adicionar(self):
+        nome = self.addNome.get()
+        sigla = self.addSymbol.get()
         
+        #chamada da função do script do conversor que adiciona uma nova moeda.
+        if(cv.add_currency(sigla, nome)):
+            self.msg["text"] = "moeda adicionada com sucesso."
+        else :
+            self.msg["text"] = "Não foi possível adicionar esta moeda."
+
   
   
 root = Tk()
