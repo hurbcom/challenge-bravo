6db74f45ce0b08d5356ecd1e313909275dec57f1
Eduardo Son <eduardo.son@outlook.com>
2019-10-16 21:10:28 -0300

    Corrigindo validacao de codigo

--- src/services/validate.ts
+++ src/services/validate.ts
@@ -1,13 +1,13 @@
 class Validation {
   public NewCurrency (currency: string, memoryCurrency: string, list: string): string {
-    if (currency.length !== 3) return 'Formato de código não suportado, tente enviar um conjunto de 3 caracteres'
+    if (currency.length !== 3 && currency.length !== 4) return 'Formato de código não suportado, tente enviar um conjunto de 3 a 4 caracteres'
     if (!list.includes(currency)) return `Infelizmente ainda não damos suporte para moedas do tipo: ${currency}`
     if (memoryCurrency.includes(currency)) return 'Não é possível adicionar uma moeda que já está registrada na plataforma'
     return ''
   }
 
   public RemoveCurrency (currency: string, memoryCurrency: string): string {
-    if (currency.length !== 3) return 'Formato de código não suportado, tente enviar um conjunto de 3 caracteres'
+    if (currency.length !== 3 && currency.length !== 4) return 'Formato de código não suportado, tente enviar um conjunto de 3 a 4 caracteres'
     if (!memoryCurrency.includes(currency)) return 'Não é possível remover uma moeda que não está registrada na plataforma'
     return ''
   }
