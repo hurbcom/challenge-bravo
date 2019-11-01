8e20c200f398263a0e0aef34b9d6d0bc87a664a8
Renan Aguiar <renan@rca.pro.br>
2019-11-01 12:48:27 -0300

    Remocao das configuracoes do banco de dados para um arquivo externo

---
+++ api/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Config
+ *
+ * This file is used to configure MySQL access variables.
+ *
+ */
+/** @var string DB_HOST Database host */
+define("DB_HOST", "127.0.0.1:4706");
+/** @var string DB_USER Databaase user */
+define("DB_USER", "root");
+/** @var string DB_PASS Database password */
+define("DB_PASS", "squeal");
+/** @var string DB_NAME Database name */
+define("DB_NAME", "bravo");
--- api/index.php
+++ api/index.php
@@ -3,12 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1); 
 error_reporting(E_ALL);
 
-
-define("DB_HOST", "127.0.0.1:4706");
-define("DB_USER", "root");
-define("DB_PASS", "squeal");
-define("DB_NAME", "bravo");
-
+include("config.php");
 include("class/api.php");
 include("class/HTTPCurrency.php");
 include("class/Currency.php");
