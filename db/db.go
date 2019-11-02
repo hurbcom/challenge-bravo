02cc53c877dbdc23ddbc4c97c402fc7455fc4466
Bispo <daniel@bisponet.com>
2019-11-02 16:27:56 -0300

    Adding a dummy database file with initial currencies already on it

---
+++ db/db.go
@@ -0,0 +1,37 @@
+package db
+
+type event struct {
+	ID       string `json:"ID"`
+	Currency string `json:"Currency"`
+	Rate     string `json:"Rate"`
+}
+
+type allEvents []event
+
+var events = allEvents{
+	{
+		ID:       "1",
+		Currency: "USD",
+		Rate:     "1",
+	},
+	{
+		ID:       "2",
+		Currency: "BRL",
+		Rate:     "0.25",
+	},
+	{
+		ID:       "3",
+		Currency: "EUR",
+		Rate:     "1,12",
+	},
+	{
+		ID:       "4",
+		Currency: "BTC",
+		Rate:     "9347.2",
+	},
+	{
+		ID:       "5",
+		Currency: "ETH",
+		Rate:     "183.91",
+	},
+}
