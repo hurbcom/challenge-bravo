7e0f49cc37188919482a49e3d5d8957b8721392a
Bispo <daniel@bisponet.com>
2019-11-02 22:54:34 -0300

    Adding mongodb support to the application and autopopulate database at the db.go file

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/bispoman/challenge-bravo/route"
 	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	//connecting bits to mongodb
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	//trying to connect
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//checking connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Info("Connected to mongodb @ localhost:27017")
+
 	log.Info("Application has been started on : 8080")
 
 	routes := route.GetRoutes()
--- models/db.go
+++ models/db.go
@@ -1,39 +1,56 @@
 package models
 
-type Event struct {
-	Data struct {
-		ID       string `json:"ID"`
-		Currency string `json:"Currency"`
-		Rate     string `json:"Rate"`
-	}
+import (
+	"context"
+
+	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Currency struct {
+	Currency string
+	Rate     float64
 }
 
-type allEvents []Event
+func PopulateDb() {
+	//mongodb connection bits
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	//trying to connect
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//checking connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Info("Connected to mongodb @ localhost:27017")
 
-var Events = allEvents{
-	{
-		ID:       "1",
-		Currency: "USD",
-		Rate:     "1",
-	},
-	{
-		ID:       "2",
-		Currency: "BRL",
-		Rate:     "0.25",
-	},
-	{
-		ID:       "3",
-		Currency: "EUR",
-		Rate:     "1,12",
-	},
-	{
-		ID:       "4",
-		Currency: "BTC",
-		Rate:     "9347.2",
-	},
-	{
-		ID:       "5",
-		Currency: "ETH",
-		Rate:     "183.91",
-	},
+	collection := client.Database("test").Collection("currencies")
+
+	//Cleaning Database
+	deleteResult, err := collection.DeleteMany(context.TODO(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Info("Deleted %v documents in the currencies collection", deleteResult.DeletedCount)
+
+	//creating currencies to add to the Database
+	usd := Currency{"USD", 1}
+	brl := Currency{"BRL", 0.25}
+	eur := Currency{"EUR", 1.12}
+	btc := Currency{"EUR", 9353.2}
+	eth := Currency{"ETH", 183.21}
+
+	//currencies array
+	currenciesArray := []interface{}{usd, brl, eur, btc, eth}
+
+	insertResult, err := collection.InsertMany(context.TODO(), currenciesArray)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Info("Inserted multiple documents: ", insertResult.InsertedIDs)
 }
