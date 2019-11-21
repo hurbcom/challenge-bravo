388e247caffb176cab203b7edc9c778a2da4921f
Luis Fernando <luisspaniol12@gmail.com>
2019-11-20 21:28:54 -0300

    WIP: models extract status from a internal struct

--- models/coins.go
+++ models/coins.go
@@ -54,15 +54,8 @@ func (c *CoinExchange) ValidateAmount() error {
 
 //Response
 type Response struct {
-	Status struct {
-		Timestamp    time.Time   `json:"timestamp"`
-		ErrorCode    int         `json:"error_code"`
-		ErrorMessage interface{} `json:"error_message"`
-		Elapsed      int         `json:"elapsed"`
-		CreditCount  int         `json:"credit_count"`
-		Notice       interface{} `json:"notice"`
-	} `json:"status"`
-	Data struct {
+	Status Status `json:"status"`
+	Data   struct {
 		ID          int              `json:"id"`
 		Symbol      string           `json:"symbol"`
 		Name        string           `json:"name"`
@@ -75,4 +68,13 @@ type Response struct {
 type Quote struct {
 	Price       float64   `json:"price"`
 	LastUpdated time.Time `json:"last_updated"`
+}
+
+type Status struct {
+	Timestamp    time.Time   `json:"timestamp"`
+	ErrorCode    int         `json:"error_code"`
+	ErrorMessage interface{} `json:"error_message"`
+	Elapsed      int         `json:"elapsed"`
+	CreditCount  int         `json:"credit_count"`
+	Notice       interface{} `json:"notice"`
 }
