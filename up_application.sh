faf215517a518b0f0c3c8f35aa396829a5e7c607
Victor Barros <victorbarros1130@gmail.com>
2019-11-25 01:25:17 -0300

    Added curl healthcheck to up schellscript.

--- up_application.sh
+++ up_application.sh
@@ -3,4 +3,5 @@ docker rmi -f hurb-image:1.0
 docker build -t hurb-image:1.0 .
 docker run -d -p 5001:5000 --name hurb-challenge hurb-image:1.0
 docker ps
+curl -X GET http://localhost:5001/healthcheck
 # docker exec -it hurb-challenge bash
