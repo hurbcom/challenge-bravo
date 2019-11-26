adc3316f74571e2958935d006e499a7e3018abe5
victorabarros <victorbarros1130@gmail.com>
2019-11-26 01:30:48 -0300

    Update shellscript.

--- up_application.sh
+++ up_application.sh
@@ -2,5 +2,7 @@ docker rm -f hurb-challenge
 docker rmi -f hurb-image:1.0
 docker build -t hurb-image:1.0 .
 docker run -d -p 5001:5000 --name hurb-challenge hurb-image:1.0
-docker ps
+docker image ls -a
+docker ps -a
+sleep 1
 curl -X GET http://localhost:5001/healthcheck
