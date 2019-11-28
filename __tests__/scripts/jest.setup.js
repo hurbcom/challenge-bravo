e5e946177b34964e46c22cc1c4a37145d877189b
Pelegrinetti <pelegrinetti.dev@gmail.com>
2019-11-27 23:05:15 -0300

    feat(test): added seeder before tests

--- __tests__/scripts/jest.setup.js
+++ __tests__/scripts/jest.setup.js
@@ -1,9 +1,18 @@
+import { exec } from 'child_process'
+import { promisify } from 'util'
+
 import truncate from '../utils/truncate'
 
-beforeAll(async () => {
+beforeAll(async done => {
+  const execPromise = promisify(exec)
+
   await truncate()
+  await execPromise('yarn db:seed')
+
+  done()
 })
 
-afterAll(async () => {
+afterAll(async done => {
   await truncate()
+  done()
 })
