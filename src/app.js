fe89517e42bee0bd6521f1859c0ad21d951ebe97
Pelegrinetti <pelegrinetti.dev@gmail.com>
2019-11-30 00:09:52 -0300

    feat(app): added cors support

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 
 import routes from './routes'
 
@@ -11,6 +12,7 @@ class App {
   }
 
   middlewares () {
+    this.app.use(cors())
     this.app.use(express.json())
   }
 
