9b4ce783e57dcbb63ba0eb0b31c6d363b3584c35
Lefel <felipe.pbgomes@gmail.com>
2019-12-27 19:50:56 -0300

    added main calls on main.go

--- conversion-api/main.go
+++ conversion-api/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"challenge-bravo/conversion-api/currency/gateway"
+	"challenge-bravo/conversion-api/currency/repository"
+	conversion_http "challenge-bravo/conversion-api/currency/server/http"
+	"challenge-bravo/conversion-api/currency/service"
+	"fmt"
+	"github.com/go-chi/chi"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Start of api!")
+	fmt.Println("Starting API...")
+	fmt.Println("Connecting to Database...")
+	repo, err := repository.NewRepository()
+
+	if err != nil {
+		fmt.Println("Error Connecting to Database: ", err)
+		os.Exit(0)
+	}
+
+	fmt.Println("Database Connected!")
+
+	fmt.Println("Opening the Gateway...")
+	gate := gateway.NewGateway()
+	fmt.Println("Gateway Opened!")
+
+	fmt.Println("Starting the Service...")
+	service := service.NewService(repo, gate)
+	fmt.Println("Service Started!")
+
+	fmt.Println("Starting the Router...")
+	r := chi.NewMux()
+	conversion_http.NewHandler(service, r)
+	fmt.Println("Router Started!")
+	port := ":8081"
+	fmt.Println(fmt.Sprintf("API Started! Listening on Port: %s", port))
+	http.ListenAndServe(port, r)
+
 }
