e181da3930bdc5e64122dfddf62648665951db5a
geschopf <geschopf@gmail.com>
2020-01-25 00:11:24 -0300

    Docker file and server

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:10
+
+RUN mkdir -p /home/node/app/node_modules && chown -R node:node /home/node/app
+
+WORKDIR /home/node/app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+COPY --chown=node:node . .
+
+USER node
+
+EXPOSE 8080
+
+CMD [ "node", "app.js" ]
--- bin/server.js
+++ bin/server.js
@@ -1,30 +1,79 @@
+#!/usr/bin/env node
+
 const app = require('../src/app');
 const schedule = require('../src/schedule/currencyLoad');
 const currencies = require('../src/services/currenciesService');
 const cacheProvider = require('../src/services/cacheService').instance();
+const debug = require('debug')('teste:server');
+const http = require('http');
+var port = normalizePort(process.env.PORT || '3000');
 
-const port = normalizaPort(process.env.PORT || '3000');
-
-function normalizaPort(val) {
-    const port = parseInt(val, 10);
-    if (isNaN(port)) {
-        return val;
-    }
-
-    if (port >= 0) {
-        return port;
-    }
-
-    return false;
-}
 const {loadRates} = new currencies();
+const server = http.createServer(app);
 
 loadRates().then(response => {
     schedule.init();
-    app.listen(port, function () {
-        const {loadRates} = new currencies();
-        console.log(`app listening on port ${port}`)
-    })
+    app.set('port', port);
+
+    server.listen(port);
+    server.on('error', onError);
+    server.on('listening', onListening);
+    
 });
 
 
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+  
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+  
+    return false;
+  }
+  
+  /**
+   * Event listener for HTTP server "error" event.
+   */
+  
+  function onError(error) {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+  
+    var bind = typeof port === 'string'
+      ? 'Pipe ' + port
+      : 'Port ' + port;
+  
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  }
+  
+  /**
+   * Event listener for HTTP server "listening" event.
+   */
+  
+  function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+  }
