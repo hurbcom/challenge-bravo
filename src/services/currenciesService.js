848715118d914a18909fe175719a73aa8daa4567
geschopf <geschopf@gmail.com>
2020-01-26 16:06:41 -0300

    try catch when request datas from API of external currencies API

--- src/services/currenciesService.js
+++ src/services/currenciesService.js
@@ -2,34 +2,44 @@ require('express');
 const request = require("request"), cacheProvider = require('./cacheService').instance();
 
 exports.getRate = function (from, to) {
+
     request(`https://min-api.cryptocompare.com/data/price?fsym=${from}&tsyms=${to}`, function (error, response, body) {
-        const currentRate = JSON.parse(body);
-        let currencies = Array.from(cacheProvider.get("currencies", 'valid'));
-        Object.entries(currentRate).forEach(([key, value]) => {
-            cacheProvider.delete("Rates", key.toString());
-            cacheProvider.delete("Rates", "base");
-            cacheProvider.set("Rates", 'base', from, 86400000);
-            cacheProvider.set("Rates", key.toString(), value, 86400000);
-        });
-        cacheProvider.set("currencies", 'valid', currencies);
+        try {
+            const currentRate = JSON.parse(body);
+            let currencies = Array.from(cacheProvider.get("currencies", 'valid'));
+            Object.entries(currentRate).forEach(([key, value]) => {
+                cacheProvider.delete("Rates", key.toString());
+                cacheProvider.delete("Rates", "base");
+                cacheProvider.set("Rates", 'base', from, 86400000);
+                cacheProvider.set("Rates", key.toString(), value, 86400000);
+            });
+            cacheProvider.set("currencies", 'valid', currencies);
+        } catch (e) {
+            console.info("Error to loading datas")
+        }
+
     });
 };
 
 
 exports.addRate = function (from, to) {
     request(`https://min-api.cryptocompare.com/data/price?fsym=${from}&tsyms=${to}`, function (error, response, body) {
-        const currentRate = JSON.parse(body);
-        let currencies = Array.from(cacheProvider.get("currencies", 'valid'));
-        Object.entries(currentRate).forEach(([key, value]) => {
-            cacheProvider.delete("Rates", key.toString());
-            cacheProvider.delete("Rates", "base");
-            cacheProvider.set("Rates", 'base', from, 86400000);
-            cacheProvider.set("Rates", key.toString(), value, 86400000);
-            currencies.push(key.toString());
-        });
-        cacheProvider.set("currencies", 'valid', currencies);
-
+        try {
+            const currentRate = JSON.parse(body);
+            let currencies = Array.from(cacheProvider.get("currencies", 'valid'));
+            Object.entries(currentRate).forEach(([key, value]) => {
+                cacheProvider.delete("Rates", key.toString());
+                cacheProvider.delete("Rates", "base");
+                cacheProvider.set("Rates", 'base', from, 86400000);
+                cacheProvider.set("Rates", key.toString(), value, 86400000);
+                currencies.push(key.toString());
+            });
+            cacheProvider.set("currencies", 'valid', currencies);
+        } catch (e) {
+            console.info("Error to loading datas")
+        }
     });
+
 };
 
 
