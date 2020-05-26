f98a700ef1cb6a5d860dbe0d97396474c368f9a7
carvrodrigo <carvrodrigo@gmail.com>
2020-05-25 22:49:25 -0300

    Basic project structure and ping endpoint

---
+++ CurrencyConverter/Controllers/PingController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace currencyConverter.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PingController : ControllerBase
+    {
+        private readonly ILogger<PingController> _logger;
+
+        public PingController(ILogger<PingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<object> Get()
+        {
+            string d = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+            var cli = new HttpClient();
+            var market = cli.GetAsync("https://api.exchangeratesapi.io/latest?base=USD").Result;
+            var cryto = cli.GetAsync("https://api.bitaps.com/market/v1/ticker/btcusd").Result;
+
+            object ping = new
+            {
+                Date = d,
+                Msg = "I'm alive",
+                MarketPing = market.IsSuccessStatusCode,
+                CryptoPing = cryto.IsSuccessStatusCode
+            };
+
+            _logger.LogInformation($"User called ping at {d}");
+            return ping;
+        }
+    }
+}
--- CurrencyConverter/Startup.cs
+++ CurrencyConverter/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -22,13 +22,11 @@ namespace CurrencyConverter
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
