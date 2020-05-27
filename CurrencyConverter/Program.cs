ea56888a24124395bdeb331d8d5602e871d27760
carvrodrigo <carvrodrigo@gmail.com>
2020-05-27 02:46:03 -0300

    fix Https router

--- CurrencyConverter/Program.cs
+++ CurrencyConverter/Program.cs
@@ -19,6 +19,7 @@ namespace CurrencyConverter
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseIISIntegration()
                 .UseStartup<Startup>();
     }
 }
