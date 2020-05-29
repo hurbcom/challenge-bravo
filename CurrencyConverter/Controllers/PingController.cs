4d0429cb8094d1d39459a4b09fa531d11793bdbb
carvrodrigo <carvrodrigo@gmail.com>
2020-05-29 09:06:52 -0300

    Added backgroundWorker check to ping

--- CurrencyConverter/Controllers/PingController.cs
+++ CurrencyConverter/Controllers/PingController.cs
@@ -5,6 +5,8 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using CurrencyConverter.Infrasctructure.Interfaces;
 using CurrencyConverter.Infrastructure;
+using Hangfire;
+using Hangfire.Storage;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -28,12 +30,14 @@ namespace currencyConverter.API.Controllers
         {
             string d = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
             var rate = CryptoComparer.GetLastestRate("BRL");
+            IMonitoringApi monitoringApi = JobStorage.Current.GetMonitoringApi();
 
             object ping = new
             {
                 Date = d,
                 Msg = "I'm alive",
                 Brl_rate = rate,
+                Background_worker = monitoringApi.Servers().Any()
             };
 
             _logger.LogInformation($"User called ping at {d}");
