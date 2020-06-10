ecd47d4d7d1303aca2b7a5e14ace0289606bd504
Rafael lima <rafaellrf09@gmail.com>
2020-06-10 20:57:51 -0300

    add mongoose

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(
+        'mongodb://localhost:27017/coinsconversor',
+        { useUnifiedTopology: true, useNewUrlParser: true },
+    );
+console.log('Banco de dados Mongo conectado;');
 
 app.use(bodyParser.json())
 app.use(routes);
 app.listen(3333)
+console.log("Servidor conectado na porta 3333");
 
 
