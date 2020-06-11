c836efa51143cfb9782be695f21f272ff4f4f1a6
Rafael lima <rafaellrf09@gmail.com>
2020-06-11 12:02:09 -0300

    criando conversão

--- src/app/controllers/CoinController.js
+++ src/app/controllers/CoinController.js
@@ -1,16 +1,15 @@
 const Coin = require('../schemas/Coin');
-
 class CoinController {
     async index(req, res) {
         const coins = await Coin.find();
         return res.send(coins);
     }
     async store(req, res) {
-        const { code, name, value } = req.body;
+        const { code, name, lastro } = req.body;
         const coin = await Coin.create({
             code,
             name,
-            value
+            lastro
         });
         return res.json(coin);
 
@@ -22,7 +21,17 @@ class CoinController {
 
     }
     async conversion(req, res) {
+        const {from, to, amount } = req.query;
+        //localizando as moedas no banco de dados
+        const coinFrom = await Coin.findOne({ code: from })
+        const coinTo = await Coin.findOne({ code: to })
+        /* realizando a conversão entre as moedas:
+        Converção em que coloco elas no valor do lastro definido como BRL e
+        divido pelo valor que será convertido
+        */
+        const conversion = ( amount * coinFrom.lastro ) / coinTo.lastro;
 
+        return res.json({ value: conversion });
     }
 }
 
