0e23d0a7a024393ec72217f22add44478253890b
Luccas Maia <luccasmaia@poli.ufrj.br>
2020-06-12 14:00:09 -0300

    interceptors error

--- src/middlewares/errors-handler.js
+++ src/middlewares/errors-handler.js
@@ -1,7 +1,7 @@
 module.exports = async (err, req, res, next) => {
     if (!err.statusCode) err.statusCode = 500;
     if (err.statusCode === 500) {
-      console.error({ message: err.message, stack: err.stack });
+        console.error({ message: err.message, stack: err.stack });
     }
     res.status(err.statusCode).send({ message: err.message, stack: err.stack });
-  }
+};
--- src/startServer.js
+++ src/startServer.js
@@ -1,54 +1,55 @@
 const erroHandler = require("@middlewares/errors-handler");
 
 module.exports = class Server {
-    constructor ({dependencies, routes}){
+    constructor({ dependencies, routes }) {
         this.dependencies = {
-            ...dependencies
-        }
-        this.routes = routes
-    }// crio classe Server que recebe as dependencias e as rotas
+            ...dependencies,
+        };
+        this.routes = routes;
+    } // crio classe Server que recebe as dependencias e as rotas
 
-    async start (){
-        const {dependencies, routes} = this;
-        this.setServerDependencies (dependencies);
+    async start() {
+        const { dependencies, routes } = this;
+        this.setServerDependencies(dependencies);
         this.configureExpress(dependencies);
-        this.startApi(dependencies, routes)
+        this.startApi(dependencies, routes);
     }
     set(name, dependencie) {
         this.dependencies[name] = dependencie;
     }
-    setServerDependencies({express}){
+    setServerDependencies({ express }) {
         const app = express();
-        this.set("app", app)
+        this.set("app", app);
     }
-    configureExpress({app, cors, logger, bodyParser}){
+    configureExpress({ app, cors, logger, bodyParser }) {
         app.use(cors());
-        app.use(logger('dev'));
+        app.use(logger("dev"));
         app.use(erroHandler);
         app.use(
             bodyParser.urlencoded({
                 extended: true,
-                limit: "500mb"
+                limit: "500mb",
             })
         );
         app.use(
             bodyParser.json({
-                limit: "500mb"
+                limit: "500mb",
             })
         );
     }
-    startApi (dependencies, routes){
-        const {app} = dependencies;
-        const {env: {PORT}} = dependencies
-        Object.entries(routes).forEach(([method,route]) =>{
-            route.forEach(({path, callback})=>{
-                app[method](path,callback)
-            }
-            )
-        })
-        app.listen(PORT, ()=>{
-            console.log(`Servidor rodando em http://localhost:${PORT}` )
-        })
-    }
+    startApi(dependencies, routes) {
+        const { app } = dependencies;
+        const {
+            env: { PORT },
+        } = dependencies;
 
-}
+        Object.entries(routes).forEach(([method, route]) => {
+            route.forEach(({ path, callback }) => {
+                app[method](path, callback);
+            });
+        });
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando em http://localhost:${PORT}`);
+        });
+    }
+};
