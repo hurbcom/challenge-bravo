1c5df877960c424d47ee7a2d7c0c89f9c1a7d138
Luccas Maia <luccasmaia@poli.ufrj.br>
2020-06-12 17:32:46 -0300

    utils write json

---
+++ src/utils/update-json.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+module.exports = function updateJson(json) {
+    let message;
+    try {
+        fs.writeFileSync("src/utils/coin-base.json", json);
+        return (message = `Success`);
+    } catch (err) {
+        return (message = `Fatal error: ${err.message}`);
+    }
+};
