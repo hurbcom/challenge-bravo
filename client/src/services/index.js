50ecd1369036601cfc09c0ba8a368a459204e21b
Luccas Maia <luccasmaia@poli.ufrj.br>
2020-06-15 19:34:32 -0300

    create services

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,1 +1,27 @@
 import { api } from "./../utils/api-instance";
+
+export const getCoins = async () => {
+    const result = await api.get("/coins");
+    return result;
+};
+
+export const convertCoin = async (to, from, amount) => {
+    const result = await api.get(
+        `/coin/conversion?to=${to}&from=${from}&amount=${amount}`
+    );
+    return result;
+};
+export const createCoin = async coin => {
+    const result = await api.post(`/coin`, coin);
+    return result;
+};
+
+export const deleteCoin = async name => {
+    const result = await api.delete(`/coin/${name}`);
+    return result;
+};
+
+export const updateCoin = async (name, value) => {
+    const result = await api.put(`/coin/${name}`, value);
+    return result;
+};
