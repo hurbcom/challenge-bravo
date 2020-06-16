e055f9ccea58993387886a8500d9c94a87a72b77
Rafael lima <rafaellrf09@gmail.com>
2020-06-16 16:09:54 -0300

    adicionando mais testes ao CoinController

--- __tests__/integration/coin.test.js
+++ __tests__/integration/coin.test.js
@@ -1,31 +1,66 @@
 const request = require('supertest');
 const express = require('express');
+
+const faker = require('faker');
+
 const routes = require('../../src/routes');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json())
 app.use(routes);
 
+const Coin = {
+    code: faker.random.word(),
+    name: faker.name.findName(),
+    lastro: faker.random.number(),
+}
+
 describe('Coin', () => {
-    it('SHOUD BE ABLE TO REGISTER', async () => {
+    it('shoud be able to register', async () => {
         const response = await request(app)
             .post('/coins')
-            .send({
-                code: 'TEST',
-                name: 'Moeda de teste',
-                lastro: 171
-            })
+            .send(Coin)
         expect(response.body).toHaveProperty('code');
     })
-    it('should show error if i try to register an existing currency', async () => {
+    it('should not to be able to register with duplicated coin code', async () => {
         const response = await request(app)
             .post('/coins')
-            .send({
-                code: 'USD',
-                name: 'Dólar Comercial',
-                lastro: 5.1559
-            })
-        expect(response.body).toHaveProperty('erro');
+            .send(Coin)
+        expect(response.status).toBe(400);
     })
+
+    it('should be able to update a coin', async () => {
+        const responseUpdate = await request(app)
+            .put(`/coins/1`)
+            .send({
+                code: 'USDTUP',
+                name: 'Dólar de teste',
+                lastro: 6.0
+            })
+        expect(responseUpdate.body.code).toEqual('USDTUP');
+    })
+
+    it('should not to be able to update with wrong id coin ', async () => {
+        const response = await request(app)
+            .put('/coins/5')
+            .send({
+                code: 'USDTUP',
+                name: 'Dólar de teste',
+                lastro: 6.0
+            })
+        expect(response.status).toBe(400);
+    })
+
+    it('should not to be able to update with wrong json key data coin ', async () => {
+        const response = await request(app)
+            .put('/coins/1')
+            .send({
+                codes: 'USDTUP',
+                name: 'Dólar de teste',
+                lastro: 6.0
+            })
+        expect(response.status).toBe(400);
+    })
+
 })
 
