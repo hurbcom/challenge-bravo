16210b2e3783773c5fa503877a4682fd299f43b3
Luccas Maia <luccasmaia@poli.ufrj.br>
2020-06-19 15:55:44 -0300

    create component home

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,69 +1,61 @@
 <template>
-    <v-app>
-        <v-card class="mx-auto" max-width="800">
-            <v-card-title>Desafio Bravo - Conversor de Moedas</v-card-title>
-            <div name="nav-bar">
-                <v-bottom-navigation color="black">
-                    <v-btn bottomNav="home">
-                        <span>Início</span>
-                        <v-icon>mdi-home</v-icon>
-                    </v-btn>
-                    <v-btn @click="bottomNav = 'converter'">
-                        <span>Conversor</span>
-                        <v-icon color="yellow darken-1"
-                            >mdi-transfer-right</v-icon
-                        >
-                    </v-btn>
+  <v-app>
+    <v-card outlined class="mx-auto" max-width="600">
+      <v-card-title>Desafio Bravo - Conversor de Moedas</v-card-title>
+      <div name="nav-bar">
+        <v-bottom-navigation color="black">
+          <v-btn @click="bottomNav='home'">
+            <span>Início</span>
+            <v-icon>mdi-home</v-icon>
+          </v-btn>
+          <v-btn @click="bottomNav = 'converter'">
+            <span>Conversor</span>
+            <v-icon color="yellow darken-1">mdi-transfer-right</v-icon>
+          </v-btn>
 
-                    <v-btn @click="bottomNav = 'addCoin'">
-                        <span>Adicionar Moedas</span>
-                        <v-icon color="green darken-2"
-                            >mdi-plus-circle-multiple</v-icon
-                        >
-                    </v-btn>
+          <v-btn @click="bottomNav = 'addCoin'">
+            <span>Adicionar Moedas</span>
+            <v-icon color="green darken-2">mdi-plus-circle-multiple</v-icon>
+          </v-btn>
 
-                    <v-btn @click="bottomNav = 'deleteCoin'">
-                        <span>Deletar Moedas</span>
-                        <v-icon color="red darken-2"
-                            >mdi-minus-circle-multiple</v-icon
-                        >
-                    </v-btn>
+          <v-btn @click="bottomNav = 'deleteCoin'">
+            <span>Deletar Moedas</span>
+            <v-icon color="red darken-2">mdi-minus-circle-multiple</v-icon>
+          </v-btn>
 
-                    <v-btn @click="bottomNav = 'updateCoin'">
-                        <span>Atualizar Moedas</span>
-                        <v-icon color="blue darken-2">mdi-reload</v-icon>
-                    </v-btn>
-                </v-bottom-navigation>
-            </div>
-            <div name="converter" v-if="bottomNav == 'converter'">
-                <converter />
-            </div>
-            <div name="add" v-if="bottomNav == 'addCoin'">
-                <add-coin />
-            </div>
-            <div name="delete" v-if="bottomNav == 'deleteCoin'">
-                <deleteCoin />
-            </div>
-            <div name="update" v-if="bottomNav == 'updateCoin'">
-                <update-coin />
-            </div>
-        </v-card>
-        <v-snackbar
-            v-model="snackbar.status"
-            :color="snackbar.color"
-            :timeout="snackbar.timeout"
-        >
-            <div class="flex row justify-center">
-                {{ snackbar.text }}
-            </div>
-        </v-snackbar>
-        <v-footer app absolute class="font-weight-medium">
-            <v-col class="text-center" cols="12">
-                {{ new Date().getFullYear() }} —
-                <strong>Com ❤️ Luccas Maia</strong>
-            </v-col>
-        </v-footer>
-    </v-app>
+          <v-btn @click="bottomNav = 'updateCoin'">
+            <span>Atualizar Moedas</span>
+            <v-icon color="blue darken-2">mdi-reload</v-icon>
+          </v-btn>
+        </v-bottom-navigation>
+      </div>
+
+      <div name="home" v-if="bottomNav == 'home'">
+        <home />
+      </div>
+      <div name="converter" v-if="bottomNav == 'converter'">
+        <converter />
+      </div>
+      <div name="add" v-if="bottomNav == 'addCoin'">
+        <add-coin />
+      </div>
+      <div name="delete" v-if="bottomNav == 'deleteCoin'">
+        <deleteCoin />
+      </div>
+      <div name="update" v-if="bottomNav == 'updateCoin'">
+        <update-coin />
+      </div>
+    </v-card>
+    <v-snackbar v-model="snackbar.status" :color="snackbar.color" :timeout="snackbar.timeout">
+      <div class="flex row justify-center">{{ snackbar.text }}</div>
+    </v-snackbar>
+    <v-footer app absolute class="font-weight-medium">
+      <v-col class="text-center" cols="12">
+        {{ new Date().getFullYear() }} —
+        <strong>Com ❤️ Luccas Maia</strong>
+      </v-col>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
@@ -71,26 +63,27 @@ import converter from "./components/Converter";
 import addCoin from "./components/Add-Coin";
 import deleteCoin from "./components/Delete-coin";
 import updateCoin from "./components/Update-Coin";
+import home from "./components/Home";
 import { mapState, mapActions, mapGetters } from "vuex";
 export default {
-    name: "App",
+  name: "App",
 
-    components: { converter, addCoin, deleteCoin, updateCoin },
+  components: { converter, addCoin, deleteCoin, updateCoin, home },
 
-    data: () => ({
-        bottomNav: "home"
-    }),
-    async created() {
-        if (!this.nameCoins.length) {
-            await this.getCoins();
-        }
-    },
-    methods: {
-        ...mapActions(["getCoins"])
-    },
-    computed: {
-        ...mapState(["snackbar"]),
-        ...mapGetters(["nameCoins"])
+  data: () => ({
+    bottomNav: "home"
+  }),
+  async created() {
+    if (!this.nameCoins.length) {
+      await this.getCoins();
     }
+  },
+  methods: {
+    ...mapActions(["getCoins"])
+  },
+  computed: {
+    ...mapState(["snackbar"]),
+    ...mapGetters(["nameCoins"])
+  }
 };
 </script>
---
+++ client/src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div name="addCoin" align="start">
+    <v-row align="center">
+      <v-col cols="12">
+        <v-card-title>Bem-vindo ao Conversor de Moedas</v-card-title>
+        <v-card-text>
+          A aplicação é tem como principal função a conversão de moedas, porém também é capaz de realizar criação,
+          atualização, exclusão e listagem de moedas.
+        </v-card-text>
+        <v-card-text>
+          O valor base utilizado para conversão foi o Dólar Americano (lastro), então para inclusão e atualização de uma nova moeda,
+          seu valor deve ser baseado no valor de 1 dólar. Exemplo: Para adicionar a moeda Euro,
+          deve informar o valor de x Euros equivalente a 1 dólar.
+        </v-card-text>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
