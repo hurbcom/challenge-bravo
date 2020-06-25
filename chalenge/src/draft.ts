a554db7c3b80185bbd9b026fd5956339177934b9
Vinicius Gomes <vinigomescunha@gmail.com>
2020-06-25 02:03:32 -0300

    add draft example with sequelize

---
+++ chalenge/src/draft.ts
@@ -0,0 +1,21 @@
+import { Sequelize, Model, DataTypes, Dialect } from 'sequelize';
+import { getCurrencyModel } from './model/currency';
+
+const sequelize = new Sequelize({
+    database: process.env.ORM_DATABASE,
+    dialect: <Dialect> process.env.ORM_DIALECT,
+    storage: process.env.ORM_STORAGE
+});
+
+class CurrencyModel extends Model { }
+
+CurrencyModel.init(getCurrencyModel() ,{ sequelize, modelName: process.env.ORM_TABLENAME });
+
+const create = async (currency: any) => {
+    await sequelize.sync();
+    const user =  await CurrencyModel.create(currency);
+    return user;
+}
+create({
+    name: 'USC', value: 2000
+}).then(c => console.log(c.toJSON()))
