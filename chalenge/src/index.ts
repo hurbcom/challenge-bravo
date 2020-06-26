7d8eb0cf97a338c7a488f2499e49b2b183a104e9
Vinicius Gomes <vinigomescunha@gmail.com>
2020-06-26 11:04:06 -0300

    fix content type

--- chalenge/src/index.ts
+++ chalenge/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 import { router } from './routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
 const app: Application = express();
 
@@ -8,8 +10,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use((request: Request, response: Response, next: NextFunction) => {
-    response.setHeader('Content-Type', 'application/json');
     response.removeHeader("x-powered-by");
     next();
 });
