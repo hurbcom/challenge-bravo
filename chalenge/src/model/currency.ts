bd6e9da46066be08eea4b10d382a3d979513d074
Vinicius Gomes <vinigomescunha@gmail.com>
2020-06-26 01:51:07 -0300

    add init model

--- chalenge/src/model/currency.ts
+++ chalenge/src/model/currency.ts
@@ -1,5 +1,6 @@
-import { DataTypes } from "sequelize";
-// obtenho a estrutura do ORM para a criação da moeda
+import { Sequelize, Model, Op, Dialect, DataTypes } from 'sequelize';
+
+// obtenho a estrutura do ORM para a criação da moeda e para importar o modelo para o banco
 export const getCurrencyModel = () => (
     {
         id: {
@@ -25,10 +26,10 @@ export const getCurrencyModel = () => (
 export const getDefaultCurrencyData = () => (
     [
         {
-            name: 'USD', value: 1, isbase: true
+            name: 'USD', value: 1, isBase: true
         },
         {
-            name: 'BRL', value: 5.35
+            name: 'BRL', value: 5.35// dolar esta custando aproximadamente esse valor mesmo :(
         },
         {
             name: 'EUR', value: 0.89
@@ -41,3 +42,35 @@ export const getDefaultCurrencyData = () => (
         }
     ]
 );
+
+const sequelize = new Sequelize({
+    database: process.env.ORM_DATABASE,
+    dialect: <Dialect>process.env.ORM_DIALECT,
+    storage: process.env.ORM_STORAGE
+});
+
+class CurrencyModel extends Model { }
+
+CurrencyModel.init(getCurrencyModel(), { sequelize, modelName: process.env.ORM_TABLENAME });
+
+export const createCurrency = async (currency: {
+    name: string,
+    value: Number
+}) => {
+    await sequelize.sync();
+    const user = await CurrencyModel.create(currency);
+    return user;
+}
+
+export const existByName = async (name: string) => {
+    const result = await CurrencyModel.findOne({ where: { name } });
+    return result ? true : false;
+}
+
+export const getCurrencies = async (currencies: string[]): Promise<any[]> => {
+    const results = await CurrencyModel.findAll({
+        raw: true,
+        where: { name: { [Op.in]: currencies } }
+    });
+    return results;
+}
