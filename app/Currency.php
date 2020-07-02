52f34fe7836dbda29937f4ae75cb9b90db077c0c
Wagner Lamoglia <wagnyero@gmail.com>
2020-07-02 20:05:27 -0300

    Migrations

---
+++ app/Currency.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Currency extends Model
+{
+    use SoftDeletes;
+    
+    protected $table = 'currency';
+}
---
+++ database/migrations/2020_07_02_224537_create_currencies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currency', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 3);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currency');
+    }
+}
