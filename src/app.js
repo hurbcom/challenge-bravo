08dd3ad4618d463b6165605b810f801c8c57af33
Ariel Freire <argonfreire@gmail.com>
2020-07-17 04:37:03 -0300

    Adicionado endpoint currency-rates à api.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 let express = require("express");
+let updateJob = require("./jobs/update-rates-job")
+let ExchangeRatesService = require( "./services/exchange-rates-service");
 
 app = express(),
 	port = 3000;
 app.listen(port);
-app.get('/', function (req, res) {res.json({status:"alive"})});
+app.get('/api/v1/currency-rates/latest', function (req, res) { 
+	ExchangeRatesService.getLatestExchangeRates().then( x=>{
+		return res.json(x);
+	})
+ });
 
+console.log('Challenge Bravo RESTful API server started on: ' + port);
+
+updateJob.initJob()
