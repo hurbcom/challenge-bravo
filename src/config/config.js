a4b61719884ddd215022d7dcb1c73dafdceeff90
Ariel Freire <argonfreire@gmail.com>
2020-07-22 19:38:34 -0300

    -Adicionada classe modelo para resposta de erro.

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 class Configuration
 {
+	static DEFAULT_DATE_FORMAT = "YYYY-MM-DD HH:mm:ss";
 	static CRON_JOB_STRING = "0 * * ? * *";
 	static DB_NAME = "currency-converter";
 	static MONGODB_DEFAULT_URL = "mongodb://127.0.0.1:27017/";
