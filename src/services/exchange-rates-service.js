c882a58fe4c6ea55863994cf8f7aebb3bb840d2f
Ariel Freire <argonfreire@gmail.com>
2020-07-22 19:35:49 -0300

    -Adicionada interface para a api CoinGecko -Alterado serviço ExchangeRates para utilizar a interface criada

--- src/services/exchange-rates-service.js
+++ src/services/exchange-rates-service.js
@@ -1,12 +1,13 @@
-let CoinGecko = require("coingecko-api");
+
 const HistoricalRatesDao = require("../dao/historical-rates-dao");
-let ExchangeRates = require("../models/exchange-rates")
+let ExchangeRates = require("../models/exchange-rates");
+const ICoinService = require("./coin-service-interface");
 
 class ExchangeRatesService
 {
 	constructor(container)
 	{
-		this.coinGeckoClient = container.get(CoinGecko);
+		this.coinService = container.get(ICoinService);
 		this.dao = container.get(HistoricalRatesDao);
 	}
 
@@ -17,12 +18,12 @@ class ExchangeRatesService
 
 	async getExchangeRates()
 	{
-		let exchangeRates = await this.coinGeckoClient.exchangeRates.all();
-		let usdValue = exchangeRates.data.rates.usd.value;
-		let brl = exchangeRates.data.rates.brl.value/usdValue;
-		let eur = exchangeRates.data.rates.eur.value/usdValue;
-		let btc = exchangeRates.data.rates.btc.value/usdValue;
-		let eth = exchangeRates.data.rates.eth.value/usdValue;
+		let exchangeRates = await this.coinService.getAll();
+		let usdValue = exchangeRates.usd.value;
+		let brl = exchangeRates.brl.value/usdValue;
+		let eur = exchangeRates.eur.value/usdValue;
+		let btc = exchangeRates.btc.value/usdValue;
+		let eth = exchangeRates.eth.value/usdValue;
 		let updatedExchangeRates = new ExchangeRates(btc,brl,eth,eur);
 		await this.dao.insert(updatedExchangeRates);
 		return updatedExchangeRates;
