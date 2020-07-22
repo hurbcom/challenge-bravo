ec2af123be3725564dcb66d53e97326c0608cc79
Ariel Freire <argonfreire@gmail.com>
2020-07-22 19:41:22 -0300

    -Adicionada arquivo router e controlador CurrencyController

--- src/app.js
+++ src/app.js
@@ -1,17 +1,14 @@
-let express = require("express");
-const { default: Container } = require("typedi");
-let updateJob = require("./jobs/update-rates-job")
-let ExchangeRatesService = require( "./services/exchange-rates-service");
+const express = require("express");
+const updateJob = require("./jobs/update-rates-job")
+const Router = require('./router');
 
 app = express(),
 	port = 3000;
-app.listen(port);
-app.get('/api/v1/currency-rates/latest', function (req, res) { 
-	Container.get(ExchangeRatesService).getLatestExchangeRates().then( x=>{
-		return res.json(x);
-	})
- });
 
+app.listen(port);
+
+app.use(express.json());
+app.use("/", Router);
 console.log('Challenge Bravo RESTful API server started on: ' + port);
 
 updateJob.initJob()
