d4c716e99fdaf0d20ce7d801f401c4553cc3ee40
Ariel Freire <argonfreire@gmail.com>
2020-07-29 01:50:02 -0300

    - Alterada referencia de port em app.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,24 @@
 require('dotenv').config();
 const express = require('express');
-const { default: Container } = require('typedi');
+
+const { Container } = require('typedi');
+
 const UpdateExchangeRatesJob = require('./jobs/update-exchange-rates-job');
 const Router = require('./api/router');
 const responseHeadersMiddleware = require('./services/middlewares/response-headers-interceptor');
 const Configuration = require('./config/config');
 
 const app = express();
-let port = process.env.PORT;
-if (!port) {
-    port = Configuration.DEFAULT_PORT;
-    console.log(`No PORT set into .ENV configuration, using default.`);
-}
 
-app.listen(port);
+const port = process.env.PORT ? process.env.PORT : Configuration.DEFAULT_PORT;
+
 app.use(responseHeadersMiddleware);
 app.use(express.json());
+// API Routes
 app.use('/api', Router);
-console.log(`Challenge Bravo REST API server started on: ${port}`);
 
+app.listen(port, () => {
+    console.log(`Challenge Bravo REST API server started on: ${port}`);
+});
 const updateJob = Container.get(UpdateExchangeRatesJob);
 updateJob.initJob();
