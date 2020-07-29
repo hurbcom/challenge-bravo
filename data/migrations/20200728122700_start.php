1458a9ecf1da3d14a89738b2e1a2495a9137bfcc
Mario Costa <mario.costa@zooxsmart.com>
2020-07-29 17:55:33 -0300

    Fix migration deprecated message when inserting data

--- data/migrations/20200728122700_start.php
+++ data/migrations/20200728122700_start.php
@@ -17,35 +17,39 @@ class Start extends AbstractMigration
 
         $now = date('Y-m-d H:i:s');
 
-        $this->insert('currency', [
-            'id' => '84fea129-fd33-4e90-be11-ba128c942d16',
-            'name' => 'USD',
-            'createdAt' => $now,
-            'updatedAt' => $now,
-        ]);
-        $this->insert('currency', [
-            'id' => '84fea129-fd84-402d-b4b4-dee6ea420b4c',
-            'name' => 'BRL',
-            'createdAt' => $now,
-            'updatedAt' => $now,
-        ]);
-        $this->insert('currency', [
-            'id' => '84fea129-fd91-42e2-9969-31c1db08261b',
-            'name' => 'EUR',
-            'createdAt' => $now,
-            'updatedAt' => $now,
-        ]);
-        $this->insert('currency', [
-            'id' => '84fea129-fd9d-4762-b900-f249eb758f87',
-            'name' => 'BTC',
-            'createdAt' => $now,
-            'updatedAt' => $now,
-        ]);
-        $this->insert('currency', [
-            'id' => '84fea129-fdb4-4993-b499-e21fef0596b2',
-            'name' => 'ETH',
-            'createdAt' => $now,
-            'updatedAt' => $now,
-        ]);
+        $this->table($table->getName())
+            ->insert([
+                [
+                    'id' => '84fea129-fd33-4e90-be11-ba128c942d16',
+                    'name' => 'USD',
+                    'createdAt' => $now,
+                    'updatedAt' => $now,
+                ],
+                [
+                    'id' => '84fea129-fd84-402d-b4b4-dee6ea420b4c',
+                    'name' => 'BRL',
+                    'createdAt' => $now,
+                    'updatedAt' => $now,
+                ],
+                [
+                    'id' => '84fea129-fd91-42e2-9969-31c1db08261b',
+                    'name' => 'EUR',
+                    'createdAt' => $now,
+                    'updatedAt' => $now,
+                ],
+                [
+                    'id' => '84fea129-fd9d-4762-b900-f249eb758f87',
+                    'name' => 'BTC',
+                    'createdAt' => $now,
+                    'updatedAt' => $now,
+                ],
+                [
+                    'id' => '84fea129-fdb4-4993-b499-e21fef0596b2',
+                    'name' => 'ETH',
+                    'createdAt' => $now,
+                    'updatedAt' => $now,
+                ]
+            ]
+        )->save();
     }
 }
