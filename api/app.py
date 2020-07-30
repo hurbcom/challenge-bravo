337867ed7a3d49cbeff2449da0b3aa0491ae19f7
Romulo Tavares <romuloctavares@gmail.com>
2020-07-30 19:50:55 -0300

    Altera api de convert para aceitar parâmetros de get

--- api/app.py
+++ api/app.py
@@ -1,16 +1,24 @@
 import json
 
-from flask import Flask, Response
+from flask import Flask, Response, request
 app = Flask(__name__)
 
 
 @app.route('/convert/')
 def currency_converter():
+    currency_from = request.args.get('from', 'USD')
+    currency_to = request.args.get('to', 'BRL')
+    amount = request.args.get('amount', 1, type=int)
+    value = 5.15
+
+    if currency_from == 'BRL':
+        value = 0.19
+
     response = {
-        'from': 'USD',
-        'to': 'BRL',
-        'amount': '1',
-        'value': '5,15'
+        'from': currency_from,
+        'to': currency_to,
+        'amount': amount,
+        'value': value
     }
 
     return Response(json.dumps(response), status=200, mimetype='application/json')
--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -4,36 +4,50 @@ import unittest
 from api.app import app
 
 
-class TestExchangeView(unittest.TestCase):
+class TestConvertView(unittest.TestCase):
     url = '/convert/'
     test_app = app.test_client()
 
-    def test_put_exchange_should_return_not_implemented(self):
+    def test_put_convert_should_return_not_implemented(self):
         response = self.test_app.put(self.url)
         self.assertEqual(response.status_code, 405)
 
-    def test_post_exchange_should_return_not_implemented(self):
+    def test_post_convert_should_return_not_implemented(self):
         response = self.test_app.post(self.url)
         self.assertEqual(response.status_code, 405)
 
-    def test_delete_exchange_should_return_not_implemented(self):
+    def test_delete_convert_should_return_not_implemented(self):
         response = self.test_app.delete(self.url)
         self.assertEqual(response.status_code, 405)
 
-    def test_get_exchange_should_return_ok(self):
+    def test_get_convert_should_return_ok(self):
         response = self.test_app.get(self.url)
         self.assertEqual(response.status_code, 200)
 
-    def test_get_exchange_should_return_json(self):
+    def test_get_convert_should_return_json(self):
         response = self.test_app.get(self.url)
         self.assertEqual(response.content_type, 'application/json')
 
-    def test_get_exchange_should_return_default_exchange(self):
+    def test_get_convert_should_return_default_conversion(self):
         response = self.test_app.get(self.url)
         data = json.loads(response.get_data(as_text=True))
         self.assertEqual(data, {
             'from': 'USD',
             'to': 'BRL',
-            'amount': '1',
-            'value': '5,15'
+            'amount': 1,
+            'value': 5.15
+        })
+
+    def test_get_convert_should_return_conversion_sended_by_get(self):
+        response = self.test_app.get(self.url, query_string={
+            'from': 'BRL',
+            'to': 'USD',
+            'amount': 1
+        })
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(data, {
+            'from': 'BRL',
+            'to': 'USD',
+            'amount': 1,
+            'value': 0.19
         })
