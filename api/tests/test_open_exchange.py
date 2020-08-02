a5f63096c8b8a3f0e0838ea88513296125589941
Romulo Tavares <romuloctavares@gmail.com>
2020-08-02 11:29:53 -0300

    Altera testes da Api do Open Exchange

--- api/tests/test_open_exchange.py
+++ api/tests/test_open_exchange.py
@@ -6,18 +6,18 @@ from unittest.mock import patch
 from api.open_exchange import OpenExchange
 
 
-# class TestIntegrationOpenExchange(unittest.TestCase):
-#     exchange = OpenExchange('9e99dd7952614fb494bc2fa538c7a7c4')
+class TestIntegrationOpenExchange(unittest.TestCase):
+    exchange = OpenExchange('9e99dd7952614fb494bc2fa538c7a7c4')
 
-#     def test_get_currencies_rate_should_return_all_currencies_rates(self):
-#         currencies = self.exchange.get_currencies_rate()
+    def test_get_currencies_rate_should_return_all_currencies_rates(self):
+        currencies = self.exchange.get_currencies_rate()
 
-#         self.assertIsInstance(currencies, dict)
-#         self.assertIsNotNone(currencies['BRL'])
+        self.assertIsInstance(currencies, dict)
+        self.assertIsNotNone(currencies['BRL'])
 
-#     def test_get_currency_rate_should_return_current_rate(self):
-#         currency_rate = self.exchange.get_currency_rate('BRL')
-#         self.assertIsInstance(currency_rate, float)
+    def test_get_currency_rate_should_return_current_rate(self):
+        currency_rate = self.exchange.get_currency_rate('BRL')
+        self.assertIsInstance(currency_rate, float)
 
 
 class TestOpenExchange(unittest.TestCase):
@@ -41,7 +41,7 @@ class TestOpenExchange(unittest.TestCase):
         self.assertEqual(currency_rate, 5.15)
 
     @patch('api.open_exchange.requests.get')
-    def test_get_currencies_rate_should_return_empty_dict_on_api_error(self, mock):
+    def test_get_currencies_rate_should_return_empty_on_api_error(self, mock):
         mock.return_value = self._fake_500_response()
 
         currencies = self.exchange.get_currencies_rate()
