28cdcecc65cc184e53be2f10a6db4a9190e846e6
Romulo Tavares <romuloctavares@gmail.com>
2020-08-02 11:37:15 -0300

    Altera testes de open exchange para facilitar leitura

--- api/tests/test_open_exchange.py
+++ api/tests/test_open_exchange.py
@@ -5,6 +5,24 @@ from unittest.mock import patch
 
 from api.open_exchange import OpenExchange
 
+FAKE_RATES = {
+    'BRL': 5.15,
+    'EUR': 0.90
+}
+
+
+def fake_200():
+    r = requests.Response()
+    r.status_code = 200
+    r.json = lambda: {'rates': FAKE_RATES}
+    return r
+
+
+def fake_500():
+    r = requests.Response()
+    r.status_code = 500
+    return r
+
 
 class TestIntegrationOpenExchange(unittest.TestCase):
     exchange = OpenExchange('9e99dd7952614fb494bc2fa538c7a7c4')
@@ -23,49 +41,22 @@ class TestIntegrationOpenExchange(unittest.TestCase):
 class TestOpenExchange(unittest.TestCase):
     exchange = OpenExchange('9e99dd7952614fb494bc2fa538c7a7c4')
 
-    @patch('api.open_exchange.requests.get')
+    @patch('api.open_exchange.requests.get', return_value=fake_200())
     def test_get_currencies_rate_should_return_all_currencies(self, mock):
-        mock.return_value = self._fake_200_response()
-
         currencies = self.exchange.get_currencies_rate()
-        self.assertEqual(currencies, {
-            'BRL': 5.15,
-            'EUR': 0.90
-        })
+        self.assertEqual(currencies, FAKE_RATES)
 
-    @patch('api.open_exchange.requests.get')
+    @patch('api.open_exchange.requests.get', return_value=fake_200())
     def test_get_currency_rate_should_return_current_rate(self, mock):
-        mock.return_value = self._fake_200_response()
-
         currency_rate = self.exchange.get_currency_rate('BRL')
         self.assertEqual(currency_rate, 5.15)
 
-    @patch('api.open_exchange.requests.get')
+    @patch('api.open_exchange.requests.get', return_value=fake_500())
     def test_get_currencies_rate_should_return_empty_on_api_error(self, mock):
-        mock.return_value = self._fake_500_response()
-
         currencies = self.exchange.get_currencies_rate()
         self.assertEqual(currencies, {})
 
-    @patch('api.open_exchange.requests.get')
+    @patch('api.open_exchange.requests.get', return_value=fake_500())
     def test_get_currency_rate_should_return_none_on_api_error(self, mock):
-        mock.return_value = self._fake_500_response()
-
         currency_rate = self.exchange.get_currency_rate('BRL')
         self.assertIsNone(currency_rate)
-
-    def _fake_200_response(self):
-        r = requests.Response()
-        r.status_code = 200
-        r.json = lambda: {
-            'rates': {
-                'BRL': 5.15,
-                'EUR': 0.90
-            }
-        }
-        return r
-
-    def _fake_500_response(self):
-        r = requests.Response()
-        r.status_code = 500
-        return r
