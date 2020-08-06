268a699046fc3fce3fbc30708ce464ad33dd195d
Lucas Scherpel <lucasscherpel@gmail.com>
2020-08-06 13:42:12 -0300

    server.ts file updated with eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
-import express, { request, response } from 'express';
+import express, { request, response } from "express";
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({messagem:'Hello, Hurb! This is just a test to see if the Typescript config is ok (:'});
-});
+app.get("/", (request, response) =>
+    response.json({
+        messagem:
+            "Hello, Hurb! This is just a test to see if the Typescript config is ok (:",
+    })
+);
 
 app.listen(3333, () => {
-    console.log('Server started at port 3333');
+    console.log("Server started at port 3333");
 });
