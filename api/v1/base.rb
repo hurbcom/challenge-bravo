4ef05613282159be0609319c7c7b015f45bdc7dd
Luiz Neiva <luizgzn@gmail.com>
2020-08-08 16:43:35 -0300

    fix module scope for base api

--- api/v1/base.rb
+++ api/v1/base.rb
@@ -1,12 +1,14 @@
 module API
-  class Base < Roda
-    plugin :json
-    plugin :all_verbs
-    route do |r|
-      r.on "api" do
-        r.on "v1" do
-          r.get 'hello' do
-            { hello: :world }
+  module V1
+    class Base < Roda
+      plugin :json
+      plugin :all_verbs
+      route do |r|
+        r.on "api" do
+          r.on "v1" do
+            r.get 'currencies' do
+              @currency = Currency.all
+            end
           end
         end
       end
