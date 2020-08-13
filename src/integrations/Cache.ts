93d14434be94665743f5ff10ed387fd7bb08f092
Lucas Scherpel <lucasscherpel@gmail.com>
2020-08-13 19:29:57 -0300

    create Cache class to cache API request

---
+++ src/integrations/Cache.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import Redis from 'ioredis';
+
+class Cache {
+    redis: Redis.Redis;
+
+    constructor() {
+      this.redis = new Redis({
+        host: 'localhost',
+        port: 6379,
+        keyPrefix: 'cache:',
+      });
+    }
+
+    async get(key: Redis.KeyType) {
+      const value = await this.redis.get(key);
+
+      return value ? JSON.parse(value) : null;
+    }
+
+    set(key: Redis.KeyType, value: any, timeExp: string | number | undefined) {
+      return this.redis.set(key, JSON.stringify(value), 'EX', timeExp);
+    }
+
+    del(key: Redis.KeyType) {
+      return this.redis.del(key);
+    }
+}
+export default Cache;
