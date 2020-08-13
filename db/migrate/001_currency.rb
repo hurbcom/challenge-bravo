45cc95bcb628c0e9656490a0ac8524da7741e52c
Luiz Neiva <luizgzn@gmail.com>
2020-08-13 00:58:36 -0300

    rebuild migration file and currency seeds

--- db/migrate/001_currency.rb
+++ db/migrate/001_currency.rb
@@ -2,10 +2,9 @@ Sequel.migration do
   change do
     create_table(:currencies) do
       primary_key :id
-      String :name, null: false
-      String :code, null: false, limit: 3, index: {unique: true}
-      String :type, null: false, default: 'fiat money'
-      Boolean :ballast, default: false, null: false
+      String :name, null: false, unique: true
+      String :code, null: false, limit: 3, index: {unique: true}, unique: true
+      String :type, null: false, default: 'fiat_money'
       DateTime :created_at
       DateTime :updated_at
     end
--- db/seeds/20200808_currency_seed.rb
+++ db/seeds/20200808_currency_seed.rb
@@ -1,16 +1,16 @@
-require_relative "../../api/v1/models/currency"
-
 Sequel.seed do
   def run
     currencies = [
-      { name: "US Dollar", code: "USD", type: "fiat money", ballast: true},
-      { name: "Brazilian Real", code: "BRL", type: "fiat money", ballast: false},
-      { name: "Euro", code: "EUR", type: "fiat money", ballast: false},
-      { name: "Bitcoin", code: "BTC", type: "cryptocurrency", ballast: false},
-      { name: "Ethereum", code: "ETH", type: "cryptocurrency", ballast: false}
+      { name: "US Dollar", code: "USD", type: "fiat_money"},
+      { name: "Brazilian Real", code: "BRL", type: "fiat_money"},
+      { name: "Euro", code: "EUR", type: "fiat_money"},
+      { name: "Bitcoin", code: "BTC", type: "cryptocurrency"},
+      { name: "Ethereum", code: "ETH", type: "cryptocurrency"}
     ]
     currencies.each do |currency|
-      Currency.create(name: currency[:name], code: currency[:code], type: currency[:type], ballast: currency[:ballast])
+      Currency.create(name: currency[:name], code: currency[:code], type: currency[:type])
     end
+    currency_id = Currency.where(code: 'USD').first.id
+    Ballast.create(currency_id: currency_id)
   end
 end
