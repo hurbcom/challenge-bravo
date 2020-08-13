9df43d56be4677824c513834929f593886841d53
Luiz Neiva <luizgzn@gmail.com>
2020-08-13 00:59:05 -0300

    add vaLidations to currency model

--- models/currency.rb
+++ models/currency.rb
@@ -1,4 +1,15 @@
 class Currency < Sequel::Model
+
+  CRYPTOCURRENCY =  "cryptocurrency"
+  FIAT_MONEY = "fiat_money"
+
   plugin :timestamps
   plugin :json_serializer
+  plugin :validation_class_methods
+  one_to_one :ballast
+  validates_inclusion_of :type, in: [CRYPTOCURRENCY, FIAT_MONEY]
+
+  def is_ballast?
+    self.ballast ? true : false
+  end
 end
