c49834d36841791bdb5c977e366f68905ed95940
Alexander Santos <dev_alex@outlook.com>
2020-08-24 16:58:32 -0300

    :package: add swagger

--- src/api/config/routes.js
+++ src/api/config/routes.js
@@ -2,6 +2,7 @@
  * Contains common configs for routes (Like using helmet, body-parser, etc)
  * Holds info about service routes, but not the routes itself
  * */
+const swaggerJSDoc = require('swagger-jsdoc')
 const { Router } = require("express")
 const helmet = require('helmet')
 const cors = require('cors')
@@ -10,7 +11,28 @@ const bodyParser = require('body-parser')
 const httpLogger = require('api/currency/infrastructure/logging/http_logger')
 const handleErr = require('api/currency/infrastructure/middlewares/handle_err')
 const R = require('ramda')
+const swaggerUI = require('swagger-ui-express')
 
+// swagger definition
+const swaggerDefinition = {
+    info: {
+        title: 'Currency API Explorer',
+        version: '1.0.0',
+        description: 'Available REST Endpoints of Currency API'
+    },
+    host: `${process.env.API_SWAGGER}:${process.env.PORT_SWAGGER}/api/${process.env.APP_VERSION}`,
+    basePath: '/',
+    securityDefinitions: {
+    }
+}
+
+// options for the swagger docs
+const options = {
+    // import swaggerDefinitions
+    swaggerDefinition: swaggerDefinition,
+    // path to the API docs
+    apis: ['api/**/routes.js']
+}
 
 module.exports = ({
     config,
@@ -20,6 +42,8 @@ module.exports = ({
     const router = Router()
 
     const apiRouter = Router()
+
+    const swaggerSpec = swaggerJSDoc(options)
 
     apiRouter.use(cors({
         origin: [
@@ -31,9 +55,24 @@ module.exports = ({
         .use(bodyParser.json())
         .use(compression())
 
+    /**
+     * @swagger
+     * /:
+     *   get:
+     *     tags:
+     *       - Status
+     *     description: Returns API status
+     *     produces:
+     *       - application/json
+     *     responses:
+     *       200:
+     *         description: API Status
+     */
     apiRouter.get('/', (req, res) => {
         res.status(200).send('API is working :)')
     })
+    apiRouter.use('/swagger.json', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
+
 
     // Feature Routers here
     apiRouter.use('/currency', currencyFeatureRouter)
