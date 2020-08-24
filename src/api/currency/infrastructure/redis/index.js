db97df3fbbe4ec18f9a998e9286a39b0bd2844f6
Alexander Santos <dev_alex@outlook.com>
2020-08-24 20:00:12 -0300

    :sparkles: Add redis passwd to env

--- src/api/currency/infrastructure/redis/index.js
+++ src/api/currency/infrastructure/redis/index.js
@@ -6,7 +6,8 @@ else {
 
     client = redis.createClient({
         host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT
+        port: process.env.REDIS_PORT,
+        password: process.env.REDIS_PASSWD
     })
 
     client.on("error", function (err) {
