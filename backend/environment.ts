6918c2702a28948ba8e1063eef7a3d82b3b43277
Roberto Albano Dias Junior <robert.o.o@hotmail.com>
2020-09-21 09:38:07 -0300

    Arquivos de configurações

---
+++ backend/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    externalServices: {
+        currencyApiUrl: "https://api.exchangeratesapi.io/latest",
+        baseCurrency: "USD"
+    },
+    connectionStrings: {
+        port: 3000,
+        host: 'localhost'
+    }
+}
---
+++ backend/readme.md
@@ -0,0 +1,1 @@
+<h1>Onion Architecture</h1>
