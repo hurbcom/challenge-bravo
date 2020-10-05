144541ea8eeb8cc0d68d0d40ae1caef46e4d5454
hnogueira <hnogueira@funcionalcorp.com.br>
2020-10-05 10:41:21 -0300

    ensure not throw if convertAmount is called with valid params

--- src/exchange/exchange.service.spec.ts
+++ src/exchange/exchange.service.spec.ts
@@ -2,24 +2,24 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CurrenciesService } from '../currencies/currencies.service';
 import { ExchangeService } from './exchange.service';
 
-const serviceCurrenciesMock = {
+const currenciesServiceMock = {
   getCurrency: jest.fn(),
 };
 
 describe('ExchangeService', () => {
   let service: ExchangeService;
-  let serviceCurrencies: CurrenciesService;
+  let currenciesService: CurrenciesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         ExchangeService,
-        { provide: CurrenciesService, useFactory: () => serviceCurrenciesMock },
+        { provide: CurrenciesService, useFactory: () => currenciesServiceMock },
       ],
     }).compile();
 
     service = module.get<ExchangeService>(ExchangeService);
-    serviceCurrencies = module.get<CurrenciesService>(CurrenciesService);
+    currenciesService = module.get<CurrenciesService>(CurrenciesService);
   });
 
   it('should be defined', () => {
@@ -31,9 +31,16 @@ describe('ExchangeService', () => {
       await expect(service.convertAmount({ from: '', to: '', amount: 0 })).rejects.toThrow();
     });
 
+    it('should be not throw if convertAmount is called with valid params', async () => {
+      await expect(
+        service.convertAmount({ from: 'BRL', to: 'USD', amount: 2 }),
+      ).resolves.not.toThrow();
+    });
+
     it('should be called getCurrency twice', async () => {
+      currenciesService.getCurrency = jest.fn().mockReturnValue({ value: 1 });
       await service.convertAmount({ from: 'USD', to: 'BRL', amount: 1 });
-      expect(serviceCurrencies.getCurrency).toBeCalledTimes(2);
+      expect(currenciesService.getCurrency).toBeCalledTimes(2);
     });
   });
 });
