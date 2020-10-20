4edd018a9bbec09dc0f23287aa3e282f2b4d601b
Vinicius de Oliveira <vi.o@hotmail.com>
2020-10-20 20:22:43 -0300

    Criaçao e definiçao do model e resource

--- hurb-project/converter/models.py
+++ hurb-project/converter/models.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+    symbol = models.CharField(max_length=5, null=True, blank=True, default=None)
+    description = models.CharField(max_length=50, null=True, blank=True, default=None)
+
+    class Meta():
+        db_table = 'currency'
---
+++ hurb-project/converter/resources.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from tastypie.resources import ModelResource
+from converter.models import Currency
+from tastypie.authorization import Authorization
+from django.conf.urls import url
+from tastypie.utils import trailing_slash
+from converter.connector import ExternalApiConnector
+from converter.utils import conversion_calculate, verify_if_exists_symbols
+from django.http import Http404
+from django.core.exceptions import ValidationError
+
+
+# Create your views here.
+
+
+class CurrencyResource(ModelResource):
+    class Meta:
+        queryset = Currency.objects.all()
+        resource_name = 'currency'
+        authorization = Authorization()
+
+
+    def obj_create(self, bundle, request=None, **kwargs):
+
+        symbol = bundle.data.get('symbol')
+        description = bundle.data.get('description')
+
+        if not (symbol and description):
+            raise ValidationError(message='Não é possível cadastrar uma moeda sem um símbolo ou uma descrição.')
+
+        if Currency.objects.filter(symbol=symbol):
+            raise ValidationError(message='Não é possível cadastrar uma moeda com símbolo repetido.')
+
+        bundle = super(CurrencyResource, self).obj_create(bundle, request=bundle.request, **kwargs)
+        return bundle
+
+
+    def prepend_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/convert_value%s$" %
+                (self._meta.resource_name, trailing_slash()),
+                self.wrap_view('convert_value'), name="api_teste"),
+        ]
+
+    def convert_value(self, request, **kwargs):
+        self.method_check(request, allowed=['post'])
+
+
+
+        currency_from = request.GET.get('from')
+        currency_to = request.GET.get('to')
+
+        is_valid_entry = verify_if_exists_symbols(currency_from, currency_to)
+
+        if not is_valid_entry:
+            raise Http404("O simbolo fornecido não foi encontrado. É necessário cadastra-lo antes.")
+
+            #return self.create_response(request, {"error_message": "O símbolo fornecido não está cadastrado. É necessário cadastra-lo anteriormente."}, 404)
+
+
+        amount = float(request.GET.get('amount'))
+
+        response_json = ExternalApiConnector.get_quotation(self, currency_from, currency_to)
+
+        value_to = response_json[currency_from][currency_to]
+        total = conversion_calculate(value_to, amount)
+
+
+        response_json['total'] = total
+
+
+
+        return self.create_response(request, response_json)
