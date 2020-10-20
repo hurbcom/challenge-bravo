20db4f8418db3e277b9a96ac717b9e3c77d79221
Vinicius de Oliveira <vi.o@hotmail.com>
2020-10-20 20:42:13 -0300

    Criaçao de arquivo utils.py para funcçoes auxiliares

---
+++ hurb-project/converter/utils.py
@@ -0,0 +1,15 @@
+from converter.models import Currency
+from django.db.models import Q
+
+
+
+def conversion_calculate(amount, quotation):
+    return amount * quotation
+
+
+def verify_if_exists_symbols(symbol_from, symbol_to):
+
+    currency_qs = Currency.objects.filter(Q(symbol=symbol_to) | Q(symbol=symbol_from))
+    if not currency_qs.count() == 2:
+        return False
+    return True
