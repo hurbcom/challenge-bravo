38c3357554969633d8e3cf2234f141bc45abfd0e
Vinicius de Oliveira <vi.o@hotmail.com>
2020-10-20 21:12:38 -0300

    Criaçao de um command para inicializar o banco de dados com os paises pre definidos

---
+++ hurb-project/converter/management/commands/create_initial_currencies.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from converter.models import *
+class Command(BaseCommand):
+    help = 'Displays current time'
+
+    def handle(self, *args, **kwargs):
+        initial_countries = ['BRL', 'USD', 'EUR', 'BTC', 'ETH']
+        currencies_qs = Currency.objects.all()
+        for country in initial_countries:
+            if not currencies_qs.filter(symbol=country):
+                Currency.objects.create(symbol=country, description="Moeda do pais " + country)
+        print("Terminando o command")
