5f4bd9a5a5332107d3467254cfa7c354889148ef
anacletogiovanna <giovanna18batalha@gmail.com>
2020-10-29 14:21:18 -0300

    Fix enpoints dir and adding blueprints in app.py

--- currency_conversion/src/app.py
+++ currency_conversion/src/app.py
@@ -1,4 +1,11 @@
 #Main file application to load all other modules
 from flask import Flask
 
+#load modules 
+from src.endpoints.conversion import currency_api
+
 app = Flask(__name__)
+
+#register blueprints that responsible to structure endpoints
+app.register_blueprint(currency_api, url_prefix="/api", errors=errors)
+
