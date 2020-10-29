f538e65db3e48d50c7ea3252f338f4630b557d31
anacletogiovanna <giovanna18batalha@gmail.com>
2020-10-29 14:03:37 -0300

    Adding endpoints

---
+++ currency_conversion/endpoints/conversion.py
@@ -0,0 +1,25 @@
+#Setting endpoints for api 
+from flask import Blueprint, jsonify
+
+#define the blueprint name
+currency_api = Blueprint(name="currency_api", import_name=__name__)
+
+#endpoint to get all available currency codes
+@currency_api.route('/conversion', methods=['GET'])
+def all_currency_codes():
+    ...
+
+#endpoint to get currency conversion between two currencies by from_code, to_code and amount
+@currency_api.route('/conversion/<string:from_code>/<string:to_code>/<int:ammount>', methods=['POST'])
+def currency_conversion(from_code, to_code, ammount):
+    ...
+
+#endpoint to save currency code
+@currency_api.route('/conversion/<string:currency_code>', methods=['POST'])
+def add_currency(from_code, to_code, ammount):
+    ...
+
+#endpoint to delete currency code
+@currency_api.route('/conversion/<string:currency_code>', methods=['DELETE'])
+def delete_currency(id):
+    ...
