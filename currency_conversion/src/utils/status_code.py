bb4d1742d314153c45bb5b70d8d52860764f5e91
anacletogiovanna <giovanna18batalha@gmail.com>
2020-11-01 22:05:20 -0300

    Adding Postman test file, remove the flask_api from status_code.py and customize README

--- currency_conversion/src/utils/status_code.py
+++ currency_conversion/src/utils/status_code.py
@@ -1,13 +1,12 @@
 #-- Customizing status code for return methods --#
-from flask_api import status
 
 #successful messages 
-SUCESSFUL_REGISTRATION_201 = {"message": "Currency Code register successfully", "status": status.HTTP_201_CREATED}
-SUCCESFUL_REMOVE_200 = {"message": "Currency Code remove successfully", "status": status.HTTP_200_OK}
+SUCESSFUL_REGISTRATION_201 = {"message": "Currency Code register successfully", "status": 201}
+SUCCESFUL_REMOVE_200 = {"message": "Currency Code remove successfully", "status": 200}
 
 #error messages
-NOT_FOUND_404 = {"message": "Opps, the currency code is not found!", "status": status.HTTP_404_NOT_FOUND}
-NOT_FOUND_DB_405 = {"message": "Opps, the currency code is not found in database!", "status": status.HTTP_405_METHOD_NOT_ALLOWED}
-NOT_SUPPORTED_CODE_500 = {"message": "Opps, the currency code is not supported!", "status": status.HTTP_500_INTERNAL_SERVER_ERROR}
-CONFLICT_409 = {"message": "Opps, the currency code already exists!", "status": status.HTTP_409_CONFLICT}
-INTERNAL_SERVER_ERROR_500 = {"message": "Internal Server Error", "status": status.HTTP_500_INTERNAL_SERVER_ERROR}
+NOT_FOUND_404 = {"message": "Opps, the currency code is not found!", "status": 404}
+NOT_FOUND_DB_405 = {"message": "Opps, the currency code is not found in database!", "status": 405}
+NOT_SUPPORTED_CODE_500 = {"message": "Opps, the currency code is not supported!", "status": 500}
+CONFLICT_409 = {"message": "Opps, the currency code already exists!", "status":409}
+INTERNAL_SERVER_ERROR_500 = {"message": "Internal Server Error", "status": 500}
