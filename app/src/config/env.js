9e79546ba02ac6ef646ebe08979742bdd4eebe52
Gustavo Cunha <gustavo.cunha@ibm.com>
2020-11-15 00:11:17 -0300

    env config file

---
+++ app/src/config/env.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+    port: process.env.PORT,
+    nodeEnv: process.env.NODE_ENV,
+    api: {
+        url: process.env.EXCHANGE_RATE_API_URL,
+        key: process.env.EXCHANGE_RATE_API_KEY
+    }
+}
---
+++ app/src/config/index.js
@@ -0,0 +1,5 @@
+import env from './env';
+
+export {
+    env
+}
