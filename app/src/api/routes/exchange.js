0ef0278b49ba6332b7dfa6fd1881c11012ceabab
Gustavo Cunha <gustavo.cunha@ibm.com>
2020-11-15 19:13:12 -0300

    injecting 'getRate' function for later unit test

--- app/src/api/routes/exchange.js
+++ app/src/api/routes/exchange.js
@@ -1,16 +1,21 @@
 import { Router } from 'express';
 import Status from 'http-status';
-import { getRate } from '../../services/exchangeRateAPI';
+import { getRate as getRates } from '../../services/exchangeRateAPI';
 
-export default () => {
+export default ({ getRate } = { getRate: getRates }) => {
     const router = Router();
     
    /**
     * @todo
-    * - add a validator to check the query params
-    * - separate the logic from the routes
     * - standardize api response object
-    * 
+    *   . always return comparison to USD because of 'lastro'
+    *   . return from, to, the exchange rate and the conversion value
+    * - add a validator
+    *   . check the query params
+    *   . check if it is allowed currency
+    * - separate the logic from the routes
+    * - error handling
+    * - tests
     */
     router.get('/exchange', async (req, res, next) => {
         const { from, to, amount } = req.query;
