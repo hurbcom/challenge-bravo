c115186e0d3e70d475dfa06faafb4f712ca72905
Gustavo Cunha <gustavo.cunha@ibm.com>
2020-11-15 00:11:53 -0300

    conversion rate external api

---
+++ app/src/services/exchangeRateAPI.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+import { env } from '../config';
+
+async function getRate(from, to) {
+    const response = await axios.get(`${env.api.url}?fsym=${from}&tsyms=${to}`, {
+        headers: {
+            'Authorization': `Apikey ${env.api.key}`
+        }
+    });
+    
+    return response.data;
+}
+
+export default () => ({
+    getRate
+})
