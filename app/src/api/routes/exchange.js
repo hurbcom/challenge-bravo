37a9c44e99e9ea3c636bf03ecb7d74b9c9249a9e
Gustavo Cunha <gustavo.cunha@ibm.com>
2020-11-15 02:18:37 -0300

    created /exchange route

---
+++ app/src/api/routes/exchange.js
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import Status from 'http-status';
+import { getRate } from '../../services/exchangeRateAPI';
+
+export default () => {
+    const router = Router();
+    
+    router.get('/exchange', async (req, res, next) => {
+        const { from, to, amount } = req.query;
+
+        const rate = await getRate(from, to);
+
+        return res.status(Status.OK).json(rate);
+    });
+
+    return router;
+}
