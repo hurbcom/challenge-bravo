7fd98172894220c1881c10dcf7af9697e5207c5a
Gustavo Cunha <gustavo.cunha@ibm.com>
2020-11-14 21:33:10 -0300

    added morgan for http loggin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import Status from 'http-status';
+import morgan from 'morgan';
 
 dotenv.config();
 
 const app = express();
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => res.status(Status.OK).json('Up and running!'));
 
