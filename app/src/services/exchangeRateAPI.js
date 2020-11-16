a43ede5cbc6f52bb8abb9a6a2744476644f6a9d2
Gustavo Cunha <gustavo.cunha@ibm.com>
2020-11-15 22:11:25 -0300

    using 'USD' as reference for every conversion

--- app/src/services/exchangeRateAPI.js
+++ app/src/services/exchangeRateAPI.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 import { env } from '../config';
 
-async function getRate(from, to) {
-    const response = await axios.get(`${env.api.url}?fsym=${from}&tsyms=${to}`);
+async function getRate(from, to, reference) {
+    const response = await axios.get(`${env.api.url}?fsym=${reference}&tsyms=${from},${to}`);
     
     return response.data;
 }
