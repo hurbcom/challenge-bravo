b87666a19021da7fe8818148f0d3e1537001ad71
Gabriel Mendes <gabriel.mendes@dotlib.com.br>
2020-11-16 12:33:27 -0300

    feat: testes unitarios na api de conversao

--- src/useCases/ConvertCoin/ConvertCoinUseCase.spec.ts
+++ src/useCases/ConvertCoin/ConvertCoinUseCase.spec.ts
@@ -1,36 +1,67 @@
-import { Transaction } from 'knex';
-import { Coin } from '../../entities/Coin';
-import knex from '../../config/knex';
+import request from 'supertest';
+import axios from 'axios';
+import app from '../../app';
+import { apiUrl } from '../../config/apiConvert';
 
 describe('Convert Coin', () => {
-  let trx: Transaction;
+  const path = '/convert';
 
-  beforeEach((done) => {
-    knex.transaction((newTrx) => {
-      trx = newTrx;
-      done();
-    });
+  it('deveria retornar erro 400 porque parâmetros não existem', async (done) => {
+    request(app).get(path).expect(400, done);
   });
 
-  afterEach(async (done) => {
-    await trx.rollback();
-    done();
+  it('deveria retornar erro 400 se as moedas não estiverem cadastradas', async (done) => {
+    const from = 'ZCF';
+    const to = 'ATA';
+    const amount = 10;
+
+    request(app)
+      .get(`${path}?from=${from}&to=${to}&amount=${amount}`)
+      .expect(400, done);
   });
 
-  it('should be able to convert new coin', async () => {
-    // const data = {
-    //   name: 'Real',
-    //   acronym: 'BRL',
-    // };
-    // const coin = new Coin(data);
-    // await trx('coins').insert(coin);
-    // const list = await trx('coins').where('acronym', data.acronym);
-    // expect(list).toEqual(
-    //   expect.arrayContaining([
-    //     expect.objectContaining({
-    //       acronym: data.acronym,
-    //     }),
-    //   ])
-    // );
+  it('deveria retornar erro 400 se apenas uma moeda está cadastrado', async (done) => {
+    const from = 'ZCF';
+    const to = 'BTC';
+    const amount = 10;
+
+    request(app)
+      .get(`${path}?from=${from}&to=${to}&amount=${amount}`)
+      .expect(400, done);
+  });
+
+  it('deveria retornar erro 400 porque não possui o amount', async (done) => {
+    const from = 'ZCF';
+    const to = 'BTC';
+
+    request(app).get(`${path}?from=${from}&to=${to}`).expect(400, done);
+  });
+
+  it('deveria retornar erro 400 porque o amount não é um número', async (done) => {
+    const from = 'ZCF';
+    const to = 'BTC';
+    const amount = 'teste';
+
+    request(app)
+      .get(`${path}?from=${from}&to=${to}&amount=${amount}`)
+      .expect(400, done);
+  });
+
+  it('deveria retornar o resultado correto na conversão', async (done) => {
+    const from = 'ETH';
+    const to = 'BTC';
+    const amount = 1;
+
+    const responseAPI = await axios.get(
+      `${apiUrl}/data/price?fsym=${from}&tsyms=${to}`
+    );
+
+    request(app)
+      .get(`${path}?from=${from}&to=${to}&amount=${amount}`)
+      .expect(201)
+      .end((err, res) => {
+        expect(Number(res.body)).toBe(responseAPI.data[to]);
+        done();
+      });
   });
 });
