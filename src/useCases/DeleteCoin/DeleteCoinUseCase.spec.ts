fe7da7eaed8c212811357b3dc80831dbf4878113
Gabriel Mendes <gabriel.mendes@dotlib.com.br>
2020-11-17 14:51:30 -0300

    fix: testes unitarios rota update e delete

--- src/useCases/DeleteCoin/DeleteCoinUseCase.spec.ts
+++ src/useCases/DeleteCoin/DeleteCoinUseCase.spec.ts
@@ -6,6 +6,20 @@ describe('Delete Coin', () => {
   const path = '/coin';
 
   it('deveria permitir deletar uma moeda', async (done) => {
-    done();
+    knex.transaction(async (newTrx) => {
+      const fakeUID = 'teste';
+
+      request(app)
+        .post(`${path}`)
+        .send({
+          name: 'ATAB9',
+          uid: fakeUID,
+        })
+        .expect(201);
+
+      request(app).delete(`${path}/${fakeUID}`).expect(201);
+
+      done();
+    });
   });
 });
--- src/useCases/UpdateCoin/UpdateCoinUseCase.spec.ts
+++ src/useCases/UpdateCoin/UpdateCoinUseCase.spec.ts
@@ -6,6 +6,25 @@ describe('Update Coin', () => {
   const path = '/coin';
 
   it('deveria permitir atualizar uma moeda', async (done) => {
-    done();
+    knex.transaction(async (newTrx) => {
+      const fakeUID = 'teste';
+
+      request(app)
+        .post(`${path}`)
+        .send({
+          name: 'ATAB9',
+          uid: fakeUID,
+        })
+        .expect(201);
+
+      request(app)
+        .put(`${path}/${fakeUID}`)
+        .send({
+          name: 'hi',
+        })
+        .expect(201);
+
+      done();
+    });
   });
 });
