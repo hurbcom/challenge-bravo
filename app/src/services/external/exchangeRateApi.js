1c5e8d668c4de15cf92acc088386e9ad310c1deb
Gustavo Cunha <gustavo.cunha@ibm.com>
2020-11-17 20:37:58 -0300

    file rename with 'Api' sufix

--- app/src/services/external/exchangeRateApi.js
+++ app/src/services/external/exchangeRateApi.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 import { env } from '../../config';
 
-async function getRate(from, to, reference) {
+async function getRate({from, to, reference}) {
     const response = await axios.get(`${env.api.url}?fsym=${reference}&tsyms=${from},${to}`);
     
     return response.data;
