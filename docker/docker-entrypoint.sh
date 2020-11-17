e9084763d6d30ad0d5f3de64f6ce928a0db78559
Gabriel Mendes <gabriel.mendes@dotlib.com.br>
2020-11-17 14:35:06 -0300

    feat: migrations e seeds rodam no docker-compose

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 yarn
+yarn knex migrate:latest --knexfile ./src/knexfile.ts
+yarn knex seed:run --knexfile ./src/knexfile.ts
 yarn dev
--- src/migrations/20201114185708_create_table_coin.ts
+++ src/migrations/20201114185708_create_table_coin.ts
@@ -3,7 +3,7 @@ import knex from 'knex';
 export async function up(Knex: knex): Promise<void> {
   return Knex.schema.createTable('coins', (tbl) => {
     tbl.string('uid', 36).notNullable();
-    tbl.string('name', 128).notNullable();
+    tbl.string('name', 128).notNullable().unique();
     tbl.timestamps(true, true);
   });
 }
