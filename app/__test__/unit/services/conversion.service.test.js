75ba16f2fad63a9bd8590af0bb146967ceb686d7
Gustavo Cunha <gustavo.cunha@ibm.com>
2020-11-18 00:01:54 -0300

    first test case for calculateExchangeRate function

--- app/__test__/unit/services/conversion.service.test.js
+++ app/__test__/unit/services/conversion.service.test.js
@@ -1,17 +1,53 @@
-const chai = require('chai');
-const sinon = require('sinon');
-const conversionService = require('../../../src/services/conversion.service');
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import ConversionService from '../../../src/services/conversion.service';
 
+chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 describe('services/conversion.service.js', () => {
+    const reference = 'USD';
+    let service;
+
+    beforeEach(() => {
+        const getRatesFromApiMock = () => ({
+            'BRL': 5.36,
+            'EUR': 0.841
+        });
+        
+        service = ConversionService({
+            getRates: getRatesFromApiMock
+        });
+    });
 
     describe('calculateExchangeRate()', () => {
 
         describe('Given that all parameters are correct', () => {
-            it('should return an object containing the "from" amount, "to" conversion amount and the "reference" rate.', () => {
-                
-            });
-        })
+            const testCases = [
+                { 
+                    amount: 1, 
+                    to: 0.15690298507462686, 
+                    rate: 0.15690298507462686 
+                }
+            ];
+
+            for (const expected of testCases) {
+                it(`should return 'BRL: ${expected.amount}, EUR: ${expected.to}, USD: ${expected.rate}`, async () => {
+                    
+                    const conversion = await service.calculateExchangeRate({ 
+                        from: 'BRL', 
+                        to: 'EUR', 
+                        amount: expected.amount, 
+                        reference 
+                    });
+
+                    expect(conversion).to.eql({
+                        'BRL': expected.amount,
+                        'EUR': expected.to,
+                        'USD': expected.rate
+                    });
+                });
+            }
+        });
     });
 });
