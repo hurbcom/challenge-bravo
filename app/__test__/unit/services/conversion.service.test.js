da6484274937e6b49a13d477f8d3d1b346af18fe
Gustavo Cunha <gustavo.cunha@ibm.com>
2020-11-17 21:14:26 -0300

    setting up conversion service tests

---
+++ app/__test__/unit/services/conversion.service.test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const conversionService = require('../../../src/services/conversion.service');
+
+const expect = chai.expect;
+
+describe('services/conversion.service.js', () => {
+
+    describe('calculateExchangeRate()', () => {
+
+        describe('Given that all parameters are correct', () => {
+            it('should return an object containing the "from" amount, "to" conversion amount and the "reference" rate.', () => {
+                
+            });
+        })
+    });
+});
