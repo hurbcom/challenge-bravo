ac796c5a8885a6cf483f6ee588abaa77d778d7b7
Gustavo Cunha <gustavo.cunha@ibm.com>
2020-11-22 20:55:12 -0300

    added currency routes to router

--- app/src/api/routes/index.js
+++ app/src/api/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import conversion from './conversion.routes';
+import currency from './currency.routes';
 
 export default () => {
     const router = Router();
 
     router.use(conversion());
+    router.use(currency());
 
     return router;
 }
