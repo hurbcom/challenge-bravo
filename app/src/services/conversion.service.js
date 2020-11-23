f06adc19c28375ce8e26c9c7da6901cd0a823234
Gustavo Cunha <gustavo.cunha@ibm.com>
2020-11-23 19:58:38 -0300

    caching the external api response

--- app/src/services/conversion.service.js
+++ app/src/services/conversion.service.js
@@ -1,10 +1,24 @@
 import { getRatesFromApi } from './external/exchangeRateApi';
+import { promisify } from 'util';
+import { client } from '../databases/redis';
 
-export default ({ getRates } = { getRates: getRatesFromApi }) => {
+const redis = {
+    getAsync: promisify(client.get).bind(client),
+    setAsync: promisify(client.set).bind(client)
+}
+
+export default ({ getRates, cache } = { getRates: getRatesFromApi, cache: redis }) => {
 
     async function calculateExchangeRate({ from, to, amount, reference }) {
+        const cacheKey = `external-${from}-${to}`;
         
+        const cached = await cache.getAsync(cacheKey);
+        if (cached) {
+            return JSON.parse(cached);
+        }
+
         const conversion = await getRates({ from, to, reference });
+        await cache.setAsync(cacheKey, JSON.stringify(conversion), 'EX', 20);
 
         const rate = (conversion[to] / conversion[from]);
         
