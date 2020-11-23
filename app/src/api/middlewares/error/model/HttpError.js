2e8446e98524f57662e0335e97c662f8170ab0d4
Gustavo Cunha <gustavo.cunha@ibm.com>
2020-11-23 17:20:54 -0300

    added CONFLICT error

--- app/src/api/middlewares/error/model/HttpError.js
+++ app/src/api/middlewares/error/model/HttpError.js
@@ -9,12 +9,17 @@ export default class HttpError extends Error {
     getCode() {
       if (this instanceof BadRequest) {
         return Status.BAD_REQUEST;
-      } if (this instanceof NotFound) {
+      } 
+      if (this instanceof NotFound) {
         return Status.NOT_FOUND;
+      }
+      if (this instanceof Conflict) {
+        return Status.CONFLICT;
       }
       return 500;
     }
   }
   
   export class BadRequest extends HttpError { }
+  export class Conflict extends HttpError { }
   export class NotFound extends HttpError { }
