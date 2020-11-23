2ca4a4f8ebee5088756ec772b8f23ff6186ee7ce
Gustavo Cunha <gustavo.cunha@ibm.com>
2020-11-22 23:04:59 -0300

    fixing reference value

--- app/src/services/conversion.service.js
+++ app/src/services/conversion.service.js
@@ -11,7 +11,7 @@ export default ({ getRates } = { getRates: getRatesFromApi }) => {
         return {
             [from]: amount,
             [to]: amount * rate,
-            [reference]: rate
+            [reference]: 1 / conversion[from]
         };
     }
 
