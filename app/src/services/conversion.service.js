1220e70e2fb98b2b21ce25db5d7297d4869f555f
Gustavo Cunha <gustavo.cunha@ibm.com>
2020-11-27 16:01:09 -0300

    refactoring moving cache from service to api call

--- app/src/services/conversion.service.js
+++ app/src/services/conversion.service.js
@@ -1,24 +1,14 @@
 import { getRatesFromApi as getRates } from './external/exchangeRateApi';
-import Redis from '../databases/redis';
 
 export default class ConversionService { 
     
-    constructor(exchangeRatesApi, redisInstance) {
+    constructor(exchangeRatesApi) {
         this._getRates = exchangeRatesApi || getRates;
-        this._redis = redisInstance || Redis.instance;
     }
 
     async calculateExchangeRate({ from, to, amount, reference }) {
-        const cacheKey = `external-${from}-${to}`;
-        
-        const cached = await this._redis.get(cacheKey);
-        if (cached) {
-            return JSON.parse(cached);
-        }
-
         const conversion = await this._getRates({ from, to, reference });
-        await this._redis.set(cacheKey, JSON.stringify(conversion), 'EX', 20);
-
+        
         const rate = (conversion[to] / conversion[from]);
         
         return {
--- app/src/services/external/exchangeRateApi.js
+++ app/src/services/external/exchangeRateApi.js
@@ -1,10 +1,22 @@
 const axios = require('axios');
+import Redis from '../../databases/redis';
 import { env } from '../../config';
 
 async function getRatesFromApi({from, to, reference}) {
+    const cacheKey = `external-${from}-${to}`;
+    const redis = Redis.instance;
+
+    const cached = await redis.get(cacheKey);
+    if (cached) {
+        return JSON.parse(cached);
+    }
+
     const response = await axios.get(`${env.api.url}?fsym=${reference}&tsyms=${from},${to}`);
     
-    return response.data;
+    const conversion = response.data;
+    await redis.set(cacheKey, JSON.stringify(conversion), 'EX', 20);
+
+    return conversion;
 }
 
 export {
