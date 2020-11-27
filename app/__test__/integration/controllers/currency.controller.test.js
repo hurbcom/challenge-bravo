35d52125c13acfd3a77d1c6d36d0e4fd377e8a22
Gustavo Cunha <gustavo.cunha@ibm.com>
2020-11-27 18:22:36 -0300

    removed stub to do integration testing

--- app/__test__/integration/controllers/currency.controller.test.js
+++ app/__test__/integration/controllers/currency.controller.test.js
@@ -2,42 +2,35 @@ import express from 'express';
 import request from 'supertest';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import sinon from 'sinon';
 import server from '../../../src/api/server';
-import CurrencyService from '../../../src/services/currency.service';
 
 chai.use(chaiAsPromised);
 const expect = chai.expect;
-const assert = chai.assert;
 
 describe('Currency Routes', () => {
     let app;
-    let getAllStub;
 
     before(async () => {
         app = server.start({ expressApp: express() });
         
-        getAllStub = sinon.stub(CurrencyService.prototype, 'getAll');
+        // getAllStub = sinon.stub(CurrencyService.prototype, 'getAll');
     });
     
 
     describe('GET /api/currency', async () => {
         it('should respond with status 200 and an array of currencies in the body', async () => {
 
-            const currencies = [
+            const expectedCurrencies = [
                 'USD',
                 'BRL',
                 'EUR',
                 'BTC',
                 'ETH'
             ];
-
-            getAllStub.returns(Promise.resolve(currencies));
             
             const response = await request(app).get('/api/currency');
 
-            assert(getAllStub.calledOnce, true);
-            expect(response.body).to.eql(currencies);
+            expect(response.body).to.eql(expectedCurrencies);
         });
     });
 });
--- app/__test__/setupDatabases.test.js
+++ app/__test__/setupDatabases.test.js
@@ -2,9 +2,10 @@ import sinon from 'sinon';
 import chai, { assert } from 'chai';
 import * as redis from 'redis';
 import redisMock from 'redis-mock';
-import Redis from '../../src/databases/redis';
+import Redis from '../src/databases/redis';
 import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
+import { seed } from '../src/databases/seeds/index';
 
 let mongoServer;
 let createClientStub;
@@ -13,6 +14,8 @@ before(async () => {
     mongoServer = new MongoMemoryServer();
     const mongoUri = await mongoServer.getUri();
     await mongoose.connect(mongoUri);
+
+    seed(mongoUri);
 
     createClientStub = sinon.stub(redis, 'createClient').callsFake(() => redisMock.createClient());
 
