e069bbf4ec1eb748b743f076b59b4378b18fff4d
Gustavo Cunha <gustavo.cunha@ibm.com>
2020-11-27 18:21:27 -0300

    wrapped it in a func to be called on test setup

--- app/src/databases/seeds/index.js
+++ app/src/databases/seeds/index.js
@@ -3,18 +3,24 @@ const { Seeder } = require('mongo-seeding');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const config = {
-    database: process.env.MONGO_URL,
-    databaseReconnectTimeout: 120000
-};
-const seeder = new Seeder(config);
-const collections = seeder.readCollectionsFromPath(
-    path.resolve(`${__dirname}/collections`)
-);
+function seedMongo(uri) {
+    const env = process.env.NODE_ENV;
 
-seeder.import(collections).then(() => {
-    console.log('MongoDB Seeding Success');
-}).catch(err => {
-    if (err.code == 11000) return; // duplication error
-    console.log('MongoDB Seeding Error', err);
-});
+    const config = {
+        database: env === 'test'? uri : process.env.MONGO_URL,
+        databaseReconnectTimeout: 120000
+    };
+    const seeder = new Seeder(config);
+    const collections = seeder.readCollectionsFromPath(
+        path.resolve(`${__dirname}/collections`)
+    );
+
+    seeder.import(collections).then(() => {
+        console.log('MongoDB Seeding Success');
+    }).catch(err => {
+        if (err.code == 11000) return; // duplication error
+        console.log('MongoDB Seeding Error', err);
+    });
+}
+
+module.exports.seed = (uri) => seedMongo(uri);
