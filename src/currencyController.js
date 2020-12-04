a4ab1ecdd5dcaf91e16fbb530fccd00548ae5aeb
Glauco Foeppel <glauco.tf@gmail.com>
2020-12-04 16:07:40 -0300

    Sets up routes and controller

---
+++ src/currencyController.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+exports.ChangeCurrency = (req, res) => {
+    return res.json({message: "Change Currency Routes"});
+};
+
+exports.AddCurrency = async (req, res) => {
+    return res.json({message: "Add Currency Routes"});
+};
+
+exports.RemoveCurrency = async (req, res) => {
+    return res.json({message: "Remove Currency Routes"});
+};
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var CurrentCurrency = require("./currencyController");
 
 // we will use 3 routes (endpoint) for this api
-router.get('/ChangeCurrency', (req, res) => {
-    return res.json({message: "Change Currency Routes"})
-});
-
-router.get('/AddCurrency', (req, res) => {
-    return res.json({message: "Change Currency Routes"})
-});
-
-router.get('/RemoveCurrency', (req, res) => {
-    return res.json({message: "Change Currency Routes"})
-});
+router.get('/ChangeCurrency', CurrentCurrency.ChangeCurrency);
+router.get('/AddCurrency', CurrentCurrency.AddCurrency);
+router.get('/RemoveCurrency', CurrentCurrency.RemoveCurrency);
 
 module.exports = router
