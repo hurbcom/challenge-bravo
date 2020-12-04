075b8dc93f66004f3f07a72d3b704114444e1855
Glauco Foeppel <glauco.tf@gmail.com>
2020-12-04 16:17:52 -0300

    Add controller test

---
+++ __test__/currencyController.test.js
@@ -0,0 +1,26 @@
+const app = require("../src/server");
+const currencyController = require("../src/currencyController");
+const supertest = require("supertest");
+const request = supertest(app);
+
+
+describe("Currency Controller Test", () => {
+    
+    describe("ChangeCurrency", () => {
+        
+    });
+      
+
+    describe("Add Currency Test", () => {
+        
+        
+
+    });
+
+
+    describe("Remove Currency Test", () => {
+        
+
+
+    });
+});
