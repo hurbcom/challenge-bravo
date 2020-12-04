71b4d8e81e00d0b942b9af09e179da70640349bd
Glauco Foeppel <glauco.tf@gmail.com>
2020-12-04 15:40:29 -0300

    Create server

---
+++ src/app.js
@@ -0,0 +1,5 @@
+const app = require("./server");
+
+app.listen(3000, () => {
+    console.log(`Example app listening at http://localhost:3000`);
+});
---
+++ src/server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+
+module.exports = app;
