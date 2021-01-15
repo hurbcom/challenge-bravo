a50bbea166e07133973092c819d33de97eb00e24
Gabriel Barboza <gabriel@folha.com.br>
2021-01-15 00:04:27 -0300

    Cria o arquivo de rotas

---
+++ backend/routes.js
@@ -0,0 +1,23 @@
+const { Router } = require('express');
+
+const CurrenciesController = require('./app/controllers/CurrenciesController');
+const CurrencyExchangesController = require('./app/controllers/CurrencyExchangesController');
+
+const routes = Router();
+
+/* Currencies */
+
+routes.get("/currencies", CurrenciesController.index);
+routes.get("/currencies/:id", CurrenciesController.show);
+routes.post("/currencies", CurrenciesController.store);
+routes.put("/currencies/:id", CurrenciesController.update);
+routes.delete("/currencies/:id", CurrenciesController.destroy);
+
+/* CurrencyExchanges */
+
+routes.get("/exchanges", CurrencyExchangesController.index);
+routes.get("/exchanges/:id", CurrencyExchangesController.show);
+routes.post("/exchanges", CurrencyExchangesController.store);
+routes.delete("/exchanges/:id", CurrencyExchangesController.destroy);
+
+module.exports = routes;
