ef4e91da3c8a5ccca3166c3295a4de87e494331b
Gabriel Barboza <gabriel@folha.com.br>
2021-01-16 01:42:18 -0300

    Adiciona lib mongoose-seed. Configura seed de Currency

---
+++ backend/seeds.js
@@ -0,0 +1,43 @@
+// const dotenv = require('dotenv');
+const seeder = require('mongoose-seed');
+const databaseConfig = require('./config/database');
+
+seeder.connect(databaseConfig.uri, () => {
+    seeder.loadModels([
+        'app/models/Currency.js'
+    ]);
+
+seeder.clearModels(['Currency'], () => {
+    seeder.populateModels(data, () => {
+      seeder.disconnect();
+    });
+  });
+});
+
+const data = [
+    {
+        'model': 'Currency',
+        'documents': [
+            {
+                'symbol': 'USD',
+                'label': 'United States dollar'
+            },
+            {
+                'symbol': 'BRL',
+                'label': 'Brazilian Real'
+            },
+            {
+                'symbol': 'EUR',
+                'label': 'European Euro'
+            },
+            {
+                'symbol': 'BTC',
+                'label': 'Bitcoin'
+            },
+            {
+                'symbol': 'ETH',
+                'label': 'Ethereum'
+            }
+        ]
+    }
+];
