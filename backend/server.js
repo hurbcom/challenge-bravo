fade6b7b8efc161086c8ccb86b7ddb0821ff102d
Gabriel Barboza <gabrielmbarboza@gmail.com>
2021-01-20 00:11:55 -0300

    Cria job cron para atualizar as taxas de cambio

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,14 @@
 const app = require("./app");
+const { currencyExchangeImport } = require('./src/services/CurrencyExchangeImportService');
+const { CronJob } = require('cron');
+
+// currencyExchangeImport();
+const job = new CronJob('*/30 * * * *', () => {
+    (async() => {
+        await currencyExchangeImport();
+    })();
+}, null, true, 'America/Sao_Paulo');
+
+job.start();
 
 app.listen(5000);
