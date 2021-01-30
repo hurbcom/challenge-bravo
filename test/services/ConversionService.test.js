bd000a362d6f1109396b243cd29306063174a4a3
Gustavo Consentini <gustavoconsentini@gmail.com>
2021-01-29 21:16:04 -0300

    Creating only one test

--- test/services/ConversionService.test.js
+++ test/services/ConversionService.test.js
@@ -1,7 +1,62 @@
 const ConversionService = require('../../src/services/ConversionService')
+const Conversion = require('../../src/interfaces/Conversion')
+const assert = require('assert').strict;
 
 describe('ConversionService', () => {
-    describe('get the latest conversion rate', () => {
-        const ConversionService = new ConversionService({});
+    it('should get the latest conversion rates', async () =>{
+        const mock = {
+            coinGeckoService: {},
+            conversionRepository: {
+                getLatest: function () {
+                    return {
+                        "_id": "601490041639827c937b2731",
+                        "referenceDate": "2021-01-29T22:45:24.220Z",
+                        "usd": 1,
+                        "brl": 1,
+                        "eur": 1,
+                        "eth": 1,
+                        "btc": 1
+                    }
+                }
+            },
+            currencyRepository: {}
+        }
+        const conversionService = new ConversionService(mock);
+        const result = await conversionService.getLatestConversionRate();
+
+        assert.deepStrictEqual(result, {
+            "_id": "601490041639827c937b2731",
+            "referenceDate": "2021-01-29T22:45:24.220Z",
+            "usd": 1,
+            "brl": 1,
+            "eur": 1,
+            "eth": 1,
+            "btc": 1
+        })
+    })
+''
+    it('should convert usd to a brl', async () => {
+        mock = {
+            coinGeckoService: {},
+            conversionRepository: {
+                getLatest: function () {
+                    return {
+                        "_id": "601490041639827c937b2731",
+                        "referenceDate": "2021-01-29T22:45:24.220Z",
+                        "usd": 2,
+                        "brl": 1,
+                        "eur": 1,
+                        "eth": 1,
+                        "btc": 1
+                    }
+                }
+            },
+            currencyRepository: {}
+        }
+        const conversionService = new ConversionService(mock);
+        const result = await conversionService.convertFromTo('brl', 'usd', 100);
+        // bypass date
+        result.referenceDate = new Date('2021-01-29T22:45:24.220Z');
+        assert.deepStrictEqual(result, new Conversion('brl', 'usd', 100, 200, new Date('2021-01-29T22:45:24.220Z')));
     })
 })
