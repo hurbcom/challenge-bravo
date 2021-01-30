062782fa9eaec2d4e6c5ccad3492f4d80c91a88f
brouck <jgbrouck@gmail.com>
2021-01-30 05:02:35 -0300

    Add job for updating rates

---
+++ src/jobs/updateRatesJob.js
@@ -0,0 +1,32 @@
+const cron = require("node-cron");
+const configService = require("../services/configService");
+const currencyExchangeService = require("../services/currencyExchangeService");
+const currenciesDao = require("../database/currenciesDao");
+const dbContext = require("../database/dbContext");
+
+const { UPDATE_RATE_JOBS_CRON } = process.env;
+
+dbContext.connect();
+
+cron.schedule(UPDATE_RATE_JOBS_CRON, async function () {
+    try {
+        const {
+            data: { rates },
+        } = await currencyExchangeService.getCurrency(
+            configService.BASE_CURRENCY
+        );
+
+        const currencies = await currenciesDao.find({});
+
+        for await (const currency of currencies) {
+            await currenciesDao.updateOne(
+                { code: currency.code },
+                { $set: { rateToBase: rates[currency.code] } }
+            );
+        }
+        console.log("rodou");
+    } catch (error) {
+        console.error(error.message);
+        throw error;
+    }
+});
