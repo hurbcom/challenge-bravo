2454d38cd0570eaf8af8cb917da57b68d6e77b6c
unknown <allanbmalves@gmail.com>
2021-03-16 00:16:53 -0300

    Parcial modelos (tabelas DB)

--- bravo/api/migrations/0001_initial.py
+++ bravo/api/migrations/0001_initial.py
@@ -1,4 +1,5 @@
 from django.db import migrations, models
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
@@ -14,9 +15,16 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('simbolo', models.CharField(max_length=5)),
-                ('nome', models.CharField(max_length=255)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Cotacao',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('ultima_atualizacao', models.DateField(blank=True, null=True)),
                 ('cotacao', models.DecimalField(decimal_places=2, max_digits=10)),
-                ('ultima_atualizacao', models.DateTimeField(null=True, blank=True)),
+                ('moeda_de', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='moeda_de', to='api.moedas')),
+                ('moeda_para', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='moeda_para', to='api.moedas')),
             ],
         ),
     ]
--- bravo/api/models.py
+++ bravo/api/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 class Moedas(models.Model):
     simbolo = models.CharField(max_length=5)
-    nome = models.CharField(max_length=255)
-    cotacao = models.DecimalField(max_digits=10, decimal_places=2)
-    ultima_atualizacao = models.DateTimeField(null=True, blank=True)
 
+class Cotacao(models.Model):
+    moeda_para = models.ForeignKey('Moedas',on_delete=models.CASCADE, related_name='moeda_para')
+    moeda_de = models.ForeignKey('Moedas',on_delete=models.CASCADE, related_name='moeda_de')
+    ultima_atualizacao = models.DateField(null=True, blank=True)
+    cotacao = models.DecimalField(max_digits=10, decimal_places=2)
