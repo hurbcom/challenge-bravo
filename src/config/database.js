7f70e4c01e9398121e87d8be8f151c52a08183bb
Iago Passos <passosiago@gmail.com>
2021-03-21 16:41:16 -0300

    adding database configs

---
+++ src/config/database.js
@@ -0,0 +1,8 @@
+module.exports = {
+    uri: process.env.MONGODB_ENDPOINT || "mongodb://localhost:27017/currencies",
+    options: {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+};
--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const databaseConfig = require('./config/database');
+const mongoose = require('mongoose');
+const mongo = mongoose.connect(databaseConfig.uri, databaseConfig.options);
+
+mongo.then(() => {
+    console.log('connected');
+}, error => {
+    console.log(error, 'error');
+})
 
 app.use('/', (req, res) => {
     res.send({"data": "ok"})
