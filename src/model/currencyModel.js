07808243365601f97a6ebb4b79d8b7061e884f67
Iago Passos <passosiago@gmail.com>
2021-03-22 19:04:57 -0300

    generating currency model

---
+++ src/model/currencyModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const CurrencySchema = new mongoose.Schema({
+    symbol: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+module.exports = mongoose.model("Currency", CurrencySchema);
