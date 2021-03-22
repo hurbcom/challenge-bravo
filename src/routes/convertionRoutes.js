92823ecbc72b65941dab9fe568031687927c393e
Iago Passos <passosiago@gmail.com>
2021-03-22 19:15:01 -0300

    adding routing capabilities

---
+++ src/routes/convertionRoutes.js
@@ -0,0 +1,7 @@
+const convertionControllers = require('../controllers/convertionControllers');
+const currencyValidator = require('../middleware/currencyValidator');
+const router = require('express').Router();
+
+router.get('/convert', currencyValidator.validate, convertionControllers.convertCurrency)
+
+module.exports = router;
---
+++ src/routes/currencyRoutes.js
@@ -0,0 +1,9 @@
+const currencyController = require('../controllers/currencyControllers');
+const router = require('express').Router();
+
+router.get('/currency', currencyController.findCurrencies);
+router.get('/currency/:symbol', currencyController.findCurrency);
+router.post('/currency', currencyController.createCurrency);
+router.delete('/currency/:symbol', currencyController.deleteCurrency);
+
+module.exports = router;
