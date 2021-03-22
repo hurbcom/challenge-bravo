059e7d0fa2258e5e175d8e40b24fa88c5afc7141
Iago Passos <passosiago@gmail.com>
2021-03-22 19:04:14 -0300

    adding controllers logic

---
+++ src/controllers/convertionControllers.js
@@ -0,0 +1,25 @@
+const convertionService = require('../services/convertionServices');
+
+const convertCurrency = async (req, res, next) => {
+    try {
+        let {
+            from,
+            to,
+            amount
+        } = req.query;
+
+        let convertedAmount = await convertionService.convert(from, to, amount);
+        res.status(200).json({
+            data: {
+                amount: convertedAmount
+            }
+        })
+    } catch (err) {
+        next(err)
+    }
+
+}
+
+module.exports = {
+    convertCurrency
+}
---
+++ src/controllers/currencyControllers.js
@@ -0,0 +1,60 @@
+const currencyService = require('../services/currencyServices');
+
+const findCurrencies = async (req, res, next) => {
+    try {
+        let currencies = await currencyService.getCurrencies();
+        res.status(200).json({
+            data: currencies
+        })
+    } catch (err) {
+        next(err)
+    }
+}
+
+const findCurrency = async (req, res, next) => {
+    try {
+        let {
+            symbol
+        } = req.params;
+        let currency = await currencyService.getCurrency(symbol);
+        res.status(200).json({
+            data: currency
+        })
+    } catch (err) {
+        next(err)
+    }
+}
+
+const createCurrency = async (req, res, next) => {
+    try {
+        let currency = await currencyService.createCurrency(req.body);
+        res.status(200).json({
+            data: currency
+        })
+    } catch (err) {
+        next(err)
+    }
+}
+
+const deleteCurrency = async (req, res, next) => {
+    try {
+        let {
+            symbol
+        } = req.params;
+        let currency = await currencyService.removeCurrency(symbol);
+        res.status(200).json({
+            data: currency
+        })
+    } catch (err) {
+        next(err)
+    }
+
+}
+
+
+module.exports = {
+    findCurrencies,
+    findCurrency,
+    createCurrency,
+    deleteCurrency
+}
