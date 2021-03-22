132f03eb5bf0f7012d6d029d9eeded6a5c364150
Iago Passos <passosiago@gmail.com>
2021-03-22 19:25:10 -0300

    adding validators

---
+++ src/middleware/currencyValidator.js
@@ -0,0 +1,23 @@
+const currencyServices = require('../services/currencyServices');
+const invalidCurrencyError = require('../error/types/invalidCurrencyError');
+
+const validate = async (req, res, next) => {
+    let {
+        from,
+        to
+    } = req.query;
+
+    let fromCurrency = await currencyServices.getCurrency(from)
+    let toCurrency = await currencyServices.getCurrency(to)
+
+    if (!fromCurrency || !toCurrency) {
+        next(new invalidCurrencyError())
+    }
+
+    next()
+
+}
+
+module.exports = {
+    validate
+}
