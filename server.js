b095815e25a708fb66b84813ce8807b8c3ce89c3
Junior Nunes <nunes.junior92@gmail.com>
2021-04-22 18:44:13 -0300

    sync response log

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const sync = require('./sync/updateCurrenciesValue');
 // Atualização da taxa de câmbio de 1 em 1 hora
 setInterval(() => {
     console.log('Sincronizando moedas...');
-    sync.syncCurrencies();
+    sync.syncCurrencies(async () => {
+        console.log('Moedas sincronizadas!');
+    });
 }, 3600000);
 
 // Server
