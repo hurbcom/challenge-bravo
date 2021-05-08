80c7b9fa1cbe6abab13eab0356b47d10b3d2dc31
Alfaya <flavioalfaya@feegow.com.br>
2021-05-08 13:25:43 -0300

    iniciando express

---
+++ backend/src/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+class AppController{
+    constructor(){
+        this.express= express()
+        this.middlewares()
+        this.routes()
+    }
+
+    middlewares(){
+        this.express.use(express.json())
+    }
+
+    routes(){
+        this.express.use(require("./routes"))
+    }
+}
+
+module.exports = new AppController().express
