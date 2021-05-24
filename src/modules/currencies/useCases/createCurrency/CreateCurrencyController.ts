fe0dca0d4ace815b34c5138a106bf646a4b61f6e
Eric Martins <ericmartins.martins@gmail.com>
2021-05-24 08:57:32 -0300

    verificando se currencyCode foi passado e garantindo que seja enviado em caixa alta

--- src/modules/currencies/useCases/createCurrency/CreateCurrencyController.ts
+++ src/modules/currencies/useCases/createCurrency/CreateCurrencyController.ts
@@ -1,15 +1,22 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 
+import { AppError } from "../../../../shared/errors/AppError";
 import { CreateCurrencyUseCase } from "./CreateCurrencyUseCase";
 
 class CreateCurrencyController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { currencyCode } = request.body;
 
+    if (!currencyCode) {
+      throw new AppError("currencyCode not informed!");
+    }
+
+    const upperCaseCurrencyCode = currencyCode.toLocaleUpperCase();
+
     const createCurrencyUseCase = container.resolve(CreateCurrencyUseCase);
 
-    const currency = await createCurrencyUseCase.execute(currencyCode);
+    const currency = await createCurrencyUseCase.execute(upperCaseCurrencyCode);
 
     return response.status(201).json(currency);
   }
