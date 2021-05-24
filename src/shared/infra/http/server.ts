7ccf0935f9790bb183fbc26e19cc2ed52b2618f4
Eric Martins <ericmartins.martins@gmail.com>
2021-05-24 08:39:30 -0300

    usando express para tratar excecoes

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,8 +1,10 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 
 import "../typeorm";
 import "../../container";
+import { AppError } from "../../errors/AppError";
 import { routes } from "./routes";
 
 const app = express();
@@ -10,5 +12,20 @@ const PORT = process.env.PORT || 3333;
 
 app.use(express.json());
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${error.message}`,
+    });
+  }
+);
 
 app.listen(PORT, () => console.log(`server is running in port ${PORT}`));
