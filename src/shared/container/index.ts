8be2a397e626c2db1312ffab0cc9a9f41196a133
Eric Martins <ericmartins.martins@gmail.com>
2021-05-24 08:18:17 -0300

    registrando singleton do RedisService e ExchangeApiService

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,8 +2,19 @@ import { container } from "tsyringe";
 
 import { CurrenciesRepository } from "../../modules/currencies/infra/typeorm/repositories/CurrenciesRepository";
 import { ICurrenciesRepository } from "../../modules/currencies/repositories/ICurrenciesRepositories";
+import { exchangeApiService } from "../infra/services/ExchangeApiService";
+import { RedisService } from "../infra/services/RedisService";
+import { ICacheService } from "../services/ICacheService";
+import { IExchangeApiService } from "../services/IExchangeApiService";
 
 container.registerSingleton<ICurrenciesRepository>(
   "CurrenciesRepository",
   CurrenciesRepository
+);
+
+container.registerSingleton<ICacheService>("CacheService", RedisService);
+
+container.registerSingleton<IExchangeApiService>(
+  "ExchangeApiService",
+  exchangeApiService
 );
