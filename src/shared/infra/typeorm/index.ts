fb9056140316c9ed18d925e374159c398211292f
Eric Martins <ericmartins.martins@gmail.com>
2021-05-27 15:42:52 -0300

    rodando migrations automaticamente

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,4 +2,7 @@ import { createConnection } from "typeorm";
 
 import ORMConfig from "../../../../ormconfig";
 
-createConnection(ORMConfig).then(() => console.log("database connected"));
+createConnection(ORMConfig).then((conn) => {
+  console.log("database connected!");
+  conn.runMigrations();
+});
