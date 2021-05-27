a1aefe94016bb058415f603b3497c9752178b19a
Eric Martins <ericmartins.martins@gmail.com>
2021-05-27 15:42:41 -0300

    removendo servico de cache

--- src/shared/services/ICacheService.ts
+++
@@ -1,6 +0,0 @@
-interface ICacheService {
-  saveAllCurrenciesNames(names: { name: string }[]): Promise<void>;
-  getCurrencyNameByCode(currencyCode: string): Promise<string>;
-}
-
-export { ICacheService };
