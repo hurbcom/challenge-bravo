60f01401e782acb3af4c03bf5644dccb7dc9650a
domingos <dgcarvalho@brasilseg.com.br>
2021-06-07 20:20:28 -0300

    Creating endpoints

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
+import logging
 from flask import Flask
-app = Flask(__name__)
+from waitress import serve
+from views.coin_rates import set_coin_rates_external_api_to_redis, convert_two_currencies, set_fake_coin_to_redis, \
+    remove_rate_from_redis
 from os.path import join, dirname
 from dotenv import load_dotenv
 
+app = Flask(__name__)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 dotenv_path = join(dirname(__file__), '.env')
+app.add_url_rule('/health', view_func=lambda: 'ok', methods=['GET'])
+app.add_url_rule('/convert', view_func=convert_two_currencies, methods=['GET'])
+app.add_url_rule('/setFakeRate', view_func=set_fake_coin_to_redis, methods=['POST'])
+app.add_url_rule('/rates', view_func=remove_rate_from_redis, methods=['DELETE'])
 load_dotenv(dotenv_path)
+set_coin_rates_external_api_to_redis()
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    serve(app, host='0.0.0.0', port=5000, threads=20)
--- views/coin_rates.py
+++ views/coin_rates.py
@@ -1,29 +1,60 @@
+import logging
 import os
 import json
 from flask import request
-from flask import jsonify
 from db.redis import connect_to_redis
 import requests
 
 from utils.urls import URL_GET_USD_RATES
 
 
+def set_coin_rates_external_api_to_redis():
+    with connect_to_redis() as redis:
+        flag_is_cache_alive = redis.exists('isCacheAlive')
+        if flag_is_cache_alive:
+            return
+        key = os.environ.get('COIN_API_KEY')
+        ttl = os.environ.get('TIME_TO_LIVE', 300)
+        result = requests.get(URL_GET_USD_RATES, headers={'X-CoinAPI-Key': key})
+        redis.set('isCacheAlive', 1, ex=ttl)
+        logging.info("Setting redis cache")
+        if result.status_code == 200:
+            dict_result = json.loads(result.content)
+            result = dict_result['rates']
+            redis.set('isCacheAlive', 1, ex=ttl)
+            redis.set('USD', 1, ex=ttl)
+            for item in result:
+                redis.set(item.get('asset_id_quote'), item.get('rate'), ex=ttl)
 
-def get_coin_rates_external_api():
-    key = os.environ.get('COIN_API_KEY')
-    result = requests.get(URL_GET_USD_RATES, headers={'X-CoinAPI-Key': key})
-    if result.status_code == 200:
-        dict_result = json.loads(result.content)
-        dict_weather = dict_result['rates'][0]
-        dict_main = dict_result['main']
-        formated_result = {
-            'description': dict_weather['description'],
-            'icon': dict_weather['icon'],
-            'name': dict_result['name'],
-            'temp': dict_main['temp'],
-            'temp_min': dict_main['temp_min'],
-            'temp_max': dict_main['temp_max']
-        }
-        return formated_result
-    return jsonify({'error': 'not found'}), 400
 
+def convert_two_currencies():
+    with connect_to_redis() as redis:
+        set_coin_rates_external_api_to_redis()
+        args = request.args
+        from_currency, to_currency, amount = args.get('from'), args.get('to'), args.get('amount')
+        from_rate, to_rate = tuple(redis.mget(from_currency, to_currency))
+        if not from_rate or not to_rate:
+            return json.dumps({'error': 'One or more rates not found, try another! :)'})
+        from_amount_in_usd = float(amount)/float(from_rate)
+        result_conversion = from_amount_in_usd * float(to_rate)
+        return json.dumps({'convertedValue': result_conversion})
+
+
+def set_fake_coin_to_redis():
+    with connect_to_redis() as redis:
+        try:
+            request_body = json.loads(request.data)
+            redis.set(request_body['rateName'], request_body['usdValue'])
+            return json.dumps({"message": f'success to register {request_body["rateName"]} to the database!'})
+        except Exception as e:
+           return json.dumps({"error": "Error while executing the rate registering process! Sorry"})
+
+
+def remove_rate_from_redis():
+    with connect_to_redis() as redis:
+        try:
+            request_body = json.loads(request.data)
+            redis.delete(request_body['rateName'])
+            return json.dumps({"message": f'success to delete {request_body["rateName"]} from the database!'})
+        except Exception as e:
+            return json.dumps({"error": "Error while executing the rate deleting process! Sorry"})
