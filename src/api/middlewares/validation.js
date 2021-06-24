5de6647175f7da26152d019dd05bb88a0d3c2744
Daniela Rocha <danirocha.rmd@gmail.com>
2021-06-24 12:20:55 -0300

    refactor: implements ValidationMiddleware.currency

--- src/api/middlewares/validation.js
+++ src/api/middlewares/validation.js
@@ -1,7 +1,16 @@
 import Schema from '../../libs/Schema';
 
-const currency = (req, res, next) => {
-    next();
+const currency = async (req, res, next) => {
+    const currencyDTO = req.body;
+    const currencySchema = Schema.generateCurrencySchema();
+
+    try {
+        await Schema.validate(currencySchema, currencyDTO);
+
+        next();
+    } catch (err) {
+        return res.status(400).json({ message: 'invalid params', data: { errors: err.errors }});
+    }
 };
 
 export { currency };
--- src/libs/Schema.js
+++ src/libs/Schema.js
@@ -5,6 +5,19 @@ class Schema {
         // -
     }
 
+    generateCurrencySchema() {
+        const currencySchema = {
+            symbol: yup.string().min(3).required(),
+            quotation: yup.number()
+        }
+        
+        return yup.object().shape(currencySchema);
+    }
+
+    async validate (schema, DTO) {
+        return await schema.validate(DTO, { abortEarly: false });
+    }
+
 }
 
 export default new Schema();
