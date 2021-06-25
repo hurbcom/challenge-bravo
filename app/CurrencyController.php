a85c2516ff99026bd6bc4b39212013afcbeb2d2d
Heric Branco <hericbranco@gmail.com>
2021-06-25 18:43:07 -0300

    Refatoração para melhor legibilidade do controller

--- app/CurrencyController.php
+++ app/CurrencyController.php
@@ -3,26 +3,35 @@
 namespace App;
 
 use App\Models\Currency;
+use Src\Request;
 
 class CurrencyController extends Controller
 {
+    private $model;
+
+    public function __construct(Request $request)
+    {
+        $this->model = new Currency();
+        parent::__construct($request);
+    }
+
     public function index()
     {
-        echo json_encode((new Currency())->get());
+        echo json_encode($this->model->get());
     }
 
     public function store()
     {
-        echo json_encode((new Currency())->insert($this->request->all(['name', 'base'])));
+        echo json_encode($this->model->insert($this->request->all(['name', 'base'])));
     }
 
     public function delete($name)
     {
-        echo json_encode((new Currency())->where('name', $name)->delete());
+        echo json_encode($this->model->where('name', $name)->delete());
     }
 
     public function update($name)
     {
-        echo json_encode((new Currency())->where('name', $name)->update($this->request->all(['name', 'base'])));
+        echo json_encode($this->model->where('name', $name)->update($this->request->all(['name', 'base'])));
     }
 }
