2e2de0c319fb5efb6c7375566f0d47d0086d1653
Heric Branco <hericbranco@gmail.com>
2021-06-25 12:21:06 -0300

    Atualização para uso do mongo e teste de uso do mesmo

--- app/Controller.php
+++ app/Controller.php
@@ -6,6 +6,26 @@ class Controller
 {
     public function teste($uid)
     {
+        $manager = new \MongoDB\Driver\Manager("mongodb://root:A123456@hurb_mongodb:27017");
+
+        $bulk = new \MongoDB\Driver\BulkWrite;
+        $bulk->insert(['x' => 1]);
+        $bulk->insert(['x' => 2]);
+        $bulk->insert(['x' => 3]);
+        $manager->executeBulkWrite('db.test', $bulk);
+
+        $filter = ['x' => ['$gt' => 1]];
+        $options = [
+            'projection' => ['_id' => 0],
+            'sort' => ['x' => -1],
+        ];
+
+        $query = new \MongoDB\Driver\Query($filter, $options);
+        $cursor = $manager->executeQuery('db.test', $query);
+
+        foreach ($cursor as $document) {
+            print_r($document);
+        }
 
         echo 'FOI AQUI'.$uid;
     }
