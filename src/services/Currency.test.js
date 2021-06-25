3ecf3a82e04a4e1ac985e3fbd09e255fea14a8e4
Daniela Rocha <danirocha.rmd@gmail.com>
2021-06-24 22:33:15 -0300

    adds unit tests for CurrencyService.deleteCurrency

--- src/services/Currency.test.js
+++ src/services/Currency.test.js
@@ -10,8 +10,11 @@ describe('#storeCurrency', () => {
         const currencyDTO = {
             currencyCode: "HURB"
         };
+        const currency = {
+            code: currencyDTO.currencyCode
+        };
 
-        CurrencyDB.listCurrencyByCode.mockResolvedValue([ currencyDTO ]);
+        CurrencyDB.listCurrencyByCode.mockResolvedValue([ currency ]);
 
         try {
             await CurrencyService.storeCurrency(currencyDTO);
@@ -80,6 +83,72 @@ describe('#storeCurrency', () => {
             expect(CurrencyDB.storeFictitiousCurrency).toBeCalledTimes(1);
             expect(CurrencyDB.storeFictitiousCurrency).toReturn();
             expect(CurrencyDB.storeRealCurrency).not.toBeCalled();
+        } catch (err) {
+            expect(err).toBeFalsy();
+        }
+    });
+});
+
+describe('#deleteCurrency', () => {
+    test('it throws an error when the provided currency does not exist', async () => {
+        const currencyDTO = {
+            currencyCode: 'HURB'
+        }
+
+        CurrencyDB.listCurrencyByCode.mockResolvedValue([]);
+
+        try {
+            await CurrencyService.deleteCurrency(currencyDTO);
+        } catch (err) {
+            expect(err).toBeTruthy();
+            expect(CurrencyDB.listCurrencyByCode).toBeCalledTimes(1);
+            expect(CurrencyDB.listCurrencyByCode).toReturn();
+        }
+    });
+
+    test('it throws an error when the currency cannot be deleted', async () => {
+        const currencyDTO = {
+            currencyCode: 'HURB'
+        }
+        const currency = {
+            id: 42,
+            code: currencyDTO.currencyCode
+        };
+
+        CurrencyDB.listCurrencyByCode.mockResolvedValue([ currency ]);
+        CurrencyDB.deleteCurrency.mockRejectedValue(new Error());
+
+        try {
+            await CurrencyService.deleteCurrency(currencyDTO);
+        } catch (err) {
+            expect(err).toBeTruthy();
+            expect(CurrencyDB.listCurrencyByCode).toBeCalledTimes(1);
+            expect(CurrencyDB.listCurrencyByCode).toReturn();
+            expect(CurrencyDB.deleteCurrency).toBeCalledTimes(1);
+        }
+    });
+
+    test('it returns the provided currency when the same is successfully deleted', async () => {
+        const currencyDTO = {
+            currencyCode: 'HURB'
+        }
+        const currency = {
+            id: 42,
+            code: currencyDTO.currencyCode
+        };
+
+        CurrencyDB.listCurrencyByCode.mockResolvedValue([ currency ]);
+        CurrencyDB.deleteCurrency.mockResolvedValue({});
+
+        try {
+            const deletedCurrency = await CurrencyService.deleteCurrency(currencyDTO);
+
+            expect(deletedCurrency).toBeTruthy();
+            expect(deletedCurrency).toEqual(currencyDTO);
+            expect(CurrencyDB.listCurrencyByCode).toBeCalledTimes(1);
+            expect(CurrencyDB.listCurrencyByCode).toReturn();
+            expect(CurrencyDB.deleteCurrency).toBeCalledTimes(1);
+            expect(CurrencyDB.deleteCurrency).toReturn();
         } catch (err) {
             expect(err).toBeFalsy();
         }
