925c65e10568113a64020bf4b3ec2fb3172fd184
Daniela Rocha <danirocha.rmd@gmail.com>
2021-06-25 23:43:16 -0300

    refactor: adds arrayAContainsB to utils lib

--- src/libs/Utils.js
+++ src/libs/Utils.js
@@ -3,10 +3,14 @@ class Utils {
         // -
     }
 
-    newObject(...args) {
+    newObject (...args) {
         const objects = [{}].concat(args);
         
         return objects.reduce((acc, cur) => ({ ...acc, ...cur }));
+    }
+
+    arrayAContainsB (arrayA, arrayB) {
+        return arrayB.every(item => arrayA.includes(item));
     }
 }
 
