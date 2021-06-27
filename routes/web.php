dd0b91a3a40b323b6c34a11b1db6c62307d147ce
Renato <renato87rj@gmail.com>
2021-06-27 16:38:34 -0300

    atualizacao nas rotas

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,19 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-$router->group(['prefix' => 'api'], function () use($router) {
-    $router->get('/', function () use ($router) {
-        return $router->app->version();
-    });
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
 
-    $router->group(['prefix' => 'moedas'], function () use($router) {
+$router->group(['prefix' => 'api'], function () use($router) {
+
+    $router->group(['prefix' => 'currency'], function () use($router) {
         $router->get('', 'MoedaController@index');
         $router->get('{uid}', 'MoedaController@show');
         $router->post('', 'MoedaController@store');
         $router->patch('{uid}', 'MoedaController@update');
         $router->delete('{uid}', 'MoedaController@destroy');
+        $router->get('conversion', 'MoedaController@currencyConverter');
     });
 
-    $router->group(['prefix' => 'currency'], function () use($router) {
-        $router->get('', 'MoedaController@currencyConverter');
-    });
 });
