2bf0e83286f22030006756899b48f9ccf4607640
Heric Branco <hericbranco@gmail.com>
2021-06-28 11:55:04 -0300

    Teste

--- src/Dispatcher.php
+++ src/Dispatcher.php
@@ -24,6 +24,8 @@ class Dispatcher
         $controller = "App\\".$controller;
         echo $controller;
         echo ' - '.$method;
+        echo '<pre>';
+        print_r((new $controller($request)));
         if (!class_exists($controller) || !method_exists($controller, $method)) {
             echo 'Não achou a classe';exit;
             throw new \Exception("Erro ao despachar: controller não pode ser instanciado, ou método não exite");
