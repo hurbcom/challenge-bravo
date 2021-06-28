c07e6fee82ad0237b2408ffe13d9f0c0a96bf73d
Renato <renato87rj@gmail.com>
2021-06-27 21:29:47 -0300

    correcao nas rotas

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,12 @@ $router->get('/', function () use ($router) {
 $router->group(['prefix' => 'api'], function () use($router) {
 
     $router->group(['prefix' => 'currency'], function () use($router) {
+        $router->get('conversion', 'MoedaController@currencyConverter');
         $router->get('', 'MoedaController@index');
         $router->get('{uid}', 'MoedaController@show');
         $router->post('', 'MoedaController@store');
         $router->patch('{uid}', 'MoedaController@update');
         $router->delete('{uid}', 'MoedaController@destroy');
-        $router->get('conversion', 'MoedaController@currencyConverter');
     });
 
 });
