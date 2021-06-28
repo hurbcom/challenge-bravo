cde1c5e21eba51b77080c1a376f6daece1496b15
Heric Branco <hericbranco@gmail.com>
2021-06-28 11:31:26 -0300

    Teste

--- src/Dispatcher.php
+++ src/Dispatcher.php
@@ -12,9 +12,6 @@ class Dispatcher
         if (is_callable($callback['callback'])) {
             return call_user_func_array($callback['callback'], array_values($params));
         }
-
-        print_r($callback);exit;
-
         return $this->callControllerMethod($callback, $params, $request, $namespace);
     }
 
@@ -26,6 +23,7 @@ class Dispatcher
         list($controller, $method) = explode('@', $callback['callback']);
         $controller = "App\\".$controller;
         if (!class_exists($controller) || !method_exists($controller, $method)) {
+            echo 'Não achou a classe';exit;
             throw new \Exception("Erro ao despachar: controller não pode ser instanciado, ou método não exite");
         }
 
