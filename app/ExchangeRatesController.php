7517c6ed9666b6febd6e3dc6b59404d1cd43ad22
Heric Branco <hericbranco@gmail.com>
2021-07-01 14:02:51 -0300

    Adicionada validação no fomrulário de exchange

--- app/ExchangeRatesController.php
+++ app/ExchangeRatesController.php
@@ -6,6 +6,7 @@ use App\Models\ExchangeRate;
 use App\Models\Currency;
 use App\Services\ExchangeService;
 use Src\Request;
+use Src\Validator;
 
 class ExchangeRatesController extends Controller
 {
@@ -17,6 +18,15 @@ class ExchangeRatesController extends Controller
 
     public function index()
     {
+        $validator = Validator::make($this->request->all(['from', 'to', 'amount']), [
+            'from' => 'required',
+            'to' => 'required',
+            'amount' => 'required|numeric'
+        ]);
+        if ($validator->fails()) {
+            echo json_encode($validator->getErrors());
+            exit;
+        }
         echo json_encode($this->model->from(new Currency(['name' => $this->request->from]))
             ->to(new Currency(['name' => $this->request->to]))
             ->amount($this->request->amount)
