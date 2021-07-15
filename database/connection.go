c123ea413fae27a6f22c683e797f12c811462f4d
Gustavo Willer <gustavo.willer@hotmail.com>
2021-07-15 19:10:12 -0300

    Read the connection string from environment

--- database/connection.go
+++ database/connection.go
@@ -1,12 +1,13 @@
 package database
 
 import (
+	"os"
 	"gorm.io/gorm"
 	"gorm.io/driver/mysql"
 )
 
 func Connect() {
-	data_source_name := "user:@tcp(localhost:3306)/database"
+	data_source_name := os.Getenv("DATA_SOURCE_NAME")
 	_, error := gorm.Open(mysql.Open(data_source_name), &gorm.Config{})
 	if error != nil {
 		panic("Failed to connect database")
