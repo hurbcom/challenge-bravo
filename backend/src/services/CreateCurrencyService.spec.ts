035955413c77d85911cae5026504af97ab502381
ribeirogab <ribeirogabx@gmail.com>
2021-07-23 22:15:29 -0300

    test(backend): unit test created for create currency service

---
+++ backend/src/services/CreateCurrencyService.spec.ts
@@ -0,0 +1,75 @@
+import 'reflect-metadata';
+
+import { AppError } from '@errors/AppError';
+
+import { FakeCurrenciesRepository } from '@repositories/fakes/FakeCurrenciesRepository';
+
+import { CreateCurrencyService } from './CreateCurrencyService';
+
+let fakeCurrenciesRepository: FakeCurrenciesRepository;
+let createCurrencyService: CreateCurrencyService;
+
+describe('CreateCurrencyService', () => {
+  beforeEach(() => {
+    fakeCurrenciesRepository = new FakeCurrenciesRepository();
+
+    createCurrencyService = new CreateCurrencyService(fakeCurrenciesRepository);
+  });
+
+  it('should be able to create a new currency', async () => {
+    const code = 'abc';
+
+    const currency = await createCurrencyService.execute({
+      code,
+      backingCurrency: {
+        code: 'btc',
+        amount: 0.1,
+      },
+    });
+
+    expect(currency.code).toBe(code);
+    expect(currency.backingCurrency.code).toBe('btc');
+    expect(currency.backingCurrency.amount).toBe(0.1);
+  });
+
+  it('should not be able to create currency with code same as the original currencies', async () => {
+    await expect(
+      createCurrencyService.execute({
+        code: 'brl',
+        backingCurrency: {
+          code: 'usd',
+          amount: 10,
+        },
+      }),
+    ).rejects.toEqual(
+      new AppError(
+        'The currency code cannot be the same as the original currencies.',
+        409,
+      ),
+    );
+  });
+
+  it('should not be able to create two currencies with the same code', async () => {
+    const code = 'abc';
+
+    await fakeCurrenciesRepository.create({
+      code,
+      backingCurrency: {
+        code: 'btc',
+        amount: 0.1,
+      },
+    });
+
+    await expect(
+      createCurrencyService.execute({
+        code,
+        backingCurrency: {
+          code: 'usd',
+          amount: 10,
+        },
+      }),
+    ).rejects.toEqual(
+      new AppError('A currency with this code already exists.', 409),
+    );
+  });
+});
