d7700c641556d23c5ded571196ad04aa5fdb706b
ribeirogab <ribeirogabx@gmail.com>
2021-07-23 22:14:38 -0300

    feat(backend): service for creation of a new currency added

---
+++ backend/src/services/CreateCurrencyService.ts
@@ -0,0 +1,53 @@
+import { inject, injectable } from 'tsyringe';
+
+import { AppError } from '@errors/AppError';
+
+import { ICurrency } from '@interfaces/ICurrency';
+
+import { ICurrenciesRepository } from '@repositories/models/ICurrenciesRepository';
+
+import {
+  OriginalCurrencyCode,
+  originalCurrencyCodes,
+} from '@utils/originalCurrencyCodes';
+
+interface IRequest {
+  code: string;
+  backingCurrency: {
+    code: OriginalCurrencyCode;
+    amount: number;
+  };
+}
+
+@injectable()
+export class CreateCurrencyService {
+  constructor(
+    @inject('CurrenciesRepository')
+    private currenciesRepository: ICurrenciesRepository,
+  ) {}
+
+  public async execute({
+    code,
+    backingCurrency,
+  }: IRequest): Promise<ICurrency> {
+    if (originalCurrencyCodes.includes(code)) {
+      throw new AppError(
+        'The currency code cannot be the same as the original currencies.',
+        409,
+      );
+    }
+
+    const currencyExists = await this.currenciesRepository.findOne({ code });
+
+    if (currencyExists) {
+      throw new AppError('A currency with this code already exists.', 409);
+    }
+
+    const currency = await this.currenciesRepository.create({
+      code,
+      backingCurrency,
+    });
+
+    return currency;
+  }
+}
