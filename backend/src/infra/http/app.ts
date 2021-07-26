ed9b0a152c03f1c38ae7018b34b4168480938234
ribeirogab <ribeirogabx@gmail.com>
2021-07-26 13:01:16 -0300

    docs(backend): swagger documentation implemented on API

--- backend/src/infra/http/app.ts
+++ backend/src/infra/http/app.ts
@@ -8,9 +8,11 @@ import '@infra/databases/mongoose/connection';
 import { errors as celebrateErrors } from 'celebrate';
 import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import { AppError } from '@errors/AppError';
 
+import swaggerFile from '../../swagger.json';
 import { routes } from './routes';
 
 const app = express();
@@ -19,6 +21,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(celebrateErrors());
 app.use((error: Error, req: Request, res: Response, _: NextFunction) => {
