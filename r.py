784f3f3d32de83b99d488da1d9b1e5060e9cdbaf
Rafael Pereira <rafael.pereira@lactec.org.br>
2021-08-06 21:46:43 -0300

    chore: adding google_currency pipy library to convert currency values from google.

--- r.py
+++ r.py
@@ -1,27 +1,45 @@
-from requests import Request, Session
-from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
-import json
+import requests
+from bs4 import BeautifulSoup
+import re
+from google_currency import convert
 
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-parameters = {
-  'start':'1',
-  'limit':'5000',
-  'convert':'USD'
-}
-headers = {
-  'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': 'ddf3a033-02eb-4095-a0cb-f976c703b6e9',
-}
+page = requests.get('https://www.google.com/search?q=2+brl+usd')
+soup = BeautifulSoup(page.text, 'lxml')
+# results = soup.findAll("div", {"data-exchange-rate" : True})
+# results = soup.findAll("div", {"data-exchange-rate" : re.compile(r".*")})
 
-session = Session()
-session.headers.update(headers)
+results = soup.findAll("div")
 
-try:
-  response = session.get(url, params=parameters)
-  data = json.loads(response.text)
-  print(data)
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-  print(e)
+print(convert('USD', 'BRL', 1)  )
+
+# # part1 = soup.find(class_="ccOutputTrail").previous_sibling
+# # part2 = soup.find(class_="ccOutputTrail").get_text(strip=True)
+# # rate = "{}{}".format(part1,part2)
+
+# # def gbp_to_usd(rate,gbp):
+# #     dollars=gbp*rate
+# #     return dollars
+
+# # gbp = input("Enter GBP amount: ")
+# # finalamt = gbp_to_usd(float(rate),float(gbp))
+# # print(str(gbp)+" GBP is equvalent to "+"$"+str(finalamt)+" USD")
 
 
-  https://bitpay.com/api/rates
+# # # data-exchange-rate
+
+# from urllib.parse import urlencode, urlparse, parse_qs
+
+# from lxml.html import fromstring
+# from requests import get
+
+# raw = get("https://www.google.com/search?q=StackOverflow").text
+# page = fromstring(raw)
+
+# for result in page.tagselect("div"):
+#     print(result)
+
+# # for result in page.cssselect(".r a"):
+# #     url = result.get("href")
+# #     if url.startswith("/url?"):
+# #         url = parse_qs(urlparse(url).query)['q']
+# #     print(url[0])
