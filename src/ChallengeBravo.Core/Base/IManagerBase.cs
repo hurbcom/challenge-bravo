f0e55fd5390253434f1510d3da08ef515d9f3b68
Matheus Oliveira Vidal <matheus.vidal@fgv.br>
2021-08-14 18:54:33 -0300

    Manager "genérico" para realizar o CRUD básico. Dessa forma poderemos herdar esses métodos em Managers específicos de cada classe.

---
+++ src/ChallengeBravo.Core/Base/IManagerBase.cs
@@ -0,0 +1,25 @@
+﻿using Abp.Domain.Entities.Auditing;
+using Abp.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeBravo.Base
+{
+    public interface IManagerBase<TEntidade> : IDomainService
+       where TEntidade : FullAuditedEntity<Guid>
+    {
+        TEntidade ObterPorId(Guid guid);
+
+        List<TEntidade> ObterTodos();
+
+        void Inserir(TEntidade entidade);
+
+        void Atualizar(TEntidade entidade);
+
+        void ApgarPorId(Guid guid);
+
+    }
+}
---
+++ src/ChallengeBravo.Core/Base/ManagerBase.cs
@@ -0,0 +1,70 @@
+﻿using Abp.Domain.Entities.Auditing;
+using Abp.Domain.Repositories;
+using Abp.Domain.Services;
+using Abp.Domain.Uow;
+using Abp.UI;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ChallengeBravo.Base
+{
+    public class ManagerBase<TEntidade> : DomainService, IManagerBase<TEntidade>
+        where TEntidade : FullAuditedEntity<Guid>
+    {
+        private readonly IRepository<TEntidade, Guid> _repository;
+        private readonly IUnitOfWorkManager _unitOfWorkManager;
+
+        public ManagerBase(IRepository<TEntidade, Guid> repository, IUnitOfWorkManager unitOfWorkManager)
+        {
+            _repository = repository;
+            _unitOfWorkManager = unitOfWorkManager;
+
+        }
+
+        public void Inserir(TEntidade entidade)
+        {
+            _repository.Insert(entidade);
+        }
+
+        public void Atualizar(TEntidade entidade)
+        {
+            _repository.Update(entidade);
+        }
+
+        public TEntidade ObterPorId(Guid guid)
+        {
+            return _repository.FirstOrDefault(var => var.Id == guid);
+        }
+
+        public List<TEntidade> ObterTodos()
+        {
+            return _repository.GetAllList();
+
+        }
+
+        public void ApgarPorId(Guid guid)
+        {
+            using (var unitofWork = _unitOfWorkManager.Begin())
+            {
+                try
+                {
+                    _repository.HardDelete(var => var.Id == guid);
+                }
+                catch (Exception e)
+                {
+                    throw new UserFriendlyException(e.Message);
+                }
+
+                unitofWork.Complete();
+            }
+
+            
+        }
+
+    }
+}
