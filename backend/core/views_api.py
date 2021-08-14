2c0f94d9e5131b263b19a0d435cfab472b4ce411
Talvane A. de Magalhães <talvane.magalhaes@gmail.com>
2021-08-14 13:02:51 -0300

    refactore view_api

--- backend/core/views_api.py
+++ backend/core/views_api.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import action
 
 from .models import MyCoin
 from .serializers import MyCoinSerializer, ConvertSerializer
-from backend.services import ApiCoin, Convert
+from backend.services import Convert
 
 
 class MyCoinViewSet(viewsets.ModelViewSet):
@@ -19,11 +19,11 @@ class MyCoinViewSet(viewsets.ModelViewSet):
         to_coin = request.query_params.get('to')
         amount = float(request.query_params.get('amount', '1'))
 
-        apicoin = ApiCoin()
-        convert = apicoin.set_convert(from_coin, to_coin)
+        convert = Convert(from_coin=from_coin, to_coin=to_coin, amount=amount)
+        # Returns the already converted value
+        value_convert = convert.set_convert()
 
-        if convert:
-            value_convert = (amount * convert[f'{from_coin}_{to_coin}'])
+        if value_convert > 0:
             convert_obj = Convert(
                 from_coin=from_coin, to_coin=to_coin, amount=value_convert
             )
