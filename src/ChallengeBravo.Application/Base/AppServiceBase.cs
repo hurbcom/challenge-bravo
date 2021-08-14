e5c8de0ff3fa52603f53eec12117576995cb7f78
Matheus Oliveira Vidal <matheus.vidal@fgv.br>
2021-08-14 18:58:28 -0300

    Criação de AppServiceBase para interações com a interface do ManagerBase e permitir o CRUD básico pela camada de aplicação.

---
+++ src/ChallengeBravo.Application/Base/AppServiceBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using Abp.Domain.Entities.Auditing;
+using Abp.UI;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChallengeBravo.Base
+{
+    public abstract class AppServiceBase<TEntidade, TInputDto, TOutputDto> : ApplicationService
+        where TEntidade : FullAuditedEntity<Guid>
+        where TInputDto : EntityDto
+        where TOutputDto : EntityDto<Guid>
+    {
+        private readonly IManagerBase<TEntidade> _manager;
+
+        public AppServiceBase(IManagerBase<TEntidade> manager)
+        {
+            _manager = manager;
+        }
+
+        public void Inserir(TInputDto objeto)
+        {
+            var entidade = ObjectMapper.Map<TEntidade>(objeto);
+            _manager.Inserir(entidade);
+        }
+
+        public TOutputDto ObterPorId(Guid guid)
+        {
+            var retorno = _manager.ObterPorId(guid);
+            if (retorno == null)
+                throw new UserFriendlyException(ObterMensagemNaoEncontrado());
+
+            return ObjectMapper.Map<TOutputDto>(retorno);
+        }
+
+        public List<TOutputDto> ObterTodos()
+        {
+            var retorno = _manager.ObterTodos();
+            return ObjectMapper.Map<List<TOutputDto>>(retorno);
+        }
+
+        [HttpDelete]
+        public void ApgarPorId(Guid guid)
+        {
+            _manager.ApgarPorId(guid);
+        }
+
+
+        public abstract string ObterMensagemNaoEncontrado();
+    }
+}
---
+++ src/ChallengeBravo.Application/Base/Consts/MoedasConsts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeBravo.Base.Consts
+{
+    public class MoedasConsts
+    {
+        //Moedas originais
+        public static List<string> listaMoedasOriginais = new List<string> { "USD", "BRL", "EUR", "BTC", "ETH" };
+
+        public static string obterCotacao = "/last/{0}-USD";
+
+        public static string obterTodasAsMoedas = "/json/available/uniq";
+
+
+    }
+}
