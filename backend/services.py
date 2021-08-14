a9c761f75dfd98624e998ed78e783d495686c24d
Talvane A. de Magalhães <talvane.magalhaes@gmail.com>
2021-08-14 13:02:25 -0300

    add list currencies and convert value

--- backend/services.py
+++ backend/services.py
@@ -17,10 +17,31 @@ class ApiCoin():
         else:
             return response.status_code
 
+    def get_currencies(self):
+        response = requests.get(
+            f'{self.api}/currencies?apiKey={self.api_key}'
+        )
+        if response.status_code == 200:
+            return response.json()['results']
+        else:
+            return response.status_code
+
 
 class Convert():
 
     def __init__(self, from_coin, to_coin, amount):
-        self.from_coin = from_coin
-        self.to_coin = to_coin
-        self.amount = amount
+        self._from_coin = from_coin
+        self._to_coin = to_coin
+        self._amount = amount
+
+    def set_convert(self):
+        apicoin = ApiCoin()
+        convert = apicoin.set_convert(self._from_coin, self._to_coin)
+
+        if convert:
+            value_convert = (
+                self._amount * convert[f'{self._from_coin}_{self._to_coin}']
+            )
+            return value_convert
+
+        return 0
