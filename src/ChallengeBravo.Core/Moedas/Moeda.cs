8495d1d2cb78671d2861642264b1ccc7827b701f
Matheus Oliveira Vidal <matheus.vidal@fgv.br>
2021-08-14 18:52:24 -0300

    Criação da classe Moeda, tal como sua migração para a base de dados.

---
+++ src/ChallengeBravo.Core/Moedas/Moeda.cs
@@ -0,0 +1,18 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ChallengeBravo.Moedas
+{
+    public class Moeda : FullAuditedEntity<Guid>
+    {
+        public virtual string Nome { get; set; }
+        public virtual string Codigo { get; set; }
+
+        public virtual float ValorUSD { get; set; }
+        
+    }
+}
---
+++ src/ChallengeBravo.EntityFrameworkCore/Migrations/20210808191456_Create_Moeda.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChallengeBravo.Migrations
+{
+    public partial class Create_Moeda : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Moeda",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Nome = table.Column<string>(type: "text", nullable: true),
+                    Codigo = table.Column<string>(type: "text", nullable: true),
+                    ValorUSD = table.Column<float>(type: "real", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CreatorUserId = table.Column<long>(type: "bigint", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    LastModifierUserId = table.Column<long>(type: "bigint", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    DeleterUserId = table.Column<long>(type: "bigint", nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moeda", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Moeda");
+        }
+    }
+}
