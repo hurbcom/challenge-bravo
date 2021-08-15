2414030ca0168d20909c8fbda83ad9a82f067415
Matheus Oliveira Vidal <matheus.vidal@fgv.br>
2021-08-14 23:37:06 -0300

    Adicionando o log.

---
+++ src/ChallengeBravo.EntityFrameworkCore/Migrations/20210815023421_alteracao_Moeda.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChallengeBravo.Migrations
+{
+    public partial class alteracao_Moeda : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "ValorUSD",
+                table: "Moeda",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "ValorUSD",
+                table: "Moeda",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+        }
+    }
+}
