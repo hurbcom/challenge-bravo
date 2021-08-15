33c01ae3fecf54e83dca7c71302a38e23e7f60b5
Talvane A. de Magalhães <talvane.magalhaes@gmail.com>
2021-08-15 15:07:55 -0300

    add conversion with MyCoin

--- backend/services.py
+++ backend/services.py
@@ -1,6 +1,8 @@
 import requests
 from django.conf import settings
 
+from backend.core.models import MyCoin
+
 
 class ApiCoin():
 
@@ -35,13 +37,36 @@ class Convert():
         self._amount = amount
 
     def set_convert(self):
+        mycoin_from = MyCoin.objects.filter(codecoin=self._from_coin).first()
+        mycoin_price_from = 1
+        if mycoin_from:
+            mycoin_price_from = mycoin_from.price
+            self._from_coin = 'USD'
+
+        mycoin_to = MyCoin.objects.filter(codecoin=self._to_coin).first()
+        mycoin_price_to = 1
+        if mycoin_to:
+            mycoin_price_to = mycoin_to.price
+            self._to_coin = 'USD'
+
+        if mycoin_from and mycoin_to:
+            return (self._amount * (mycoin_price_from / mycoin_price_to))
+
         apicoin = ApiCoin()
         convert = apicoin.set_convert(self._from_coin, self._to_coin)
 
         if convert:
-            value_convert = (
-                self._amount * convert[f'{self._from_coin}_{self._to_coin}']
-            )
+            value_api_coin = convert[f'{self._from_coin}_{self._to_coin}']
+            if mycoin_price_from > 1:
+                value_convert = (self._amount * (
+                    value_api_coin * mycoin_price_from
+                ))
+            elif mycoin_price_to > 1:
+                value_convert = (self._amount * (
+                    value_api_coin / mycoin_price_to
+                ))
+            else:
+                value_convert = (self._amount * value_api_coin)
             return value_convert
 
         return 0
