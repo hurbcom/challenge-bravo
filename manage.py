ff5ce7630e3e4af30cfd6258a935d31721fbd4c1
Talvane A. de Magalhães <talvane.magalhaes@gmail.com>
2021-08-16 13:33:12 -0300

    add debugpy

--- manage.py
+++ manage.py
@@ -2,11 +2,18 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.conf import settings
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+    if settings.DEBUG:
+        if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
+            import debugpy
+            debugpy.listen(4000)
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
