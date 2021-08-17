2ed1da8df96b33fc26177cf54b78c42e84308941
Talvane A. de Magalhães <talvane.magalhaes@gmail.com>
2021-08-17 14:36:14 -0300

    add dj-static for wsgi

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+from dj_static import Cling
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
