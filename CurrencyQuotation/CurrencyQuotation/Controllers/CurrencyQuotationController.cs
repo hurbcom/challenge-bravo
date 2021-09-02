bc02aab33a359e7ce78d10fcf41cd074024ab91c
Vfg2006 <vfg2006@gmail.com>
2021-09-01 22:11:09 -0300

    Criação do endpoint que vai fazer a conversão da moeda

--- CurrencyQuotation/CurrencyQuotation/Controllers/CurrencyQuotationController.cs
+++ CurrencyQuotation/CurrencyQuotation/Controllers/CurrencyQuotationController.cs
@@ -1,6 +1,7 @@
 using CurrencyQuotation.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
 
 namespace CurrencyQuotation.Controllers
 {
@@ -18,5 +19,17 @@ namespace CurrencyQuotation.Controllers
             this._logger = logger;
         }
 
+        [HttpGet]
+        public IActionResult GetQuotation([FromQuery] string from, string to, decimal amount)
+        {
+            this._logger.LogInformation($"INIT - GetQuotation - From: {from}, To: {to} e Amount: {amount}");
+
+            decimal result = this._currencyQuotationService.GetQuotation(from, to, amount);
+            string jsonResult = JsonSerializer.Serialize(result);
+
+            this._logger.LogInformation($"END - GetQuotation");
+
+            return Ok(jsonResult);
+        }
     }
 }
