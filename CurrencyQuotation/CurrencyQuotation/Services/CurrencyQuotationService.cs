791a73a9720bd25d55d95199e6d805c47a4a0937
Vfg2006 <vfg2006@gmail.com>
2021-09-01 22:21:17 -0300

    Criação da classe de serviço com injeção de dependência

---
+++ CurrencyQuotation/CurrencyQuotation/Services/CurrencyQuotationService.cs
@@ -0,0 +1,12 @@
+using CurrencyQuotation.Services.Interfaces;
+
+namespace CurrencyQuotation.Services
+{
+    public class CurrencyQuotationService : ICurrencyQuotationService
+    {
+        public decimal GetQuotation(string from, string to, decimal amount)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
