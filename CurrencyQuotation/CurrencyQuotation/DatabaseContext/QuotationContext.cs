2546b7dff9972cc54f417f38ec7cd642366b42ac
Vfg2006 <vfg2006@gmail.com>
2021-09-02 21:04:44 -0300

    Adição do modelo no Context e definição de precisão no campo decimal

--- CurrencyQuotation/CurrencyQuotation/DatabaseContext/QuotationContext.cs
+++ CurrencyQuotation/CurrencyQuotation/DatabaseContext/QuotationContext.cs
@@ -1,3 +1,4 @@
+using CurrencyQuotation.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CurrencyQuotation.DatabaseContext
@@ -6,6 +7,15 @@ namespace CurrencyQuotation.DatabaseContext
     {
         public QuotationContext(DbContextOptions<QuotationContext> options) : base(options)
         {
+        }
+
+        public DbSet<Currency> Currency { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Currency>()
+                .Property(o => o.DolarAmount)
+                .HasPrecision(22, 10);
         }
     }
 }
