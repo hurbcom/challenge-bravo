3761d0533c5857d8ea0188ce113a2a639b42325c
Vfg2006 <vfg2006@gmail.com>
2021-09-02 21:29:38 -0300

    Adição do DAO via injeção de dependência na classe de serviço

--- CurrencyQuotation/CurrencyQuotation/Services/CurrencyQuotationService.cs
+++ CurrencyQuotation/CurrencyQuotation/Services/CurrencyQuotationService.cs
@@ -1,12 +1,21 @@
+using CurrencyQuotation.Daos.Interfaces;
 using CurrencyQuotation.Services.Interfaces;
 
 namespace CurrencyQuotation.Services
 {
     public class CurrencyQuotationService : ICurrencyQuotationService
     {
+        private readonly ICurrencyQuotationDao _currencyQuotationDao;
+
+        public CurrencyQuotationService(ICurrencyQuotationDao currencyQuotationDao)
+        {
+            this._currencyQuotationDao = currencyQuotationDao;
+        }
+
         public decimal GetQuotation(string from, string to, decimal amount)
         {
-            throw new System.NotImplementedException();
+            decimal result = this._currencyQuotationDao.GetQuotationByCurrencies(from, to, amount);
+            return result;
         }
     }
 }
