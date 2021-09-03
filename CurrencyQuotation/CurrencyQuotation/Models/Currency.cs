01af3ec9a6ba6ba6d9b8dc53f8df5cc00ccf5679
Vfg2006 <vfg2006@gmail.com>
2021-09-02 21:03:50 -0300

    Criação da classe modelo que vai representar a moeda

---
+++ CurrencyQuotation/CurrencyQuotation/Models/Currency.cs
@@ -0,0 +1,27 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CurrencyQuotation.Models
+{
+    [Table("currency")]
+    public class Currency
+    {
+        [Key]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("dolar_amount")]
+        public decimal DolarAmount { get; set; }
+
+        [Column("creation_date")]
+        public DateTime CreationDate { get; set; }
+
+        [Column("last_update")]
+        public DateTime LastUpdate { get; set; }
+    }
+}
