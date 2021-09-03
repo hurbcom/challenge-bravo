fbfe5a780b8cbdaa204506e881279081187bdf71
Vfg2006 <vfg2006@gmail.com>
2021-09-02 21:28:09 -0300

    Adição do contexto na classe DAO via injeção de dependência

--- CurrencyQuotation/CurrencyQuotation/Daos/CurrencyQuotationDao.cs
+++ CurrencyQuotation/CurrencyQuotation/Daos/CurrencyQuotationDao.cs
@@ -1,8 +1,15 @@
 using CurrencyQuotation.Daos.Interfaces;
+using CurrencyQuotation.DatabaseContext;
 
 namespace CurrencyQuotation.Daos
 {
     public class CurrencyQuotationDao : ICurrencyQuotationDao
     {
+        public readonly QuotationContext _context;
+
+        public CurrencyQuotationDao(QuotationContext context)
+        {
+            this._context = context;
+        }
     }
 }
