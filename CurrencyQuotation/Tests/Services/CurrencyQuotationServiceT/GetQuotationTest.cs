26a3b0a294c616270d4f0ece4d4cb1ff03301b6b
Vfg2006 <vfg2006@gmail.com>
2021-09-08 22:47:07 -0300

    Criação do projeto de testes

---
+++ CurrencyQuotation/Tests/Services/CurrencyQuotationServiceT/GetQuotationTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Tests.Services.CurrencyQuotationServiceT
+{
+    public class GetQuotationTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
