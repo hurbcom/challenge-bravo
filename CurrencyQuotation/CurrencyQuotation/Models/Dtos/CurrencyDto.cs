e423f9d4d90b3156cb8656238b270677a53627bc
Vfg2006 <vfg2006@gmail.com>
2021-09-10 21:04:11 -0300

    Adição de campo required na inserção

--- CurrencyQuotation/CurrencyQuotation/Models/Dtos/CurrencyDto.cs
+++ CurrencyQuotation/CurrencyQuotation/Models/Dtos/CurrencyDto.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CurrencyQuotation.Models.Dtos
 {
     public class CurrencyDto
     {
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public decimal Amount { get; set; }
 
         public string BaseQuotation { get; set; }
