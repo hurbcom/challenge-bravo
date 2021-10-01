a9e2010391f09a5c9934fdc34b4a9398896240e0
Fernando da Silva Sousa <wild.nando@gmail.com>
2021-10-01 18:59:58 -0300

    Update currency model with setters and getters

--- src/Model/Currency.php
+++ src/Model/Currency.php
@@ -2,27 +2,66 @@
 
 declare(strict_types=1);
 
-namespace App\Currency;
+namespace App\Model;
+
+use DateTimeInterface;
+use Money\Money;
 
 class Currency
 {
-    public function getCode()
+    private string $code;
+    private Money $value;
+    private string $source;
+    private DateTimeInterface $createdAt;
+    private DateTimeInterface $updatedAt;
+
+    public function getCode(): string
     {
-        //
+        return $this->code;
     }
 
-    public function getValue()
+    public function getValue(): Money
     {
-        //
+        return $this->value;
     }
 
-    public function getSource()
+    public function getSource(): string
     {
-        //
+        return $this->source;
     }
 
-    public function getUpdatedAt()
+    public function getCreatedAt(): DateTimeInterface
     {
-        //
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt(): DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setCode(string $code)
+    {
+        $this->code = $code;
+    }
+
+    public function setValue(Money $value)
+    {
+        $this->value = $value;
+    }
+
+    public function setSource(string $source)
+    {
+        $this->source = $source;
+    }
+
+    public function setCreatedAt(DateTimeInterface $createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function setUpdatedAt(DateTimeInterface $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
     }
 }
