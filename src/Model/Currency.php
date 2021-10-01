8b8b2a65f3a145bc2e6f210a4d06900bd17208f9
Fernando da Silva Sousa <wild.nando@gmail.com>
2021-10-01 18:37:21 -0300

    Adds basic currency model

---
+++ src/Model/Currency.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Currency;
+
+class Currency
+{
+    public function getCode()
+    {
+        //
+    }
+
+    public function getValue()
+    {
+        //
+    }
+
+    public function getSource()
+    {
+        //
+    }
+
+    public function getUpdatedAt()
+    {
+        //
+    }
+}
