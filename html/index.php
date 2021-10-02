8b5561d4464451f827b7573c6e12b10a3b171220
Fernando da Silva Sousa <wild.nando@gmail.com>
2021-10-02 15:57:23 -0300

    Add bootstrap file

--- html/index.php
+++ html/index.php
@@ -2,7 +2,7 @@
 
 use App\Application;
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/bootstrap.php';
 require __DIR__ . '/../src/Application.php';
 
 $app = new Application();
---
+++ src/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+set_error_handler(
+    function ($severity, $message, $file, $line) {
+        throw new ErrorException($message, $severity, $severity, $file, $line);
+    }
+);
+
+require __DIR__ . '/../vendor/autoload.php';
