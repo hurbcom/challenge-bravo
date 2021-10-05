03f0e6f8ee3170ad32577b9fc71d72051cd5c704
Fernando da Silva Sousa <wild.nando@gmail.com>
2021-10-05 15:36:44 -0300

    Add routes to currency controller

---
+++ src/Controller/CurrencyController.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Model\Currency;
+use App\Repository\CurrencyRepositoryInterface;
+use App\Service\CurrencyUpdater\OpenExchangeRatesUpdater;
+use Brick\Math\BigDecimal;
+use Exception;
+use Laminas\Diactoros\Response\JsonResponse;
+use League\Route\Http\Exception\NotFoundException;
+use League\Route\Http\Exception\UnprocessableEntityException;
+use PDOException;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class CurrencyController
+{
+    public function __construct(
+        protected ResponseFactoryInterface $responseFactory,
+        protected CurrencyRepositoryInterface $currencyRepository
+    ) {}
+
+    public function list(ServerRequestInterface $request, $args): array
+    {
+        // Map each currency ($x) to array
+        return array_map(
+            fn ($x) => $x->toArray(),
+            $this->currencyRepository->getAll()
+        );
+    }
+
+    public function get(ServerRequestInterface $request, $args): array
+    {
+        $code = Currency::normalizeCode($args['code']);
+        $currency = $this->currencyRepository->get($code);
+
+        if (!$currency) {
+            throw new NotFoundException();
+        }
+
+        return $currency->toArray();
+    }
+
+    public function set(ServerRequestInterface $request, $args): array
+    {
+        $code = Currency::normalizeCode($args['code']);
+        $input = json_decode((string) $request->getBody(), true);
+
+        $filledAmount = !empty($input['amount']);
+        $filledSource = !empty($input['source']);
+
+        // Note: Assign operator has precedence over xor, don't remove parentesis
+        $filledOnlyAmountOrSource = ($filledAmount xor $filledSource);
+
+        if (!$filledOnlyAmountOrSource) {
+            $parametersCount = (int) $filledAmount + (int) $filledSource;
+            throw new UnprocessableEntityException(
+                "Parameters amount or source must be present. {$parametersCount} of them found."
+            );
+        }
+
+        $currency = $this->currencyRepository->get($code) ?? Currency::create($code);
+
+        if ($filledAmount) {
+            $value = BigDecimal::of($input['amount']);
+            $currency->setValue($value);
+            $currency->setSource('static');
+        }
+
+        if ($filledSource) {
+            $oerId = OpenExchangeRatesUpdater::getId();
+
+            if ($input['source'] === $oerId) {
+                $currency->setValue(BigDecimal::zero());
+                $currency->setSource($oerId);
+            } else {
+                throw new UnprocessableEntityException("Source not compatible.");
+            }
+        }
+
+        try {
+            $this->currencyRepository->set($currency);
+        } catch (PDOException $pe) {
+            throw new Exception("Internal Server Error: Storage error ({$pe->getMessage()})", 0, $pe);
+        }
+
+        return $currency->toArray();
+    }
+
+    public function delete(ServerRequestInterface $request, $args)
+    {
+        $code = Currency::normalizeCode($args['code']);
+        $deleted = $this->currencyRepository->delete($code);
+
+        if (!$deleted) {
+            throw new NotFoundException();
+        }
+
+        return new JsonResponse(null, 204);
+    }
+}
--- src/routes.php
+++ src/routes.php
@@ -6,7 +6,15 @@
  * @var \League\Route\Router $router
  */
 
-use App\Controller\HelloWorld;
+use App\Controller\ConverterController;
+use App\Controller\CurrencyController;
 
 // map a route
-$router->map('GET', '/', HelloWorld::class);
+$router->get('/', [ConverterController::class, 'index']);
+$router->get('/api/v1/converter', [ConverterController::class, 'index']);
+
+// Currency control
+$router->get('/api/v1/currencies', [CurrencyController::class, 'list']);
+$router->get('/api/v1/currencies/{code}', [CurrencyController::class, 'get']);
+$router->put('/api/v1/currencies/{code}', [CurrencyController::class, 'set']);
+$router->delete('/api/v1/currencies/{code}', [CurrencyController::class, 'delete']);
