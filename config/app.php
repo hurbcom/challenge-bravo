46c915c38a25752b8491ade16183f9e4db6294e8
Thiago Luna <thiagoluna2@gmail.com>
2021-10-06 10:55:15 -0300

    Routes, Register Providers and Seedes

--- config/app.php
+++ config/app.php
@@ -174,6 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\RepositoryServiceProvider::class,
+        App\Providers\HttpClientServiceProvider::class,
 
     ],
 
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CurrencyController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::prefix('v1')->group(function () {
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    //Currency
+    Route::get('currencies/convert-amount', [CurrencyController::class, 'convertAmount']);
+    Route::apiResource('currencies', CurrencyController::class, ['except' => ['update']]);
 });
