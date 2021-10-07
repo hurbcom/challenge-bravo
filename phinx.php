09fda083b1de2e5c0dc1acc9cdad0cedd48ccfa0
Fernando da Silva Sousa <wild.nando@gmail.com>
2021-10-07 12:37:28 -0300

    Fix phinx config directories path setup

--- phinx.php
+++ phinx.php
@@ -3,8 +3,8 @@
 return
 [
     'paths' => [
-        'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
-        'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+        'migrations' => __DIR__ . '/db/migrations',
+        'seeds' => __DIR__ . '/db/seeds'
     ],
     'environments' => [
         'default_migration_table' => 'phinxlog',
