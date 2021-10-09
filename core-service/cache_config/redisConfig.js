296594bb50aed2280ede588584ed70f54f0ae17d
fabio.alves <fabio.alves@exablack.com>
2021-10-09 17:40:49 -0300

    Getting redis data and responding properly the request

---
+++ core-service/cache_config/redisConfig.js
@@ -0,0 +1,8 @@
+const redis = require("redis");
+const client  = redis.createClient(7001,"127.0.0.1");
+
+client.on('connect', function() {
+    console.log('Connected');
+  });
+
+module.exports = client
--- core-service/index.js
+++ core-service/index.js
@@ -1,53 +1,27 @@
 const express = require("express");
 const app = express();
-const axios = require('axios').default;
 const port = 3000;
+const redis = require("./cache_config/redisConfig")
 
 app.get("/", async function(req,res){
-    const data = await getCurrency();
-    const currencies = {
-        USDUSD: 1,
-        BRLUSD: data.BRLUSD.bid,
-        EURUSD: data.EURUSD.bid,
-        BTCUSD: data.BTCUSD.bid,
-        ETHUSD: data.ETHUSD.bid,
-        BRLBRL: 1,
-        USDBRL: data.USDBRL.bid,
-        EURBRL: data.EURBRL.bid,
-        BTCBRL: data.BTCBRL.bid,
-        ETHBRL: data.ETHBRL.bid,
-        EUREUR: 1,
-        USDEUR: data.USDEUR.bid,
-        BRLEUR: data.BRLEUR.bid,
-        BTCEUR: data.BTCEUR.bid,
-        ETHEUR: data.ETHEUR.bid,
-        //Como as criptoes não possuem cotação na api, é necessário fazer uma manipulação.
-        BTCBTC: 1,
-        USDBTC: 1/data.BTCUSD.bid,
-        BRLBTC: 1/data.BTCBRL.bid,
-        EURBTC: 1/data.BTCEUR.bid,
-        ETHBTC: data.ETHUSD.bid/data.BTCUSD.bid,
-        ETHETH: 1,
-        USDETH: 1/data.ETHUSD.bid,
-        BRLETH: 1/data.ETHBRL.bid,
-        EURETH: 1/data.ETHEUR.bid,
-        BTCETH: data.BTCUSD.bid/data.ETHUSD.bid
-    }
+
     const requestInfo = {
         from: req.query.from,
         to: req.query.to,
         amount: req.query.amount
     }
 
-
-    const ticket = requestInfo.from + requestInfo.to
-    res.json(
-        {
-            "data": currencies[ticket] * requestInfo.amount,
-            "from": requestInfo.from,
-            "to": requestInfo.to
-
-        });
+    await redis.get("currencies", (err, reply) => {
+        const data = JSON.parse(reply);
+        const ticket = requestInfo.from + requestInfo.to
+        res.json(
+            {
+                "data": data[ticket] * requestInfo.amount,
+                "from": requestInfo.from,
+                "to": requestInfo.to
+    
+            });
+    })
 
 
 })
@@ -59,18 +33,6 @@ app.get("/", async function(req,res){
 app.listen(port, () => {
     console.log("Listening on port: " + port);
 })
-
-
-
-async function getCurrency() {
-    try {
-      const response = await axios.get(`https://economia.awesomeapi.com.br/last/BRL-USD,EUR-USD,BTC-USD,ETH-USD,USD-BRL,EUR-BRL,BTC-BRL,ETH-BRL,USD-EUR,BRL-EUR,BTC-EUR,ETH-EUR`);
-      return response.data;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
 
 
 
