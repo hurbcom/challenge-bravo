4305470ff86a6d02835876734f3cba561104d2fe
fabio.alves <fabio.alves@exablack.com>
2021-10-10 14:17:32 -0300

    Implemented create/remove currency

--- core-service/index.js
+++ core-service/index.js
@@ -5,6 +5,7 @@ const redis = require("./cache_config/redisConfig")
 const areCurrenciesExist = require("./service/sanitaze")
 const requestInfoService = require("./service/requestInfoService")
 const exchangeService = require("./service/exchangeService")
+app.use(express.json());
 
 app.get("/", function(req,res){
     redis.get("currencies", (err, reply) => {
@@ -19,10 +20,50 @@ app.get("/", function(req,res){
                     "data": exchange * requestInfo.amount,
                     "from": requestInfo.from,
                     "to": requestInfo.to
-        
                 });
         }else{
             res.json({"Error": "Currency exchange not supported"})
+        }
+
+        
+    })
+})
+
+app.post("/", async function(req,res){
+    redis.get("currencies", (err, reply) => {
+
+        var data = JSON.parse(reply);
+        const pairUsd = data[req.body.exchangePairName] * req.body.exchangePairValue;
+        data[req.body.currencyName] = pairUsd;
+
+        const insertCurrency = redis.set("currencies", JSON.stringify(data));
+
+        if(insertCurrency){
+            res.json({
+                "CurrencyCreated": req.body.currencyName,
+                "ExchangeUSDpair": pairUsd
+            });
+        }else{
+            res.json({
+                "Error": "Currency could not be created"
+            });
+        }
+    })
+})
+
+app.delete("/", function(req,res){
+    redis.get("currencies", (err, reply) => {
+        
+        const data = JSON.parse(reply);
+        const requestInfo = requestInfoService(req);
+
+        if(data.hasOwnProperty(req.query.currencyName)){
+            delete data[req.query.currencyName];
+            const updateExchangePairs = redis.set("currencies", JSON.stringify(data));
+            res.json({"message": "Currency removed"})
+
+        }else{
+            res.json({"message": "Currency does not exist"})
         }
 
         
