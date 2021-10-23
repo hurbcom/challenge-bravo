7971cbcd82162d460b98e00898b1380347f27352
felipepestana <31418984+felipepestana@users.noreply.github.com>
2021-10-22 21:26:20 -0300

    Adding SharedUtils with main object

---
+++ SharedLibrary/Models/CurrencyObject.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SharedLibrary.Models
+{
+    [Table("Currencies")]
+    public class CurrencyObject
+    {
+        [Key]
+        [Required]
+        public string Name              { get; set; }
+        public double Price             { get; set; }
+        public bool   AutoUpdateRate    { get; set; }
+        public bool   DefaultCurrency   { get; set; }
+    }
+}
