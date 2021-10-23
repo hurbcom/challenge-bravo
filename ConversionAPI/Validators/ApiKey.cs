14e3a2c2f3d55bed4cbb56cbccc703c4b7fa5a05
felipepestana <31418984+felipepestana@users.noreply.github.com>
2021-10-22 21:30:03 -0300

    Adding Validator and ApiKey classes

---
+++ ConversionAPI/Validators/ApiKey.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Threading.Tasks;
+
+namespace CurrencyAPI.Validators
+{
+    [AttributeUsage(validOn: AttributeTargets.Class | AttributeTargets.Method)]
+    public class ApiKey : Attribute, IAsyncActionFilter
+    {
+        private const string APIKEYNAME = "ApiKey";
+
+        // Validate if the request has the expected key
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            StringValues extractedApiKey = String.Empty;
+            if (!context.HttpContext.Request.Query.TryGetValue(APIKEYNAME, out extractedApiKey))
+                context.HttpContext.Request.Headers.TryGetValue(APIKEYNAME, out extractedApiKey);
+
+            if (extractedApiKey == StringValues.Empty)
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode  = 401,
+                    Content     = "ApiKey was not Found"
+                };
+                return;
+            }
+
+            IConfiguration appSettings  = context.HttpContext.RequestServices.GetService<IConfiguration>();
+            string apiKey               = appSettings.GetValue<string>(APIKEYNAME);
+
+            if (!apiKey.Equals(extractedApiKey))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode  = 403,
+                    Content     = "Unauthorized Access"
+                };
+                return;
+            }
+
+            await next();
+        }
+    }
+}
---
+++ ConversionAPI/Validators/RequestValidator.cs
@@ -0,0 +1,49 @@
+﻿using CurrencyAPI.Models;
+using System;
+
+namespace CurrencyAPI.Validators
+{
+    public static class RequestValidator
+    {
+        // Validate expected fields for each request type
+        public static bool CurrencyValidation(CurrencyBaseRequestObject request, out string errorMessage)
+        {
+            errorMessage = String.Empty;
+            if (String.IsNullOrWhiteSpace(request.Name))
+                errorMessage = "Could not find expected field with currency name";
+
+            if (!String.IsNullOrWhiteSpace(errorMessage))
+                return false;
+            else
+                return true;
+        }
+
+        public static bool CurrencyChangeValidation(CurrencyChangeObject request, out string errorMessage)
+        {
+            errorMessage = String.Empty;
+            if (String.IsNullOrWhiteSpace(request.Name))
+                errorMessage = "Could not find expected field with currency name";
+            else if (request.RateValue <= 0)
+                errorMessage = "Invalid amount received. Please use a positive value";
+
+            if (!String.IsNullOrWhiteSpace(errorMessage))
+                return false;
+            else
+                return true;
+        }
+
+        public static bool ConversionValidation(ConversionRequestObject request, out string errorMessage)
+        {
+            errorMessage = String.Empty;
+            if (String.IsNullOrWhiteSpace(request.From) || String.IsNullOrWhiteSpace(request.To))
+                errorMessage = "Could not find expected fields with currency information";
+            else if (request.Amount <= 0)
+                errorMessage = "Invalid amount received. Please use a positive value";
+
+            if (!String.IsNullOrWhiteSpace(errorMessage))
+                return false;
+            else
+                return true;
+        }
+    }
+}
