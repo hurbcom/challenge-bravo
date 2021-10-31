406064d7ab74fcd00a46a18384c160b08383ffb6
Diego Diógenes <diediogenes@gmail.com>
2021-10-31 20:27:17 -0300

    feat(schemas): add pydantic models

--- app/schemas/currency.py
+++ app/schemas/currency.py
@@ -1,6 +1,28 @@
+from typing import Optional
 from pydantic import BaseModel
 
 
-class Currency(BaseModel):
+class CurrencyBase(BaseModel):
+    code: Optional[str]
+    rate: Optional[float]
+
+
+class CurrencyCreate(CurrencyBase):
     code: str
     rate: float
+
+
+class CurrencyUpdate(CurrencyBase):
+    pass
+
+
+class CurrencyInDB(CurrencyBase):
+    code: str
+    rate: float
+
+    class Config:
+        orm_mode = True
+
+
+class Currency(CurrencyInDB):
+    pass
