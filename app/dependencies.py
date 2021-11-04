2492d1626c9cd21d78004a606ef0eb724207dbee
Diego Diógenes <diediogenes@gmail.com>
2021-11-03 22:37:48 -0300

    feat: add dict responses

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,5 +1,7 @@
 from typing import Generator
 
+from starlette import status
+
 from app.db.session import SessionLocal
 
 
@@ -9,3 +11,14 @@ def get_db() -> Generator:
         yield db
     finally:
         db.close()
+
+
+def responses() -> dict:
+    return {
+        status.HTTP_404_NOT_FOUND: {
+            "description": "Currency Not Found"
+        },
+        status.HTTP_400_BAD_REQUEST: {
+            "description": "Currency with this code already Exists"
+        }
+    }
