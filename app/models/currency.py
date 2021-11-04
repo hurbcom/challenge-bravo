bfc72c3f755d51128c89d89ead800985b883c094
Diego Diógenes <diediogenes@gmail.com>
2021-11-03 22:36:16 -0300

    feat: add currency code as unique

--- app/models/currency.py
+++ app/models/currency.py
@@ -5,5 +5,5 @@ from app.db.base_class import Base
 
 class Currency(Base):
     id = Column(Integer, primary_key=True, index=True)
-    code = Column(String, index=True)
+    code = Column(String, index=True, unique=True)
     rate = Column(Float(precision=10, asdecimal=True, decimal_return_scale=2))
