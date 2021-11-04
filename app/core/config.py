802c6b14b30ac946c2b82d0fca0b99ca01bf58bd
Diego Diógenes <diediogenes@gmail.com>
2021-11-03 22:34:42 -0300

    feat: add new envs to create test datatable

--- app/core/config.py
+++ app/core/config.py
@@ -6,6 +6,9 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     API_EXCHANGE_URL: str = os.getenv("API_EXCHANGE_URL")
     API_EXCHANGE_KEY: str = os.getenv("API_EXCHANGE_KEY")
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER")
     SQLALCHEMY_DATABASE_URL: str = os.getenv('DATABASE_URL')
 
     class Config:
