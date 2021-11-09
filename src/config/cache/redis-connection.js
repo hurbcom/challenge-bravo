e313c4a2b6aa1d5034007247a748dbe5cab26e9b
Eduardo José Santos de Jesus <duudu952@gmail.com>
2021-11-09 18:10:33 -0300

    refactor: change redis connection params to set redis endpoint by node enviroment

--- src/config/cache/redis-connection.js
+++ src/config/cache/redis-connection.js
@@ -5,7 +5,7 @@ const redis = require('redis')
 function create() {
     try{
         const client = redis.createClient({
-            host: process.env.REDIS_ENDPOINT || '127.0.0.1',
+            host: process.env.NODE_ENV == 'production' ? process.env.REDIS_ENDPOINT : '127.0.0.1',
             port: process.env.REDIS_PORT || 6379
         })
         console.log("\x1b[35m", 'Connected to Redis')
