5f5085bacc3ced7b61d0dcc1cc3a0bfe3d510a95
Yan Mello <yan_mvd@hotmail.com>
2021-12-02 14:52:17 -0300

    chore: pre-config jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    roots: ['<rootDir>/src'],
+    collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
+    coverageDirectory: 'coverage',
+    testEnvironment: 'node',
+    preset: '@shelf/jest-mongodb',
+    transform: {
+      '.*\\.ts$': 'ts-jest'
+    }
+  }
