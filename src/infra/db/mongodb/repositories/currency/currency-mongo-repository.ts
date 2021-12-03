3d5094b94d5dbdabb730c2ecd651d3d39c0d00bb
Yan Mello <yan_mvd@hotmail.com>
2021-12-03 12:12:40 -0300

    feat: ensure currencyMongoRepository implements upsertCurrency

--- src/infra/db/mongodb/repositories/currency/currency-mongo-repository.ts
+++ src/infra/db/mongodb/repositories/currency/currency-mongo-repository.ts
@@ -1,12 +1,20 @@
 import { AddCurrencyRepository } from '../../../../../data/protocols/db/currency/add-currency-repository'
+import { UpsertCurrency } from '../../../../../data/protocols/db/currency/upsert-currency-repository'
 import { CurrencyModel } from '../../../../../domain/models/currency'
 import { MongoHelper } from '../../helpers/mongo-helper'
 
-export class CurrencyMongoRepository implements AddCurrencyRepository {
+export class CurrencyMongoRepository implements AddCurrencyRepository, UpsertCurrency {
     public static readonly currencyCollection = 'curencies'
+
     async add (currency: CurrencyModel): Promise<boolean> {
       const collection = await MongoHelper.getCollection(CurrencyMongoRepository.currencyCollection)
       const result = await collection.insertOne(currency)
       return result.acknowledged
+    }
+
+    async upsert (currency: CurrencyModel): Promise<boolean> {
+      const collection = await MongoHelper.getCollection(CurrencyMongoRepository.currencyCollection)
+      const result = await collection.findOneAndUpdate({ shortName: currency.shortName }, { $set: { ...currency } }, { upsert: true })
+      return result.ok === 1
     }
 }
