973ed15a8e432ccbf3f4a776496af42036264aa1
Yan Mello <yan_mvd@hotmail.com>
2021-12-06 13:55:53 -0300

    feat: exposing add currency controller on route /currency with method POST

--- src/main/routes/currency-routes.ts
+++ src/main/routes/currency-routes.ts
@@ -3,5 +3,5 @@ import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddCurrencyController } from '../factories/controllers/add-currency/add-currency-controller-factory'
 
 export default (router: Router): void => {
-  router.post('/address', adaptRoute(makeAddCurrencyController()))
+  router.post('/currency', adaptRoute(makeAddCurrencyController()))
 }
