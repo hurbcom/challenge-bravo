1958759a053bc15e121ad2f003e3417a8768bac1
Yan Mello <yan_mvd@hotmail.com>
2021-12-06 17:25:57 -0300

    feat: validating if USDValue is numeric and shortName does not contains spaces

--- src/main/factories/controllers/add-currency/add-currency-validation-factory.ts
+++ src/main/factories/controllers/add-currency/add-currency-validation-factory.ts
@@ -1,10 +1,15 @@
 import { Validation } from '../../../../presentation/controllers/protocols/validation'
+import { NumericFieldValidation } from '../../../../validation/validators/numeric-field-validation'
 import { RequiredFieldValidation } from '../../../../validation/validators/required-field-validation'
+import { StringWithoutSpaceValidation } from '../../../../validation/validators/string-without-space-validation'
 import { ValidationComposite } from '../../../../validation/validators/validiation-composite'
 
 export const makeAddCurrencyValidation = ():Validation => {
   const validations = []
   validations.push(new RequiredFieldValidation('shortName'))
+  validations.push(new StringWithoutSpaceValidation('shortName'))
   validations.push(new RequiredFieldValidation('USDvalue'))
+  validations.push(new NumericFieldValidation('USDvalue'))
+
   return new ValidationComposite(validations)
 }
