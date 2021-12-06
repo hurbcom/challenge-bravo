8bf43b2dafdc9d6e2cc3da34891088d314954658
Yan Mello <yan_mvd@hotmail.com>
2021-12-06 12:50:48 -0300

    refactor: just fix style

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    mongodbMemoryServerOptions: {
-      instance: {
-        dbName: 'jest'
-      },
-      binary: {
-        version: '5.0.3',
-        skipMD5: true
-      },
-      autoStart: false
-    }
+  mongodbMemoryServerOptions: {
+    instance: {
+      dbName: 'jest'
+    },
+    binary: {
+      version: '5.0.3',
+      skipMD5: true
+    },
+    autoStart: false
   }
+}
--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    roots: ['<rootDir>/src'],
-    collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
-    coverageDirectory: 'coverage',
-    testEnvironment: 'node',
-    preset: '@shelf/jest-mongodb',
-    transform: {
-      '.*\\.ts$': 'ts-jest'
-    }
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '.*\\.ts$': 'ts-jest'
   }
+}
