74e3fb983ab6b378a245f3669e3b7f743bf5c0bd
Yan Mello <yan_mvd@hotmail.com>
2021-12-06 12:17:18 -0300

    chore: ensure server will wait database connection to start

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,8 @@
-import app from './config/app'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 import env from './config/env'
 
-app.listen(env.port, () => console.log(`Server running on port http://localhost:${env.port}`))
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = require('./config/app').default
+    app.listen(env.port, () => console.log(`Server running on port http://localhost:${env.port}`))
+  }).catch(console.error)
