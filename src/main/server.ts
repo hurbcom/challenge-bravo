fe3a347531ca44f85b6346f2aa8fabe76e8d0a55
Yan Mello <yan_mvd@hotmail.com>
2021-12-07 18:56:50 -0300

    feat: running scheduler on app start

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,8 +1,12 @@
 import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 import env from './config/env'
+import { scheduledJobs } from './factories/jobs'
 
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
     const app = require('./config/app').default
+    // schedule jobs
+    await require('./scheduled/jobs').default(scheduledJobs)
+
     app.listen(env.port, () => console.log(`Server running on port http://localhost:${env.port}`))
   }).catch(console.error)
