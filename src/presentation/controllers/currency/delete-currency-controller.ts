db6069e21c20290d894ed25a62d650d0f1c7c1d7
Yan Mello <yan_mvd@hotmail.com>
2021-12-07 13:20:25 -0300

    feat: new USD can be deleted

--- src/presentation/controllers/currency/delete-currency-controller.ts
+++ src/presentation/controllers/currency/delete-currency-controller.ts
@@ -1,6 +1,5 @@
 import { RemoveCurrency } from '../../../domain/usecases/delete-currency'
-import { BusinessRuleError } from '../../errors/business-rule-error'
-import { badRequest, ok, serverError } from '../../helpers/http'
+import { ok, serverError } from '../../helpers/http'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 import { Controller } from '../protocols/controller'
 
@@ -13,9 +12,6 @@ export class DeleteCurrencyController implements Controller {
     try {
       let shortName = httpRequest.params.shortName
       shortName = (shortName as string).toUpperCase()
-      // we cannot delete our base coin
-      if (shortName === 'USD') return badRequest(new BusinessRuleError('USD cannot be deleted'))
-
       const deleted = await this.deleteCurrency.delete(shortName)
       return ok({ deleted })
     } catch (e) {
