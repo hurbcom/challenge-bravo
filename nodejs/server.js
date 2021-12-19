11d77a83ca623fb6d4a0a871dc58d55ae43eb0f1
Leonardo <leonardo.neves@bennu.tv>
2021-12-19 19:17:34 -0300

    Organize structure to node

---
+++ nodejs/server.js
@@ -0,0 +1,63 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const prepareDatabase = async (BRL = null) => {
+    console.log(`UPDATING DATABASE ${(new Date()).toISOString()}`)
+
+    const awesomeapi = require('./src/services/awesomeapi');
+    const CurrencyModel = require('./src/models/currency');
+    const { setCache } = require('./src/services/redis');
+
+    const currencies = await awesomeapi.all();
+    const codes = Object.keys(currencies);
+    if (codes && codes.length > 0) {
+        for (let i = 0; i < codes.length; i++) {
+            const { code, bid } = currencies[codes[i]];
+            try {
+                console.log(`> CREATE/UPDATE REGISTER ${code} ${bid}`);
+                await CurrencyModel.newCurrency(code, bid);
+            } catch (error) {
+                console.log(`🚀 ~ file: server.js ~ line 26 ~ prepareDatabase ~ error`, error);
+            }
+        }
+    }
+    if (BRL) {
+        try {
+            console.log(`> CREATE/UPDATE REGISTER BRL 1`);
+            await CurrencyModel.newCurrency('BRL', 1);
+        } catch (error) {
+            console.log(`🚀 ~ file: server.js ~ line 36 ~ prepareDatabase ~ error`, error);
+        }
+    }
+
+    const results = await CurrencyModel.getAll();
+    await setCache('ALL_CURRENCIES', results);
+
+    setInterval(() => {
+        prepareDatabase();
+    }, 1000 * 60 * 60);
+}
+prepareDatabase(true);
+
+const accessProtectionMiddleware = async (req, res, next) => {
+    next();
+};
+
+require('./src/routes/main')(app, accessProtectionMiddleware);
+require('./src/routes/swagger')(app, accessProtectionMiddleware);
+
+app.listen(3000);
