e110db18d98624849e716457d8422233430de4d9
André Gustavo de Andrade <andre.andrade@agvalue.com.br>
2021-12-19 12:43:24 -0300

    Enhancements to the parameter handling
    - Parameter validation
    - Code clean up

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -10,12 +10,6 @@ import (
 	"strings"
 )
 
-// Default values
-const (
-	defaultPort = 8080
-	defaultHost = "localhost"
-)
-
 // Server configuration parameters
 var config server.Config
 
@@ -25,40 +19,29 @@ var dbConnString = ""
 // Cache connection string
 var cacheConnString = ""
 
+// Constants
 const (
-	EnvPortKey     = "BRAVO_PORT"
-	EnvHostKey     = "BRAVO_HOST"
-	EnvCertFileKey = "BRAVO_CERT_FILE"
-	EnvKeyFileKey  = "BRAVO_KEY_FILE"
-	EnvDBKey       = "BRAVO_DB"
-	EnvCacheKey    = "BRAVO_CACHE"
+	EnvPortKey     = "BRAVO_PORT"      // EnvPortKey Server port number environment variable name
+	EnvHostKey     = "BRAVO_HOST"      // EnvHostKey Server host name environment variable name
+	EnvCertFileKey = "BRAVO_CERT_FILE" // EnvCertFileKey Server certificate file path environment variable name
+	EnvKeyFileKey  = "BRAVO_KEY_FILE"  // EnvKeyFileKey Server certificate key file path environment variable name
+	EnvDBKey       = "BRAVO_DB"        // EnvDBKey Database connection string (postgres) environment variable name
+	EnvCacheKey    = "BRAVO_CACHE"     // EnvCacheKey Cache connection string (redis) environment variable name
+
+	defaultPort = 8080        // defaultPort Default server port
+	defaultHost = "localhost" // defaultHost Default server host
+	version     = "1.0.0"     // version Bravo version
 )
 
 // main Program entry point
 func main() {
 
-	// Print welcome message
-	fmt.Print("Bravo Currency Converter Server v.1.0\n\n")
+	// Read the program's configuration
+	readConfig()
 
-	// Parse command line parameters or use system environment values read in init function
-	flag.IntVar(&config.Port, "port", config.Port, fmt.Sprintf("port number to bind the server, can be provided also by the environmental variable %s (0-65535)", EnvPortKey))
-	flag.StringVar(&config.Host, "host", config.Host, fmt.Sprintf("host name to bind the server, can be provided also by the environmental variable %s (localhost)", EnvHostKey))
-	flag.StringVar(&config.CertFile, "cert", config.CertFile, fmt.Sprintf("path to TLS certificate file, can be provided also by the environmental variable %s (.pem)", EnvCertFileKey))
-	flag.StringVar(&config.KeyFile, "key", config.KeyFile, fmt.Sprintf("path to TLS certificate key file, can be provided also by the environmental variable %s (.key)", EnvKeyFileKey))
-	flag.StringVar(&dbConnString, "db", dbConnString, fmt.Sprintf("Database connection string (Postgres),can be provided also by the environmental variable %s", EnvDBKey))
-	flag.StringVar(&cacheConnString, "cache", cacheConnString, fmt.Sprintf("Cache connection string (Redis),can be provided also by the environmental variable %s", EnvCacheKey))
-	help := flag.Bool("help", false, "print this help message")
-	flag.Usage = func() {
-		execName := strings.Split(os.Args[0], string(os.PathSeparator))
-		_, _ = fmt.Fprintf(flag.CommandLine.Output(), "\nusage: %s\n\n", execName[len(execName)-1])
-		flag.PrintDefaults()
-	}
-	flag.Parse()
-
-	// Print the help message
-	if *help {
-		flag.Usage()
-		os.Exit(0)
+	// Initalize data layer
+	if err := model.Init(dbConnString, cacheConnString); err != nil {
+		os.Exit(1)
 	}
 
 	// start http server
@@ -106,9 +89,51 @@ func init() {
 	if cache, ok := os.LookupEnv(EnvCacheKey); ok {
 		cacheConnString = cache
 	}
+}
 
-	// Initalize data layer
-	if err := model.Init(dbConnString, cacheConnString); err != nil {
-		os.Exit(1)
+// readConfig Read the program's configuration and, if necessary, print help messages before exiting.
+func readConfig() {
+
+	// Print welcome message
+	fmt.Printf("Bravo Currency Converter Server v%s\n\n", version)
+
+	// Parse command line parameters or use system environment values read in init function
+	flag.IntVar(&config.Port, "port", config.Port, fmt.Sprintf("port number to bind the server, can be provided also by the environmental variable %s (0-65535)", EnvPortKey))
+	flag.StringVar(&config.Host, "host", config.Host, fmt.Sprintf("host name to bind the server, can be provided also by the environmental variable %s (localhost)", EnvHostKey))
+	flag.StringVar(&config.CertFile, "cert", config.CertFile, fmt.Sprintf("path to TLS certificate file, can be provided also by the environmental variable %s (.pem)", EnvCertFileKey))
+	flag.StringVar(&config.KeyFile, "key", config.KeyFile, fmt.Sprintf("path to TLS certificate key file, can be provided also by the environmental variable %s (.key)", EnvKeyFileKey))
+	flag.StringVar(&dbConnString, "db", dbConnString, fmt.Sprintf("Database connection string (Postgres),can be provided also by the environmental variable %s", EnvDBKey))
+	flag.StringVar(&cacheConnString, "cache", cacheConnString, fmt.Sprintf("Cache connection string (Redis),can be provided also by the environmental variable %s", EnvCacheKey))
+	help := flag.Bool("help", false, "print this help message")
+	flag.Usage = func() {
+		execName := strings.Split(os.Args[0], string(os.PathSeparator))
+		_, _ = fmt.Fprintf(flag.CommandLine.Output(), "\nusage: %s\n\n", execName[len(execName)-1])
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	// Validate the value of the arguments and print a help message
+	if (*help) || config.Port < 0 || config.Port > 0xffff || len(config.Host) == 0 ||
+		len(dbConnString) == 0 || len(cacheConnString) == 0 {
+		exitCode := 0
+
+		if config.Port < 0 || config.Port > 0xffff {
+			fmt.Printf("Invalid port number: %d. The port number must be between 0 and 65535.\n", config.Port)
+			exitCode = 1
+		}
+		if len(config.Host) == 0 {
+			fmt.Println("Missing hostname")
+			exitCode = 1
+		}
+		if len(dbConnString) == 0 {
+			fmt.Println("Missing database connection string")
+			exitCode = 1
+		}
+		if len(cacheConnString) == 0 {
+			fmt.Println("Missing cache connection string")
+			exitCode = 1
+		}
+		flag.Usage()
+		os.Exit(exitCode)
 	}
 }
