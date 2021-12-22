60d6b875d81b617496fd34f8273b6570a730aeb0
André Luiz <alcostatorres@gmail.com>
2021-12-21 21:19:15 -0300

    refact: ajustando permissões para teste

--- permissions_paths.sh
+++ permissions_paths.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-chmod -R 777 app/storage/logs/
-chmod -R 777 app/bootstrap/cache/
-chmod -R 777 app/storage/framework/sessions/
-chmod -R 777 app/storage/framework/views/
-chmod -R 777 app/storage/framework/cache/
+chmod -R 755 app/storage/logs/
+chmod -R 755 app/bootstrap/cache/
+chmod -R 755 app/storage/framework/sessions/
+chmod -R 755 app/storage/framework/views/
+chmod -R 755 app/storage/framework/cache/
