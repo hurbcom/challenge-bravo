b933e080211097e9b9db4e02995fecb9c09180fd
André Gustavo de Andrade <andre.andrade@agvalue.com.br>
2021-12-28 15:16:31 -0300

    Angular 2 and Prime NG front end setup

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+import {PrimeNGConfig} from "primeng/api";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  constructor(private primengConfig: PrimeNGConfig) {
+  }
+
+  ngOnInit(): void {
+    // Enable ripple effect globally
+    this.primengConfig.ripple = true;
+  }
+}
