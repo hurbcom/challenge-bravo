084dfad6ad1cb6f45dcfbe6b4ea98006a9084248
duchometson <ducho@hotmail.com>
2022-01-10 22:16:20 -0300

    Arrumando arquivo de teste de estresse

--- locust.py
+++ locust.py
@@ -5,7 +5,3 @@ class WebsiteUser(HttpUser):
     @task(1)
     def index(self):
         self.client.get("http://127.0.0.1:5000/currency?name=USD")
-
-   # @task(1)
-   # def create(self):
-   #     self.client.post("/tasks/", {"title": lorem.sentence()})
