f730c7aca5bc935125935aa1ce30e01260c8843e
duchometson <ducho@hotmail.com>
2022-01-10 22:45:35 -0300

    deslocando arquivo de teste de estresse

--- src/locust.py
+++ src/locust.py
@@ -4,4 +4,4 @@ from locust import HttpUser, task, between
 class WebsiteUser(HttpUser):
     @task(1)
     def index(self):
-        self.client.get("http://127.0.0.1:5000/currency?name=USD")
+        self.client.get("http://172.18.0.2:5000/currency?name=USD")
