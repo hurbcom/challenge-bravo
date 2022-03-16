109659bfcee97726dfd61d20e37a2cd0257c43b6
lucasfreitas-dev <lucas.freitas.dev@gmail.com>
2022-03-16 01:52:09 -0300

    Add mock Currency Model methods

--- api-challenge-bravo/Model/Currency.cs
+++ api-challenge-bravo/Model/Currency.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace api_challenge_bravo.Model
 {
     public class Currency
     {
+        // Using for Mock testing, remove after DB configuration
+        public static List<Currency> Currencies = new List<Currency>();
+
         public int Id { get; set; }
         public string Symbol { get; set; }
         public string Name { get; set; }
@@ -11,12 +15,33 @@ namespace api_challenge_bravo.Model
         public bool AutoUpdateExchangeRate { get; set; }
         public DateTime? LastTimeUpdatedExchangeRate { get; set; }
 
+        public Currency()
+        {
+
+        }
         public Currency(string symbol, string name, float exchangeRateInUSD, bool autoUpdateExchangeRate)
         {
             this.Symbol = symbol;
             this.Name = name;
             this.ExchangeRateInUSD = exchangeRateInUSD;
             this.AutoUpdateExchangeRate = autoUpdateExchangeRate;
+
+            Currencies.Add(this);
+        }
+
+        public static List<Currency> GetAll()
+        {
+            return Currencies;
+        }
+
+        public static Currency Get(string symbol)
+        {
+            return Currencies.Find(x => x.Symbol == symbol);
+        }
+
+        public static bool Delete(string symbol)
+        {
+            return Currencies.Remove(Get(symbol));
         }
     }
 }
