6c8b1062137d43678c2c0c45516fcaf6a87dbb66
lucasfreitas-dev <lucas.freitas.dev@gmail.com>
2022-03-16 11:05:09 -0300

    Add CurrencyConvertService

---
+++ api-challenge-bravo/Services/CurrencyConvertService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using api_challenge_bravo.Model;
+
+namespace api_challenge_bravo.Services
+{
+    public class CurrencyConvertService
+    {
+        public static decimal Convert(string fromSymbol, string toSymbol, decimal amount)
+        {
+            var fromCurrencyExchangeRate = Currency.Get(fromSymbol).ExchangeRateInUSD;
+            var toCurrencyExchangeRate = Currency.Get(toSymbol).ExchangeRateInUSD;
+
+            var ExchangeRate = fromCurrencyExchangeRate / toCurrencyExchangeRate;
+
+            return amount * ExchangeRate;
+        }
+    }
+}
