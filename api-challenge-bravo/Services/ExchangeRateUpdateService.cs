f30e0ed8a670ddd960363f7dc613f3983d24b53b
lucasfreitas-dev <lucas.freitas.dev@gmail.com>
2022-03-16 17:05:44 -0300

    Add CallExternalAwesomeApi

--- api-challenge-bravo/Services/ExchangeRateUpdateService.cs
+++ api-challenge-bravo/Services/ExchangeRateUpdateService.cs
@@ -1,24 +1,51 @@
 ﻿using System;
+using System.Globalization;
 using api_challenge_bravo.Model;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace api_challenge_bravo.Services
 {
     public class ExchangeRateUpdateService
     {
         private const int TIME_TO_LIVE_EXCHANGE_RATE_SECONDS = 30;
-        public static void Update(Currency currency)
+        private static async Task Update(Currency currency)
         {
-            // Call API
-            // Get ExchangeRate and Time(maybe from header) from request
-            currency.UpdateExchangeRate(0.10M, DateTime.Now);
+            JObject response;
+            // Need to add some lock, only one thread should update ExchangeRate from each symbol
+            try
+            {
+                response = await CallExternalAwesomeApi(currency.Symbol);
+
+                var newExchangeRat = Decimal.Parse(response[$"{currency.Symbol}USD"]["bid"].ToString(),CultureInfo.InvariantCulture);
+                var dateTimeUpdate = DateTime.Parse(response[$"{currency.Symbol}USD"]["create_date"].ToString());
+
+                currency.UpdateExchangeRate(newExchangeRat, dateTimeUpdate);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception("External API error:",exception);
+            }
         }
 
-        public static void CheckTTLForNewUpdate(Currency currency)
+        public static async Task CheckTTLForNewUpdate(Currency currency)
         {
             if (!currency.AutoUpdateExchangeRate)
                 return;
-            if (currency.LastTimeUpdatedExchangeRate <= DateTime.Now.AddSeconds(-TIME_TO_LIVE_EXCHANGE_RATE_SECONDS))
-                Update(currency);
+            if (currency.LastTimeUpdatedExchangeRate == null
+                || currency.LastTimeUpdatedExchangeRate <= DateTime.Now.AddSeconds(-TIME_TO_LIVE_EXCHANGE_RATE_SECONDS))
+                await Update(currency);
+        }
+
+        private static async Task<JObject> CallExternalAwesomeApi(string symbol)
+        {
+            string baseURL = "https://economia.awesomeapi.com.br/";
+            string path = $"last/{symbol.ToUpper()}-USD";
+
+            HttpClient req = new HttpClient();
+            var content = await req.GetAsync(baseURL + path);
+            return JObject.Parse(await content.Content.ReadAsStringAsync());
         }
     }
 }
