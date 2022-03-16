2d8e067f5a4c1a2aadfe29338cafec288a7c5e1c
lucasfreitas-dev <lucas.freitas.dev@gmail.com>
2022-03-16 00:26:20 -0300

    Add Model Currency

---
+++ api-challenge-bravo/Model/Currency.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace api_challenge_bravo.Model
+{
+    public class Currency
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public float ExchangeRateInUSD { get; set; }
+        public bool AutoUpdateExchangeRate { get; set; }
+        public DateTime? LastTimeUpdatedExchangeRate { get; set; }
+
+        public Currency(string symbol, string name, float exchangeRateInUSD, bool autoUpdateExchangeRate)
+        {
+            this.Symbol = symbol;
+            this.Name = name;
+            this.ExchangeRateInUSD = exchangeRateInUSD;
+            this.AutoUpdateExchangeRate = autoUpdateExchangeRate;
+        }
+    }
+}
