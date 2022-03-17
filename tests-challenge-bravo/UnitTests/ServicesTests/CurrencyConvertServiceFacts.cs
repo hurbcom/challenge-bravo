fa425450e6139fff05b8f3d06d4d5fc6354e4395
lucasfreitas-dev <lucas.freitas.dev@gmail.com>
2022-03-17 00:50:24 -0300

    Add CurrencyConvertServiceFacts

---
+++ tests-challenge-bravo/UnitTests/ServicesTests/CurrencyConvertServiceFacts.cs
@@ -0,0 +1,23 @@
+﻿using api_challenge_bravo.Model;
+using api_challenge_bravo.Services;
+using Xunit;
+
+namespace tests_challenge_bravo.UnitTests.ServicesTests
+{
+    public class CurrencyConvertServiceFacts
+    {
+        public CurrencyConvertServiceFacts()
+        {
+            new Currency("TEST1", "Testing Currency 1", 0.1948M, false);
+            new Currency("TEST2", "Testing Currency 2", 1.0996M, false);
+        }
+
+        [Fact]
+        public void ConvertingCorrectValue()
+        {
+            var result = CurrencyConvertService.Convert("TEST1","TEST2",10.42M);
+
+            Assert.Equal(1.8459585303746817024372499086M,result.Result);
+        }
+    }
+}
