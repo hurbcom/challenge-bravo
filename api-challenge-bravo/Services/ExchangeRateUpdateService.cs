376d3ebaa22bf22071d5eae06ef4223e377002fd
lucasfreitas-dev <lucas.freitas.dev@gmail.com>
2022-03-17 17:29:33 -0300

    Add Mutex when checking last update of the currency
    Optimizing the number of calls to the external API

--- api-challenge-bravo/Services/ExchangeRateUpdateService.cs
+++ api-challenge-bravo/Services/ExchangeRateUpdateService.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Globalization;
 using api_challenge_bravo.Model;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 
@@ -9,7 +10,10 @@ namespace api_challenge_bravo.Services
 {
     public static class ExchangeRateUpdateService
     {
+        static int countExternalCalls = 1;
         private const int TIME_TO_LIVE_EXCHANGE_RATE_SECONDS = 30;
+        static SemaphoreSlim mutex = new SemaphoreSlim(1,1);
+
         private static async Task Update(Currency currency)
         {
             decimal newExchangeRat;
@@ -31,9 +35,26 @@ namespace api_challenge_bravo.Services
         {
             if (!currency.AutoUpdateExchangeRate)
                 return;
-            if (currency.LastTimeUpdatedExchangeRate == null
-                || currency.LastTimeUpdatedExchangeRate <= DateTime.Now.AddSeconds(-TIME_TO_LIVE_EXCHANGE_RATE_SECONDS))
-                await Update(currency);
+
+            // Only one thread by time can check if the currency need an update, making this a thread-safe logic
+            await mutex.WaitAsync();
+            try
+            {
+                if (currency.LastTimeUpdatedExchangeRate == null
+                    || currency.LastTimeUpdatedExchangeRate <=
+                    DateTime.Now.AddSeconds(-TIME_TO_LIVE_EXCHANGE_RATE_SECONDS))
+                    await Update(currency);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                mutex.Release();
+            }
+
         }
 
         private static async Task<Tuple<decimal,DateTime>> CallExternalAwesomeApi(string symbol)
@@ -48,6 +69,7 @@ namespace api_challenge_bravo.Services
             var newExchangeRat = Decimal.Parse(response[$"{symbol}USD"]["bid"].ToString(),CultureInfo.InvariantCulture);
             var dateTimeUpdate = DateTime.Parse(content.Headers.Date.ToString());
 
+            Console.WriteLine("Calling external API: #" + countExternalCalls++);
             return Tuple.Create(newExchangeRat,dateTimeUpdate);
         }
     }
