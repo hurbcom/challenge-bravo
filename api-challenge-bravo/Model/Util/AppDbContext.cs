8307734702940cc7f6fd3843776bcdacfcc792d0
lucasfreitas-dev <lucas.freitas.dev@gmail.com>
2022-03-18 22:57:03 -0300

    Fix typo

--- api-challenge-bravo/Model/Util/AppDbContext.cs
+++ api-challenge-bravo/Model/Util/AppDbContext.cs
@@ -6,7 +6,7 @@ namespace api_challenge_bravo.Model.Util
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var host = System.Diagnostics.Debugger.IsAttached ? "localhost" : "bd_MySQL";
+            var host = System.Diagnostics.Debugger.IsAttached ? "localhost" : "db_MySQL";
             optionsBuilder
                 .UseMySql(@$"Server={host};Database=currencydb;Uid=root;Pwd=dbdevpassword;");
         }
