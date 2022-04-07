1cc17f988f542c7a8cf3a4e54066e7b7ce0ff0c8
Wilker Souza <wilker.souza@123milhas.com.br>
2022-04-07 17:33:45 -0300

    fix: force artisan migrate

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ touch database/database.sqlite
 chown 1000:1000 database/database.sqlite
 chmod 766 database/database.sqlite
 redis-server --daemonize yes
-php artisan migrate --seed
+php artisan migrate --seed --force
 php -S lumen:8000 -t public
 echo 'Done!'
