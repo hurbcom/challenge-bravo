d052662019ffe4bf00841ea843c2f7fff18b694c
danicatalao <catalaodanillo@gmail.com>
2022-04-30 21:27:10 -0300

    Adicionando connectionString de conexão com a base nas configurações. Adicionando suporte ao PostgreSQL.

--- currency-conversion/currency-conversion.infrastructure/Data/DbContext.cs
+++ currency-conversion/currency-conversion.infrastructure/Data/DbContext.cs
@@ -1,3 +1,4 @@
+using currency_conversion.Core.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace currency_conversion.infrastructure.Data
@@ -7,5 +8,7 @@ namespace currency_conversion.infrastructure.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+
+        public DbSet<Currency> Currency => Set<Currency>();
     }
 }
