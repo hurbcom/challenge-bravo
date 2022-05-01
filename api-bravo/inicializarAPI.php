6234b67959ea6022c52897f685e06870e145cf8d
alexandre_abdalla <xandeabdalla5@gmail.com>
2022-05-01 20:36:34 -0300

    Adicionando mensagens informativas sobre o carregamento da API

--- api-bravo/inicializarAPI.php
+++ api-bravo/inicializarAPI.php
@@ -4,6 +4,20 @@
 
   $mng = new CurrencyManager();
 
-  $mng->populateDB();
+  set_error_handler(function ($err_severity, $err_msg, $err_file, $err_line, array $err_context)
+  {
+    throw new ErrorException( $err_msg, 0, $err_severity, $err_file, $err_line );
+  }, E_WARNING);
+
+  try {
+    $mng->populateDB();
+    echo "API carregada com sucesso!";
+
+  } catch (Exception $ex) {
+    echo "A API Challenge-Bravo não foi carregada. Verifique se a chave da API externa foi inserida no arquivo apiKey.txt.";
+  }
+
+  restore_error_handler();
+
 
 ?>
