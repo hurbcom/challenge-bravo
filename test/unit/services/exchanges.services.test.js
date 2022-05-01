c0ab7cf9446a86950783c7f46fb7c47cc9c52f36
almeigab <gabrielalmeida1403@hotmail.com>
2022-05-01 19:46:20 -0300

    added tests for exchanges service

---
+++ test/unit/services/exchanges.services.test.js
@@ -0,0 +1,15 @@
+const exchangesService = require('../../../services/exchanges.services');
+const { generateCurrency } = require('../../factories/currencies.factories');
+
+describe('Exchanges Service', () => {
+    describe('convert method', () => {
+        it('should convert amount', () => {
+            const fromCurrency = generateCurrency('FROM', 1.4);
+            const toCurrency = generateCurrency('TO', 4.90);
+            const result = exchangesService.convert(fromCurrency, toCurrency, 1);
+
+            // dealing with floating point precision
+            expect(result.toPrecision(2)).toBe('3.5');
+        });
+    });
+});
