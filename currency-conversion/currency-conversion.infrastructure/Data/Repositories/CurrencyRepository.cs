9e301b44f4cffbd63dcd74dc99b64a6cea398c0c
danicatalao <catalaodanillo@gmail.com>
2022-04-30 21:27:54 -0300

    Adição de operações de CRUD no repositorio de moeda.

--- currency-conversion/currency-conversion.infrastructure/Data/Repositories/CurrencyRepository.cs
+++ currency-conversion/currency-conversion.infrastructure/Data/Repositories/CurrencyRepository.cs
@@ -1,12 +1,43 @@
 using currency_conversion.Core.Interfaces.Repositories;
+using currency_conversion.Core.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace currency_conversion.infrastructure.Data.Repositories
 {
     public class CurrencyRepository : ICurrencyRepository
     {
+        private readonly AppDbContext _dbContext;
         public CurrencyRepository(AppDbContext dbContext)
         {
+            _dbContext = dbContext;
+        }
+
+        public EntityEntry<Currency> Create(Currency currency)
+        {
+            var currencyAdded = _dbContext.Currency.Add(currency);
+            _dbContext.SaveChanges();
+            return currencyAdded;
 
         }
+
+        public Currency? Read(string code)
+        {
+            return _dbContext.Currency.Find(code);
+        }
+
+
+        public EntityEntry<Currency> Update(Currency currency)
+        {
+            var currencyUpdated = _dbContext.Currency.Update(currency);
+            _dbContext.SaveChanges();
+            return currencyUpdated;
+        }
+
+        public void Delete(Currency currency)
+        {
+            _dbContext.Currency.Remove(currency);
+            _dbContext.SaveChanges();
+        }
+
     }
 }
--- currency-conversion/currency-conversion.web/Program.cs
+++ currency-conversion/currency-conversion.web/Program.cs
@@ -1,8 +1,20 @@
+using currency_conversion.Core.Interfaces.Repositories;
+using currency_conversion.infrastructure.Data;
+using currency_conversion.infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.Services.AddTransient<ICurrencyRepository, CurrencyRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
