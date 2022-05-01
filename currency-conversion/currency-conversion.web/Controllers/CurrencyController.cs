a5fd7e80a1afe876c64d0fff249f142b1520930c
danicatalao <catalaodanillo@gmail.com>
2022-04-30 23:54:06 -0300

    Criando rotas de CRUD com validação de parâmetros

--- currency-conversion/currency-conversion.web/Controllers/CurrencyController.cs
+++ currency-conversion/currency-conversion.web/Controllers/CurrencyController.cs
@@ -1,4 +1,5 @@
 using currency_conversion.Core.Interfaces.Repositories;
+using currency_conversion.web.DTOs;
 using Microsoft.AspNetCore.Mvc;
 
 namespace currency_conversion.web.Controllers
@@ -16,8 +17,34 @@ namespace currency_conversion.web.Controllers
             _currencyRepository = currencyRepository;
         }
 
-        [HttpGet(Name = "")]
-        public OkResult Get()
+        [HttpPost(Name = "insert")]
+        public IActionResult Insert([FromBody] CurrencyDTO currency)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok();
+        }
+
+        [HttpGet(Name = "get")]
+        public IActionResult Get(string code)
+        {
+            return Ok();
+        }
+
+        [HttpPut(Name = "update")]
+        public IActionResult Update([FromBody]CurrencyDTO currency)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok();
+        }
+
+        [HttpDelete(Name = "delete")]
+        public IActionResult Delete(string code)
         {
             return Ok();
         }
---
+++ currency-conversion/currency-conversion.web/DTOs/CurrencyDTO.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace currency_conversion.web.DTOs
+{
+    public class CurrencyDTO
+    {
+        [Required]
+        //[StringLength(3, MinimumLength = 3, ErrorMessage = "Field must be 3 characters")]
+        [RegularExpression("^[a-zA-Z]{3}$", ErrorMessage = "Field must contain only a-z A-Z characters")]
+        public string? Code { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "Only positive number allowed")]
+        public double Rate { get; set; }
+    }
+}
