0b2b0180747db00e7e10a50b90670e33fcb53ec8
lcarvalho94 <lcarvalho94@hotmail.com>
2022-05-10 19:46:58 -0300

    adjusting routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,10 +3,10 @@ import { currencyController } from './controllers';
 
 const router = express.Router();
 
-router.post('/currencies/real', (req, res) => currencyController.addRealCurrency(req, res));
-router.post('/currencies/fictitious', (req, res) =>
+router.post('/fictitious-currencies', (req, res) =>
   currencyController.addFictitiousCurrency(req, res),
 );
+router.post('/real-currencies', (req, res) => currencyController.addRealCurrency(req, res));
 router.delete('/currencies', (req, res) => currencyController.deleteCurrency(req, res));
 router.get('/exchange', (req, res) => currencyController.exchangeCurrency(req, res));
 
