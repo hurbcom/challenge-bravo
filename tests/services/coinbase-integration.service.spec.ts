888790ba1f02da3bc7b5ebae9da716f907e9b6a1
lcarvalho94 <lcarvalho94@hotmail.com>
2022-05-10 22:49:27 -0300

    adding tests for CoinbaseIntegrationService

---
+++ tests/services/coinbase-integration.service.spec.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { coinbaseIntegrationService as CoinbaseIntegrationService } from '../../src/services';
+import { ICoinbaseIntegrationService } from '../../src/interfaces/coinbase-integration-service';
+import currenciesResponse from '../currenciesResponse.json';
+import exchangeRatesResponse from '../exchangeRatesResponse.json';
+
+jest.mock('axios');
+
+describe('CoinbaseIntegrationService', () => {
+  let coinbaseIntegrationService: ICoinbaseIntegrationService;
+
+  beforeAll(() => {
+    coinbaseIntegrationService = CoinbaseIntegrationService;
+  });
+
+  describe('when the method "getCurrencies" is called', () => {
+    it('should call axios and return data', async () => {
+      const spy = jest.spyOn(axios, 'request').mockResolvedValue(currenciesResponse);
+
+      const response = await coinbaseIntegrationService.getCurrencies();
+      console.log(response);
+      expect(spy).toHaveBeenCalled();
+      expect(response).toBeDefined();
+    });
+  });
+
+  describe('when the method "getExchangeRates" is called', () => {
+    it('should call axios and return data', async () => {
+      const spy = jest.spyOn(axios, 'request').mockResolvedValue(exchangeRatesResponse);
+
+      const response = await coinbaseIntegrationService.getExchangeRates();
+
+      expect(spy).toHaveBeenCalled();
+      expect(response).toBeDefined();
+    });
+  });
+});
