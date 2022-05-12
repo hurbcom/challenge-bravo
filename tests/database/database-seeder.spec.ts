fc61fd7e83e188dc51703a6557a0c0a0875f6bf6
lcarvalho94 <lcarvalho94@hotmail.com>
2022-05-12 19:40:45 -0300

    adding tests for DatabaseSeeder

---
+++ tests/database/database-seeder.spec.ts
@@ -0,0 +1,68 @@
+import { ICurrencyDao } from '../../src/interfaces/currency-dao';
+import { ICoinbaseIntegrationService } from '../../src/interfaces/coinbase-integration-service';
+import { CoinbaseIntegrationService } from '../../src/services/coinbase-integration.service';
+import { CurrencyDao } from '../../src/database/dao/currency.dao';
+import { currencyDao } from '../../src/database/dao';
+import { DatabaseMemoryServer } from '../database-memory-server';
+import { coinbaseIntegrationService } from '../../src/services';
+import { DatabaseSeeder } from '../../src/database/database-seeder';
+import { databaseSeeder } from '../../src/database';
+import { data as exchangeRates } from '../exchangeRatesResponse.json';
+
+describe('DatabaseSeeder', () => {
+  let dao: ICurrencyDao;
+  let databaseMemoryServer: DatabaseMemoryServer;
+  let coinbaseService: ICoinbaseIntegrationService;
+  let seeder: DatabaseSeeder;
+
+  beforeAll(async () => {
+    dao = currencyDao;
+    coinbaseService = coinbaseIntegrationService;
+    seeder = databaseSeeder;
+  });
+
+  describe('when the method "run" is called', () => {
+    it('should skeep the seed because the database has already been initialized previously', async () => {
+      const currencyDaoSpy = jest
+        .spyOn(CurrencyDao.prototype, 'getAllCurrencies')
+        .mockResolvedValueOnce([{ code: 'BRL', rate: '5.13' }]);
+
+      const coinbaseServiceSpy = jest.spyOn(
+        CoinbaseIntegrationService.prototype,
+        'getExchangeRates',
+      );
+
+      const daoSpy = jest.spyOn(CurrencyDao.prototype, 'save');
+
+      await seeder.run();
+
+      expect(currencyDaoSpy).toHaveBeenCalled();
+      expect(coinbaseServiceSpy).not.toHaveBeenCalled();
+      expect(daoSpy).not.toHaveBeenCalled();
+    });
+
+    it('should add the base currencies to the database', async () => {
+      databaseMemoryServer = await DatabaseMemoryServer.setup();
+      await databaseMemoryServer.connect();
+
+      const currencyDaoSpy = jest
+        .spyOn(CurrencyDao.prototype, 'getAllCurrencies')
+        .mockResolvedValueOnce([]);
+
+      const coinbaseServiceSpy = jest
+        .spyOn(CoinbaseIntegrationService.prototype, 'getExchangeRates')
+        .mockResolvedValueOnce(exchangeRates.data);
+
+      const daoSpy = jest.spyOn(CurrencyDao.prototype, 'save');
+
+      await seeder.run();
+
+      await databaseMemoryServer.clearDatabase();
+      await databaseMemoryServer.disconnectAndStop();
+
+      expect(currencyDaoSpy).toHaveBeenCalled();
+      expect(coinbaseServiceSpy).toHaveBeenCalled();
+      expect(daoSpy).toHaveBeenCalledTimes(5);
+    });
+  });
+});
