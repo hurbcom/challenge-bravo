a84960844ec4677a93d5314bd0a301d91cfb5248
lcarvalho94 <lcarvalho94@hotmail.com>
2022-05-15 19:41:01 -0300

    undoing unintentional  changes to database

--- src/database/database.ts
+++ src/database/database.ts
@@ -2,26 +2,14 @@ import mongoose from 'mongoose';
 
 const { MONGODB_HOST, MONGODB_PORT, MONGODB_COLLECTION } = process.env;
 
-// export class Database {
-//   public static async connect() {
-//     try {
-//       await mongoose.connect(`mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_COLLECTION}`);
-//       console.log('Database connection established!');
-//     } catch (e) {
-//       console.log(e);
-//     }
-//   }
-// }
-
 export class Database {
   public static async connect() {
     try {
-      await mongoose.connect(
-        `mongodb+srv://admin:EvdtsnTbQB1bfWao@currencydb.7viib.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-      );
+      await mongoose.connect(`mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_COLLECTION}`);
       console.log('Database connection established!');
     } catch (e) {
       console.log(e);
     }
   }
+}
 }
