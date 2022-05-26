51019cd51c84a6e9abb208193667ce53776c6880
Fellipe Maia <fellipe.apm@gmail.com>
2022-05-26 00:33:22 -0300

    Adjusted how docker-compose works

---
+++ mongodb/initdb.d/mongo-init.sh
@@ -0,0 +1,1 @@
+mongo --eval "db.auth('$MONGO_INITDB_ROOT_USERNAME', '$MONGO_INITDB_ROOT_PASSWORD'); db = db.getSiblingDB('$DB_NAME'); db.createUser({ user: '$DB_USER', pwd: '$DB_PASSWORD', roles: [{ role: 'readWrite', db: '$DB_NAME' }] });"
