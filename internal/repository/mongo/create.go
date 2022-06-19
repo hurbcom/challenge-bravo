d43fa9bf1605ed10c4ed5f619b476bc3da845008
joaofreitas <jhgfreitas@ext.worten.pt>
2022-06-19 07:49:22 -0300

    improve err and add create if not exist

--- internal/repository/mongo/create.go
+++ internal/repository/mongo/create.go
@@ -3,6 +3,9 @@ package mongo
 import (
 	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func (e *Engine) Create(ctx context.Context, data interface{}) (interface{}, error) {
@@ -12,10 +15,38 @@ func (e *Engine) Create(ctx context.Context, data interface{}) (interface{}, err
 		if !ok {
 			return nil, fmt.Errorf("error parsing data %v", ok)
 		}
-		return e.collection.InsertMany(ctx, data)
+		result, err := e.collection.InsertMany(ctx, data)
+		if err != nil {
+			return nil, fmt.Errorf("error create many data on mongo: %w", err)
+		}
+		return result, nil
 	default:
-
-		return e.collection.InsertOne(ctx, data)
-
+		result, err := e.collection.InsertOne(ctx, data)
+		if err != nil {
+			return nil, fmt.Errorf("error create data on mongo: %w", err)
+		}
+		return result, nil
 	}
+}
+
+func (e *Engine) CreateIfNotExist(ctx context.Context, filter bson.M, data interface{}) (interface{}, error) {
+	var result = e.collection.FindOneAndUpdate(
+		ctx,
+		filter,
+		bson.M{"$setOnInsert": data},
+		options.FindOneAndUpdate().SetUpsert(true),
+		options.FindOneAndUpdate().SetReturnDocument(options.After))
+	var err = result.Err()
+	if err == mongo.ErrNoDocuments && err == mongo.ErrNilDocument {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, fmt.Errorf("error create or updating data from mongo: %w", err)
+	}
+	var doc map[string]interface{}
+	err = result.Decode(&doc)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding data from mongo: %w", err)
+	}
+	return doc, nil
 }
