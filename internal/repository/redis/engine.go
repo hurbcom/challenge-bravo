573eca6aa85aaf25c9baa791d3380f8fa8deea95
joaofreitas <jhgfreitas@ext.worten.pt>
2022-06-19 08:03:06 -0300

    add new redis addr to save types on redis

--- internal/repository/redis/engine.go
+++ internal/repository/redis/engine.go
@@ -5,14 +5,48 @@ import (
 	"os"
 )
 
+// Engine struct for redis
 type Engine struct {
-	client *redis.Client
+	clientCurrency     *redis.Client
+	clientCurrencyType *redis.Client
 }
 
+// NewEngine creates a new redis engine
 func NewEngine() *Engine {
-	return &Engine{redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("REDIS_ADDR"),
-		Password: os.Getenv("REDIS_PASSWORD"),
-		DB:       0,
-	})}
+	var engine = &Engine{
+		clientCurrency: redis.NewClient(&redis.Options{
+			Addr:     os.Getenv("REDIS_ADDR"),
+			Password: os.Getenv("REDIS_PASSWORD"),
+			DB:       0}),
+		clientCurrencyType: redis.NewClient(&redis.Options{
+			Addr:     os.Getenv("REDIS_ADDR"),
+			Password: os.Getenv("REDIS_PASSWORD"),
+			DB:       1}),
+	}
+	return engine
+}
+
+// Close closes all redis connections
+func (e *Engine) Close() error {
+	// close redis currency db connection
+	var err = e.clientCurrency.Close()
+	if err != nil {
+		return err
+	}
+	// close redis currency type db connection
+	err = e.clientCurrencyType.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// HasCurrency checks if the currency db has been populated
+func (e *Engine) HasCurrency() bool {
+	return e.clientCurrency.DBSize().Val() > 0
+}
+
+// HasType checks if the currency type db has been populated
+func (e *Engine) HasType() bool {
+	return e.clientCurrencyType.DBSize().Val() > 0
 }
