0fa3395f4a6bc902fb182e601825e2c1154d1ee8
joaofreitas <jhgfreitas@ext.worten.pt>
2022-06-19 08:03:28 -0300

    add interface to interact with repositories

--- internal/repository/engine.go
+++ internal/repository/engine.go
@@ -12,6 +12,17 @@ type Engine struct {
 	Redis *redis.Engine
 }
 
+// ModelBuilder interface to all domain could use the same code to
+// interact with the repositories
+type ModelBuilder interface {
+	GetCode() string
+	GetPrice() float64
+	GetPriceString() string
+	GetName() string
+	ParseToMap() (map[string]interface{}, error)
+	Validate() map[string]string
+}
+
 // NewEngine creates a new engine witch has all repositories
 func NewEngine(ctx context.Context) *Engine {
 	var (
@@ -20,25 +31,37 @@ func NewEngine(ctx context.Context) *Engine {
 		redisEngine = new(redis.Engine)
 	)
 	wg.Add(2)
+
+	// create a go routine to connect to mongo
 	go func() {
 		mongoEngine = mongo.NewEngine(ctx)
 		wg.Done()
+		return
 	}()
 
+	// createa a go routine to connect to redis
 	go func() {
 		redisEngine = redis.NewEngine()
 		wg.Done()
-
+		return
 	}()
 	wg.Wait()
-	return &Engine{
+	engine := &Engine{
 		Mongo: mongoEngine,
 		Redis: redisEngine,
 	}
+	return engine
 }
 
+// Close closes all repositories connections
 func (e *Engine) Close(ctx context.Context) {
+	// close mongo connection
 	var err = e.Mongo.Close(ctx)
+	if err != nil {
+		panic(err)
+	}
+	// close all redis connections
+	err = e.Redis.Close()
 	if err != nil {
 		panic(err)
 	}
