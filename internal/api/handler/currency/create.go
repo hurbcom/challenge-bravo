0945d72fcc59297fa765ca0896c374bc4d93734d
joaofreitas <jhgfreitas@ext.worten.pt>
2022-06-20 15:46:38 -0300

    improve set models

--- internal/api/handler/currency/create.go
+++ internal/api/handler/currency/create.go
@@ -4,17 +4,20 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/joaohgf/challenge-bravo/internal/api/handler"
+	"github.com/joaohgf/challenge-bravo/internal/repository/models"
 	"net/http"
 )
 
 // Create handles the creation of a new currency
 func (h *Handler) Create(c *gin.Context) {
 	// get the currency data from the request
-	var err = c.ShouldBindJSON(h.domain.Models)
+	var currency = new(models.Currency)
+	var err = c.ShouldBindJSON(currency)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, handler.MakeResponse("invalid json"))
 		return
 	}
+	h.domain.Models = currency
 	// validate the fields of the currency
 	erros := h.domain.Models.Validate()
 	if erros != nil {
