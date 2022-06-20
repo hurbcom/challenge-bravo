7f0e5eef6acc9e4e46a6711485f5107888a3d285
joaofreitas <jhgfreitas@ext.worten.pt>
2022-06-19 22:32:47 -0300

    improve create

--- internal/repository/create.go
+++ internal/repository/create.go
@@ -2,35 +2,31 @@ package repository
 
 import (
 	"context"
+	"github.com/joaohgf/challenge-bravo/internal/repository/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"strings"
 )
 
 // Create the currency on all repositories
-func (e *Engine) Create(ctx context.Context, model ModelBuilder) (interface{}, error) {
+func (e *Engine) Create(ctx context.Context, model *models.Currency) (interface{}, error) {
 	// create the currency on mongo if it doesn't exist with the same code
 	result, err := e.Mongo.CreateIfNotExist(
 		ctx,
-		bson.M{"code": strings.ToUpper(model.GetCode())},
+		bson.M{"code": strings.ToUpper(model.Code)},
 		model)
 	if err != nil {
 		return nil, err
 	}
-	// if not created, don't update the redis
+	// if not created, don't set the redis
 	if result == nil {
 		return nil, nil
 	}
 
 	// set the price on redis to be used to convert
-	err = e.Redis.SetPrice(model.GetCode(), model.GetPriceString())
+	err = e.Redis.SetPrice(model.Code, model.GetPriceString())
 	if err != nil {
 		return nil, err
 	}
 
-	// set the types wich are base currencies on redis
-	err = e.Redis.SetType(model.GetCode(), model.GetName())
-	if err != nil {
-		return nil, err
-	}
 	return result, nil
 }
