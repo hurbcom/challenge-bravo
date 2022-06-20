75cc0f92be889a1351031dc639b6104e96943c5c
joaofreitas <jhgfreitas@ext.worten.pt>
2022-06-19 22:40:28 -0300

    improve get price

--- internal/repository/get.go
+++ internal/repository/get.go
@@ -5,39 +5,23 @@ import (
 	"fmt"
 	"github.com/joaohgf/challenge-bravo/internal/repository/models"
 	"go.mongodb.org/mongo-driver/bson"
-	"time"
 )
 
-func (e *Engine) GetPrice(ctx context.Context, code string) (*models.Currency, error) {
-	ctype, err := e.Redis.GetType(code)
-	if err != nil {
-		return nil, err
-	}
-	if ctype == "" {
-		return nil, fmt.Errorf("currency not found %s", code)
-	}
+func (e *Engine) GetPrice(ctx context.Context, code string) (*float64, error) {
 	latestPrice, err := e.Redis.GetPrice(code)
 	if err != nil {
 		return nil, err
 	}
-	var doc = new(models.Currency)
 	if latestPrice != nil {
-		doc.Code = code
-		doc.Price = latestPrice
-		duration, err := e.Redis.GetTTl(code)
-		if err != nil {
-			return nil, err
-		}
-		updateAt := time.Now().Add(-*duration)
-		doc.UpdatedAt = &updateAt
-		return doc, nil
+		return latestPrice, nil
 	}
+	var doc = new(models.Currency)
 	oldestResult, err := e.Mongo.GetOne(ctx, bson.M{"code": code})
 	if err != nil {
 		return nil, err
 	}
 	if oldestResult == nil {
-		return nil, fmt.Errorf("currency not found %s, talk support", code)
+		return nil, fmt.Errorf("currency not found %s", code)
 	}
 	oldestBytes, err := bson.Marshal(oldestResult)
 	if err != nil {
@@ -47,5 +31,5 @@ func (e *Engine) GetPrice(ctx context.Context, code string) (*models.Currency, e
 	if err != nil {
 		return nil, err
 	}
-	return doc, nil
+	return doc.Price, nil
 }
