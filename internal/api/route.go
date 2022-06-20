c991987fce15c9d75e9a652c3c6c25726a29616b
joaofreitas <jhgfreitas@ext.worten.pt>
2022-06-19 22:43:01 -0300

    add new routes

--- internal/api/route.go
+++ internal/api/route.go
@@ -7,13 +7,17 @@ import (
 	"github.com/joaohgf/challenge-bravo/internal/api/handler/health_check"
 )
 
+// defaultRouter is the default router path
+const defaultRouter = "/api/v1"
+
+// Routes define all routes of the api
 func (s *Server) Routes() *gin.Engine {
 	var r = gin.Default()
+	// set default router path
 	var api = r.Group(defaultRouter)
 
 	//health check route
 	api.GET("/health/", health_check.Handler)
-
 	//currency routes
 	var cRouter = api.Group("/currency")
 	{
@@ -22,15 +26,14 @@ func (s *Server) Routes() *gin.Engine {
 		cRouter.POST("/", cHandler.Create)
 		// get all currency
 		cRouter.GET("/", cHandler.GetAll)
-		//
-		// get currency by name
+		// get currency by code
 		cRouter.GET("/:code", cHandler.GetByCode)
-
+		// patch the currency by code
 		cRouter.PATCH("/:code", cHandler.Update)
+		// delete the currency by code
+		cRouter.DELETE("/:code", cHandler.Delete)
 
-		//cRouter.PUT("/")
-		//cRouter.DELETE("/")
-
+		// handler to convert currency
 		conversionHandler := conversion.NewHandler(s.Repository)
 		cRouter.GET("/conversion", conversionHandler.Handle)
 	}
