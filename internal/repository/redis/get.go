5c278b7877ba15fe0422c89f5f6397ce38ea7628
joaofreitas <jhgfreitas@ext.worten.pt>
2022-06-19 22:40:06 -0300

    remove unsed funcs

--- internal/repository/redis/get.go
+++ internal/repository/redis/get.go
@@ -2,12 +2,11 @@ package redis
 
 import (
 	"github.com/go-redis/redis"
-	"time"
 )
 
 // GetPrice returns a price from redis
 func (e *Engine) GetPrice(key string) (*float64, error) {
-	var result, err = e.clientCurrency.Get(key).Float64()
+	var result, err = e.client.Get(key).Float64()
 	if err == redis.Nil {
 		return nil, nil
 	}
@@ -15,29 +14,4 @@ func (e *Engine) GetPrice(key string) (*float64, error) {
 		return nil, err
 	}
 	return &result, nil
-}
-
-// GetTTl returns the ttl of price from redis
-func (e *Engine) GetTTl(key string) (*time.Duration, error) {
-	var result, err = e.clientCurrency.TTL(key).Result()
-	if err == redis.Nil {
-		return nil, nil
-	}
-	if err != nil {
-		return nil, err
-	}
-	return &result, nil
-
-}
-
-// GetType returns a price type from redis
-func (e *Engine) GetType(key string) (string, error) {
-	var result, err = e.clientCurrencyType.Get(key).Result()
-	if err == redis.Nil {
-		return "", nil
-	}
-	if err != nil {
-		return "", err
-	}
-	return result, nil
 }
