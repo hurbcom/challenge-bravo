d6565f72d9f8bd4c9ab48ab9a396784e685c60f8
joaofreitas <jhgfreitas@ext.worten.pt>
2022-06-20 14:15:39 -0300

    improve new expirate time

--- internal/repository/redis/set.go
+++ internal/repository/redis/set.go
@@ -7,7 +7,7 @@ import (
 
 // SetPrice currently price of currency on Redis
 func (e *Engine) SetPrice(key string, value string) error {
-	var err = e.client.Set(key, value, time.Hour*24).Err()
+	var err = e.client.Set(key, value, time.Hour*8).Err()
 	if err != nil {
 		return fmt.Errorf("error set price on redis: %w", err)
 	}
