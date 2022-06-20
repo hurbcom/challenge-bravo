b3425336f22f47979141dd3a48cc773bbc08aef5
joaofreitas <jhgfreitas@ext.worten.pt>
2022-06-19 22:44:49 -0300

    add domain currency update

---
+++ internal/domain/currency/update.go
@@ -0,0 +1,15 @@
+package currency
+
+import (
+	"context"
+	"github.com/joaohgf/challenge-bravo/internal/repository/models"
+)
+
+func (d *Domain) UpdateCode(ctx context.Context, code string, data models.Currency) (interface{}, error) {
+
+	result, err := d.repository.Update(ctx, code, &data)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
