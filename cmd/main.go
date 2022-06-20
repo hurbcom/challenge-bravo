d0534ce330ae1d10f6232f773d284fcd986c4387
joaofreitas <jhgfreitas@ext.worten.pt>
2022-06-19 22:41:59 -0300

    improve main

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"context"
+	"github.com/joaohgf/challenge-bravo/internal/api"
+	"github.com/joaohgf/challenge-bravo/internal/job"
+	"log"
+)
+
+// main is the entry point of the application
 func main() {
+	// main context of application
+	var ctx = context.Background()
+	// create the server
+	var server = api.NewServer(ctx)
+	// create the job
+	jobs := job.NewJob(server.Repository)
+	go jobs.Run(ctx)
+	var err = server.Run(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
