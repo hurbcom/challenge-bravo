5589b7c4ea8bd740157fac52c7f34187f68ed773
joaofreitas <jhgfreitas@ext.worten.pt>
2022-06-19 22:33:13 -0300

    add cron job

---
+++ internal/job/cron.go
@@ -0,0 +1,31 @@
+package job
+
+import (
+	"context"
+	"github.com/joaohgf/challenge-bravo/internal/action"
+	"github.com/joaohgf/challenge-bravo/internal/repository"
+	"github.com/robfig/cron/v3"
+)
+
+// Job is the job to be runned by the cron
+type Job struct {
+	Action action.Action
+}
+
+// NewJob creates a new job
+func NewJob(repository *repository.Engine) *Job {
+	return &Job{Action: action.NewUpdate(repository)}
+}
+
+// Run runs the jobs
+func (j *Job) Run(ctx context.Context) {
+	var c = cron.New(cron.WithChain())
+	var _, err = c.AddFunc("@every 10s", func() {
+		// job to update the currency
+		go j.Action.Act(ctx)
+	})
+	if err != nil {
+		c.Stop()
+	}
+	c.Start()
+}
