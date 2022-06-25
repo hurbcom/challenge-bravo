9739993a57da180e1e5867cdbd79ad7d96fa9db1
Etnan Sousa <poketnans@gmail.com>
2022-06-25 16:55:42 -0300

    feat: implements currencies model

---
+++ app/models/currencies_model.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from datetime import datetime
+from uuid import uuid4
+
+from sqlalchemy import Boolean, Column, DateTime, String
+
+from app.configs.database import db
+
+
+@dataclass
+class Currency(db.Model):
+
+    id: str
+    code: str
+    label: str
+    backing_currency: bool
+    created_at: datetime
+    updated_at: datetime
+
+    __tablename__ = "currencies"
+
+    id = Column(String, primary_key=True, default=str(uuid4()))
+    code = Column(String(50), unique=True, nullable=False)
+    label = Column(String(100), unique=True, nullable=False)
+    backing_currency = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.now())
+    updated_at = Column(DateTime, default=datetime.now())
