20244bc258e94fc75e66dcfa597fac0f19b3feca
Rodrigo V. Cortezi <rodrigovcortezi@gmail.com>
2022-06-26 13:37:34 -0300

    feat: add cors support

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,15 @@
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
+const cors = require('@koa/cors')
 
 const { currencyRouter } = require('./routes/currencyRoutes')
 const { conversionRouter } = require('./routes/conversionRoutes')
 
 const app = new Koa()
+
 app.use(bodyParser())
+app.use(cors())
+
 app.use(currencyRouter.routes())
 app.use(conversionRouter.routes())
 
