cfa4ec5b6b4c8aa19ba12920a3e43c756552cad3
Etnan Sousa <poketnans@gmail.com>
2022-07-18 21:38:38 -0300

    test: over param types validation

--- tests/integration/test_get_conversion.py
+++ tests/integration/test_get_conversion.py
@@ -84,3 +84,103 @@ def test_get_conversion_unregistered_currency_404(
     assert response.status_code == HTTPStatus.NOT_FOUND
 
     assert set(response.json).issuperset(expected), colorized(response)
+
+
+@mark.parametrize(
+    ("params", "missing"),
+    [
+        ("from=USD&to=BRL", ["amount"]),
+        ("from=USD&amount=BRL", ["to"]),
+        ("to=BRL&amount=1", ["from"]),
+        ("from=USD", ["to", "amount"]),
+        ("to=BRL", ["from", "amount"]),
+        ("amount=1", ["from", "to"]),
+        ("", ["from", "to", "amount"]),
+    ],
+)
+def test_missing_query_params(client: FlaskClient, params, missing, colorized):
+    """
+    GIVEN the conversion route
+    WHEN I do not inform necessary params
+    THEN I received correct error message
+    THEN I receive the status code 400
+    """
+
+    path = f"/api?{params}"
+
+    expected = {"error": "Missing params.", "missing": missing}
+
+    response = client.get(path)
+
+    assert set(response.json).issuperset(expected), colorized(response.json)
+
+    for key, value in response.json.items():
+        assert set(value).issuperset(expected[key]), colorized(response.json)
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+
+
+@mark.parametrize("from_curr", [123, "COIN02", "0.23"])
+def test_wrong_from_param_value_types(client, from_curr, colorized):
+    """
+    GIVEN the conversion route
+    WHEN I pass wrong value types to `from` param
+    THEN I received correct error message
+    THEN I receive the status code 400
+    """
+
+    path = f"/api?from={from_curr}&to=EUR&amount=1"
+
+    expected = {
+        "error": "Invalid value types.",
+        "from": {"expected": "alphabetical", "received": f"{from_curr}"},
+    }
+
+    response = client.get(path)
+
+    assert response.json == expected, colorized(response.json)
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+
+
+@mark.parametrize("to_curr", [123, "COIN02", "0.23"])
+def test_wrong_to_curr_param_value_types(client, to_curr, colorized):
+    """
+    GIVEN the conversion route
+    WHEN I pass wrong value types to `to` param
+    THEN I received correct error message
+    THEN I receive the status code 400
+    """
+
+    path = f"/api?from=EUR&to={to_curr}&amount=1"
+
+    expected = {
+        "error": "Invalid value types.",
+        "to": {"expected": "alphabetical", "received": f"{to_curr}"},
+    }
+
+    response = client.get(path)
+
+    assert response.json == expected, colorized(response.json)
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+
+
+@mark.parametrize("amount", ["abc", "USD"])
+def test_wrong_amount_param_value_types(client, amount, colorized):
+    """
+    GIVEN the conversion route
+    WHEN I pass wrong value types to `to` param
+    THEN I received correct error message
+    THEN I receive the status code 400
+    """
+
+    path = f"/api?from=EUR&to=BRL&amount={amount}"
+
+    expected = {
+        "error": "Invalid value types.",
+        "amount": {"expected": "floatable", "received": f"{amount}"},
+    }
+
+    response = client.get(path)
+
+    assert response.json == expected, colorized(response.json)
+    assert response.status_code == HTTPStatus.BAD_REQUEST
