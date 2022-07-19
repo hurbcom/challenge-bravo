3ef48d08d6d0a809fdff4268ffc821a645b50f4c
Etnan Sousa <poketnans@gmail.com>
2022-07-19 15:31:57 -0300

    refactor: adds custom error messages

--- app/schemas/conversion_schema.py
+++ app/schemas/conversion_schema.py
@@ -4,14 +4,25 @@ from marshmallow import Schema, fields, validate
 
 
 class ConversionSchema(Schema):
+    REGEX_ALPHA_PATTERN = r"^[a-z]+$"
+    REGEX_ERROR_MESSAGE = "Value must be alphabetical."
+
     from_curr = fields.Str(
-        validate=validate.Regexp(r"[a-z]+", flags=re.IGNORECASE),
+        validate=validate.Regexp(
+            REGEX_ALPHA_PATTERN,
+            flags=re.IGNORECASE,
+            error=REGEX_ERROR_MESSAGE,
+        ),
         required=True,
         data_key="from",
         attribute="from",
     )
     to_curr = fields.Str(
-        validate=validate.Regexp(r"[a-z]+", flags=re.IGNORECASE),
+        validate=validate.Regexp(
+            REGEX_ALPHA_PATTERN,
+            flags=re.IGNORECASE,
+            error=REGEX_ERROR_MESSAGE,
+        ),
         required=True,
         data_key="to",
         attribute="to",
