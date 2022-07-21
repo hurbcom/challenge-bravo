5f59dae252f994fa93c6ba6293ae641bb8842ced
Etnan Sousa <poketnans@gmail.com>
2022-07-21 13:35:52 -0300

    refactor: changes required params error message

--- app/schemas/conversion_schema.py
+++ app/schemas/conversion_schema.py
@@ -4,27 +4,22 @@ from marshmallow import Schema, fields, validate
 
 
 class ConversionSchema(Schema):
-    REGEX_ALPHA_PATTERN = r"^[a-z]+$"
-    REGEX_ERROR_MESSAGE = "Value must be alphabetical."
+    REQUIRED_ERROR_MESSAGE = "Missing param."
 
     from_curr = fields.Str(
-        validate=validate.Regexp(
-            REGEX_ALPHA_PATTERN,
-            flags=re.IGNORECASE,
-            error=REGEX_ERROR_MESSAGE,
-        ),
         required=True,
         data_key="from",
         attribute="from",
+        error_messages={"required": REQUIRED_ERROR_MESSAGE},
     )
     to_curr = fields.Str(
-        validate=validate.Regexp(
-            REGEX_ALPHA_PATTERN,
-            flags=re.IGNORECASE,
-            error=REGEX_ERROR_MESSAGE,
-        ),
         required=True,
         data_key="to",
         attribute="to",
+        error_messages={"required": REQUIRED_ERROR_MESSAGE},
     )
-    amount = fields.Decimal(required=True, places=4)
+    amount = fields.Decimal(
+        required=True,
+        places=4,
+        error_messages={"required": REQUIRED_ERROR_MESSAGE},
+    )
