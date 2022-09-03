75c4b8434798f78c4b4204b998b02d0648994ad7
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2022-09-03 17:11:55 -0300

    feat: add read operation to quote router
    tests passed

--- app/routers/quote.py
+++ app/routers/quote.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
+from sqlalchemy.orm import Session
+
+from app.database import get_db
+from app.operators.currency import CurrencyOperator
+from app.schemas import CurrencyOut, CurrencyResponse, MultipleCurrencyResponse
 
 
 
@@ -7,21 +12,30 @@ router = APIRouter(
     tags=["quote"]
 )
 
-@router.get("/", status_code=status.HTTP_200_OK)
-def read_quotes():
+
+@router.get("/", status_code=status.HTTP_200_OK, response_model=MultipleCurrencyResponse)
+def read_all_quotes(db: Session = Depends(get_db)):
     """ Return all currencies in both oficial and fantasy tables """
-    pass
+
+    currency = CurrencyOperator()
+    currency_list = currency.read_all(db=db)
+    return MultipleCurrencyResponse(data=currency_list)
 
 
-@router.get("/{currency_code}", status_code=status.HTTP_200_OK)
-def read_quote(currency_code: str):
+@router.get("/{currency_code}", status_code=status.HTTP_200_OK, response_model=CurrencyResponse)
+def read_quote(currency_code: str, db: Session = Depends(get_db)):
     """ Return one specific currency information """
-    pass
+
+    currency = CurrencyOperator(currency_code=currency_code)
+    currency_db = currency.read(db=db)
+    currency_output = CurrencyOut(**currency_db.dict())
+    return CurrencyResponse(data=currency_output)
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def create_quote():
     """ Create new quote in `fantasy_coins` table """
+    pass
 
 
 @router.put("/{currency_code}", status_code=status.HTTP_200_OK)
