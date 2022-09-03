4ff43e35a60bffa311a5169f5b082f1ff050c982
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2022-09-03 02:18:19 -0300

    feat: create schema for currency input validation

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, validator, root_validator, Field
 from typing import Optional
+from datetime import datetime
 
 
 
@@ -18,3 +19,31 @@ class Currency(BaseModel):
     def uppercase_backed_by(cls, backed_by: str):
         """ force backed currency code to uppercase """
         return backed_by.upper()
+
+
+class CurrencyInput(Currency):
+    rate: Optional[float]
+    backed_by: str
+    amount: Optional[float]
+    backed_currency_amount: Optional[float]
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
+    currency_type: Optional[str]
+
+    @root_validator(pre=True)
+    def check_rate_or_amount(cls, values):
+        """ Checks if either `rate` value or (`amount` and `backed_currency_amount`) are provided """
+        if "rate" not in values and ("amount" not in values or "backed_currency_amount" not in values):
+            raise ValueError("You should provide whether a rate field or an amount and backed_currency_amount fields")
+        elif "rate" in values and ("amount" in values or "backed_currency_amount" in values):
+            raise ValueError("You should provide only a rate field or an amount and backed_currency_amount fields")
+        return values
+
+    @root_validator(pre=True)
+    def get_rate_from_input(cls, values):
+        """ Calculates the `rate` field"""
+        if "rate" in values:
+            return values
+        values['rate'] = values['amount'] / values['backed_currency_amount']
+        values['amount'] = None
+        values['backed_currency_amount'] = None
+        return values
