d56438766a7ed40e5acab3d13c21094c9cb458dc
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2022-09-03 00:02:01 -0300

    test: set up database for tests and create fixtures for setup and teardown

---
+++ tests/unit_tests/pg-init-script/create-test-postgresql-database.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+set -u
+
+function create_user_and_database() {
+	local database=$1
+	echo "  Creating user and database '$database'"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+	    CREATE USER $database;
+	    CREATE DATABASE $database;
+	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+EOSQL
+}
+    create_user_and_database currency_converter_test
+	echo "Test database created"
+fi
