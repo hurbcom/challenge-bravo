11d1539561aea8cd9f6a2a723810c53bf659fb1b
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2022-09-05 07:36:55 -0300

    perf: increase number of database connections

--- app/database.py
+++ app/database.py
@@ -8,7 +8,7 @@ from app.config import settings
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=2500, max_overflow=20)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
