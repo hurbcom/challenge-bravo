3b7119ae6245da86e59f64dd4975d86822396265
Moises Junior <m.junior@eq.ufrj.br>
2022-09-22 17:20:27 -0300

    docs: api documentation using Swagger

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import express, { json } from 'express'
 import './setup.js'
 import 'express-async-errors'
+import swaggerUi from 'swagger-ui-express'
 import cors from 'cors'
 import initRedis from './db.js'
 import router from './routes/routes.js'
 import errorHandler from './middlewares/errorHandlingMiddleware.js'
+import swaggerDocs from '../swagger.json' assert { type: 'json' }
 
 const app = express()
 
 app.use(cors())
 app.use(json())
 app.use(router)
+app.use('/api-documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use(errorHandler)
 
 export function init() {
