7a072199db89bf8e738594361ee8a9e3037adb6d
Gustavo Presa <gustavo@vcx.solutions>
2022-10-10 19:19:51 -0300

    chore: renomear projeto Django

--- bravo_exchanger/wsgi.py
+++ bravo_exchanger/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for config project.
+WSGI config for bravo_exchanger project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bravo_exchanger.settings')
 
 application = get_wsgi_application()
