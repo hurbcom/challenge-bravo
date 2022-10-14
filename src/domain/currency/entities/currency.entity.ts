4d678de6f0ebb4631e9a0cd57edc88d7a2850331
hdbenosx10 <benosx03@gmail.com>
2022-10-14 00:03:24 -0300

    chore: change property name

--- src/domain/currency/entities/currency.entity.ts
+++ src/domain/currency/entities/currency.entity.ts
@@ -1,24 +1,38 @@
 import Big from 'big.js';
+import { randomUUID } from 'crypto';
 
 export class Currency {
-  private _id!: string;
-  private _currency!: string;
-  private _backingCurrency?: string;
-  private _unitCost!: string;
+  private _id: string;
+  private _code: string;
+  private _backingCurrency: string;
+  private _unitCost: string;
 
-  constructor(id: string, currency: string, unitCost: string, backingCurrency = 'USD') {
-    this._id = id;
-    this._currency = currency;
+  constructor(code: string, unitCost: string, backingCurrency = 'USD') {
+    this._id = randomUUID();
+    this._code = code;
     this._unitCost = unitCost;
     this._backingCurrency = backingCurrency;
+    this.validate();
+  }
+
+  validate() {
+    if(this._unitCost.length === 0 || !Number(this._unitCost)) {
+      throw new Error('Invalid unit cost');
+    }
+    if(this._code.length === 0) {
+      throw new Error('Invalid code');
+    }
+    if(this._backingCurrency.length === 0) {
+      throw new Error('Invalid backingCurrency');
+    }
   }
 
   get id(): string {
     return this._id;
   }
   
-  get currency(): string {
-    return this._currency;
+  get code(): string {
+    return this._code;
   }
 
   get backingCurrency(): string | undefined {
@@ -30,12 +44,12 @@ export class Currency {
   }
 
   convert(to: Currency, amount: string) {
-    if(this._backingCurrency === to.currency) {
+    if(this._backingCurrency === to.code) {
       return amount;
     }
 
     if(this._backingCurrency !== to.backingCurrency) {
-      throw new Error(`This currency has no relation with provided "to" (${to.currency})`);
+      throw new Error(`This code has no relation with provided "to" (${to.code})`);
     }
     
     return new Big(this._unitCost)
