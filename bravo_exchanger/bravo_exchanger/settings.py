1072b0c1ca9db7c2c78f6c788d01e21f1d3050dc
Gustavo Presa <gustavo@vcx.solutions>
2022-10-15 20:43:01 -0300

    chore: mover projeto de backend para novo diretorio

--- bravo_exchanger/bravo_exchanger/settings.py
+++ bravo_exchanger/bravo_exchanger/settings.py
@@ -29,7 +29,7 @@ CACHES = {
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'asd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
