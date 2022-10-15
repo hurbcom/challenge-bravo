65b38e6e5745672f57704554b5e8b15aa8a15d17
Gustavo Presa <gustavo@vcx.solutions>
2022-10-15 01:56:18 -0300

    style: corrigir ordenação de imports

--- currency/api/views.py
+++ currency/api/views.py
@@ -1,6 +1,6 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status
 
 from currency.api.serializer import FictionalCurrencySerializer
 from currency.converters import get_currency_conversion_data
