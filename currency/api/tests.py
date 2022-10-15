f0f71e3b8784dee5fbfbabd3c74e41ff73de0f67
Gustavo Presa <gustavo@vcx.solutions>
2022-10-15 15:49:51 -0300

    test: mover fixtures currency_short_name, fictional_currency e fictional_currency_data para o conftest.py

--- currency/api/tests.py
+++ currency/api/tests.py
@@ -13,31 +13,8 @@ pytestmark = pytest.mark.django_db
 
 
 @pytest.fixture
-def currency_short_name(fake) -> str:
-    return fake.currency_code()
-
-
-@pytest.fixture
 def expected_currency(fictional_currency) -> dict:
     return model_to_dict(fictional_currency, exclude='id')
-
-
-@pytest.fixture
-def fictional_currency(fictional_currency_data) -> FictionalCurrency:
-    return FictionalCurrency.objects.create(
-        currency_amount = fictional_currency_data['currency_amount'],
-        currency_backing = fictional_currency_data['currency_backing'],
-        currency_short_name = fictional_currency_data['currency_short_name'],
-    )
-
-
-@pytest.fixture
-def fictional_currency_data(currency_short_name, fake) -> dict:
-    return {
-        'currency_amount': fake.pyfloat(left_digits=2, positive=True, right_digits=2),
-        'currency_backing': currency_short_name,
-        'currency_short_name': fake.currency_code(),
-    }
 
 
 class TestFictionalCurrenciesView:
