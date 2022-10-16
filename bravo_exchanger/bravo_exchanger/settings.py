1500333d3552ccb95c46922f3474616b4750e2c2
Gustavo Presa <gustavo@vcx.solutions>
2022-10-16 10:06:04 -0300

    feat: adicionar docker-compose no projeto e ajustar Dockerfile

--- bravo_exchanger/bravo_exchanger/settings.py
+++ bravo_exchanger/bravo_exchanger/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'bravo_exchanger.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.environ['DB_ENGINE'],
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
     }
 }
 
