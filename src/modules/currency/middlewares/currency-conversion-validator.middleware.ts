c09905071a064424e4d45f0d757f7432f0b648bb
sampaiorafael <rafasampaio021@hotmail.com>
2022-10-17 20:48:30 -0300

    feat: currency conversion validator middleware

---
+++ src/modules/currency/middlewares/currency-conversion-validator.middleware.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from 'express'
+import joi from 'joi'
+
+export class CurrencyConversionValidator {
+  public async validateSchema(req: Request, res: Response, next: NextFunction) {
+    const schema = joi.object({
+      from: joi.string()
+        .alphanum()
+        .min(1)
+        .max(10)
+        .required(),
+  
+      to: joi.string()
+        .alphanum()
+        .min(1)
+        .max(10)
+        .required(),
+      
+      amount: joi.number()
+        .positive()
+        .required()
+    })
+
+    const validation = schema.validate(req.query, { abortEarly: false })
+
+    if(validation.error) {
+      return res.status(400).send({
+        message: 'Was not possible to validate your request payload',
+        error: validation.error
+      })
+    }
+
+    next()
+  }
+}
