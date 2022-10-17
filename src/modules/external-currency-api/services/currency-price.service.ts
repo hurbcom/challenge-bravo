793ff8d0fdb47f062604d727f25f6042d6219e18
sampaiorafael <rafasampaio021@hotmail.com>
2022-10-17 20:13:12 -0300

    refactor: error handling

--- src/modules/external-currency-api/services/currency-price.service.ts
+++ src/modules/external-currency-api/services/currency-price.service.ts
@@ -3,9 +3,13 @@ import { CurrencyPriceResponseDTO } from '../dtos/currency-price.dto'
 
 export class CurrencyPriceService {
   public async execute(fromCurrencyCode: string, toCurrencyCode: string): Promise<CurrencyPriceResponseDTO> {
-    const currencyPriceResponse = await axios.get(`https://economia.awesomeapi.com.br/last/${fromCurrencyCode}-${toCurrencyCode}`)
-    if(currencyPriceResponse.status != 200) throw new Error('Was not possible to find this currency price')
-    const currencyPriceResponseKeys = Object.keys(currencyPriceResponse.data)
-    return currencyPriceResponse.data[currencyPriceResponseKeys[0]]
+    try {
+      const currencyPriceResponse = await axios.get(`https://economia.awesomeapi.com.br/last/${fromCurrencyCode}-${toCurrencyCode}`)
+      if(currencyPriceResponse.status != 200) return null
+      const currencyPriceResponseKeys = Object.keys(currencyPriceResponse.data)
+      return currencyPriceResponse.data[currencyPriceResponseKeys[0]] 
+    } catch (error) { 
+      return null 
+    }
   }
 }
