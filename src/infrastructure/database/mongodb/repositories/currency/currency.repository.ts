15681ed3ff77c0842d364fa5ac94ed8215a4ba29
hdbenosx10 <benosx03@gmail.com>
2022-10-17 12:37:50 -0300

    feat: add mongodb currency repository

---
+++ src/infrastructure/database/mongodb/repositories/currency/currency.repository.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client';
+import { Currency } from '../../../../../domain/currency/entities/currency.entity';
+import { CurrencyRepository } from '../../../../../domain/currency/repositories/currency.repository';
+
+export class MongoCurrencyRepository implements CurrencyRepository {
+  constructor(private readonly prismaClient: PrismaClient) {}
+  async findByCurrencyCode(code: string): Promise<Currency | null> {
+    const result = await this.prismaClient.currency.findFirst({
+      where: {
+        code
+      }
+    });
+    if (!result) return null;
+    return new Currency(
+      result.code,
+      result.unitCost,
+      result.backingCurrency,
+      result.id
+    );
+  }
+  async create(entity: Currency): Promise<void> {
+    await this.prismaClient.currency.create({
+      data: {
+        id: entity.id,
+        backingCurrency: entity.backingCurrency,
+        code: entity.code,
+        unitCost: entity.unitCost
+      }
+    });
+  }    
+}
