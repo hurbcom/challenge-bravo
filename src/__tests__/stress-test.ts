f27c684c5171169ca31322913e70412433739a05
sampaiorafael <rafasampaio021@hotmail.com>
2022-10-18 11:05:07 -0300

    feat: stress test only on cli

--- src/__tests__/stress-test.ts
+++
@@ -1,8 +0,0 @@
-import * as autocannon from 'autocannon'
-
-autocannon.default({
-  url: 'http://localhost:8080/currency/conversion?from=EUR&amount=1&to=BRL',
-  connections: 50,
-  amount: 1000,
-  duration: 1,
-}, (err, result) => console.log(result))
