e7a408f95bbba0ffae7a755368b074a3aec42973
Felipe Pinheiro <lipesousap@gmail.com>
2022-10-18 12:22:11 -0300

    fix: added custom ports

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('docs', app, document);
 
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
