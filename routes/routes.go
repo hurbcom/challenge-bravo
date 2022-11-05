8ccba22643a0f516d0f157b759526aae5bff3b53
felipepnascimento <felipeplnasc92@gmail.com>
2022-11-04 21:19:57 -0300

    remove delete

--- routes/routes.go
+++ routes/routes.go
@@ -10,7 +10,6 @@ func HandleRequests() {
 	r.GET("currency", controllers.ListCurrencies)
 	r.POST("currency", controllers.CreateCurrency)
 	r.GET("currency/:id", controllers.ShowCurrency)
-	r.DELETE("currency/:id", controllers.DeleteCurrency)
 
 	r.GET("conversion", controllers.Convert)
 
