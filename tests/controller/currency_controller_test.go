2aa6ea050f92f73e48709454d8a39088f29df877
felipepnascimento <felipeplnasc92@gmail.com>
2022-11-04 21:20:21 -0300

    currency controller tests

--- tests/controller/currency_controller_test.go
+++ tests/controller/currency_controller_test.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -17,16 +20,14 @@ var ID int
 
 func SetupTestsRoutes() *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
-	rotas := gin.Default()
-	return rotas
+	routes := gin.Default()
+	return routes
 }
 
 func CreateCurrencyMock() {
-	currency := models.Currency{Key: "USD", Description: "USD", Enabled: true}
+	currency := models.Currency{Key: "USD", Description: "USD description", QuotationType: "QuotationType"}
 	database.DB.Create(&currency)
 	ID = int(currency.ID)
-	fmt.Println("Opa")
-	fmt.Println(ID)
 }
 
 func DeleteCurrencyMock() {
@@ -39,11 +40,48 @@ func TestListCurrencies(t *testing.T) {
 	CreateCurrencyMock()
 	defer DeleteCurrencyMock()
 	r := SetupTestsRoutes()
-	r.GET("/currency", controllers.ListCurrencies)
+	r.GET("currency", controllers.ListCurrencies)
+
 	req, _ := http.NewRequest("GET", "/currency", nil)
-	resposta := httptest.NewRecorder()
-	r.ServeHTTP(resposta, req)
-	assert.Equal(t, http.StatusOK, resposta.Code)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, req)
+
+	assert.Equal(t, http.StatusOK, response.Code)
+}
+
+func TestCreateCurrencyWithNilValues(t *testing.T) {
+	r := SetupTestsRoutes()
+	r.POST("currency", controllers.CreateCurrency)
+
+	req, _ := http.NewRequest("POST", "/currency", nil)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, req)
+
+	assert.Equal(t, http.StatusBadRequest, response.Code)
+}
+
+func TestCreateCurrency(t *testing.T) {
+	database.ConnectDatabase()
+	defer DeleteCurrencyMock()
+
+	r := SetupTestsRoutes()
+	r.POST("currency", controllers.CreateCurrency)
+
+	var jsonStr = []byte(`{"key":"created key", "description": "created description", "quotationType": "created quotationType"}`)
+
+	req, _ := http.NewRequest("POST", "/currency", bytes.NewBuffer(jsonStr))
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, req)
+
+	var expectedCurrency models.Currency
+	json.Unmarshal(response.Body.Bytes(), &expectedCurrency)
+
+	ID = int(expectedCurrency.ID)
+
+	assert.Equal(t, http.StatusOK, response.Code)
+	assert.Equal(t, "created key", expectedCurrency.Key)
+	assert.Equal(t, "created description", expectedCurrency.Description)
+	assert.Equal(t, "created quotationType", expectedCurrency.QuotationType)
 }
 
 func TestShowCurrency(t *testing.T) {
@@ -51,9 +89,32 @@ func TestShowCurrency(t *testing.T) {
 	CreateCurrencyMock()
 	defer DeleteCurrencyMock()
 	r := SetupTestsRoutes()
-	r.GET("/currency/:cpf", controllers.ShowCurrency)
+	r.GET("currency/:id", controllers.ShowCurrency)
+
 	req, _ := http.NewRequest("GET", fmt.Sprintf("/currency/%d", ID), nil)
-	resposta := httptest.NewRecorder()
-	r.ServeHTTP(resposta, req)
-	assert.Equal(t, http.StatusOK, resposta.Code)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, req)
+
+	var expectedCurrency models.Currency
+	json.Unmarshal(response.Body.Bytes(), &expectedCurrency)
+
+	assert.Equal(t, http.StatusOK, response.Code)
+	assert.Equal(t, "USD", expectedCurrency.Key)
+	assert.Equal(t, "USD description", expectedCurrency.Description)
+	assert.Equal(t, "QuotationType", expectedCurrency.QuotationType)
+}
+
+func TestShowCurrencyNotFound(t *testing.T) {
+	r := SetupTestsRoutes()
+	r.GET("currency/:id", controllers.ShowCurrency)
+
+	req, _ := http.NewRequest("GET", "/currency/1", nil)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, req)
+
+	expectedResponse := `{"Not found":"Currency not found"}`
+	responseBody, _ := ioutil.ReadAll(response.Body)
+
+	assert.Equal(t, http.StatusNotFound, response.Code)
+	assert.Equal(t, expectedResponse, string(responseBody))
 }
