dc52dc625b89332f6053d11e1134fb87a76f2dc7
felipepnascimento <felipeplnasc92@gmail.com>
2022-11-08 20:33:00 -0300

    remove script to create database

--- postgres.Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM postgres
-WORKDIR /docker-entrypoint-initdb.d
-ADD ./sql/currency.sql /docker-entrypoint-initdb.d
-EXPOSE 5432
--- sql/currency.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS currencies
-(
-    id          serial PRIMARY KEY,
-    key    VARCHAR(128) NOT NULL,
-    description      VARCHAR(256)         NOT NULL,
-    created_at  timestamptz  NOT NULL DEFAULT Now(),
-    updated_at timestamptz  NOT NULL DEFAULT Now()
-)
