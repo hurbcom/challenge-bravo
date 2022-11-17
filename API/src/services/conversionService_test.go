420fb6969c024e8902d8d4c7e1e8d1d9bf8ef099
dellatorreadrian <dellatorre.adrian@gmail.com>
2022-11-17 20:38:46 -0300

    test: refactor conversionService scenarios

--- API/src/services/conversionService_test.go
+++ API/src/services/conversionService_test.go
@@ -78,56 +78,56 @@ func (searchConversionService *SearchConversionServiceMock) GetCurrencyFromDatab
 	return currency, nil
 }
 
-type scenario struct {
+type conversionScenario struct {
 	fromCurrencyName       string
 	toCurrencyName         string
 	amount                 float64
 	expectedConvertedValue float64
 }
 
-var successScenarios = []scenario{
-	{
-		fromCurrencyName:       "USD",
-		toCurrencyName:         "BRL",
-		amount:                 1.0,
-		expectedConvertedValue: 5.36,
-	},
-	{
-		fromCurrencyName:       "USD",
-		toCurrencyName:         "EUR",
-		amount:                 7.0,
-		expectedConvertedValue: 6.7571,
-	},
-	{
-		fromCurrencyName:       "USD",
-		toCurrencyName:         "BTC",
-		amount:                 1.0,
-		expectedConvertedValue: 0.00005992,
-	},
-	{
-		fromCurrencyName:       "USD",
-		toCurrencyName:         "ETH",
-		amount:                 10.0,
-		expectedConvertedValue: 0.008256,
-	},
-}
-
-var errorScenarios = []scenario{
-	{
-		fromCurrencyName:       "XONPLK",
-		toCurrencyName:         "ZENIAT",
-		amount:                 10.0,
-		expectedConvertedValue: 0,
-	},
-	{
-		fromCurrencyName:       "USD",
-		toCurrencyName:         "ZENIAT",
-		amount:                 10.0,
-		expectedConvertedValue: 0,
-	},
-}
-
 func TestConvertCurrency(t *testing.T) {
+
+	successScenarios := []conversionScenario{
+		{
+			fromCurrencyName:       "USD",
+			toCurrencyName:         "BRL",
+			amount:                 1.0,
+			expectedConvertedValue: 5.36,
+		},
+		{
+			fromCurrencyName:       "USD",
+			toCurrencyName:         "EUR",
+			amount:                 7.0,
+			expectedConvertedValue: 6.7571,
+		},
+		{
+			fromCurrencyName:       "USD",
+			toCurrencyName:         "BTC",
+			amount:                 1.0,
+			expectedConvertedValue: 0.00005992,
+		},
+		{
+			fromCurrencyName:       "USD",
+			toCurrencyName:         "ETH",
+			amount:                 10.0,
+			expectedConvertedValue: 0.008256,
+		},
+	}
+
+	errorScenarios := []conversionScenario{
+		{
+			fromCurrencyName:       "XONPLK",
+			toCurrencyName:         "ZENIAT",
+			amount:                 10.0,
+			expectedConvertedValue: 0,
+		},
+		{
+			fromCurrencyName:       "USD",
+			toCurrencyName:         "ZENIAT",
+			amount:                 10.0,
+			expectedConvertedValue: 0,
+		},
+	}
 
 	searchService := SearchConversionServiceMock{}
 	conversionService := services.ConversionService{SearchService: &searchService}
