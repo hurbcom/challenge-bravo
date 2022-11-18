bc92735568bea28abb0a7386f85b03e547469164
Vinicius Nunes <viniiciuspereiira@hotmail.com>
2022-11-18 17:15:29 -0300

    docs: [wip] adding a swagger doc

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,4 @@ COPY . .
 
 EXPOSE 3000
 
-CMD nodemon -L --watch . src/server.js
+CMD nodemon -e yaml -L --watch . src/server.js
--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDoc = YAML.load('./src/swagger.yaml')
 
 app.use(bodyParser.json())
-
 app.use(express.urlencoded({ extended: false }))
+app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 
 module.exports = app
