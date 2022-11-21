f155dd29690f7b3df8e8a586d6595f5410934aaf
Vinicius Nunes <viniiciuspereiira@hotmail.com>
2022-11-21 17:31:36 -0300

    test: more some tests

---
+++ __mock__/api/quotation.js
@@ -0,0 +1,23 @@
+exports.AWESOME_API_SUCCESS_RESPONSE = {
+	BRLUSD: {
+		code: 'BRL',
+		codein: 'USD',
+		name: 'Real Brasileiro/Dólar Americano',
+		high: '0.1888',
+		low: '0.1858',
+		varBid: '0.0018',
+		pctChange: '0.98',
+		bid: '0.1875',
+		ask: '0.1876',
+		timestamp: '1669039772',
+		create_date: '2022-11-21 11:09:32',
+	},
+}
+
+exports.LAST_QUOTATION_SUCCESS_RESPONSE = [
+	{
+		code: 'BRL',
+		name: 'Real Brasileiro',
+		quotation: { buy: '0.1875', sell: '0.1876' },
+	},
+]
