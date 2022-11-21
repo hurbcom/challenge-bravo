0082fe1c78734d4b68aa74e5e07671af749971b2
Vinicius Nunes <viniiciuspereiira@hotmail.com>
2022-11-21 13:07:33 -0300

    feat: route to update appi quotations

--- src/repository/coin.js
+++ src/repository/coin.js
@@ -1,5 +1,10 @@
 const coinModel = require('../models/coin')
 
+/**
+ * Busca todas as moedas baseada na sua origem - API ou MANUAL
+ * @param {string} origin Origeem da cotação [API ou MANUAL]
+ * @returns {Promise<Array<Document>>} Array dos registros encontrados
+ */
 exports.findAllByOrigin = (origin) => {
 	return coinModel
 		.find({
@@ -14,4 +19,22 @@ exports.findAllByOrigin = (origin) => {
 			)
 			throw err
 		})
+}
+
+/**
+ * Realiza a atualização das cotações de uma moeda
+ * @param {Object} coin Objeto da Moeda contendo seu código e cotação
+ * @returns {Promise<Document>} Resultado do update
+ */
+exports.updateCoinQuotation = (coin) => {
+	const mongoQuery = { code: coin.code }
+	const mongoUpdate = {
+		quotation: {
+			buy: coin.quotation.buy,
+			sell: coin.quotation.sell,
+		},
+	}
+	const mongoOptions = { new: true }
+
+	return coinModel.findOneAndUpdate(mongoQuery, mongoUpdate, mongoOptions)
 }
