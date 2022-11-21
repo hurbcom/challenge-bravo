8d194457d5c124dee0697e2f31d7f632d234f5d3
Vinicius Nunes <viniiciuspereiira@hotmail.com>
2022-11-21 00:09:10 -0300

    refactor: changing the module export to index file insiide a folder

---
+++ src/controllers/quotation.js
@@ -0,0 +1,7 @@
+const models = require('../models')
+const api = require('../api')
+const repository = require('../repository')
+
+exports.updateApiQuotatiions = () => {
+	throw 'Not Implemented'
+}
--- src/controllers/quotation/index.js
+++
@@ -1,7 +0,0 @@
-const { getLastQuotation } = require('../../api/quotation')
-
-exports.currencyConverter = async (from, to, value) => {
-	const res = await getLastQuotation(from, to)
-
-	return 'ok'
-}
