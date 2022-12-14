5715caa4d8fb73e78f5f09910feb2808e9058a49
Thiago Assis <thiago.medassis@gmail.com>
2022-12-14 15:15:47 -0300

    currency model

--- exchange/core/models.py
+++ exchange/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+    FIAT = 'fiat'
+    CRYPTO = 'crypto'
+    FICTIONAL = 'fictional'
+    TYPES = (
+        (FIAT, 'FIAT'),
+        (CRYPTO, 'Crypto'),
+        (FICTIONAL,'Fictional'),
+    )
+
+    code = models.CharField(max_length=10, unique=True, db_index=True)
+    rate = models.FloatField()
+    type = models.CharField(max_length=50, choices=TYPES, default=FIAT)
+    backed_to = models.CharField(max_length=10, default='USD')
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.code
