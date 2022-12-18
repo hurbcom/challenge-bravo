85eb74f53eef4d66744a41e22b65f68e050ffe6b
Thiago Assis <thiago.medassis@gmail.com>
2022-12-18 11:33:23 -0300

    delete unnecessary files and imports

--- exchange/core/views.py
+++ exchange/core/views.py
@@ -2,8 +2,7 @@ from django.conf import settings
 from django.core.cache import cache
 from django.http import Http404
 from django.shortcuts import get_object_or_404
-from rest_framework import views, viewsets
-from rest_framework.decorators import action
+from rest_framework import viewsets
 from rest_framework.response import Response
 
 from exchange.core.models import Currency
