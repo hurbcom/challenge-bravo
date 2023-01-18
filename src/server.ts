ba98b859b11c3becc394d947efca13419bf31903
IgorVeludo2 <velvet.guimaraes@gmail.com>
2023-01-18 16:11:33 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+
+import express from 'express'
+
+import { Router, Request, Response } from 'express';
+
+const app = express();
+
+const route = Router()
+
+app.use(express.json())
+
+route.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'hello world with Typescript' })
+})
+
+app.use(route)
+
+
+app.listen(3333, () => console.log('server running on port 3333'))
