941a2bbd5e8ad55633a053389eec213d88c73819
LukeDias42 <lhollowwizard@gmail.com>
2023-01-19 02:50:34 -0300

    feat: set up all current services

--- src/Cuco.Application/Extensions/SetupCucoApplicationsExtension.cs
+++ src/Cuco.Application/Extensions/SetupCucoApplicationsExtension.cs
@@ -1,4 +1,9 @@
+using Cuco.Application.AddCurrency;
+using Cuco.Application.CurrencyConversion;
+using Cuco.Application.DeleteCurrency;
+using Cuco.Application.ListCurrencies;
 using Cuco.Application.Tests;
+using Cuco.Application.UpdateCurrency;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Cuco.Application.Extensions;
@@ -7,5 +12,10 @@ public static class SetupCucoApplicationsExtension
 {
     public static IServiceCollection SetupApplicationCucoApi(this IServiceCollection services)
         => services
+            .AddSingleton<IAddCurrencyService, AddCurrencyService>()
+            .AddSingleton<IUpdateCurrencyService, UpdateCurrencyService>()
+            .AddSingleton<IListCurrenciesService, ListCurrenciesService>()
+            .AddSingleton<IDeleteCurrencyService, DeleteCurrencyService>()
+            .AddSingleton<ICurrencyConversionService, CurrencyConversionService>()
             .AddSingleton<IRedisPing, RedisPing>();
 }
