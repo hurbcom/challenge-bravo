90af6a023841a79c353c793612da2a491f0783b6
LukeDias42 <lhollowwizard@gmail.com>
2023-01-19 02:20:07 -0300

    feat: add Delete and Update Currency Services

---
+++ src/Cuco.Application/DeleteCurrency/DeleteCurrencyService.cs
@@ -0,0 +1,9 @@
+namespace Cuco.Application.DeleteCurrency;
+
+public class DeleteCurrencyService : IDeleteCurrencyService
+{
+    public Task<bool> DeleteCurrency(string currencyName)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Cuco.Application/UpdateCurrency/UpdateCurrencyService.cs
@@ -0,0 +1,9 @@
+namespace Cuco.Application.UpdateCurrency;
+
+public class UpdateCurrencyService : IUpdateCurrencyService
+{
+    public Task<bool> UpdateCurrency(string currency, string baseCurrency, decimal newValue)
+    {
+        throw new NotImplementedException();
+    }
+}
