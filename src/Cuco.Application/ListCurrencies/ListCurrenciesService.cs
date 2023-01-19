5dcbd3cda2eef338b35468fcd66c4c3b593649d3
LukeDias42 <lhollowwizard@gmail.com>
2023-01-19 02:50:49 -0300

    feat: add List Currencies Service

---
+++ src/Cuco.Application/ListCurrencies/ListCurrenciesService.cs
@@ -0,0 +1,8 @@
+namespace Cuco.Application.ListCurrencies;
+internal class ListCurrenciesService : IListCurrenciesService
+{
+    public Task<IList<string>> ListCurrencies()
+    {
+        throw new NotImplementedException();
+    }
+}
