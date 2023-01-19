44e25f4797d95908c45fa7d10a6fd2616f0f5e09
LukeDias42 <lhollowwizard@gmail.com>
2023-01-19 04:33:27 -0300

    feat: partial implementation of service that converts currencies

--- src/Cuco.Application/CurrencyConversion/CurrencyConversionService.cs
+++ src/Cuco.Application/CurrencyConversion/CurrencyConversionService.cs
@@ -1,9 +1,20 @@
+using Cuco.Application.GetCurrencyInUSD;
+
 namespace Cuco.Application.CurrencyConversion;
 internal class CurrencyConversionService : ICurrencyConversionService
 {
-    public async Task<decimal> ConvertCurrency(string fromCurrency, string toCurrency, decimal value)
+    private readonly IGetCurrencyInUSDService _getCurrencyInUSDService;
+    public async Task<decimal> ConvertCurrency(string fromCurrency, string toCurrency, decimal amount)
     {
-        if (value <= 0) return 0;
-        return 0;
+        if (string.Equals(fromCurrency, toCurrency, StringComparison.OrdinalIgnoreCase)) return amount;
+
+        if (await _getCurrencyInUSDService.GetCurrencyInUSDAsync(fromCurrency) is not { } fromCurrencyInUSD
+            || await _getCurrencyInUSDService.GetCurrencyInUSDAsync(toCurrency) is not { } toCurrencyInUSD)
+            return 0;
+
+        return ConvertValue(fromCurrencyInUSD, toCurrencyInUSD, amount);
     }
+
+    private decimal ConvertValue(decimal fromCurrencyInUSD, decimal toCurrencyInUSD, decimal amount)
+        => (toCurrencyInUSD / fromCurrencyInUSD) * amount;
 }
