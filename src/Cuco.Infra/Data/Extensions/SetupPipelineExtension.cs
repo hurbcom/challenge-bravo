1693055f75e666523e0735842ba428bca5d9761a
LukeDias42 <lhollowwizard@gmail.com>
2023-01-18 23:45:49 -0300

    refactor: null check on context

--- src/Cuco.Infra/Data/Extensions/SetupPipelineExtension.cs
+++ src/Cuco.Infra/Data/Extensions/SetupPipelineExtension.cs
@@ -8,11 +8,9 @@ public static class SetupPipelineExtensions
 {
     public static IApplicationBuilder SetupDataPipeline(this IApplicationBuilder app)
     {
-        using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
-        {
-            using var context = serviceScope.ServiceProvider.GetService<CucoDbContext>();
-            context.Database.Migrate();
-        }
+        using var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
+        using var context = serviceScope.ServiceProvider.GetService<CucoDbContext>();
+        context?.Database.Migrate();
 
         return app;
     }
