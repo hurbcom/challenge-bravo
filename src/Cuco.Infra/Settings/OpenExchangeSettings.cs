d0964d0246536903dfd990119c937571b234afe2
LukeDias42 <lhollowwizard@gmail.com>
2023-01-20 14:19:35 -0300

    feat: added open exchange settings for external API communication

---
+++ src/Cuco.Infra/Settings/OpenExchangeSettings.cs
@@ -0,0 +1,8 @@
+namespace Cuco.Infra.Settings;
+
+public class OpenExchangeSettings
+{
+    public string Url { get; set; }
+    public string AppId { get; set; }
+    public int TimeToUpdateInSeconds { get; set; }
+}
