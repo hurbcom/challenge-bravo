42ccd36191ec295499cd1d91ba6dff7609c74afd
IgorVeludo2 <velvet.guimaraes@gmail.com>
2023-01-22 19:52:22 -0300

    Quotations migration

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:latest
+FROM node
 
 WORKDIR /usr/app
 
---
+++ src/database/migrations/1674423830519-CreateQuotations.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateQuotations1674423830519 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "coins",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                    },
+                    {
+                        name: "code",
+                        type: "varchar",
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                    },
+                    {
+                        name: "high",
+                        type: "varchar",
+                    },
+                    {
+                        name: "low",
+                        type: "varchar",
+                    },
+                    {
+                        name: "type",
+                        type: "varchar",
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                ],
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("coins");
+    }
+}
