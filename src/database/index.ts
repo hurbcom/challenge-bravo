b372c748448e258da1885f777695abbb9d1c23bd
IgorVeludo2 <velvet.guimaraes@gmail.com>
2023-01-22 18:38:28 -0300

    typeorm config

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,13 +1,3 @@
-import { DataSource } from "typeorm";
+import { createConnection } from "typeorm";
 
-const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    username: "admin",
-    password: "admin",
-    database: "challenge-bravo",
-});
-
-AppDataSource.initialize()
-    .then(() => console.log("Data Source has been initialized!"))
-    .catch(() => console.log("Error during Data Source initialization"));
+createConnection();
