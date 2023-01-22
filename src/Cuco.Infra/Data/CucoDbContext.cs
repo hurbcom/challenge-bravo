2beebfdb673e157bd332dcfba0ad4f74b9f49939
LukeDias42 <lhollowwizard@gmail.com>
2023-01-22 12:25:18 -0300

    feat: refactor: less lines of code

--- src/Cuco.Infra/Data/CucoDbContext.cs
+++ src/Cuco.Infra/Data/CucoDbContext.cs
@@ -5,16 +5,10 @@ namespace Cuco.Infra.Data;
 
 public class CucoDbContext : DbContext
 {
-    public CucoDbContext(DbContextOptions<CucoDbContext> options) : base(options)
-    {
-    }
+    public CucoDbContext(DbContextOptions<CucoDbContext> options) : base(options)  { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-    }
+        => modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)  { }
 }
