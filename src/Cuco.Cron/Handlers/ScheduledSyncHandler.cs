02c51443c008629f22c1aff545a68ff88d0d0508
LukeDias42 <lhollowwizard@gmail.com>
2023-01-21 21:38:07 -0300

    feat: added class that handles the logic of the cronjob

---
+++ src/Cuco.Cron/Handlers/ScheduledSyncHandler.cs
@@ -0,0 +1,47 @@
+using Cuco.Cron.Models;
+using Flurl.Http;
+using Hangfire;
+
+namespace Cuco.Cron.Handlers;
+
+internal static class ScheduledSyncHandler
+{
+    private static readonly string SyncCurrenciesEndpointUrl =
+        Environment.GetEnvironmentVariable("SyncCurrenciesEndpointUrl") ?? "http://localhost:5010/api/currency/sync";
+    private static bool _started;
+
+    public static async Task SyncCurrenciesAsync()
+    {
+        try
+        {
+            Console.WriteLine("Entered.");
+            var timestamp = (await SyncCurrenciesEndpointUrl.GetJsonAsync<Result>())?.ExchangeRateChangeResponse
+                ?.Timestamp;
+            if (timestamp is null or 0)
+            {
+                ProcessFailure();
+                return;
+            }
+
+            var nextCallTime = GetTimeOfNextCallFromTimestamp(timestamp.Value);
+            RecurringJob.AddOrUpdate("SyncCurrencies", () => SyncCurrenciesAsync(),
+                Hangfire.Cron.Daily(nextCallTime.Hour, nextCallTime.Minute + 1));
+            _started = true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error occurred while trying to Sync Currencies." +
+                              $"\nError: {e.Message}");
+        }
+    }
+
+    private static void ProcessFailure()
+    {
+        if (_started) return;
+        RecurringJob.AddOrUpdate("SyncCurrencies", () => SyncCurrenciesAsync(), Hangfire.Cron.Hourly);
+        _started = true;
+    }
+
+    private static DateTime GetTimeOfNextCallFromTimestamp(long unixTimestamp)
+        => DateTimeOffset.FromUnixTimeSeconds(unixTimestamp).AddHours(1).ToUniversalTime().DateTime;
+}
