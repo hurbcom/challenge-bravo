645fe33000657b84ec5655916d2b7039d81459e7
LukeDias42 <lhollowwizard@gmail.com>
2023-01-21 21:38:37 -0300

    feat: add Program class for configuration and calling Scheduled Sync Handler

---
+++ src/Cuco.Cron/Program.cs
@@ -0,0 +1,15 @@
+﻿using Cuco.Cron;
+using Cuco.Cron.Handlers;
+using Hangfire;
+using Hangfire.MemoryStorage;
+
+// Configure Hangfire
+GlobalConfiguration.Configuration.UseMemoryStorage();
+using var server = new BackgroundJobServer();
+
+// Run and setup recurring
+await ScheduledSyncHandler.SyncCurrenciesAsync();
+
+// Wait for the user to exit the application
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
