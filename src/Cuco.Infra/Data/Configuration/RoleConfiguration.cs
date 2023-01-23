75c622ed9b1e20b4ab836425a5d5e8cb0cf2c0c5
LukeDias42 <lhollowwizard@gmail.com>
2023-01-23 18:08:12 -0300

    feat: implemented Role Configuration

--- src/Cuco.Infra/Data/Configuration/RoleConfiguration.cs
+++ src/Cuco.Infra/Data/Configuration/RoleConfiguration.cs
@@ -1,5 +1,19 @@
+using Cuco.Domain.Currencies.Models.Entities;
+using Cuco.Domain.Roles.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace Cuco.Infra.Data.Configuration;
 
-public class RoleConfiguration
+public class RoleConfiguration : IEntityTypeConfiguration<Role>
 {
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder.ToTable(nameof(Role));
+
+        builder.HasKey(r => r.Id);
+
+        builder.Property(r => r.Name)
+            .HasMaxLength(50);
+    }
 }
