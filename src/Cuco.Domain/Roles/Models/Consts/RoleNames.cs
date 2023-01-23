027c8c42324b67f99033b008637ff01f2277d4d6
LukeDias42 <lhollowwizard@gmail.com>
2023-01-23 20:02:09 -0300

    feat: added Role Names for the existing names In Memory

---
+++ src/Cuco.Domain/Roles/Models/Consts/RoleNames.cs
@@ -0,0 +1,8 @@
+namespace Cuco.Domain.Roles.Models.Consts;
+
+public static class RoleNames
+{
+    public const string Admin = "ADMIN";
+    public const string Sync = "SYNC";
+    public const string User = "USER";
+}
