acb7410e649bc60650a32fc4b7553cfefd258702
LukeDias42 <lhollowwizard@gmail.com>
2023-01-23 10:15:53 -0300

    feat: removed Cache Currency Value and helper

--- src/Cuco.Application/GetCurrencyValueFromCache/ICurrencyValueHelper.cs
+++
@@ -1,8 +0,0 @@
-namespace Cuco.Application.GetCurrencyValueFromCache;
-
-public interface ICurrencyValueHelper
-{
-    Task<bool> IsReal(string symbol);
-    Task<decimal> ValueInDollar(string symbol);
-    Task SetImaginaryCurrencyValue(string symbol, decimal value);
-}
