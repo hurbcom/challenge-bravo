284a763af5ace18d24df4348ec2ba80d27ea7117
LukeDias42 <lhollowwizard@gmail.com>
2023-01-23 10:31:21 -0300

    feat: Added Authentication setup

--- src/Cuco.API/Program.cs
+++ src/Cuco.API/Program.cs
@@ -1,4 +1,7 @@
+using System.Text;
 using Cuco.IoC.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,14 +14,52 @@ builder.Services.AddMvc().AddControllersAsServices();
 
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
-        Title = "Cuco.Api",
-        Version = "v1"
+        In = ParameterLocation.Header,
+        Description = "Please insert JWT with Bearer into field",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
     });
 });
 
 builder.Services.AddHttpClient();
+
+builder.Services.AddAuthorization(options =>
+{
+});
+
+var key = Encoding.ASCII.GetBytes(builder.Configuration.GetSection("Security:Secret").Value ?? string.Empty);
+builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 var app = builder.Build();
 
@@ -33,5 +74,8 @@ app.UseSwaggerUI(c =>
 app.UseRouting();
 
 app.MapControllers();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
