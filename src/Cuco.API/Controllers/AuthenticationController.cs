18b7cabc21f618fc0e00247c9cfd8b7d34e28e9b
LukeDias42 <lhollowwizard@gmail.com>
2023-01-23 10:27:25 -0300

    feat: added Authentication Controller

---
+++ src/Cuco.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,35 @@
+using Cuco.Application.Token;
+using Cuco.Domain.Users.Models.Entities;
+using Cuco.Domain.Users.Services.Extensions;
+using Cuco.Domain.Users.Services.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cuco.API.Controllers;
+
+[Route("auth")]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IUserRepository _userRepository;
+    private readonly ITokenAdapter _tokenAdapter;
+
+    public AuthenticationController(IUserRepository userRepository, ITokenAdapter tokenAdapter)
+    {
+        _userRepository = userRepository;
+        _tokenAdapter = tokenAdapter;
+    }
+
+    [HttpPost("Authenticate")]
+    public async Task<ActionResult<string>> Authenticate([FromBody] User model)
+    {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+
+        var user = await _userRepository.GetUserInfo(model.Name, model.Password.Hash());
+
+        if (user is null)
+            return NotFound(new { message = "Invalid username or password" });
+
+        var token = _tokenAdapter.GenerateToken(user);
+
+        return Ok(token);
+    }
+}
