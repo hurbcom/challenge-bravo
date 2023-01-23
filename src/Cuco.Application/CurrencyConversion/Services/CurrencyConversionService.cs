f56436cebc1d4fb89b352463af95b521e67be854
LukeDias42 <lhollowwizard@gmail.com>
2023-01-23 10:20:39 -0300

    feat: stop verifying if currency exists before trying to return it

--- src/Cuco.Application/CurrencyConversion/Services/CurrencyConversionService.cs
+++ src/Cuco.Application/CurrencyConversion/Services/CurrencyConversionService.cs
@@ -1,31 +1,22 @@
 using Cuco.Application.Base;
 using Cuco.Application.CurrencyConversion.Models;
 using Cuco.Application.GetCurrencyInUSD.Models;
-using Cuco.Domain.Currencies.Services.Repositories;
 
 namespace Cuco.Application.CurrencyConversion.Services;
 internal class CurrencyConversionService : IService<CurrencyConversionInput, CurrencyConversionOutput>
 {
     private const string SameCurrencyMessage = "The currencies are the same, therefore the amount doesn't change.";
     private const string CouldNotFindCurrenciesValueMessageBase = "Couldn't get the value in dollar from the currency with symbol: ";
-    private const string CouldNotFindCurrenciesMessageBase = "Couldn't find the currency with symbol: ";
 
     private readonly IService<GetCurrencyInUsdInput, GetCurrencyInUsdOutput> _getCurrencyInUsdService;
-    private readonly ICurrencyRepository _currencyRepository;
 
-    public CurrencyConversionService(IService<GetCurrencyInUsdInput, GetCurrencyInUsdOutput> getCurrencyInUsdService,
-        ICurrencyRepository currencyRepository)
+    public CurrencyConversionService(IService<GetCurrencyInUsdInput, GetCurrencyInUsdOutput> getCurrencyInUsdService)
     {
         _getCurrencyInUsdService = getCurrencyInUsdService;
-        _currencyRepository = currencyRepository;
     }
 
     public async Task<CurrencyConversionOutput> Handle(CurrencyConversionInput input)
     {
-        var errorMessageCurrencyExist = await CheckCurrenciesExist(input);
-        if (!string.IsNullOrEmpty(errorMessageCurrencyExist))
-            return GetOutput(null, errorMessageCurrencyExist);
-
         if (CheckCurrenciesEquals(input))
             return GetOutput(input.Amount, SameCurrencyMessage);
 
@@ -51,19 +42,6 @@ internal class CurrencyConversionService : IService<CurrencyConversionInput, Cur
                             $"{CouldNotFindCurrenciesValueMessageBase}{input.ToCurrency}";
 
         return (errorMessage, fromCurrencyResult.ValueInDollar, toCurrencyResult.ValueInDollar);
-    }
-
-    private async Task<string> CheckCurrenciesExist(CurrencyConversionInput input)
-    {
-        var errorMessage = string.Empty;
-        if (!await _currencyRepository.ExistsBySymbolAsync(input.FromCurrency))
-            errorMessage += $"{CouldNotFindCurrenciesMessageBase}{input.FromCurrency}";
-
-        if (!await _currencyRepository.ExistsBySymbolAsync(input.ToCurrency))
-            errorMessage += (errorMessage == string.Empty ? "" : "\n") +
-                            $"{CouldNotFindCurrenciesMessageBase}{input.ToCurrency}";
-
-        return errorMessage;
     }
 
     private static bool CheckCurrenciesEquals(CurrencyConversionInput input)
