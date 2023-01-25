34794898bbf3e2934cfad8e7b271190cb1643b5e
LukeDias42 <lhollowwizard@gmail.com>
2023-01-25 09:16:35 -0300

    feat: added Redis Settings to build the string connection from

---
+++ src/Cuco.Commons/Settings/RedisSettings.cs
@@ -0,0 +1,8 @@
+namespace Cuco.Commons.Settings;
+
+public class RedisSettings
+{
+    public string Host { get; private set; }
+    public int Port { get; private set; }
+    public string Password { get; private set; }
+}
