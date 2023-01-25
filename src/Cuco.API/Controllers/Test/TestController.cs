5f31edd3738182e05a12f22a483b651c337186b7
LukeDias42 <lhollowwizard@gmail.com>
2023-01-25 09:12:40 -0300

    feat; added back redis-ping to tests controller

--- src/Cuco.API/Controllers/Test/TestController.cs
+++ src/Cuco.API/Controllers/Test/TestController.cs
@@ -1,4 +1,5 @@
 using Cuco.Commons;
+using Cuco.Commons.Base;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cuco.API.Controllers.Test;
@@ -12,5 +13,13 @@ public class TestController : ControllerBase
     public ActionResult Ping()
     {
         return Ok(new Result<string> { Output = "PONG" });
+    }
+
+    [HttpGet("redis-ping")]
+    [ProducesResponseType(typeof(Result<string>), StatusCodes.Status200OK)]
+    public async Task<ActionResult> RedisPing([FromServices] IRedisCache service)
+    {
+        await service.SetAsync("ping", "pong");
+        return Ok(await service.GetAsync("ping"));
     }
 }
