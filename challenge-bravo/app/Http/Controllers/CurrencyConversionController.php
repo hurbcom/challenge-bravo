9b08e04add062306a41dc9c6183c72b70c7e276d
kaetaen <kaetaen@tutanota.com>
2023-02-08 21:15:03 -0300

    feat:add CurrencyConversionController

---
+++ challenge-bravo/app/Http/Controllers/CurrencyConversionController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CurrencyConversionController extends Controller
+{
+    //
+}
--- challenge-bravo/routes/api.php
+++ challenge-bravo/routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/convert", function (){
+    return "ok";
+});
