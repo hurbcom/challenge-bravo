b2ed7050285ba6cc4d5c2c375201cca5349a36ea
kaetaen <kaetaen@tutanota.com>
2023-02-10 14:54:26 -0300

    Feat: create Model and Migration to Coins

---
+++ challenge-bravo/app/Models/Coin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Coin extends Model
+{
+    use HasFactory;
+
+    protected $table = 'coins';
+    protected $fillable = ['name', 'code', 'dolarValue'];
+}
