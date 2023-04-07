d46fcc65ebe78bf7b5a5bcdd0d242efa94041419
Victor de Nápoles dos Santos <victor_napoles@sicredi.com.br>
2023-04-07 18:03:53 -0300

    Carrengando a infra e as conexões externas

--- main.go
+++ main.go
@@ -1,23 +1,21 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "github.com/VictorNapoles/challenge-bravo/src/gateway"
-    "github.com/VictorNapoles/challenge-bravo/src/infra"
+	"github.com/VictorNapoles/challenge-bravo/gateway"
+	"github.com/VictorNapoles/challenge-bravo/infra"
 )
 
 func main() {
-    infra.LoadInfra()
-    gateway.LoadGateways()
+	infra.LoadInfra()
+	gateway.LoadGateways()
 
-    pong, err := infra.GetRedisCacheConnection().Ping(context.Background()).Result()
-    fmt.Println(pong, err)
+	/*pong, err := infra.GetRedisCacheConnection().Ping(context.Background()).Result()
+	  fmt.Println(pong, err)
 
-    price, err := gateway.GetAwesomeApiClient().GetPrice("BRL", "USD")
-    if err != nil {
-        fmt.Println(err)
-    }
+	  price, err := gateway.GetAwesomeApiClient().GetPrice("BRL", "USD")
+	  if err != nil {
+	  	fmt.Println(err)
+	  }
 
-    fmt.Println(price)
+	  fmt.Println(price)*/
 }
