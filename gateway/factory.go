eb24539e262568f238e7c7f55b6e07bb3c1e7813
Victor de Nápoles dos Santos <victor_napoles@sicredi.com.br>
2023-04-07 17:42:37 -0300

    Criaçao das operações de banco e chamadas da API de cotação

--- gateway/factory.go
+++ gateway/factory.go
@@ -1,28 +1,38 @@
 package gateway
 
 import (
-    "github.com/VictorNapoles/challenge-bravo/gateway/awesomeapi"
-    "github.com/VictorNapoles/challenge-bravo/gateway/repository"
-    "github.com/VictorNapoles/challenge-bravo/infra"
+	"github.com/VictorNapoles/challenge-bravo/gateway/awesomeapi"
+	"github.com/VictorNapoles/challenge-bravo/gateway/repository"
+	"github.com/VictorNapoles/challenge-bravo/infra"
 )
 
 var (
-    awesomeApiClient awesomeapi.AwesomeApiClient
-    priceRepository  repository.PriceRepository
+	awesomeApiClient awesomeapi.AwesomeApiClient
+	priceRepository  repository.PriceRepository
 )
 
 func loadAwesomeApiClient() {
-    awesomeApiClient = awesomeapi.NewAwesomeApiClient(infra.GetHttpClient(), infra.GetEnvironment())
+	awesomeApiClient = awesomeapi.NewAwesomeApiClient(infra.GetHttpClient(), infra.GetEnvironment())
 }
 
 func loadPriceRepository() {
-    priceRepository = repository.NewPriceRepository(infra.GetDatabaseConnection(), infra.GetRedisCacheConnection())
+	priceRepository = repository.NewPriceRepository(infra.GetDatabaseConnection(), infra.GetRedisCacheConnection())
 }
 
 func LoadGateways() {
-    loadAwesomeApiClient()
+	loadAwesomeApiClient()
+	loadPriceRepository()
 }
 
 func GetAwesomeApiClient() awesomeapi.AwesomeApiClient {
-    return awesomeApiClient
+	if awesomeApiClient == nil {
+		loadAwesomeApiClient()
+	}
+	return awesomeApiClient
+}
+func GetPriceRepository() repository.PriceRepository {
+	if priceRepository == nil {
+		loadPriceRepository()
+	}
+	return priceRepository
 }
