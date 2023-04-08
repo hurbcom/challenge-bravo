a9f620ce8b1b283bd82c35cadffe477b0cfee109
Jefferson Santos <jefferson@incentive.me>
2023-04-07 22:31:02 -0300

    fix: remove app service unecessary

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 describe('AppController', () => {
-  let appController: AppController;
+    let appController: AppController;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+    beforeEach(async () => {
+        const app: TestingModule = await Test.createTestingModule({
+            controllers: [AppController],
+        }).compile();
 
-    appController = app.get<AppController>(AppController);
-  });
-
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+        appController = app.get<AppController>(AppController);
     });
-  });
+
+    // describe('root', () => {
+    //     it('should return {version: number}', () => {
+    //         expect(appController.getVersion()).toBe();
+    //     });
+    // });
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-
+import * as npmPackage from './../package.json';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getVersion(): { version: string } {
+        return { version: npmPackage.version };
+    }
 }
