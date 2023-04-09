1509cec35d6a2fc0da4cc9b213ddfa1cbc2cf65a
Victor de Nápoles dos Santos <victor_napoles@sicredi.com.br>
2023-04-09 20:49:58 -0300

    Troca do postgres pelo mongoDB

--- gateway/repository/price_repository.go
+++ gateway/repository/price_repository.go
@@ -1,48 +1,49 @@
 package repository
 
 import (
-	"github.com/VictorNapoles/challenge-bravo/infra/cache"
-	"github.com/VictorNapoles/challenge-bravo/infra/database"
+    "github.com/VictorNapoles/challenge-bravo/infra/cache"
+    "github.com/VictorNapoles/challenge-bravo/infra/database"
+    "go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type (
-	PriceEntity struct {
-		ID                    int64
-		Code                  string
-		CurrencyName          string
-		UnitValueBankCurrency float64
-	}
+    PriceEntity struct {
+        ID                    primitive.ObjectID `bson:"_id"`
+        Code                  string             `bson:"code"`
+        CurrencyName          string             `bson:"currencyName"`
+        UnitValueBankCurrency float64            `bson:"unitValueBankCurrency"`
+    }
 
-	PriceRepository interface {
-		GetByCode(code string) (PriceEntity, error)
-		Save(entity PriceEntity) (PriceEntity, error)
-		Remove(code string) (bool, error)
-	}
+    PriceRepository interface {
+        GetByCode(code string) (PriceEntity, error)
+        Save(entity PriceEntity) (PriceEntity, error)
+        Remove(code string) (bool, error)
+    }
 
-	priceRepositoryImpl struct {
-		db    database.MongoDatabaseConnection
-		cache cache.RedisCacheConnection
-	}
+    priceRepositoryImpl struct {
+        db    database.MongoDatabaseConnection
+        cache cache.RedisCacheConnection
+    }
 )
 
 func NewPriceRepository(db database.MongoDatabaseConnection, cache cache.RedisCacheConnection) PriceRepository {
-	return &priceRepositoryImpl{
-		db:    db,
-		cache: cache,
-	}
+    return &priceRepositoryImpl{
+        db:    db,
+        cache: cache,
+    }
 }
 
 func (p priceRepositoryImpl) GetByCode(code string) (PriceEntity, error) {
-	//TODO implement me
-	panic("implement me")
+    //TODO implement me
+    panic("implement me")
 }
 
 func (p priceRepositoryImpl) Save(entity PriceEntity) (PriceEntity, error) {
-	//TODO implement me
-	panic("implement me")
+    //TODO implement me
+    panic("implement me")
 }
 
 func (p priceRepositoryImpl) Remove(code string) (bool, error) {
-	//TODO implement me
-	panic("implement me")
+    //TODO implement me
+    panic("implement me")
 }
