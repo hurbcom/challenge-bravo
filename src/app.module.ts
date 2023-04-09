84c6f5db3f343ecd277960c863125ffdc7ccf169
Jefferson Santos <jefferson@incentive.me>
2023-04-09 01:07:18 -0300

    fix: remove fixed database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
     imports: [
         ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
-        MongooseModule.forRoot('mongodb://localhost/:27017'), //TODO using env here
+        MongooseModule.forRoot(
+            `${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}`,
+        ), //TODO use asyncForRoot to read env from configModule
         CurrencyModule,
     ],
     controllers: [AppController],
