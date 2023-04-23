edaa35f02432da33cfb4716af12cac2ea70091bb
Victor de Nápoles dos Santos <victor_napoles@sicredi.com.br>
2023-04-23 17:43:36 -0300

    Carregando os dados iniciais

--- gateway/awesomeapi/awesomeapi_client.go
+++ gateway/awesomeapi/awesomeapi_client.go
@@ -8,8 +8,9 @@ import (
 )
 
 const (
-	GetAvailableQuotesUri   = "/json/available"
-	ApiAwesomeBaseUrlEnvVar = "API_AWESOME_BASE_URL"
+	GetAvailableQuotesUri     = "/json/available"
+	GetAvailableCurrenciesUri = "/json/available/uniq"
+	ApiAwesomeBaseUrlEnvVar   = "API_AWESOME_BASE_URL"
 )
 
 type (
@@ -25,6 +26,7 @@ type (
 	AwesomeApiClient interface {
 		GetQuote(from, to string) (*QuoteDto, error)
 		GetAvailableQuotes() (map[string]string, error)
+		GetAvailableCurrencies() (map[string]string, error)
 	}
 
 	awesomeApiClientImpl struct {
@@ -55,6 +57,19 @@ func (a *awesomeApiClientImpl) GetQuote(from, to string) (*QuoteDto, error) {
 
 func (a *awesomeApiClientImpl) GetAvailableQuotes() (map[string]string, error) {
 	url := a.getUrl(GetAvailableQuotesUri)
+	r, err := a.httpClient.Get(url)
+
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+	var result map[string]string
+	json.NewDecoder(r.Body).Decode(&result)
+	return result, nil
+}
+
+func (a *awesomeApiClientImpl) GetAvailableCurrencies() (map[string]string, error) {
+	url := a.getUrl(GetAvailableCurrenciesUri)
 	r, err := a.httpClient.Get(url)
 
 	if err != nil {
--- gateway/factory.go
+++ gateway/factory.go
@@ -4,6 +4,13 @@ import (
 	"github.com/VictorNapoles/challenge-bravo/gateway/awesomeapi"
 	"github.com/VictorNapoles/challenge-bravo/gateway/repository"
 	"github.com/VictorNapoles/challenge-bravo/infra"
+	"regexp"
+	"strings"
+)
+
+const (
+	InitialCurrenciesEnvVar = "INITIAL_CURRENCIES"
+	BankCurrencyCodeEnvVar  = "BANK_CURRENCY_CODE"
 )
 
 var (
@@ -28,10 +35,95 @@ func loadQuoteRepository() {
 	quoteRepository = repository.NewQuoteRepository(infra.GetRedisCacheConnection())
 }
 
+func loadInitialData() {
+	bankCurrencyCode, err := infra.GetEnvironment().Get(BankCurrencyCodeEnvVar)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	loadAvailableQuotes(bankCurrencyCode)
+	loadInitialCurrencies(bankCurrencyCode)
+}
+
+func loadInitialCurrencies(bankCurrencyCode string) {
+
+	availableCurrencies, err := GetAwesomeApiClient().GetAvailableCurrencies()
+	if err != nil {
+		panic(err.Error())
+	}
+	initialCurrencies, err := infra.GetEnvironment().Get(InitialCurrenciesEnvVar)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	currencies := regexp.MustCompile(`,`).Split(initialCurrencies, -1)
+	for _, code := range currencies {
+
+		currencyEntity, err := GetCurrencyRepository().GetByCode(code)
+		if err != nil {
+			panic(err.Error())
+		}
+		if currencyEntity.Code == "" {
+			currencyName := availableCurrencies[code]
+
+			var entity *repository.CurrencyEntity
+			if code == bankCurrencyCode {
+				entity = &repository.CurrencyEntity{
+					CurrencyName:          availableCurrencies[code],
+					Code:                  code,
+					QuoteType:             repository.QuoteNotAvailable,
+					UnitValueBankCurrency: 1,
+					Deletable:             false,
+				}
+			} else {
+				availableQuote, err := GetQuoteRepository().CheckIsAvailableQuote(code, bankCurrencyCode)
+				if err != nil {
+					panic(err.Error())
+				}
+
+				var quoteType repository.QuoteTypeEntity
+
+				if availableQuote {
+					quoteType = repository.QuoteToBankCurrency
+				} else {
+					quoteType = repository.QuoteFromBankCurrency
+				}
+
+				entity = &repository.CurrencyEntity{
+					CurrencyName: currencyName,
+					Code:         code,
+					QuoteType:    quoteType,
+					Deletable:    false,
+				}
+
+			}
+			GetCurrencyRepository().Save(entity)
+		}
+	}
+}
+
+func loadAvailableQuotes(bankCurrencyCode string) {
+	quotes, err := GetAwesomeApiClient().GetAvailableQuotes()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for key, _ := range quotes {
+		if strings.HasSuffix(key, bankCurrencyCode) || strings.HasPrefix(key, bankCurrencyCode) {
+			currencyCodes := regexp.MustCompile(`-`).Split(key, -1)
+			err := GetQuoteRepository().SetAvailableQuote(currencyCodes[0], currencyCodes[1])
+			if err != nil {
+				panic(err.Error())
+			}
+		}
+	}
+}
+
 func LoadGateways() {
 	loadAwesomeApiClient()
 	loadCurrencyRepository()
 	loadQuoteRepository()
+	loadInitialData()
 }
 
 func GetAwesomeApiClient() awesomeapi.AwesomeApiClient {
@@ -53,8 +145,4 @@ func GetQuoteRepository() repository.QuoteRepository {
 		loadQuoteRepository()
 	}
 	return quoteRepository
-}
-
-func loadInitialData() {
-
 }
