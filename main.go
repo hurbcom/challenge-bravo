4fe8d5c44344363bac2829b970b238aca25b93a8
Victor de Nápoles dos Santos <victor_napoles@sicredi.com.br>
2023-04-23 17:56:17 -0300

    Remoção de código desnecessário

--- main.go
+++ main.go
@@ -21,45 +21,4 @@ func main() {
 	gateway.LoadGateways()
 	usecase.LoadUsecases()
 	presenter.LoadPresenters()
-
-	/*apiClient := gateway.GetAwesomeApiClient()
-	  quotes, err := apiClient.GetAvailableQuotes()
-	  if err != nil {
-	  	return
-	  }
-
-	  quoteRepository := gateway.GetQuoteRepository()
-
-	  for key, element := range quotes {
-	  	if strings.HasSuffix(key, "USD") || strings.HasPrefix(key, "USD") {
-
-	  		currencyCodes := regexp.MustCompile(`-`).Split(key, -1)
-	  		fmt.Println(fmt.Sprintf("%s - %s", key, element))
-	  		fmt.Println(fmt.Sprintf("%s - %s", currencyCodes[0], currencyCodes[1]))
-	  		//quoteRepository.SetAvailableQuote(currencyCodes[0], currencyCodes[1])
-	  		//availableQuote, err2 := quoteRepository.CheckIsAvailableQuote(currencyCodes[0], currencyCodes[1])
-	  		//
-	  		//if err2 == nil {
-	  		//    fmt.Println(fmt.Sprintf("%s - %s", element, availableQuote))
-	  		//}
-
-	  		quote, err3 := apiClient.GetQuote(currencyCodes[0], currencyCodes[1])
-	  		if err3 == nil {
-	  			fmt.Println(fmt.Sprintf("%s - %s", element, quote))
-	  		}
-
-	  		quoteRepository.SaveQuote(&repository.QuoteEntity{
-	  			From:   quote.From,
-	  			To:     quote.To,
-	  			Name:   quote.Name,
-	  			Amount: quote.Amount,
-	  		})
-
-	  		quoteEntity, err4 := quoteRepository.GetQuote(currencyCodes[0], currencyCodes[1])
-	  		if err4 == nil {
-	  			fmt.Println(fmt.Sprintf("%s - %s", element, quoteEntity))
-	  		}
-
-	  	}
-	  }*/
 }
