e2f7799a684c8e55f4bf22a774c80b0c67e150ee
mtts021 <matteusganha@gmail.com>
2023-09-14 23:00:57 -0300

    chore: initial app structure

---
+++ vitest.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: './tests/unit/coverage'
+    }
+  }
+})
