0d269b2896dc41ee4e6cd2a66411fe3b61a644dd
mtts021 <matteusganha@gmail.com>
2023-09-15 10:48:53 -0300

    chore: add express

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello World' })
+})
+
+app.listen(3000, () => {
+  console.log('server running at port 3000')
+})
