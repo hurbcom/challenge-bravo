789f59c6a5a80adac78d297e2ccc2fc501af269f
mtts021 <matteusganha@gmail.com>
2023-10-10 21:24:44 -0300

    feat: add currency api client

---
+++ src/http/client/currencyApi.client.js
@@ -0,0 +1,54 @@
+/* eslint-disable prefer-const */
+/* eslint-disable camelcase */
+import axios from 'axios'
+import { SupportedCurrencyRepository } from '../../database/supportedCurrencyRepository/supportedCurrencyRepository.js'
+import { RedisRepository } from '../../database/redis/redisRepository.js'
+
+export class CurrencyApi {
+  #request
+  #cacheRepository
+  constructor () {
+    this.#request = axios
+    this.#cacheRepository = new RedisRepository()
+  }
+
+  async getCurrencies () {
+    const { data } = await this.#request.get('https://cdn.moeda.info/api/latest.json')
+
+    return data
+  }
+
+  async getCurrencyByCode (code) {
+    let rates = await JSON.parse(await this.#cacheRepository.get('exchange_rates'))
+    this.#cacheRepository.disconnect()
+    if (!rates) {
+      const { data } = await this.#request.get('https://cdn.moeda.info/api/latest.json')
+      rates = data
+    }
+
+    return this.#getCurrencyByCodeNormalized({ code, ...rates })
+  }
+
+  async normalizeToMongo ({ rates }) {
+    const supportedCurrencyRepository = new SupportedCurrencyRepository()
+    const [{ supported_currencies }] = await supportedCurrencyRepository.getSupportedCurrencies()
+    const currencies = []
+    for (const code of supported_currencies) {
+      const currency = {
+        base: 'UDS',
+        code,
+        price: rates[code]
+      }
+      currencies.push(currency)
+    }
+    return currencies
+  }
+
+  #getCurrencyByCodeNormalized ({ code, rates }) {
+    const currency = {
+      code,
+      price: rates[code]
+    }
+    return currency
+  }
+}
