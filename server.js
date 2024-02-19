dfe42b8c98dfc86edc81711db57e47679e078af6
Leonardo Neves <leo.mvhost@hotmail.com>
2024-02-19 18:38:17 -0300

    Aplicando API rate limite e script yml para stress teste

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(helmet());
+
+const limiter = rateLimit({
+    windowMs: 1000,
+    limit: 10000,
+    standardHeaders: true,
+    message: 'You have exceeded your ~1000 requests per second limit.',
+});
+app.use(limiter);
 
 const mainRoutes = require('./routes/main');
 app.use('/', mainRoutes);
